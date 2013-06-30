028544be6c86265fcc9233eb3bb8984d6073c022
Nicholas C. Zakas <nicholas@nczconsulting.com>
2013-06-30 16:40:41 -0700

    Rule to detect constructors that don't begin with capital letters (fixes #4)

---
+++ tests/lib/rules/new-cap.js
@@ -0,0 +1,59 @@
+/**
+ * @fileoverview Tests for new-cap rule.
+ * @author Nicholas C. Zakas
+ */
+
+/*jshint node:true*/
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var vows = require("vows"),
+    assert = require("assert"),
+    jscheck = require("../../../lib/jscheck");
+
+//------------------------------------------------------------------------------
+// Constants
+//------------------------------------------------------------------------------
+
+var RULE_ID = "new-cap";
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+vows.describe(RULE_ID).addBatch({
+
+    "when evaluating 'var x = new c();'": {
+
+        topic: "var x = new c();",
+
+        "should report a violation": function(topic) {
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = jscheck.verify(topic, config);
+
+            assert.equal(messages.length, 1);
+            assert.equal(messages[0].ruleId, RULE_ID);
+            assert.equal(messages[0].message, "A constructor name should start with an uppercase letter.");
+            assert.include(messages[0].node.type, "NewExpression");
+        }
+    },
+
+    "when evaluating 'var x = new C();'": {
+
+        topic: "var x = new C();",
+
+        "should not report a violation": function(topic) {
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = jscheck.verify(topic, config);
+
+            assert.equal(messages.length, 0);
+        }
+    }
+
+}).export(module);
