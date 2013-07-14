e0b2401fc6f33b2061f3cab3d96b025ede3a2706
Joel Feenstra <jrfeenst@gmail.com>
2013-07-14 10:16:46 -0400

    Add unreachable code detection for switch cases and after continue/break.

--- tests/lib/rules/no-unreachable.js
+++ tests/lib/rules/no-unreachable.js
@@ -56,6 +56,102 @@ vows.describe(RULE_ID).addBatch({
             assert.equal(messages[0].message, "Found unexpected statement after a throw.");
             assert.include(messages[0].node.type, "ExpressionStatement");
         }
+    },
+
+    "when evaluation 'while (true) { break; x = 1; }'": {
+        topic: "while (true) { break; x = 1; }",
+
+        "should return a violation": function(topic) {
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+
+            assert.equal(messages.length, 1);
+            assert.equal(messages[0].ruleId, RULE_ID);
+            assert.equal(messages[0].message, "Found unexpected statement after a break.");
+            assert.include(messages[0].node.type, "ExpressionStatement");
+        }
+    },
+
+    "when evaluation 'while (true) { continue; x = 1; }'": {
+        topic: "while (true) { continue; x = 1; }",
+
+        "should return a violation": function(topic) {
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+
+            assert.equal(messages.length, 1);
+            assert.equal(messages[0].ruleId, RULE_ID);
+            assert.equal(messages[0].message, "Found unexpected statement after a continue.");
+            assert.include(messages[0].node.type, "ExpressionStatement");
+        }
+    },
+
+    "when evaluation 'function foo() { switch (foo) { case 1: return; x = 1; } }'": {
+        topic: "function foo() { switch (foo) { case 1: return; x = 1; } }",
+
+        "should return a violation": function(topic) {
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+
+            assert.equal(messages.length, 1);
+            assert.equal(messages[0].ruleId, RULE_ID);
+            assert.equal(messages[0].message, "Found unexpected statement after a return.");
+            assert.include(messages[0].node.type, "ExpressionStatement");
+        }
+    },
+
+    "when evaluation 'function foo() { switch (foo) { case 1: throw e; x = 1; } }'": {
+        topic: "function foo() { switch (foo) { case 1: throw e; x = 1; } }",
+
+        "should return a violation": function(topic) {
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+
+            assert.equal(messages.length, 1);
+            assert.equal(messages[0].ruleId, RULE_ID);
+            assert.equal(messages[0].message, "Found unexpected statement after a throw.");
+            assert.include(messages[0].node.type, "ExpressionStatement");
+        }
+    },
+
+    "when evaluation 'while (true) { switch (foo) { case 1: break; x = 1; } }'": {
+        topic: "while (true) { switch (foo) { case 1: break; x = 1; } }",
+
+        "should return a violation": function(topic) {
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+
+            assert.equal(messages.length, 1);
+            assert.equal(messages[0].ruleId, RULE_ID);
+            assert.equal(messages[0].message, "Found unexpected statement after a break.");
+            assert.include(messages[0].node.type, "ExpressionStatement");
+        }
+    },
+
+    "when evaluation 'while (true) { switch (foo) { case 1: continue; x = 1; } }'": {
+        topic: "while (true) { switch (foo) { case 1: continue; x = 1; } }",
+
+        "should return a violation": function(topic) {
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+
+            assert.equal(messages.length, 1);
+            assert.equal(messages[0].ruleId, RULE_ID);
+            assert.equal(messages[0].message, "Found unexpected statement after a continue.");
+            assert.include(messages[0].node.type, "ExpressionStatement");
+        }
     }
 
 }).export(module);
