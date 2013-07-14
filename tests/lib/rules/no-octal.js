1950f6b439965eb6f060cd3c6fbb704920f90f76
Ilya Volodin <ivolodin@vistaprint.com>
2013-07-14 16:55:02 -0400

    No octal and no undefined rules

---
+++ tests/lib/rules/no-octal.js
@@ -0,0 +1,61 @@
+/**
+ * @fileoverview Tests for no-octal rule.
+ * @author Nicholas C. Zakas
+ */
+
+/*jshint node:true*/
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var vows = require("vows"),
+    assert = require("assert"),
+    sinon = require("sinon"),
+    eslint = require("../../../lib/eslint");
+
+//------------------------------------------------------------------------------
+// Constants
+//------------------------------------------------------------------------------
+
+var RULE_ID = "no-octal";
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+vows.describe(RULE_ID).addBatch({
+
+    "when evaluating 'var a = 01234'": {
+
+        topic: "var a = 01234;",
+
+        "should report a violation": function(topic) {
+
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+
+            assert.equal(messages.length, 1);
+            assert.equal(messages[0].ruleId, RULE_ID);
+            assert.equal(messages[0].message, "Veriable 'a' initialized to octal literal.");
+            assert.include(messages[0].node.type, "VariableDeclarator");
+        }
+    },
+
+    "when evaluating 'var a = 0x1234'": {
+
+        topic: "var a = 0x1234;",
+
+        "should not report a violation": function(topic) {
+
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+
+            assert.equal(messages.length, 0);
+        }
+    }
+}).export(module);
