d816b8f3faacf955d94d4e9736674286deecaf2f
Mark Macdonald <Mark_MacDonald@ca.ibm.com>
2013-07-18 13:38:05 -0400

    Adds test and fix for issue #82

--- tests/lib/rules/new-parens.js
+++ tests/lib/rules/new-parens.js
@@ -55,5 +55,20 @@ vows.describe(RULE_ID).addBatch({
 
             assert.equal(messages.length, 0);
         }
+    },
+
+	"when evaluating 'var a = new Date(function() {})'": {
+
+        topic: "var a = new Date(function() {});",
+
+        "should not report a violation": function(topic) {
+
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+
+            assert.equal(messages.length, 0);
+        }
     }
 }).export(module);
