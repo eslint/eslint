45f5e34b1099e3af17a3f7e4617a31cc631ea780
Jamund Ferguson <jamund@gmail.com>
2013-07-17 22:31:45 -0700

    Formatters can now process multiple files at once

--- tests/lib/formatters/compact.js
+++ tests/lib/formatters/compact.js
@@ -17,13 +17,16 @@ var vows = require("vows"),
 
 vows.describe("formatter:compact").addBatch({
 
-    "when passed a message": {
+    "when passed a single message": {
 
         topic: [{
-            message: "Unexpected foo.",
-            line: 5,
-            column: 10,
-            ruleId: "foo"
+            filePath: "foo.js",
+            messages: [{
+                message: "Unexpected foo.",
+                line: 5,
+                column: 10,
+                ruleId: "foo"
+            }]
         }],
 
         "should return a string in the format filename: line x, col y, Error - z for errors": function(topic) {
@@ -31,7 +34,7 @@ vows.describe("formatter:compact").addBatch({
                 rules: { foo: 2 }
             };
 
-            var result = formatter(topic, "foo.js", config);
+            var result = formatter(topic, config);
             assert.equal("foo.js: line 5, col 10, Error - Unexpected foo.\n\n1 problems", result);
         },
 
@@ -40,7 +43,7 @@ vows.describe("formatter:compact").addBatch({
                 rules: { foo: 1 }
             };
 
-            var result = formatter(topic, "foo.js", config);
+            var result = formatter(topic, config);
             assert.equal("foo.js: line 5, col 10, Warning - Unexpected foo.\n\n1 problems", result);
         }
 
@@ -49,33 +52,38 @@ vows.describe("formatter:compact").addBatch({
     "when passed a fatal error message": {
 
         topic: [{
-            fatal: true,
-            message: "Unexpected foo.",
-            line: 5,
-            column: 10,
-            ruleId: "foo"
+            filePath: "foo.js",
+            messages: [{
+                fatal: true,
+                message: "Unexpected foo.",
+                line: 5,
+                column: 10,
+                ruleId: "foo"
+            }]
         }],
 
         "should return a string in the format filename: line x, col y, Error - z": function(topic) {
             var config = {};    // doesn't matter what's in the config for this test
 
-            var result = formatter(topic, "foo.js", config);
+            var result = formatter(topic, config);
             assert.equal("foo.js: line 5, col 10, Error - Unexpected foo.\n\n1 problems", result);
         }
     },
 
     "when passed multiple messages": {
         topic: [{
-            message: "Unexpected foo.",
-            line: 5,
-            column: 10,
-            ruleId: "foo"
-        }, {
-            message: "Unexpected bar.",
-            line: 6,
-            column: 11,
-            ruleId: "bar"
-
+            filePath: "foo.js",
+            messages: [{
+                message: "Unexpected foo.",
+                line: 5,
+                column: 10,
+                ruleId: "foo"
+            }, {
+                message: "Unexpected bar.",
+                line: 6,
+                column: 11,
+                ruleId: "bar"
+            }]
         }],
 
         "should return a string with multiple entries": function(topic) {
@@ -83,10 +91,39 @@ vows.describe("formatter:compact").addBatch({
                 rules: { foo: 2, bar: 1 }
             };
 
-            var result = formatter(topic, "foo.js", config);
+            var result = formatter(topic, config);
             assert.equal("foo.js: line 5, col 10, Error - Unexpected foo.\nfoo.js: line 6, col 11, Warning - Unexpected bar.\n\n2 problems", result);
         }
 
+    },
+
+    "when passed multiple files with 1 message each": {
+        topic: [{
+            filePath: "foo.js",
+            messages: [{
+                message: "Unexpected foo.",
+                line: 5,
+                column: 10,
+                ruleId: "foo"
+            }]
+        }, {
+            filePath: "bar.js",
+            messages: [{
+                message: "Unexpected bar.",
+                line: 6,
+                column: 11,
+                ruleId: "bar"
+            }]
+        }],
+
+        "should return a string with multiple entries": function(topic) {
+            var config = {
+                rules: { foo: 2, bar: 1 }
+            };
+
+            var result = formatter(topic, config);
+            assert.equal("foo.js: line 5, col 10, Error - Unexpected foo.\nbar.js: line 6, col 11, Warning - Unexpected bar.\n\n2 problems", result);
+        }
     }
 
-}).export(module);
+    }).export(module);
