f0c443bad36396b5273cf1f9b9f7b2bf35630f4a
Josh Perez <josh@goatslacker.com>
2013-07-21 10:39:50 -0700

    Changes dotnotation rule to match JSHint style

--- tests/lib/rules/dotnotation.js
+++ tests/lib/rules/dotnotation.js
@@ -28,27 +28,29 @@ function noViolation(topic) {
     assert.equal(messages.length, 0);
 }
 
-function violation(topic) {
-    var config = { rules: {} };
-    config.rules[RULE_ID] = 1;
+function violation(message) {
+    return function(topic) {
+        var config = { rules: {} };
+        config.rules[RULE_ID] = 1;
 
-    var messages = eslint.verify(topic, config);
+        var messages = eslint.verify(topic, config);
 
-    assert.equal(messages.length, 1);
-    assert.equal(messages[0].ruleId, RULE_ID);
-    assert.equal(messages[0].message, "It is preferrable to use dot notation.");
-    assert.include(messages[0].node.type, "MemberExpression");
+        assert.equal(messages.length, 1);
+        assert.equal(messages[0].ruleId, RULE_ID);
+        assert.equal(messages[0].message, "['" + message + "'] is better written in dot notation.");
+        assert.include(messages[0].node.type, "MemberExpression");
+    }
 }
 
 vows.describe(RULE_ID).addBatch({
     "when evaluating a computed MemberExpression": {
         topic: "a['b'];",
-        "should report a violation": violation
+        "should report a violation": violation('b')
     },
 
     "when evaluating a chained computed and uncomputed MemberExpression": {
         topic: "a.b['c'];",
-        "should report a violation": violation
+        "should report a violation": violation('c')
     },
 
     "when evaluating an uncomputed MemberExpression": {
