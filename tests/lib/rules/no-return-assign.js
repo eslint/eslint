7eec8c6e7e25e2194da555bf0ffc1be30cd0dd3b
Ilya Volodin <ivolodin@gmail.com>
2013-07-22 23:24:22 -0400

    Rule: no assignment in return statement

---
+++ tests/lib/rules/no-return-assign.js
@@ -0,0 +1,58 @@
+/**
+ * @fileoverview Tests for no-return-assign.
+ * @author Ilya Volodin
+ */
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var vows = require("vows"),
+    assert = require("assert"),
+    eslint = require("../../../lib/eslint");
+
+//------------------------------------------------------------------------------
+// Constants
+//------------------------------------------------------------------------------
+
+var RULE_ID = "no-return-assign";
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+vows.describe(RULE_ID).addBatch({
+
+    "when evaluating 'function x() { return result = a * b; }": {
+
+        topic: "function x() { return result = a * b; };",
+
+        "should report a violation": function(topic) {
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+
+            assert.equal(messages.length, 1);
+            assert.equal(messages[0].ruleId, RULE_ID);
+            assert.equal(messages[0].message, "Return statement should not contain assigment");
+            assert.include(messages[0].node.type, "ReturnStatement");
+        }
+    },
+
+    "when evaluating 'function x() { var result = a * b; return result; }": {
+
+        topic: "function x() { var result = a * b; return result; };",
+
+        "should not report a violation": function(topic) {
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+
+            assert.equal(messages.length, 0);
+        }
+    }
+
+
+}).export(module);
