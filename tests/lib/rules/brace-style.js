32d5b58338068b9893b96c924ec06c3e8e1ba610
Josh Perez <josh@goatslacker.com>
2013-07-24 02:23:53 -0700

    Adds eslint test to make testing easier and dry
    Testing is currently very verbose and repetitive, this commit adds
    the initial `eslint-test` module which makes testing concise.
    
    This also refactors `rules/brace-style.js` to take advantage
    of the new module.

--- tests/lib/rules/brace-style.js
+++ tests/lib/rules/brace-style.js
@@ -8,14 +8,23 @@
 //------------------------------------------------------------------------------
 
 var vows = require("vows"),
-    assert = require("assert"),
-    eslint = require("../../../lib/eslint");
+    eslintTest = require('../../eslint-test');
 
 //------------------------------------------------------------------------------
 // Constants
 //------------------------------------------------------------------------------
 
 var RULE_ID = "brace-style";
+
+//------------------------------------------------------------------------------
+// Helpers
+//------------------------------------------------------------------------------
+
+function isViolationForType(type) {
+    return eslintTest.violation(RULE_ID,
+            "Opening curly brace does not appear on the same line as the block identifier.",
+            type);
+}
 
 //------------------------------------------------------------------------------
 // Tests
@@ -27,279 +36,126 @@ vows.describe(RULE_ID).addBatch({
 
         topic: "function foo() \n { \n return; }",
 
-        "should report a violation": function(topic) {
-
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-            assert.equal(messages.length, 1);
-            assert.equal(messages[0].ruleId, RULE_ID);
-            assert.equal(messages[0].message, "Opening curly brace does not appear on the same line as the block identifier.");
-            assert.include(messages[0].node.type, "FunctionDeclaration");
-        }
+        "should report a violation": isViolationForType("FunctionDeclaration")
     },
 
     "when evaluating function declaration with one true brace style": {
 
         topic: "function foo () { return; }",
 
-        "should not report a violation": function(topic) {
-
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-            assert.equal(messages.length, 0);
-        }
+        "should not report a violation": eslintTest.noViolation
     },
 
     "when evaluating if statement without one true brace style": {
 
         topic: "if (foo) \n { \n bar(); }",
 
-        "should report a violation": function(topic) {
-
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-            assert.equal(messages.length, 1);
-            assert.equal(messages[0].ruleId, RULE_ID);
-            assert.equal(messages[0].message, "Opening curly brace does not appear on the same line as the block identifier.");
-            assert.include(messages[0].node.type, "IfStatement");
-        }
+        "should report a violation": isViolationForType("IfStatement")
     },
 
     "when evaluating if statement with one true brace style": {
 
         topic: "if (foo) { \n bar(); }",
 
-        "should not report a violation": function(topic) {
-
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-            assert.equal(messages.length, 0);
-        }
+        "should not report a violation": eslintTest.noViolation
     },
 
     "when evaluating while statement without one true brace style": {
 
         topic: "while (foo) \n { \n bar(); }",
 
-        "should report a violation": function(topic) {
-
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-            assert.equal(messages.length, 1);
-            assert.equal(messages[0].ruleId, RULE_ID);
-            assert.equal(messages[0].message, "Opening curly brace does not appear on the same line as the block identifier.");
-            assert.include(messages[0].node.type, "WhileStatement");
-        }
+        "should report a violation": isViolationForType("WhileStatement")
     },
 
     "when evaluating while statement with one true brace style": {
 
         topic: "while (foo) { \n bar(); }",
 
-        "should not report a violation": function(topic) {
-
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-            assert.equal(messages.length, 0);
-        }
+        "should not report a violation": eslintTest.noViolation
     },
 
     "when evaluating for statement without one true brace style": {
 
         topic: "for (;;) \n { \n bar(); }",
 
-        "should report a violation": function(topic) {
-
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-            assert.equal(messages.length, 1);
-            assert.equal(messages[0].ruleId, RULE_ID);
-            assert.equal(messages[0].message, "Opening curly brace does not appear on the same line as the block identifier.");
-            assert.include(messages[0].node.type, "ForStatement");
-        }
+        "should report a violation": isViolationForType("ForStatement")
     },
 
     "when evaluating for statement with one true brace style": {
 
         topic: "for (;;) { \n bar(); }",
 
-        "should not report a violation": function(topic) {
-
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-            assert.equal(messages.length, 0);
-        }
+        "should not report a violation": eslintTest.noViolation
     },
 
     "when evaluating with statement without one true brace style": {
 
         topic: "with (foo) \n { \n bar(); }",
 
-        "should report a violation": function(topic) {
-
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-            assert.equal(messages.length, 1);
-            assert.equal(messages[0].ruleId, RULE_ID);
-            assert.equal(messages[0].message, "Opening curly brace does not appear on the same line as the block identifier.");
-            assert.include(messages[0].node.type, "WithStatement");
-        }
+        "should report a violation": isViolationForType("WithStatement")
     },
 
     "when evaluating with statement with one true brace style": {
 
         topic: "with (foo) { \n bar(); }",
 
-        "should not report a violation": function(topic) {
-
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-            assert.equal(messages.length, 0);
-        }
+        "should not report a violation": eslintTest.noViolation
     },
 
     "when evaluating switch statement without one true brace style": {
 
         topic: "switch (foo) \n { \n case \"bar\": break; }",
 
-        "should report a violation": function(topic) {
-
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-            assert.equal(messages.length, 1);
-            assert.equal(messages[0].ruleId, RULE_ID);
-            assert.equal(messages[0].message, "Opening curly brace does not appear on the same line as the block identifier.");
-            assert.include(messages[0].node.type, "SwitchStatement");
-        }
+        "should report a violation": isViolationForType("SwitchStatement")
     },
 
     "when evaluating switch statement with one true brace style": {
 
         topic: "switch (foo) { \n case \"bar\": break; }",
 
-        "should not report a violation": function(topic) {
-
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-            assert.equal(messages.length, 0);
-        }
+        "should not report a violation": eslintTest.noViolation
     },
 
     "when evaluating try statement without one true brace style": {
 
         topic: "try \n { \n bar(); \n } catch (e) {}",
 
-        "should report a violation": function(topic) {
-
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-            assert.equal(messages.length, 1);
-            assert.equal(messages[0].ruleId, RULE_ID);
-            assert.equal(messages[0].message, "Opening curly brace does not appear on the same line as the block identifier.");
-            assert.include(messages[0].node.type, "TryStatement");
-        }
+        "should report a violation": isViolationForType("TryStatement")
     },
 
     "when evaluating try/catch statement with one true brace style": {
 
         topic: "try { \n bar();\n } catch (e) {\n baz(); \n }",
 
-        "should not report a violation": function(topic) {
-
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-            assert.equal(messages.length, 0);
-        }
+        "should not report a violation": eslintTest.noViolation
     },
 
     "when evaluating do/while statement without one true brace style": {
 
         topic: "do \n { \n bar(); \n} while (true)",
 
-        "should report a violation": function(topic) {
-
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-            assert.equal(messages.length, 1);
-            assert.equal(messages[0].ruleId, RULE_ID);
-            assert.equal(messages[0].message, "Opening curly brace does not appear on the same line as the block identifier.");
-            assert.include(messages[0].node.type, "DoWhileStatement");
-        }
+        "should report a violation": isViolationForType("DoWhileStatement")
     },
 
     "when evaluating do/while statement with one true brace style": {
 
         topic: "do { \n bar();\n } while (true)",
 
-        "should not report a violation": function(topic) {
-
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-            assert.equal(messages.length, 0);
-        }
+        "should not report a violation": eslintTest.noViolation
     },
 
     "when evaluating for/in statement without one true brace style": {
 
         topic: "for (foo in bar) \n { \n baz(); \n }",
 
-        "should report a violation": function(topic) {
-
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-            assert.equal(messages.length, 1);
-            assert.equal(messages[0].ruleId, RULE_ID);
-            assert.equal(messages[0].message, "Opening curly brace does not appear on the same line as the block identifier.");
-            assert.include(messages[0].node.type, "ForInStatement");
-        }
+        "should report a violation": isViolationForType("ForInStatement")
     },
 
     "when evaluating for/in statement with one true brace style": {
 
         topic: "for (foo in bar) { \n baz(); \n }",
 
-        "should not report a violation": function(topic) {
-
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-            assert.equal(messages.length, 0);
-        }
+        "should not report a violation": eslintTest.noViolation
     },
 
 }).export(module);
