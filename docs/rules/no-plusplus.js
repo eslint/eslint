c264f2840372273681d8d95eefc18c9f8c5e6ecd
Ian Christian Myers <ian@iancmyers.com>
2013-07-24 10:49:06 -0700

    Better documentation for the `no-plusplus` rule.

--- docs/rules/no-plusplus.js
+++ docs/rules/no-plusplus.js
@@ -1,6 +1,6 @@
 # no plusplus
 
-Flags the use of unary operators, `++` and `--`.
+The `no-plusplus` rule flags the use of unary operators, `++` and `--`.
 
 ```js
 var foo = 0;
@@ -8,6 +8,8 @@ foo++;
 ```
 
 ## Rule Details
+
+This rule is aimed at flagging the use of `++` and `--`. Some believe that the use of these unary operators reduces code quality and clarity. There are some programming languages that completely exclude these operators.
 
 The following patterns are considered warnings:
 
@@ -17,4 +19,14 @@ foo++;
 
 var bar = 42;
 bar--;
+```
+
+The following patterns are not considered warnings:
+
+```js
+var foo = 0;
+foo += 1;
+
+var bar = 42;
+bar -= 1;
 ```
