3e2b7ed6a82346d42ba1d0e954f7955dfe0a4b01
Josh Perez <josh@goatslacker.com>
2013-07-29 23:53:48 -0700

    Refactors no-caller test

--- tests/lib/rules/no-caller.js
+++ tests/lib/rules/no-caller.js
@@ -7,9 +7,7 @@
 // Requirements
 //------------------------------------------------------------------------------
 
-var vows = require("vows"),
-    assert = require("assert"),
-    eslint = require("../../../lib/eslint");
+var Test = require("../../eslint-test");
 
 //------------------------------------------------------------------------------
 // Constants
@@ -21,72 +19,19 @@ var RULE_ID = "no-caller";
 // Tests
 //------------------------------------------------------------------------------
 
-vows.describe(RULE_ID).addBatch({
-
-    "when evaluating 'var x = arguments.callee'": {
-
-        topic: "var x = arguments.callee",
-
-        "should report a violation": function(topic) {
-
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-
-            assert.equal(messages.length, 1);
-            assert.equal(messages[0].ruleId, RULE_ID);
-            assert.equal(messages[0].message, "Avoid arguments.callee.");
-            assert.include(messages[0].node.type, "MemberExpression");
+new Test(RULE_ID)
+    .addViolations({
+        "var x = arguments.callee": {
+            message: "Avoid arguments.callee.",
+            nodeType: "MemberExpression"
+        },
+        "var x = arguments.caller": {
+            message: "Avoid arguments.caller.",
+            nodeType: "MemberExpression"
         }
-    },
-
-    "when evaluating 'var x = arguments.caller'": {
-
-        topic: "var x = arguments.caller",
-
-        "should report a violation": function(topic) {
-
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-
-            assert.equal(messages.length, 1);
-            assert.equal(messages[0].ruleId, RULE_ID);
-            assert.equal(messages[0].message, "Avoid arguments.caller.");
-            assert.include(messages[0].node.type, "MemberExpression");
-        }
-    },
-
-    "when evaluating 'var x = arguments.length'": {
-
-        topic: "var x = arguments.length",
-
-        "should not report a violation": function(topic) {
-
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-
-            assert.equal(messages.length, 0);
-        }
-    },
-
-    "when evaluating 'var x = arguments'": {
-
-        topic: "var x = arguments",
-
-        "should not report a violation": function(topic) {
-
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-
-            assert.equal(messages.length, 0);
-        }
-    }
-
-}).export(module);
+    })
+    .addNonViolations([
+        "var x = arguments.length",
+        "var x = arguments"
+    ])
+    .export(module);
