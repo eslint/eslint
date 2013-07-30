b1a40d72cab71b6b40cc7a325413f1620bbbb01d
Josh Perez <josh@goatslacker.com>
2013-07-29 22:49:13 -0700

    Refactors curly test, renames messages

--- tests/lib/rules/curly.js
+++ tests/lib/rules/curly.js
@@ -7,9 +7,7 @@
 // Requirements
 //------------------------------------------------------------------------------
 
-var vows = require("vows"),
-    assert = require("assert"),
-    eslint = require("../../../lib/eslint");
+var Test = require('../../eslint-test');
 
 //------------------------------------------------------------------------------
 // Constants
@@ -21,162 +19,27 @@ var RULE_ID = "curly";
 // Tests
 //------------------------------------------------------------------------------
 
-vows.describe(RULE_ID).addBatch({
-
-    "when evaluating 'if (foo) bar()'": {
-
-        topic: "if (foo) bar()",
-
-        "should report a violation": function(topic) {
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-
-            assert.equal(messages.length, 1);
-            assert.equal(messages[0].ruleId, RULE_ID);
-            assert.equal(messages[0].message, "Expected { after 'if' condition.");
-            assert.include(messages[0].node.type, "IfStatement");
-        }
-    },
-
-    "when evaluating 'if (foo) { bar() }'": {
-
-        topic: "if (foo) { bar() }",
-
-        "should not report a violation": function(topic) {
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-
-            assert.equal(messages.length, 0);
-        }
-    },
-
-    "when evaluating 'if (foo) { bar() } else baz()'": {
-
-        topic: "if (foo) { bar() } else baz()",
-
-        "should report a violation": function(topic) {
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-
-            assert.equal(messages.length, 1);
-            assert.equal(messages[0].ruleId, RULE_ID);
-            assert.equal(messages[0].message, "Expected { after 'else'.");
-            assert.include(messages[0].node.type, "IfStatement");
-        }
-    },
-
-    "when evaluating 'if (foo) { bar() } else if (foo2) { baz() }'": {
-
-        topic: "if (foo) { bar() } else if (foo2) { baz() }",
-
-        "should not report a violation": function(topic) {
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-
-            assert.equal(messages.length, 0);
-        }
-    },
-
-
-    "when evaluating 'while (foo) bar()'": {
-
-        topic: "while (foo) bar()",
-
-        "should report a violation": function(topic) {
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-
-            assert.equal(messages.length, 1);
-            assert.equal(messages[0].ruleId, RULE_ID);
-            assert.equal(messages[0].message, "Expected { after 'while' condition.");
-            assert.include(messages[0].node.type, "WhileStatement");
-        }
-    },
-
-    "when evaluating 'while (foo) { bar() }'": {
-
-        topic: "while (foo) { bar() }",
-
-        "should not report a violation": function(topic) {
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-
-            assert.equal(messages.length, 0);
-        }
-    },
-
-    "when evaluating 'do bar(); while (foo)'": {
-
-        topic: "do bar(); while (foo)",
-
-        "should report a violation": function(topic) {
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-
-            assert.equal(messages.length, 1);
-            assert.equal(messages[0].ruleId, RULE_ID);
-            assert.equal(messages[0].message, "Expected { after 'do'.");
-            assert.include(messages[0].node.type, "DoWhileStatement");
-        }
-    },
-
-    "when evaluating 'do { bar(); } while (foo)'": {
-
-        topic: "do { bar(); } while (foo)",
-
-        "should not report a violation": function(topic) {
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-
-            assert.equal(messages.length, 0);
-        }
-    },
-
-    "when evaluating 'for (;foo;) bar()'": {
-
-        topic: "for (;foo;) bar()",
-
-        "should report a violation": function(topic) {
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-
-            assert.equal(messages.length, 1);
-            assert.equal(messages[0].ruleId, RULE_ID);
-            assert.equal(messages[0].message, "Expected { after 'for' condition.");
-            assert.include(messages[0].node.type, "ForStatement");
-        }
-    },
-
-    "when evaluating 'for (;foo;) { bar() }'": {
-
-        topic: "for (;foo;) { bar() }",
-
-        "should not report a violation": function(topic) {
-            var config = { rules: {} };
-            config.rules[RULE_ID] = 1;
-
-            var messages = eslint.verify(topic, config);
-
-            assert.equal(messages.length, 0);
-        }
-    }
-
-}).export(module);
+new Test(RULE_ID)
+    .addViolations("Expected { after 'if' condition.", {
+        "if (foo) bar()": "IfStatement"
+    })
+    .addViolations("Expected { after 'else' condition.", {
+        "if (foo) { bar() } else baz()": "IfStatement"
+    })
+    .addViolations("Expected { after 'while' statement.", {
+        "while (foo) bar()": "WhileStatement"
+    })
+    .addViolations("Expected { after 'do'.", {
+        "do bar(); while (foo)": "DoWhileStatement"
+    })
+    .addViolations("Expected { after 'for' statement.", {
+        "for (;foo;) bar()": "ForStatement"
+    })
+    .addNonViolations([
+        "if (foo) { bar() }",
+        "if (foo) { bar() } else if (foo2) { baz() }",
+        "while (foo) { bar() }",
+        "do { bar(); } while (foo)",
+        "for (;foo;) { bar() }"
+    ])
+    .export(module);
