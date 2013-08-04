a1ce81b67c8ccb4a88e39bc1c0450da65bac8ee0
Michael Ficarra <mficarra@groupon.com>
2013-08-03 22:33:42 -0500

    add back tests for loading rules from a directory

--- tests/lib/rules.js
+++ tests/lib/rules.js
@@ -9,13 +9,35 @@
 
 var vows = require("vows"),
     assert = require("assert"),
-    rules = require("../../lib/rules");
+    rules = require("../../lib/rules"),
+    path = require("path");
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
 vows.describe("rules").addBatch({
+
+    "when given an invalid rules directory": {
+
+        topic: "invaliddir",
+
+        "should log an error and exit": function(topic) {
+            assert.throws(function() { rules.load(topic); });
+        }
+
+    },
+
+    "when given a valid rules directory": {
+
+        topic: path.join(process.cwd(), "tests/fixtures/rules"),
+
+        "should load rules and not log an error or exit": function(topic) {
+            assert.equal(typeof rules.get("fixture-rule"), "undefined");
+            rules.load(topic);
+            assert.equal(typeof rules.get("fixture-rule"), "object");
+        }
+    },
 
     "when a rule has been defined": {
 
