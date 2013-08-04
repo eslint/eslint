7dd6bab7474d780302a0c4246914388a5d8f0f07
Michael Ficarra <mficarra@groupon.com>
2013-08-03 21:41:18 -0500

    add back in ability to load rules from a directory

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -2,14 +2,17 @@ bundleFile=eslint-min.js
 
 # generate a module that lists all rules in the rules directory
 rulesFile=lib/rules/index.js
-rules=lib/rules/*.js
-for ruleFile in $rules; do
+echo 'module.exports = function() {' > $rulesFile
+echo '  var rules = Object.create(null);' >> $rulesFile
+for ruleFile in lib/rules/*.js; do
     if [ $ruleFile '!=' $rulesFile ]; then
         ruleFile=${ruleFile#lib/rules/}
         ruleName=${ruleFile%.js}
-        echo "exports[\"$ruleName\"] = require(\"./$ruleFile\");" >> $rulesFile
+        echo "  rules[\"$ruleName\"] = require(\"./$ruleFile\");" >> $rulesFile
     fi
 done
+echo '  return rules;' >> $rulesFile
+echo '};' >> $rulesFile
 
 # bundle, using our generated rule module instead of dynamic loader in /lib/load-rules.js
 node_modules/.bin/cjsify \
