4b5a3d4404781d86531547044a7a119feb22e273
Matthew DuVall <mduvall89@gmail.com>
2013-08-08 09:42:42 -0700

    bug: Add ForStatement for no-cond-assign check

--- tests/lib/rules/no-cond-assign.js
+++ tests/lib/rules/no-cond-assign.js
@@ -77,6 +77,24 @@ vows.describe(RULE_ID).addBatch({
         }
     },
 
+    "when evaluating 'var x = 0; for(; x+=1 ;){};'": {
+
+        topic: "var x; for(; x+=1 ;){};",
+
+        "should report a violation": function(topic) {
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+
+            assert.equal(messages.length, 1);
+            assert.equal(messages[0].ruleId, RULE_ID);
+            assert.equal(messages[0].message,
+                    "Expected a conditional expression and instead saw an assignment.");
+            assert.include(messages[0].node.type, "ForStatement");
+        }
+    },
+
     "when evaluating 'var x = 0; if (x == 0) { var b = 1; }'": {
 
         topic: "var x = 0; if (x == 0) { var b = 1; }",
