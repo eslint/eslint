4f1376f258adbf1dbc3c97beba99760167e1429c
davidjnelson <david.jonathan.nelson@gmail.com>
2013-08-11 16:57:31 -0700

    fix: 210 - missed checkin of native-objects.js

---
+++ conf/native-objects.js
@@ -0,0 +1,15 @@
+/**
+ * @fileoverview A list of all native objects to be reused among rules.
+ * @author David Nelson
+ */
+
+module.exports = {
+    nativeObjects: ["Object", "String"]
+    /*["Array", "Boolean", "Date", "decodeURI",
+        "decodeURIComponent", "encodeURI", "encodeURIComponent",
+        "Error", "eval", "EvalError", "Function", "isFinite",
+        "isNaN", "JSON", "Math", "Number", "Object", "parseInt",
+        "parseFloat", "RangeError", "ReferenceError", "RegExp",
+        "String", "SyntaxError", "TypeError", "URIError",
+        "Map", "NaN", "Set", "WeakMap", "Infinity", "undefined"]*/
+};
