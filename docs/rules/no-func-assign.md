a935eb33e92a2002db90b37c33aa4605ec734095
Ian Christian Myers <ian@iancmyers.com>
2013-08-28 14:50:55 -0700

    Rule: no-func-assign
    The `no-func-assign` rule disallows overwriting functions that were written as a FunctionDeclaration.
    
    function foo() {}
    foo = bar;
    
    This rule matches JSHint rule W021.

---
+++ docs/rules/no-func-assign.md
@@ -0,0 +1,38 @@
+# no func assign
+
+JavaScript functions can be written as a FunctionDeclaration `function foo() { ... }` or as a FunctionExpression `var foo = function() { ... };`. While a JavaScript interpreter might tolerate it, overwriting/reassigning a function written as a FunctionDeclaration is often indicative of a mistake or issue.
+
+```js
+function foo() {}
+foo = bar;
+```
+
+## Rule Details
+
+This rule is aimed at flagging probable mistakes and issues in the form of overwriting a function that was written as a FunctionDeclaration. As such it will warn when this issue is encountered.
+
+The following patterns are considered warnings:
+
+```js
+function foo() {}
+foo = bar;
+
+function foo {
+    foo = bar;
+}
+```
+
+The following patterns are not considered warnings:
+
+```js
+var foo = function () {}
+foo = bar;
+
+function foo(foo) { // `foo` is shadowed.
+    foo = bar;
+}
+
+function foo() {
+    var foo = bar;  // `foo` is shadowed.
+}
+```
---
+++ tests/lib/rules/no-func-assign.js
@@ -0,0 +1,144 @@
+/**
+ * @fileoverview Tests for no-func-assign.
+ * @author Ian Christian Myers
+ */
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var vows = require("vows"),
+    assert = require("assert"),
+    eslint = require("../../../lib/eslint");
+
+//------------------------------------------------------------------------------
+// Constants
+//------------------------------------------------------------------------------
+
+var RULE_ID = "no-func-assign";
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+vows.describe(RULE_ID).addBatch({
+
+    "when evaluating 'function foo() {} foo = bar;'": {
+
+        topic: "function foo() {} foo = bar;",
+
+        "should report a violation": function(topic) {
+
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+            assert.equal(messages.length, 1);
+            assert.equal(messages[0].ruleId, RULE_ID);
+            assert.equal(messages[0].message, "'foo' is a function.");
+            assert.include(messages[0].node.type, "AssignmentExpression");
+        }
+    },
+
+    "when evaluating 'function foo() { foo = bar; }'": {
+
+        topic: "function foo() { foo = bar; }",
+
+        "should report a violation": function(topic) {
+
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+            assert.equal(messages.length, 1);
+            assert.equal(messages[0].ruleId, RULE_ID);
+            assert.equal(messages[0].message, "'foo' is a function.");
+            assert.include(messages[0].node.type, "AssignmentExpression");
+        }
+    },
+
+    "when evaluating 'function foo() { var foo = bar; }'": {
+
+        topic: "function foo() { var foo = bar; }",
+
+        "should not report a violation": function(topic) {
+
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+            assert.equal(messages.length, 0);
+        }
+    },
+
+    "when evaluating 'function foo(foo) { foo = bar; }'": {
+
+        topic: "function foo(foo) { foo = bar; }",
+
+        "should not report a violation": function(topic) {
+
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+            assert.equal(messages.length, 0);
+        }
+    },
+
+    "when evaluating 'function foo() { var foo; foo = bar; }'": {
+
+        topic: "function foo() { var foo; foo = bar; }",
+
+        "should not report a violation": function(topic) {
+
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+            assert.equal(messages.length, 0);
+        }
+    },
+
+    "when evaluating 'var foo = function() {}; foo = bar;'": {
+
+        topic: "var foo = function() {}; foo = bar;",
+
+        "should not report a violation": function(topic) {
+
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+            assert.equal(messages.length, 0);
+        }
+    },
+
+    "when evaluating 'var foo = function() { foo = bar; };'": {
+
+        topic: "var foo = function() { foo = bar; };",
+
+        "should not report a violation": function(topic) {
+
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+            assert.equal(messages.length, 0);
+        }
+    },
+
+    "when evaluating 'foo = bar; function foo() { };'": {
+
+        topic: "foo = bar; function foo() { };",
+
+        "should not report a violation": function(topic) {
+
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+            assert.equal(messages.length, 0);
+        }
+    },
+
+}).export(module);
