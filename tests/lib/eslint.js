c4c34f03e066ccdc84d8fbbbc3feb697dab60319
Ilya Volodin <ivolodin@gmail.com>
2013-10-18 23:28:42 -0400

    Fixing unittests and finishing code

--- tests/lib/eslint.js
+++ tests/lib/eslint.js
@@ -710,6 +710,35 @@ vows.describe("eslint").addBatch({
             var messages = eslint.verify(topic, config);
             assert.equal(messages.length, 0);
         }
+    },
+
+    "when evaluating code with comments to enable multiple rules": {
+        topic: "/*eslint no-alert:1 no-console:1*/ alert('test'); console.log('test');",
+        "should report a violation": function(topic) {
+
+            var config = { rules: {} };
+
+            var messages = eslint.verify(topic, config);
+            assert.equal(messages.length, 2);
+            assert.equal(messages[0].ruleId, "no-alert");
+            assert.equal(messages[0].message, "Unexpected alert.");
+            assert.include(messages[0].node.type, "CallExpression");
+            assert.equal(messages[1].ruleId, "no-console");
+        }
+    },
+
+    "when evaluating code with comments to enable and disable multiple rules": {
+        topic: "/*eslint no-alert:1 no-console:0*/ alert('test'); console.log('test');",
+        "should report a violation": function(topic) {
+
+            var config = { rules: { "no-console" : 1, "no-alert" : 0 } };
+
+            var messages = eslint.verify(topic, config);
+            assert.equal(messages.length, 1);
+            assert.equal(messages[0].ruleId, "no-alert");
+            assert.equal(messages[0].message, "Unexpected alert.");
+            assert.include(messages[0].node.type, "CallExpression");
+        }
     }
 
 }).export(module);
