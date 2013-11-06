18011e58ec2f143edd10b3996aff4bfc0c468e6b
Michael Ficarra <mficarra@groupon.com>
2013-11-06 00:27:51 -0600

    recognise CallExpression variant of RegExp ctor in no-control-regex rule

--- tests/lib/rules/no-control-regex.js
+++ tests/lib/rules/no-control-regex.js
@@ -57,6 +57,23 @@ vows.describe(RULE_ID).addBatch({
         }
     },
 
+    "when evaluating a regular expression object with a control character (without new)": {
+
+        topic: "var regex = RegExp('\\x1f')",
+
+        "should report a violation": function(topic) {
+
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+            assert.equal(messages.length, 1);
+            assert.equal(messages[0].ruleId, RULE_ID);
+            assert.equal(messages[0].message, "Unexpected control character in regular expression.");
+            assert.include(messages[0].node.type, "Literal");
+        }
+    },
+
     "when evaluating a regular expression literal without a control character": {
 
         topic: "var regex = /x1f/",
@@ -85,9 +102,37 @@ vows.describe(RULE_ID).addBatch({
         }
     },
 
+    "when evaluating a regular expression object without a control character (without new)": {
+
+        topic: "var regex = RegExp('x1f')",
+
+        "should not report a violation": function(topic) {
+
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+            assert.equal(messages.length, 0);
+        }
+    },
+
     "when evaluating a regular expression object with invalid pattern": {
 
         topic: "new RegExp('[')",
+
+        "should not report a violation": function(topic) {
+
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+            assert.equal(messages.length, 0);
+        }
+    },
+
+    "when evaluating a regular expression object with invalid pattern (without new)": {
+
+        topic: "RegExp('[')",
 
         "should not report a violation": function(topic) {
 
