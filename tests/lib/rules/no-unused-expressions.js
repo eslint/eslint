520dca0bf498da4f8d280e401dc8138284c311fe
Michael Ficarra <mficarra@groupon.com>
2013-11-11 22:49:25 -0600

    fixes #369: restrict UnaryExpressions to delete in no-unused-expressions

--- tests/lib/rules/no-unused-expressions.js
+++ tests/lib/rules/no-unused-expressions.js
@@ -240,6 +240,57 @@ vows.describe(RULE_ID).addBatch({
         }
     },
 
+    "when evaluating '!a'": {
+
+        topic: "!a",
+
+        "should report a violation": function(topic) {
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+
+            assert.equal(messages.length, 1);
+            assert.equal(messages[0].ruleId, RULE_ID);
+            assert.equal(messages[0].message, "Expected an assignment or function call and instead saw an expression.");
+            assert.include(messages[0].node.type, "ExpressionStatement");
+        }
+    },
+
+    "when evaluating '+a'": {
+
+        topic: "+a",
+
+        "should report a violation": function(topic) {
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+
+            assert.equal(messages.length, 1);
+            assert.equal(messages[0].ruleId, RULE_ID);
+            assert.equal(messages[0].message, "Expected an assignment or function call and instead saw an expression.");
+            assert.include(messages[0].node.type, "ExpressionStatement");
+        }
+    },
+
+    "when evaluating 'void a'": {
+
+        topic: "void a",
+
+        "should report a violation": function(topic) {
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+
+            assert.equal(messages.length, 1);
+            assert.equal(messages[0].ruleId, RULE_ID);
+            assert.equal(messages[0].message, "Expected an assignment or function call and instead saw an expression.");
+            assert.include(messages[0].node.type, "ExpressionStatement");
+        }
+    },
+
     "when evaluating '\"use strict\";'": {
 
         topic: "\"use strict\";",
