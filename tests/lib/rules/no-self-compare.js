7b40e80b4c6b570800edc9801e3faa6291352317
Ilya Volodin <ivolodin@vistaprint.com>
2013-11-12 22:02:27 -0500

    Fixing no-self-compare rule to check for operator

--- tests/lib/rules/no-self-compare.js
+++ tests/lib/rules/no-self-compare.js
@@ -162,7 +162,155 @@ vows.describe(RULE_ID).addBatch({
 
             assert.equal(messages.length, 0);
         }
+    },
+
+    "when evaluating 'y=x*x'": {
+
+        topic: "y=x*x",
+
+        "should not report a violation": function(topic) {
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+
+            assert.equal(messages.length, 0);
+        }
+    },
+
+    "when evaluating 'x === x": {
+
+        topic: "x === x",
+
+        "should report a violation": function(topic) {
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+
+            assert.equal(messages.length, 1);
+            assert.equal(messages[0].ruleId, RULE_ID);
+            assert.equal(messages[0].message, "Comparing to itself is potentially pointless.");
+            assert.include(messages[0].node.type, "BinaryExpression");
+        }
+    },
+
+    "when evaluating 'x !== x": {
+
+        topic: "x !== x",
+
+        "should report a violation": function(topic) {
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+
+            assert.equal(messages.length, 1);
+            assert.equal(messages[0].ruleId, RULE_ID);
+            assert.equal(messages[0].message, "Comparing to itself is potentially pointless.");
+            assert.include(messages[0].node.type, "BinaryExpression");
+        }
+    },
+
+    "when evaluating 'x == x": {
+
+        topic: "x == x",
+
+        "should report a violation": function(topic) {
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+
+            assert.equal(messages.length, 1);
+            assert.equal(messages[0].ruleId, RULE_ID);
+            assert.equal(messages[0].message, "Comparing to itself is potentially pointless.");
+            assert.include(messages[0].node.type, "BinaryExpression");
+        }
+    },
+
+    "when evaluating 'x != x": {
+
+        topic: "x != x",
+
+        "should report a violation": function(topic) {
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+
+            assert.equal(messages.length, 1);
+            assert.equal(messages[0].ruleId, RULE_ID);
+            assert.equal(messages[0].message, "Comparing to itself is potentially pointless.");
+            assert.include(messages[0].node.type, "BinaryExpression");
+        }
+    },
+
+    "when evaluating 'x > x": {
+
+        topic: "x === x",
+
+        "should report a violation": function(topic) {
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+
+            assert.equal(messages.length, 1);
+            assert.equal(messages[0].ruleId, RULE_ID);
+            assert.equal(messages[0].message, "Comparing to itself is potentially pointless.");
+            assert.include(messages[0].node.type, "BinaryExpression");
+        }
+    },
+
+    "when evaluating 'x < x": {
+
+        topic: "x !== x",
+
+        "should report a violation": function(topic) {
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+
+            assert.equal(messages.length, 1);
+            assert.equal(messages[0].ruleId, RULE_ID);
+            assert.equal(messages[0].message, "Comparing to itself is potentially pointless.");
+            assert.include(messages[0].node.type, "BinaryExpression");
+        }
+    },
+
+    "when evaluating 'x >= x": {
+
+        topic: "x == x",
+
+        "should report a violation": function(topic) {
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+
+            assert.equal(messages.length, 1);
+            assert.equal(messages[0].ruleId, RULE_ID);
+            assert.equal(messages[0].message, "Comparing to itself is potentially pointless.");
+            assert.include(messages[0].node.type, "BinaryExpression");
+        }
+    },
+
+    "when evaluating 'x <= x": {
+
+        topic: "x != x",
+
+        "should report a violation": function(topic) {
+            var config = { rules: {} };
+            config.rules[RULE_ID] = 1;
+
+            var messages = eslint.verify(topic, config);
+
+            assert.equal(messages.length, 1);
+            assert.equal(messages[0].ruleId, RULE_ID);
+            assert.equal(messages[0].message, "Comparing to itself is potentially pointless.");
+            assert.include(messages[0].node.type, "BinaryExpression");
+        }
     }
-
-
 }).export(module);
