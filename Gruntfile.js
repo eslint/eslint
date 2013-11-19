ed963b8943d18213d3bac4d84022feebda828531
Nicholas C. Zakas <nicholas@nczconsulting.com>
2013-11-18 16:48:49 -0800

    Reformatted Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,82 +1,82 @@
-'use strict';
+"use strict";
 module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         jshint: {
             options: {
-                jshintrc: '.jshintrc'
+                jshintrc: ".jshintrc"
             },
             lib: {
-                src: ['lib/**/*.js', 'bin/*.js']
+                src: ["lib/**/*.js", "bin/*.js"]
             }
         },
         vows: {
             all : {
-                src : 'build/instrument/tests/lib/**/*.js',
+                src : "build/instrument/tests/lib/**/*.js",
                 options : {
-                    reporter : 'spec'
+                    reporter : "spec"
                 }
             }
         },
         coverage: {
             options: {
                 thresholds: {
-                    'statements': 95,
-                    'branches': 95,
-                    'lines': 95,
-                    'functions': 95
+                    "statements": 95,
+                    "branches": 95,
+                    "lines": 95,
+                    "functions": 95
                 },
-                dir: 'reports',
-                root: 'build'
+                dir: "reports",
+                root: "build"
             }
         },
-        clean: ['build/'],
-            instrument : {
-                files : ['lib/**/*.js', 'bin/*.js'],
-                options : {
+        clean: ["build/"],
+        instrument : {
+            files : ["lib/**/*.js", "bin/*.js"],
+            options : {
                 lazy : true,
-                basePath : 'build/instrument/'
+                basePath : "build/instrument/"
             }
         },
         copy: {
             all: {
-                src: ['tests/**/*', 'conf/*', 'tests/fixtures/configurations/single-quotes/.eslintrc'],
-                dest: 'build/instrument/'
+                src: ["tests/**/*", "conf/*", "tests/fixtures/configurations/single-quotes/.eslintrc"],
+                dest: "build/instrument/"
             }
         },
         storeCoverage : {
             options : {
-                dir : 'build/reports/'
+                dir : "build/reports/"
             }
         },
         makeReport : {
-            src : 'build/reports/**/*.json',
+            src : "build/reports/**/*.json",
             options : {
-                type : 'lcov',
-                dir : 'build/reports/',
-                print: 'print'
+                type : "lcov",
+                dir : "build/reports/",
+                print: "print"
             }
         },
         mochaTest: {
             test: {
                 options: {
-                    reporter: 'spec'
+                    reporter: "spec"
                 },
-                src: 'build/instrument/tests/mocha/lib/**/*.js'
+                src: "build/instrument/tests/mocha/lib/**/*.js"
             }
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-istanbul');
-    grunt.loadNpmTasks('grunt-istanbul-coverage');
-    grunt.loadNpmTasks('grunt-vows-runner');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks("grunt-contrib-copy");
+    grunt.loadNpmTasks("grunt-contrib-clean");
+    grunt.loadNpmTasks("grunt-istanbul");
+    grunt.loadNpmTasks("grunt-istanbul-coverage");
+    grunt.loadNpmTasks("grunt-vows-runner");
+    grunt.loadNpmTasks("grunt-contrib-jshint");
+    grunt.loadNpmTasks("grunt-mocha-test");
 
     // Default task.
-    grunt.registerTask('default', ['jshint']);
-    grunt.registerTask('test', ['jshint', 'instrument', 'copy', 'vows', 'storeCoverage', 'makeReport', 'coverage', 'clean']);
-    grunt.registerTask('mocha', ['jshint', 'instrument', 'copy', 'mochaTest', 'storeCoverage', 'makeReport', 'clean']);
+    grunt.registerTask("default", ["jshint"]);
+    grunt.registerTask("test", ["jshint", "instrument", "copy", "vows", "storeCoverage", "makeReport", "coverage", "clean"]);
+    grunt.registerTask("mocha", ["jshint", "instrument", "copy", "mochaTest", "storeCoverage", "makeReport", "clean"]);
 };
