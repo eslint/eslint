41a62ad85289396da8d4796e16b91aa4f27494da
Nicholas C. Zakas <nicholas@nczconsulting.com>
2013-11-27 15:13:26 -0800

    Fix false positives for no-script-url (fixes #400)

--- tests/lib/rules/no-script-url.js
+++ tests/lib/rules/no-script-url.js
@@ -16,9 +16,21 @@ var eslintTester = require("../../../lib/tests/eslintTester");
 eslintTester.add("no-script-url", {
     valid: [
         "var a = 'Hello World!';",
-        "var a = 10;"
+        "var a = 10;",
+        "var url = 'xjavascript:'",
     ],
     invalid: [
-        { code: "var a = 'javascript:void(0);';", errors: [{ message: "Script URL is a form of eval.", type: "Literal"}] }
+        {
+            code: "var a = 'javascript:void(0);';",
+            errors: [
+                { message: "Script URL is a form of eval.", type: "Literal"}
+            ]
+        },
+        {
+            code: "var a = 'javascript:';",
+            errors: [
+                { message: "Script URL is a form of eval.", type: "Literal"}
+            ]
+        }
     ]
 });
