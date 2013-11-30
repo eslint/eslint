31843583a49b1d16a98344f2da8addae61080865
Michael Ficarra <mficarra@groupon.com>
2013-11-30 00:04:42 -0600

    fixes #346: validate strings passed to the RegExp constructor

---
+++ docs/rules/no-invalid-regexp.md
@@ -0,0 +1,37 @@
+# no invalid regexp
+
+This rule validates string arguments passed to the `RegExp` constructor.
+
+## Rule Details
+
+The following patterns are considered warnings:
+
+```js
+RegExp('['])
+```
+
+```js
+RegExp('.', 'z') // invalid flags
+```
+
+```js
+new RegExp('\\')
+```
+
+The following patterns are not considered warnings:
+
+```js
+RegExp('.')
+```
+
+```js
+new RegExp
+```
+
+```js
+this.RegExp('[')
+```
+
+## Further Reading
+
+* [Annotated ES5 §7.8.5 - Regular Expression Literals](http://es5.github.io/#x7.8.5)
---
+++ tests/lib/rules/no-invalid-regexp.js
@@ -0,0 +1,27 @@
+/**
+ * @fileoverview Validate strings passed to the RegExp constructor
+ * @author Michael Ficarra
+ */
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var eslintTester = require("../../../lib/tests/eslintTester");
+
+eslintTester.addRuleTest("no-invalid-regexp", {
+    valid: [
+        "RegExp('')",
+        "RegExp()",
+        "RegExp('.', 'g')",
+        "new RegExp('.')",
+        "new RegExp",
+        "new RegExp('.', 'im')",
+        "global.RegExp('\\\\')"
+    ],
+    invalid: [
+        { code: "RegExp('[');", errors: [{ message: "Invalid regular expression: /[/: Unterminated character class", type: "CallExpression" }] },
+        { code: "RegExp('.', 'z');", errors: [{ message: "Invalid flags supplied to RegExp constructor 'z'", type: "CallExpression" }] },
+        { code: "new RegExp(')');", errors: [{ message: "Invalid regular expression: /)/: Unmatched ')'", type: "NewExpression" }] }
+    ]
+});
