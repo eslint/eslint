5feec313934695c4180da71c62b1a13eb0d7305b
Michael Ficarra <mficarra@groupon.com>
2013-12-04 01:21:48 -0600

    fixes #437: fragile no-extend-native rule
    I've also changed it to only affect the native constructors (not all
    properties of the global object) and support ctor['prototype'] access.

--- tests/lib/rules/no-extend-native.js
+++ tests/lib/rules/no-extend-native.js
@@ -7,51 +7,56 @@
 // Requirements
 //------------------------------------------------------------------------------
 
-var eslintTester = require("../../../lib/tests/eslintTester"),
-    environment = require("../../../conf/environments.json"),
-    builtins = Object.keys(environment.builtin);
+var eslintTester = require("../../../lib/tests/eslintTester");
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
-eslintTester.addRuleTest( "no-extend-native", {
+eslintTester.addRuleTest("no-extend-native", {
     valid: [
-        "x.prototype.newthing = 'good'",
-        "Object.defineProperty(x, 'newthing', {value: 'good'})",
-        "m = Math; m.prototype.times = 3",
-        "with(Object) { prototype.blast = 'off'; }",
-        "eval('Object.prototype.seven = 7')"
+        "x.prototype.p = 0",
+        "x.prototype['p'] = 0",
+        "Math.prototype.p = 0",
+        "Object.p = 0",
+        "Object.toString.bind = 0",
+        "Object['toString'].bind = 0",
+        "Object.defineProperty(x, 'p', {value: 0})",
+        "global.Object.prototype.toString = 0",
+        "this.Object.prototype.toString = 0",
+        "with(Object) { prototype.p = 0; }",
+        "o = Object; o.prototype.toString = 0",
+        "eval('Object.prototype.toString = 0')"
     ],
     invalid: [{
-        code: "Math.prototype.gonzo = 333",
+        code: "Object.prototype.p = 0",
         errors: [{
-            message: "Math prototype is read only, properties should not be added.",
+            message: "Object prototype is read only, properties should not be added.",
             type: "AssignmentExpression"
         }]
     }, {
-        code: "Object.defineProperty(Array.prototype, 'toast', {value:666})",
+        code: "Function.prototype['p'] = 0",
+        errors: [{
+            message: "Function prototype is read only, properties should not be added.",
+            type: "AssignmentExpression"
+        }]
+    }, {
+        code: "String['prototype'].p = 0",
+        errors: [{
+            message: "String prototype is read only, properties should not be added.",
+            type: "AssignmentExpression"
+        }]
+    }, {
+        code: "Number['prototype']['p'] = 0",
+        errors: [{
+            message: "Number prototype is read only, properties should not be added.",
+            type: "AssignmentExpression"
+        }]
+    }, {
+        code: "Object.defineProperty(Array.prototype, 'p', {value: 0})",
         errors: [{
             message: "Array prototype is read only, properties should not be added.",
             type: "CallExpression"
         }]
-    // loop through all of the builtins and test for adding to the prototype
-    }].concat(builtins.map(function(builtin) {
-        return {
-            code: builtin + ".prototype.fake = 'fake'",
-            errors: [{
-                message: builtin + " prototype is read only, properties should not be added.",
-                type: "AssignmentExpression"
-            }]
-        };
-    // loop through all of the builtins and test for using Object.defineProperty
-    })).concat(builtins.map(function(builtin) {
-        return {
-            code: "Object.defineProperty(" + builtin + ".prototype, 'fake', {value: 'fake'})",
-            errors: [{
-                message: builtin + " prototype is read only, properties should not be added.",
-                type: "CallExpression"
-            }]
-        };
-    }))
+    }]
 });
