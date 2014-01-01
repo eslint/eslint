34865e8adc3245f84c68b5cb4a5728b55923b873
Nicholas C. Zakas <nicholas@nczconsulting.com>
2014-01-01 12:24:40 -0800

    Bump code coverage checks

--- Makefile.js
+++ Makefile.js
@@ -94,7 +94,7 @@ target.lint = function() {
 target.test = function() {
     target.lint();
     exec(ISTANBUL + " cover " + MOCHA + "-- -c " + TEST_FILES);
-    exec(ISTANBUL + "check-coverage --statement 95 --branch 95 --function 95 --lines 95");
+    exec(ISTANBUL + "check-coverage --statement 99 --branch 98 --function 99 --lines 99");
 };
 
 target.docs = function() {
