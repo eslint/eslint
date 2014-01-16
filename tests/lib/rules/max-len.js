a2b06cf285de702699ee5f0fc6520ab2738cf90f
dmp42 <olivier@webitup.fr>
2014-01-17 00:33:41 +0100

    Change tests to read "line 1" when on line 1

--- tests/lib/rules/max-len.js
+++ tests/lib/rules/max-len.js
@@ -19,8 +19,8 @@ eslintTester.addRuleTest("max-len", {
         { code: "\t\t\tvar i = 1;\n\t\t\tvar j = 1;", args: [1, 15, 1] }
     ],
     invalid: [
-        { code: "var x = 5, y = 2, z = 5;", args: [1, 10, 4], errors: [{ message: "Line 0 exceeds the maximum line length of 10.", type: "Program"}] },
-        { code: "\t\t\tvar i = 1;", args: [1, 15, 4], errors: [{ message: "Line 0 exceeds the maximum line length of 15.", type: "Program"}] },
-        { code: "\t\t\tvar i = 1;\n\t\t\tvar j = 1;", args: [1, 15, 4], errors: [{ message: "Line 0 exceeds the maximum line length of 15.", type: "Program"}, { message: "Line 1 exceeds the maximum line length of 15.", type: "Program"}] }
+        { code: "var x = 5, y = 2, z = 5;", args: [1, 10, 4], errors: [{ message: "Line 1 exceeds the maximum line length of 10.", type: "Program"}] },
+        { code: "\t\t\tvar i = 1;", args: [1, 15, 4], errors: [{ message: "Line 1 exceeds the maximum line length of 15.", type: "Program"}] },
+        { code: "\t\t\tvar i = 1;\n\t\t\tvar j = 1;", args: [1, 15, 4], errors: [{ message: "Line 1 exceeds the maximum line length of 15.", type: "Program"}, { message: "Line 2 exceeds the maximum line length of 15.", type: "Program"}] }
     ]
 });
