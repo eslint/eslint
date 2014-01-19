ce50da655e68a9da5aa2616f7daf249c39965f0a
dmp42 <olivier@webitup.fr>
2014-01-19 20:53:55 +0100

    Test empty

--- tests/lib/rules/max-len.js
+++ tests/lib/rules/max-len.js
@@ -17,7 +17,8 @@ eslintTester.addRuleTest("max-len", {
     valid: [
         { code: "var x = 5;\nvar x = 2;", args: [1, 80, 4] },
         { code: "\t\t\tvar i = 1;\n\t\t\tvar j = 1;", args: [1, 15, 1] },
-        { code: "var i = 1;\r\nvar i = 1;\n", args: [1, 10, 4] }
+        { code: "var i = 1;\r\nvar i = 1;\n", args: [1, 10, 4] },
+        ""
     ],
     invalid: [
         { code: "var x = 5, y = 2, z = 5;", args: [1, 10, 4], errors: [{ message: "Line 1 exceeds the maximum line length of 10.", type: "Program"}] },
