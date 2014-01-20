8061f35223fd52b4d9398bd0d9a6c2c96025f365
Nicholas C. Zakas <nicholas@nczconsulting.com>
2014-01-20 12:39:08 -0800

    Bug: max-len fix to report correct line number (fixes #552)

--- tests/lib/rules/max-len.js
+++ tests/lib/rules/max-len.js
@@ -6,7 +6,6 @@
 //------------------------------------------------------------------------------
 // Requirements
 //------------------------------------------------------------------------------
-
 var eslintTester = require("../../../lib/tests/eslintTester");
 
 //------------------------------------------------------------------------------
@@ -15,14 +14,57 @@ var eslintTester = require("../../../lib/tests/eslintTester");
 
 eslintTester.addRuleTest("max-len", {
     valid: [
-        { code: "var x = 5;\nvar x = 2;", args: [1, 80, 4] },
-        { code: "\t\t\tvar i = 1;\n\t\t\tvar j = 1;", args: [1, 15, 1] },
-        { code: "var i = 1;\r\nvar i = 1;\n", args: [1, 10, 4] },
+        {
+            code: "var x = 5;\nvar x = 2;",
+            args: [1, 80, 4]
+        }, {
+            code: "\t\t\tvar i = 1;\n\t\t\tvar j = 1;",
+            args: [1, 15, 1]
+        }, {
+            code: "var i = 1;\r\nvar i = 1;\n",
+            args: [1, 10, 4]
+        },
         ""
     ],
+
     invalid: [
-        { code: "var x = 5, y = 2, z = 5;", args: [1, 10, 4], errors: [{ message: "Line 1 exceeds the maximum line length of 10.", type: "Program"}] },
-        { code: "\t\t\tvar i = 1;", args: [1, 15, 4], errors: [{ message: "Line 1 exceeds the maximum line length of 15.", type: "Program"}] },
-        { code: "\t\t\tvar i = 1;\n\t\t\tvar j = 1;", args: [1, 15, 4], errors: [{ message: "Line 1 exceeds the maximum line length of 15.", type: "Program"}, { message: "Line 2 exceeds the maximum line length of 15.", type: "Program"}] }
+        {
+            code: "var x = 5, y = 2, z = 5;",
+            args: [1, 10, 4],
+            errors: [
+                {
+                    message: "Line 1 exceeds the maximum line length of 10.",
+                    type: "Program",
+                    line: 1
+                }
+            ]
+        },
+        {
+            code: "\t\t\tvar i = 1;",
+            args: [1, 15, 4],
+            errors: [
+                {
+                    message: "Line 1 exceeds the maximum line length of 15.",
+                    type: "Program",
+                    line: 1
+                }
+            ]
+        },
+        {
+            code: "\t\t\tvar i = 1;\n\t\t\tvar j = 1;",
+            args: [1, 15, 4],
+            errors: [
+                {
+                    message: "Line 1 exceeds the maximum line length of 15.",
+                    type: "Program",
+                    line: 1
+                },
+                {
+                    message: "Line 2 exceeds the maximum line length of 15.",
+                    type: "Program",
+                    line: 2
+                }
+            ]
+        }
     ]
 });
