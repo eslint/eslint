bec7022c71b06d8f28719027a3fd2b9f73353aa6
Nicholas C. Zakas <nicholas@nczconsulting.com>
2014-01-25 16:48:39 -0800

    Fix: Make radix rule warn on invalid second parameter (fixes #563)

--- tests/lib/rules/radix.js
+++ tests/lib/rules/radix.js
@@ -10,11 +10,54 @@
 var eslintTester = require("../../../lib/tests/eslintTester");
 
 eslintTester.addRuleTest("radix", {
+
     valid: [
-        "parseInt(\"10\", 10);"
-           ],
+        "parseInt(\"10\", 10);",
+        "parseInt(\"10\", foo);"
+    ],
+
     invalid: [
-        { code: "parseInt(\"10\");",
-          errors: [{ message: "Missing radix parameter.", type: "CallExpression"}] }
+        {
+            code: "parseInt(\"10\");",
+            errors: [{
+                message: "Missing radix parameter.",
+                type: "CallExpression"
+            }]
+        },
+        {
+            code: "parseInt(\"10\", null);",
+            errors: [{
+                message: "Invalid radix parameter.",
+                type: "CallExpression"
+            }]
+        },
+        {
+            code: "parseInt(\"10\", undefined);",
+            errors: [{
+                message: "Invalid radix parameter.",
+                type: "CallExpression"
+            }]
+        },
+        {
+            code: "parseInt(\"10\", true);",
+            errors: [{
+                message: "Invalid radix parameter.",
+                type: "CallExpression"
+            }]
+        },
+        {
+            code: "parseInt(\"10\", \"foo\");",
+            errors: [{
+                message: "Invalid radix parameter.",
+                type: "CallExpression"
+            }]
+        },
+        {
+            code: "parseInt(\"10\", \"123\");",
+            errors: [{
+                message: "Invalid radix parameter.",
+                type: "CallExpression"
+            }]
+        }
     ]
 });
