5560029b9b8d332805cbe6f20ec62925117b2d83
Shmueli Englard <shmueli.yosef@englard.net>
2014-02-03 22:41:36 -0500

    Added test for bug fix #582. Test Passes

--- tests/lib/rules/no-octal-escape.js
+++ tests/lib/rules/no-octal-escape.js
@@ -17,7 +17,7 @@ eslintTester.addRuleTest("lib/rules/no-octal-escape", {
     valid: [
         "var foo = \"\\851\";",
         "var foo = \"foo \\\\251 bar\";",
-        "var foo = /([abc]) \1/g"
+        "var foo = /([abc]) \1/g;"
     ],
     invalid: [
         { code: "var foo = \"foo \\251 bar\";", errors: [{ message: "Don't use octal: '\\2'. Use '\\u....' instead.", type: "Literal"}] },
