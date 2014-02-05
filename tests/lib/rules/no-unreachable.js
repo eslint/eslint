32123cf6bced37ab903dbad730d92784f41cc9f6
Christian <me@rndm.de>
2014-02-05 17:10:34 +0100

    #586 add hoisting check to no-unreachable

--- tests/lib/rules/no-unreachable.js
+++ tests/lib/rules/no-unreachable.js
@@ -16,11 +16,20 @@ var eslintTester = require("eslint-tester");
 eslintTester.addRuleTest("lib/rules/no-unreachable", {
     valid: [
         "function foo() { function bar() { return 1; } return bar(); }",
+        "function foo() { return bar(); function bar() { return 1; } }",
+        "function foo() { return x; var x; }",
         "function foo() { var x = 1; var y = 2; }",
         "function foo() { var x = 1; var y = 2; return; }",
-        "while (true) { switch (foo) { case 1: x = 1; x = 2;} }"
+        "while (true) { switch (foo) { case 1: x = 1; x = 2;} }",
+        "while (true) { break; var x; }",
+        "while (true) { continue; var x, y; }",
+        "while (true) { throw 'message'; var x; }",
+        "switch (foo) { case 1: break; var x; }"
     ],
     invalid: [
+        { code: "function foo() { return x; var x = 1; }", errors: [{ message: "Found unexpected statement after a return.", type: "VariableDeclaration"}] },
+        { code: "function foo() { return x; var x, y = 1; }", errors: [{ message: "Found unexpected statement after a return.", type: "VariableDeclaration"}] },
+        { code: "while (true) { continue; var x = 1; }", errors: [{ message: "Found unexpected statement after a continue.", type: "VariableDeclaration"}] },
         { code: "function foo() { return; x = 1; }", errors: [{ message: "Found unexpected statement after a return.", type: "ExpressionStatement"}] },
         { code: "function foo() { throw error; x = 1; }", errors: [{ message: "Found unexpected statement after a throw.", type: "ExpressionStatement"}] },
         { code: "while (true) { break; x = 1; }", errors: [{ message: "Found unexpected statement after a break.", type: "ExpressionStatement"}] },
