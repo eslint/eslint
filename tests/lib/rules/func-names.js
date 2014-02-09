383e8506ae70a793fc6b9b51d759b9d4d3a9eb8a
Kyle Nunery <kyle.nunery@gmail.com>
2014-02-09 13:55:51 -0600

    #595 added more func-names tests

--- tests/lib/rules/func-names.js
+++ tests/lib/rules/func-names.js
@@ -17,13 +17,15 @@ eslintTester.addRuleTest("lib/rules/func-names", {
     valid: [
         "Foo.prototype.bar = function bar(){};",
         "function foo(){}",
-        "function test(d, e, f) {}"
+        "function test(d, e, f) {}",
+        "new function bar(){}"
     ],
     invalid: [
-        { code: "Foo.prototype.bar = function (){};", errors: [{ message: "Missing function expression name.", type: "FunctionExpression"}] },
+        { code: "Foo.prototype.bar = function() {};", errors: [{ message: "Missing function expression name.", type: "FunctionExpression"}] },
         { code: "(function(){}())", errors: [{ message: "Missing function expression name.", type: "FunctionExpression"}] },
         { code: "f(function(){})", errors: [{ message: "Missing function expression name.", type: "FunctionExpression"}] },
         { code: "var a = new Date(function() {});", errors: [{ message: "Missing function expression name.", type: "FunctionExpression"}] },
-        { code: "var test = function (d, e, f) {};", errors: [{ message: "Missing function expression name.", type: "FunctionExpression"}] }
+        { code: "var test = function (d, e, f) {};", errors: [{ message: "Missing function expression name.", type: "FunctionExpression"}] },
+        { code: "new function() {}", errors: [{ message: "Missing function expression name.", type: "FunctionExpression"}] }
     ]
 });
