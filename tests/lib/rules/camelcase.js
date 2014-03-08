ca06b1118c3388f7bafebf071fdf7d21bba0b36f
Nicholas C. Zakas <nicholas@nczconsulting.com>
2014-03-08 12:08:09 -0800

    Update: camelcase rule now doesn't flag function calls (fixes #656)

--- tests/lib/rules/camelcase.js
+++ tests/lib/rules/camelcase.js
@@ -18,10 +18,48 @@ eslintTester.addRuleTest("lib/rules/camelcase", {
         "firstName = \"Nicholas\"",
         "FIRST_NAME = \"Nicholas\"",
         "__myPrivateVariable = \"Patrick\"",
-        "myPrivateVariable_ = \"Patrick\""
+        "myPrivateVariable_ = \"Patrick\"",
+        "function doSomething(){}",
+        "do_something()",
+        "foo.do_something()"
     ],
     invalid: [
-        { code: "first_name = \"Nicholas\"", errors: [{ message: "Identifier 'first_name' is not in camel case.", type: "Identifier"}] },
-        { code: "__private_first_name = \"Patrick\"", errors: [{ message: "Identifier '__private_first_name' is not in camel case.", type: "Identifier"}] }
+        {
+            code: "first_name = \"Nicholas\"",
+            errors: [
+                {
+                    message: "Identifier 'first_name' is not in camel case.",
+                    type: "Identifier"
+                }
+            ]
+        },
+        {
+            code: "__private_first_name = \"Patrick\"",
+            errors: [
+                {
+                    message: "Identifier '__private_first_name' is not in camel case.",
+                    type: "Identifier"
+                }
+            ]
+        },
+        {
+            code: "function foo_bar(){}",
+            errors: [
+                {
+                    message: "Identifier 'foo_bar' is not in camel case.",
+                    type: "Identifier"
+                }
+            ]
+        },
+        {
+            code: "obj.foo_bar = function(){};",
+            errors: [
+                {
+                    message: "Identifier 'foo_bar' is not in camel case.",
+                    type: "Identifier"
+                }
+            ]
+        }
+
     ]
 });
