c591f58c1d9f9d128eede7004d02a11d85bea2c4
Brian Di Palma <offler@gmail.com>
2014-03-13 17:50:20 +0000

    Fixing issue #675. Incorrect triggering of no-else-return rule.

--- tests/lib/rules/no-else-return.js
+++ tests/lib/rules/no-else-return.js
@@ -18,6 +18,7 @@ eslintTester.addRuleTest("lib/rules/no-else-return", {
         "function foo() { if (true) { if (false) { return x; } } else { return y; } }",
         "function foo() { if (true) { return x; } return y; }",
         "function foo() { if (true) { for (;;) { return x; } } else { return y; } }",
+        "function foo() { var x = true; if (x) { return x; } else if (x === false) { return false; } }",
         "function foo() { if (true) notAReturn(); else return y; }"
     ],
     invalid: [
