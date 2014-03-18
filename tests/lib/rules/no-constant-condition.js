dfdbaf3652e38b099539dba06b6b1f14673b8a31
Michael Ficarra <mficarra@groupon.com>
2014-03-18 14:07:38 -0500

    fixes #682: rewrite no-constant-condition rule

--- tests/lib/rules/no-constant-condition.js
+++ tests/lib/rules/no-constant-condition.js
@@ -15,28 +15,33 @@ var eslintTester = require("eslint-tester");
 
 eslintTester.addRuleTest("lib/rules/no-constant-condition", {
     valid: [
-        "if(x){}",
-        "var r = q > 0 ? 1 : 2;",
-        "if(x == 0){ doSomething(); }",
-        "if(x = readBuf()){ doSomething(); }",
-        "while(isTrue()){}",
-        "while(x = y){}",
-        "for(;x < 10;){ doSomething(); }",
-        "do{ doSomething(); }while(x)"
+        "if(a);",
+        "if(a == 0);",
+        "if(a = f());",
+        "if(1, a);",
+        "while(~!a);",
+        "while(a = b);",
+        "for(;x < 10;);",
+        "for(;;);",
+        "do{ }while(x)",
+        "q > 0 ? 1 : 2;"
     ],
     invalid: [
-        { code: "for(;true;){}", errors: [{ message: "Unexpected constant condition.", type: "ForStatement"}] },
+        { code: "for(;true;);", errors: [{ message: "Unexpected constant condition.", type: "ForStatement"}] },
         { code: "do{}while(true)", errors: [{ message: "Unexpected constant condition.", type: "DoWhileStatement"}] },
         { code: "do{}while(t = -2)", errors: [{ message: "Unexpected constant condition.", type: "DoWhileStatement"}] },
-        { code: "var r = true ? 1 : 2;", errors: [{ message: "Unexpected constant condition.", type: "ConditionalExpression"}] },
-        { code: "var r = q = 0 ? 1 : 2;", errors: [{ message: "Unexpected constant condition.", type: "ConditionalExpression"}] },
-        { code: "var r = (q = 0) ? 1 : 2;", errors: [{ message: "Unexpected constant condition.", type: "ConditionalExpression"}] },
-        { code: "if(-2){}", errors: [{ message: "Unexpected constant condition.", type: "IfStatement"}] },
-        { code: "if(true){}", errors: [{ message: "Unexpected constant condition.", type: "IfStatement"}] },
-        { code: "if({}){}", errors: [{ message: "Unexpected constant condition.", type: "IfStatement"}] },
-        { code: "while([]){}", errors: [{ message: "Unexpected constant condition.", type: "WhileStatement"}] },
-        { code: "while(42){}", errors: [{ message: "Unexpected constant condition.", type: "WhileStatement"}] },
-        { code: "while(x = 1){}", errors: [{ message: "Unexpected constant condition.", type: "WhileStatement"}] },
-        { code: "while(function(){}){}", errors: [{ message: "Unexpected constant condition.", type: "WhileStatement"}] }
+        { code: "true ? 1 : 2;", errors: [{ message: "Unexpected constant condition.", type: "ConditionalExpression"}] },
+        { code: "q = 0 ? 1 : 2;", errors: [{ message: "Unexpected constant condition.", type: "ConditionalExpression"}] },
+        { code: "(q = 0) ? 1 : 2;", errors: [{ message: "Unexpected constant condition.", type: "ConditionalExpression"}] },
+        { code: "if(-2);", errors: [{ message: "Unexpected constant condition.", type: "IfStatement"}] },
+        { code: "if(true);", errors: [{ message: "Unexpected constant condition.", type: "IfStatement"}] },
+        { code: "if({});", errors: [{ message: "Unexpected constant condition.", type: "IfStatement"}] },
+        { code: "if(0 < 1);", errors: [{ message: "Unexpected constant condition.", type: "IfStatement"}] },
+        { code: "if(0 || 1);", errors: [{ message: "Unexpected constant condition.", type: "IfStatement"}] },
+        { code: "if(a, 1);", errors: [{ message: "Unexpected constant condition.", type: "IfStatement"}] },
+        { code: "while([]);", errors: [{ message: "Unexpected constant condition.", type: "WhileStatement"}] },
+        { code: "while(~!0);", errors: [{ message: "Unexpected constant condition.", type: "WhileStatement"}] },
+        { code: "while(x = 1);", errors: [{ message: "Unexpected constant condition.", type: "WhileStatement"}] },
+        { code: "while(function(){});", errors: [{ message: "Unexpected constant condition.", type: "WhileStatement"}] }
     ]
 });
