5b948f8bc2b87e3b88dec5dd411b1a3d00dbdddb
Michael Ficarra <mficarra@groupon.com>
2014-03-23 17:44:57 -0700

    rewrite block-scoped-var
    * fixes #693: block-scoped-var does not respect globals
    * fixes #701: block-scoped-var additionally enforcing that declarations come before use
    * fixes #702: block-scoped-var considers function expression names as usage, not declaration
    * fixes #703: block-scoped-var considers function expression parameters as usage, not declaration

--- tests/lib/rules/block-scoped-var.js
+++ tests/lib/rules/block-scoped-var.js
@@ -15,15 +15,27 @@ var eslintTester = require("eslint-tester");
 
 eslintTester.addRuleTest("lib/rules/block-scoped-var", {
     valid: [
-        "function doSomething() { var build, f; if (true) { build = true; } f = build; }",
-        "var build; function doSomething() { var f = build; }",
-        "function doSomething(e) { }",
-        "function doSomething(e) { var f = e; }",
-        "function doSomething() { var f = doSomething; }",
-        "function foo() { } function doSomething() { var f = foo; }"
+        "function f() { var a, b; { a = true; } b = a; }",
+        "var a; function f() { var b = a; }",
+        "function f(a) { }",
+        "!function f(a) { };",
+        "function f(a) { var b = a; }",
+        "!function f(a) { var b = a; };",
+        "function f() { var g = f; }",
+        "function f() { } function g() { var f = g; }",
+        "function f() { var hasOwnProperty; { hasOwnProperty; } }",
+        "function f(){ a; b; var a, b; }",
+        "function f(){ g(); function g(){} }",
+        { code: "new Date", globals: {Date: false} },
+        { code: "new Date", globals: {} },
+        { code: "var eslint = require('eslint');", globals: {require: false} }
     ],
     invalid: [
-        { code: "function doSomething() { var f; if (true) { var build = true; } f = build; }", errors: [{ message: "build used outside of binding context.", type: "Identifier" }] },
-        { code: "function doSomething() { try { var build = 1; } catch (e) { var f = build; } }", errors: [{ message: "build used outside of binding context.", type: "Identifier" }] }
+        { code: "function f(){ x; }", errors: [{ message: "\"x\" used outside of binding context.", type: "Identifier" }] },
+        { code: "function f(){ x; { var x; } }", errors: [{ message: "\"x\" used outside of binding context.", type: "Identifier" }] },
+        { code: "function f(){ { var x; } x; }", errors: [{ message: "\"x\" used outside of binding context.", type: "Identifier" }] },
+        { code: "function f() { var a; { var b = 0; } a = b; }", errors: [{ message: "\"b\" used outside of binding context.", type: "Identifier" }] },
+        { code: "function f() { try { var a = 0; } catch (e) { var b = a; } }", errors: [{ message: "\"a\" used outside of binding context.", type: "Identifier" }] },
+        { code: "var eslint = require('eslint');", globals: {}, errors: [{ message: "\"require\" used outside of binding context.", type: "Identifier" }] }
     ]
 });
