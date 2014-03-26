19cc7e27604dc7633284bf3b43309694228c961e
Brandon Mills <mills.brandont@gmail.com>
2014-03-26 02:16:05 -0400

    Relax eqeqeq null check (fixes #669)
    When the "smart" option is enabled, eqeqeq will now allow double-
    equals comparisons to null to check for null or undefined in a
    single expression.

--- tests/lib/rules/eqeqeq.js
+++ tests/lib/rules/eqeqeq.js
@@ -21,7 +21,9 @@ eslintTester.addRuleTest("lib/rules/eqeqeq", {
         { code: "'string' != typeof a", args: [1, "smart"] },
         { code: "'hello' != 'world'", args: [1, "smart"] },
         { code: "2 == 3", args: [1, "smart"] },
-        { code: "true == true", args: [1, "smart"] }
+        { code: "true == true", args: [1, "smart"] },
+        { code: "null == a", args: [1, "smart"] },
+        { code: "a == null", args: [1, "smart"] }
     ],
     invalid: [
         { code: "a == b", errors: [{ message: "Expected '===' and instead saw '=='.", type: "BinaryExpression"}] },
