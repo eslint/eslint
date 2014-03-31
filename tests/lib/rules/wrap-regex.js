c447debd2995e77355d4e9dd4586d31d2151324e
Michael Ficarra <git@michael.ficarra.me>
2014-03-30 19:35:47 -0500

    fixes #743: wrap-regex rule warns on regex used in dynamic member access

--- tests/lib/rules/wrap-regex.js
+++ tests/lib/rules/wrap-regex.js
@@ -16,16 +16,17 @@ var eslintTester = require("eslint-tester");
 
 eslintTester.addRuleTest("lib/rules/wrap-regex", {
     valid: [
-        "var f = function() { return (/foo/).test(bar); };",
-        "var f = function() { return (/foo/ig).test(bar); };",
-        "var f = function() { return /foo/; };",
-        "var f = 0;"
+        "(/foo/).test(bar);",
+        "(/foo/ig).test(bar);",
+        "/foo/;",
+        "var f = 0;",
+        "a[/b/];"
     ],
     invalid: [
-        { code: "var f = function() { return /foo/.test(bar); };",
-          errors: [{ message: "Wrap the /regexp/ literal in parens to disambiguate the slash operator.", type: "Literal"}] },
-        { code: "var f = function() { return /foo/ig.test(bar); };",
-          errors: [{ message: "Wrap the /regexp/ literal in parens to disambiguate the slash operator.", type: "Literal"}] }
+        { code: "/foo/.test(bar);",
+          errors: [{ message: "Wrap the regexp literal in parens to disambiguate the slash.", type: "Literal"}] },
+        { code: "/foo/ig.test(bar);",
+          errors: [{ message: "Wrap the regexp literal in parens to disambiguate the slash.", type: "Literal"}] }
 
     ]
 });
