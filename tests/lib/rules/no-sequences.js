31723d00d8bc25a95c9e1f09ce5e55e4720a3f8e
Brandon Mills <mills.brandont@gmail.com>
2014-04-14 02:15:21 -0400

    Rule: no-sequences (fixes #561)
    Adds a new rule, `no-sequences`, that disallows use of the comma
    operator, except in the initialization or update portions of for
    loops or when wrapped in parens.

---
+++ tests/lib/rules/no-sequences.js
@@ -0,0 +1,51 @@
+/**
+ * @fileoverview Tests for no-sequences rule.
+ * @author Brandon Mills
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var eslintTester = require("eslint-tester");
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var errors = [{
+    message: "Unexpected use of comma operator.",
+    type: "SequenceExpression"
+}];
+
+eslintTester.addRuleTest("lib/rules/no-sequences", {
+
+    // Examples of code that should not trigger the rule
+    valid: [
+        "var arr = [1, 2];",
+        "var obj = {a: 1, b: 2};",
+        "var a = 1, b = 2;",
+        "var foo = (1, 2);",
+        "(0,eval)(\"foo()\");",
+        "for (i = 1, j = 2;; i++, j++);",
+        "foo(a, (b, c), d);",
+        "do {} while ((doSomething(), !!test));",
+        "for ((doSomething(), somethingElse()); (doSomething(), !!test); );",
+        "if ((doSomething(), !!test));",
+        "switch ((doSomething(), !!test)) {}",
+        "while ((doSomething(), !!test));",
+        "with ((doSomething(), val)) {}"
+    ],
+
+    // Examples of code that should trigger the rule
+    invalid: [
+        { code: "a = 1, 2", errors: errors },
+        { code: "do {} while (doSomething(), !!test);", errors: errors },
+        { code: "for (; doSomething(), !!test; );", errors: errors },
+        { code: "if (doSomething(), !!test);", errors: errors },
+        { code: "switch (doSomething(), val) {}", errors: errors },
+        { code: "while (doSomething(), !!test);", errors: errors },
+        { code: "with (doSomething(), val) {}", errors: errors }
+    ]
+});
