1945c9edaa1a0d9f3daa90ba888b718c9cdd51ec
Christian <me@rndm.de>
2014-04-27 16:12:23 +0200

    Rule: no-restricted-modules (fixes #791)

---
+++ tests/lib/rules/no-restricted-modules.js
@@ -0,0 +1,32 @@
+/**
+ * @fileoverview Tests for no-restricted-modules.
+ * @author Christian Schulz
+ */
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var eslintTester = require("eslint-tester");
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+eslintTester.addRuleTest("lib/rules/no-restricted-modules", {
+    valid: [
+        { code: "require(\"fs\")", args: [2, "crypto"]},
+        { code: "require(\"path\")", args: [2, "crypto", "stream", "os"]},
+        { code: "require(\"fs \")", args: 0 },
+        { code: "require(2)", args: [2, "crypto"]},
+        { code: "require(foo)", args: [2, "crypto"]},
+        { code: "var foo = bar('crypto');", args: [2, "crypto"]}
+    ],
+    invalid: [{
+        code: "require(\"fs\")", args: [2, "fs"],
+        errors: [{ message: "'fs' module is restricted from being used.", type: "CallExpression"}]
+    },{
+        code: "require(\"os \")", args: [2, "fs", "crypto ", "stream", "os"],
+        errors: [{ message: "'os' module is restricted from being used.", type: "CallExpression"}]
+    }]
+});
