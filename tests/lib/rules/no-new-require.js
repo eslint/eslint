ea99a14d61b5399991e3701224cfdb70dc8149b7
Wil Moore III <wil.moore@wilmoore.com>
2014-05-13 09:39:44 -0600

    add new rule "no-new-require"

---
+++ tests/lib/rules/no-new-require.js
@@ -0,0 +1,26 @@
+/**
+ * @fileoverview Tests for no-new-require rule.
+ * @author Wil Moore III
+ */
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var eslintTester = require("eslint-tester");
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+eslintTester.addRuleTest("lib/rules/no-new-require", {
+    valid: [
+        "var appHeader = require('app-header')",
+        "var AppHeader = new (require('app-header'))",
+        "var AppHeader = new (require('headers').appHeader)"
+    ],
+    invalid: [
+        { code: "var appHeader = new require('app-header')", errors: [{ message: "Unexpected use of new with require.", type: "NewExpression"}] },
+        { code: "var appHeader = new require('headers').appHeader", errors: [{ message: "Unexpected use of new with require.", type: "NewExpression"}] }
+    ]
+});
