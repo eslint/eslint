c7056fa2ce08424d474a007f4e08ff404328abad
Michał Gołębiowski <m.goleb@gmail.com>
2014-05-22 00:39:17 +0200

    Update: Implement eqeqeq option "allow-null" (fixes #910)

--- tests/lib/rules/eqeqeq.js
+++ tests/lib/rules/eqeqeq.js
@@ -25,13 +25,20 @@ eslintTester.addRuleTest("lib/rules/eqeqeq", {
         { code: "2 == 3", args: [1, "smart"] },
         { code: "true == true", args: [1, "smart"] },
         { code: "null == a", args: [1, "smart"] },
-        { code: "a == null", args: [1, "smart"] }
+        { code: "a == null", args: [1, "smart"] },
+        { code: "null == a", args: [1, "allow-null"] },
+        { code: "a == null", args: [1, "allow-null"] }
     ],
     invalid: [
         { code: "a == b", errors: [{ message: "Expected '===' and instead saw '=='.", type: "BinaryExpression"}] },
         { code: "a != b", errors: [{ message: "Expected '!==' and instead saw '!='.", type: "BinaryExpression"}] },
         { code: "true == 1", args: [1, "smart"], errors: [{ message: "Expected '===' and instead saw '=='.", type: "BinaryExpression"}] },
         { code: "0 != '1'", args: [1, "smart"], errors: [{ message: "Expected '!==' and instead saw '!='.", type: "BinaryExpression"}] },
-        { code: "'wee' == /wee/", args: [1, "smart"], errors: [{ message: "Expected '===' and instead saw '=='.", type: "BinaryExpression"}] }
+        { code: "'wee' == /wee/", args: [1, "smart"], errors: [{ message: "Expected '===' and instead saw '=='.", type: "BinaryExpression"}] },
+        { code: "typeof a == 'number'", args: [1, "allow-null"], errors: [{ message: "Expected '===' and instead saw '=='.", type: "BinaryExpression"}] },
+        { code: "'string' != typeof a", args: [1, "allow-null"], errors: [{ message: "Expected '!==' and instead saw '!='.", type: "BinaryExpression"}] },
+        { code: "'hello' != 'world'", args: [1, "allow-null"], errors: [{ message: "Expected '!==' and instead saw '!='.", type: "BinaryExpression"}] },
+        { code: "2 == 3", args: [1, "allow-null"], errors: [{ message: "Expected '===' and instead saw '=='.", type: "BinaryExpression"}] },
+        { code: "true == true", args: [1, "allow-null"], errors: [{ message: "Expected '===' and instead saw '=='.", type: "BinaryExpression"}] }
     ]
 });
