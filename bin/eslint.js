bca4ae791015cf0f418e405de8902a5a572c9f7c
Nicholas C. Zakas <nicholas@nczconsulting.com>
2014-06-27 15:58:15 -0700

    Fix: Allow stdout to drain before exiting (fixes #317)

--- bin/eslint.js
+++ bin/eslint.js
@@ -1,5 +1,11 @@
 #!/usr/bin/env node
-
 var cli = require("../lib/cli");
 var exitCode = cli.execute(process.argv);
-process.exit(exitCode);
+
+/*
+ * Wait for the stdout buffer to drain.
+ * See https://github.com/eslint/eslint/issues/317
+ */
+process.on('exit', function() {
+    process.exit(exitCode);
+});
