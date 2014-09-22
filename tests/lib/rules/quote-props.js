4ee4b9eeff6501c78a1877f0c4625d83c2b60aec
Michael Ficarra <git@michael.ficarra.me>
2014-09-22 09:12:48 -0700

    Update: add "as-needed" option to quote-props rule (fixes #1279)

--- tests/lib/rules/quote-props.js
+++ tests/lib/rules/quote-props.js
@@ -13,11 +13,30 @@ var eslint = require("../../../lib/eslint"),
 var eslintTester = new ESLintTester(eslint);
 eslintTester.addRuleTest("lib/rules/quote-props", {
     valid: [
-        "var x = { 'foo': 42 }",
-        "var x = { \"foo\": 42 }"
-           ],
+        "({ '0': 0 })",
+        "({ 'a': 0 })",
+        "({ \"a\": 0 })",
+        "({ 'null': 0 })",
+        "({ 'true': 0 })",
+        "({ 'a-b': 0 })",
+        "({ 'if': 0 })",
+        "({ '@': 0 })",
+        { code: "({ a: 0, b: 0 })", args: [2, "as-needed"] },
+        { code: "({ a: 0, 0: 0 })", args: [2, "as-needed"] },
+        { code: "({ a: 0, true: 0 })", args: [2, "as-needed"] },
+        { code: "({ a: 0, null: 0 })", args: [2, "as-needed"] },
+        { code: "({ a: 0, '-b': 0 })", args: [2, "as-needed"] },
+        { code: "({ a: 0, 'if': 0 })", args: [2, "as-needed"] },
+        { code: "({ a: 0, '@': 0 })", args: [2, "as-needed"] },
+        { code: "({ a: 0, 0: 0 })", args: [2, "as-needed"] },
+        { code: "({ a: 0, '0x0': 0 })", args: [2, "as-needed"] },
+    ],
     invalid: [
-        { code: "var x = { foo: 42 }",
-          errors: [{ message: "Non-quoted property `foo` found.", type: "Property"}] }
+        {
+            code: "({ a: 0 })",
+            errors: [
+                { message: "Unquoted property `a` found.", type: "Property"}
+            ]
+        }
     ]
 });
