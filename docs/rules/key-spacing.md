8be37aa2a20503404c413b4b7f7a2d34b787e8db
bitterheart <4NV6rFHnQwTzDQ@gmail.com>
2014-09-27 14:29:23 +0000

    draft 1

---
+++ docs/rules/key-spacing.md
@@ -0,0 +1,36 @@
+#  a rule that enforces or disallows spaces after property names (and before property values) inside of objects. (key-spacing)
+
+Please describe the origin of the rule here.
+
+
+## Rule Details
+
+This rule aims to...
+
+The following patterns are considered warnings:
+
+```js
+
+// fill me in
+
+```
+
+The following patterns are not warnings:
+
+```js
+
+// fill me in
+
+```
+
+### Options
+
+If there are any options, describe them here. Otherwise, delete this section.
+
+## When Not To Use It
+
+Give a short description of when it would be appropriate to turn off this rule.
+
+## Further Reading
+
+If there are other links that describe the issue this rule addresses, please include them here in a bulleted list.
---
+++ tests/lib/rules/key-spacing.js
@@ -0,0 +1,75 @@
+/**
+ * @fileoverview  a rule that enforces or disallows spaces after property names (and before property values) inside of objects.
+ * @author Emory Merryman
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var eslint = require("../../../lib/eslint"),
+    ESLintTester = require("eslint-tester");
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var eslintTester = new ESLintTester(eslint);
+eslintTester.addRuleTest("lib/rules/key-spacing", {
+
+    valid: [
+        {code: "{foo:bar}", args:[1,'never']},
+        {code: "{foo :'bar'}", args:[1,'always']}
+    ],
+
+    invalid: [{
+        code: "{foo:'bar'}",
+        args: [1, "always"],
+        errors: [{
+            message: "A space is required before ':'",
+            type: "ObjectExpression"
+        }, {
+            message: "A space is required after ':'",
+            type: "ObjectExpression"
+        }]
+    }, {
+        code: "{foo: 'bar'}",
+        args: [1, "always"],
+        errors: [{
+            message: "A space is required before ':'",
+            type: "ObjectExpression"
+        }]
+    },{
+        code: "{foo :'bar'}",
+        args: [1, "always"],
+        errors: [{
+            message: "A space is required after ':'",
+            type: "ObjectExpression"
+        }]
+    },{
+        code: "{foo : 'bar'}",
+        args: [1, "never"],
+        errors: [{
+            message: "There must not be any spaces before ':'",
+            type: "ObjectExpression"
+        }, {
+            message: "There must not be any spaces after ':'",
+            type: "ObjectExpression"
+        }]
+    }, {
+        code: "{foo :'bar'}",
+        args: [1, "never"],
+        errors: [{
+            message: "There must not be any spaces before ':'",
+            type: "ObjectExpression"
+        }]
+    },{
+        code: "{foo: 'bar'}",
+        args: [1, "never"],
+        errors: [{
+            message: "A space is required after ':'",
+            type: "ObjectExpression"
+        }]
+    }]
+});
