7559d8081920eef05a9b8eb38efc0a76b58b6a3a
Mathias Schreck <schreck.mathias@googlemail.com>
2014-10-03 00:03:06 +0200

    New: add space-before-blocks rule (fixes #1277)

---
+++ docs/rules/space-before-blocks.md
@@ -0,0 +1,92 @@
+# Require Or Disallow Space Before Blocks (space-before-blocks)
+
+Consistency is an important part of any style guide.
+While it is a personal preference where to put the opening brace of blocks,
+it should be consistent across a whole project.
+Having an inconsistent style distracts the reader from seeing the important parts of the code.
+
+## Rule Details
+
+This rule will enforce consistency of spacing before blocks. It is only applied on blocks that don’t begin on a new line.
+
+This rule takes one argument. If it is `"always"` then blocks must always have at least one preceding space. If `"never"`
+then all blocks should never have any preceding space. The default is `"always"`.
+
+The following patterns are considered warnings when configured `"always"`:
+
+```js
+if (a){
+    b();
+}
+
+if (a) {
+    b();
+} else{
+    c();
+}
+
+function a(){}
+
+for (;;){
+    b();
+}
+
+try {} catch(a){}
+```
+
+The following patterns are not considered warnings when configured `"always"`:
+
+```js
+if (a) {
+    b();
+}
+
+function a() {}
+
+for (;;) {
+    b();
+}
+
+try {} catch(a) {}
+```
+
+The following patterns are considered warnings when configured `"never"`:
+
+```js
+if (a) {
+    b();
+}
+
+function a() {}
+
+for (;;) {
+    b();
+}
+
+try {} catch(a) {}
+```
+
+The following patterns are not considered warnings when configured `"never"`:
+
+```js
+if (a){
+    b();
+}
+
+function a(){}
+
+for (;;){
+    b();
+}
+
+try{} catch(a){}
+```
+
+## When Not To Use It
+
+You can turn this rule off if you are not concerned with the consistency of spacing before blocks.
+
+## Related Rules
+
+* [space-after-keywords](space-after-keywords.md)
+* [brace-style](brace-style.md)
---
+++ tests/lib/rules/space-before-blocks.js
@@ -0,0 +1,100 @@
+/**
+ * @fileoverview Tests for space-before-block rule.
+ * @author Mathias Schreck <https://github.com/lo1tuma>
+ * @copyright 2014 Mathias Schreck. All rights reserved.
+ */
+
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var eslint = require("../../../lib/eslint"),
+    ESLintTester = require("eslint-tester");
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var eslintTester = new ESLintTester(eslint),
+    neverArgs = [1, "never"],
+    expectedSpacingErrorMessage = "Missing space before opening brace.",
+    expectedSpacingError = { message: expectedSpacingErrorMessage },
+    expectedNoSpacingError = { message: "Unexpected space before opening brace."};
+
+eslintTester.addRuleTest("lib/rules/space-before-blocks", {
+    valid: [
+        { code: "if(a) {}" },
+        { code: "if(a)  {}" },
+        { code: "if(a){}", args: neverArgs },
+        { code: "if(a)\n{}" },
+        { code: "if(a)\n{}", args: neverArgs },
+        { code: "if(a) {}else {}" },
+        { code: "if(a){}else{}", args: neverArgs },
+        { code: "function a() {}" },
+        { code: "function a(){}", args: neverArgs },
+        { code: "try {}catch(a) {}" },
+        { code: "try{}catch(a){}", args: neverArgs },
+        { code: "for(;;) {}" },
+        { code: "for(;;){}", args: neverArgs },
+        { code: "while(a) {}" },
+        { code: "while(a){}", args: neverArgs }
+    ],
+    invalid: [
+        {
+            code: "if(a){}",
+            errors: [ { message: expectedSpacingErrorMessage, line: 1, column: 5 } ]
+        },
+        {
+            code: "if(a) {}",
+            args: neverArgs,
+            errors: [ expectedNoSpacingError ]
+        },
+        {
+            code: "if(a) {}else{}",
+            errors: [ expectedSpacingError ]
+        },
+        {
+            code: "if(a){}else {}",
+            args: neverArgs,
+            errors: [ expectedNoSpacingError ]
+        },
+        {
+            code: "function a(){}",
+            errors: [ expectedSpacingError ]
+        },
+        {
+            code: "function a() {}",
+            args: neverArgs,
+            errors: [ expectedNoSpacingError ]
+        },
+        {
+            code: "try{}catch(a){}",
+            errors: [ expectedSpacingError, expectedSpacingError ]
+        },
+        {
+            code: "try {}catch(a) {}",
+            args: neverArgs,
+            errors: [ expectedNoSpacingError, expectedNoSpacingError ]
+        },
+        {
+            code: "for(;;){}",
+            errors: [ expectedSpacingError ]
+        },
+        {
+            code: "for(;;) {}",
+            args: neverArgs,
+            errors: [ expectedNoSpacingError ]
+        },
+        {
+            code: "while(a){}",
+            errors: [ expectedSpacingError ]
+        },
+        {
+            code: "while(a) {}",
+            args: neverArgs,
+            errors: [ expectedNoSpacingError ]
+        }
+    ]
+});
