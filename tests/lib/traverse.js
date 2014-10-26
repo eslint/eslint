4aa1845df00d98ab97b2e929f84e6d92f96b308f
Mathias Schreck <schreck.mathias@googlemail.com>
2014-10-27 00:37:29 +0100

    Fix: let fs.stat throw if a file does not exist (fixes #1296)

--- tests/lib/traverse.js
+++ tests/lib/traverse.js
@@ -8,6 +8,8 @@
 
 var assert = require("chai").assert,
     path = require("path"),
+    fs = require("fs"),
+    sinon = require("sinon"),
     traverse = require("../../lib/util/traverse");
 
 
@@ -56,5 +58,19 @@ describe("traverse", function() {
 
         assert.notEqual(files.length, 0);
     });
+
+    it("should throw if fs.statSync throws", sinon.test(function () {
+        var error = new Error("anyError"),
+            options = {
+                files: [ "/any/file.js" ],
+                exclude: false
+            },
+            callback = this.spy();
+
+        this.stub(fs, "statSync").throws(error);
+
+        assert.throws(traverse.bind(null, options, callback), "anyError");
+        sinon.assert.notCalled(callback);
+    }));
 
 });
