e8392162385f93a1cee5135cb83a12d0fbe49a9a
Nicholas C. Zakas <nicholas@nczconsulting.com>
2014-11-25 13:38:08 -0800

    Fix: no-fallthrough should work when semis are missing (fixes #1447)

--- tests/lib/rules/no-fallthrough.js
+++ tests/lib/rules/no-fallthrough.js
@@ -20,6 +20,7 @@ var eslintTester = new ESLintTester(eslint);
 eslintTester.addRuleTest("lib/rules/no-fallthrough", {
     valid: [
         "switch(foo) { case 0: a(); /* falls through */ case 1: b(); }",
+        "switch(foo) { case 0: a()\n /* falls through */ case 1: b(); }",
         "function foo() { switch(foo) { case 0: a(); return; case 1: b(); }; }",
         "switch(foo) { case 0: a(); throw 'foo'; case 1: b(); }",
         "while (a) { switch(foo) { case 0: a(); continue; case 1: b(); } }",
