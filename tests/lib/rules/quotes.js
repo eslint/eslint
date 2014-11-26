317f5ce58144988f240393efa27a81e1f0f163ad
Nicholas C. Zakas <nicholas@nczconsulting.com>
2014-11-25 16:17:46 -0800

    Fix: quotes rule ignores JSX literals (fixes #1477)

--- tests/lib/rules/quotes.js
+++ tests/lib/rules/quotes.js
@@ -13,12 +13,16 @@ var eslint = require("../../../lib/eslint"),
 var eslintTester = new ESLintTester(eslint);
 eslintTester.addRuleTest("lib/rules/quotes", {
     valid: [
-        {code: "var foo = 'bar';", args: [1, "single"] },
-        {code: "var foo = \"bar\";", args: [1, "double"] },
+        { code: "var foo = 'bar';", args: [1, "single"] },
+        { code: "var foo = \"bar\";", args: [1, "double"] },
         { code: "var foo = 1;", args: [1, "single"] },
         { code: "var foo = 1;", args: [1, "double"] },
         { code: "var foo = \"'\";", args: [1, "single", "avoid-escape"] },
-        { code: "var foo = '\"';", args: [1, "double", "avoid-escape"] }
+        { code: "var foo = '\"';", args: [1, "double", "avoid-escape"] },
+        { code: "var foo = <div>Hello world</div>;", args: [1, "single"], settings: { jsx: true } },
+        { code: "var foo = <div id=\"foo\"></div>;", args: [1, "single"], settings: { jsx: true } },
+        { code: "var foo = <div>Hello world</div>;", args: [1, "double"], settings: { jsx: true } },
+        { code: "var foo = <div>Hello world</div>;", args: [1, "avoid-escape"], settings: { jsx: true } }
     ],
     invalid: [
         { code: "var foo = \"bar\";",
