008efc9807283f7b997b1dc7bef63445444f7e7c
Nicholas C. Zakas <nicholas@nczconsulting.com>
2014-11-30 09:03:32 -0800

    Fix: Don't warn on JSX literals with newlines (fixes #1533)

--- tests/lib/rules/no-multi-str.js
+++ tests/lib/rules/no-multi-str.js
@@ -17,7 +17,8 @@ var eslint = require("../../../lib/eslint"),
 var eslintTester = new ESLintTester(eslint);
 eslintTester.addRuleTest("lib/rules/no-multi-str", {
     valid: [
-        "var a = 'Line 1 Line 2';"
+        "var a = 'Line 1 Line 2';",
+        { code: "var a = <div>\n<h1>Wat</h1>\n</div>;", settings: { jsx: true }}
     ],
     invalid: [
         { code: "var x = 'Line 1 \\\n Line 2'", errors: [{ message: "Multiline support is limited to browsers supporting ES5 only.", type: "Literal"}] },
