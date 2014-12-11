0ca51889a6bbac53cf77e04e78f735c53cc54d82
Brandon Mills <mills.brandont@gmail.com>
2014-12-10 19:59:12 -0500

    Update: add yoda exception for range tests (fixes #1561)

--- tests/lib/rules/yoda.js
+++ tests/lib/rules/yoda.js
@@ -16,20 +16,53 @@ var eslint = require("../../../lib/eslint"),
 var eslintTester = new ESLintTester(eslint);
 eslintTester.addRuleTest("lib/rules/yoda", {
     valid: [
-        { code: "if (value === \"red\") {}", args: ["2", "never"] },
-        { code: "if (value === value) {}", args: ["2", "never"] },
-        { code: "if (value != 5) {}", args: ["2", "never"] },
-        { code: "if (5 & foo) {}", args: ["2", "never"] },
-        { code: "if (\"blue\" === value) {}", args: ["2", "always"] },
-        { code: "if (value === value) {}", args: ["2", "always"] },
-        { code: "if (4 != value) {}", args: ["2", "always"] },
-        { code: "if (foo & 4) {}", args: ["2", "always"] }
+        // "never" mode
+        { code: "if (value === \"red\") {}", args: [2, "never"] },
+        { code: "if (value === value) {}", args: [2, "never"] },
+        { code: "if (value != 5) {}", args: [2, "never"] },
+        { code: "if (5 & foo) {}", args: [2, "never"] },
+
+        // "always" mode
+        { code: "if (\"blue\" === value) {}", args: [2, "always"] },
+        { code: "if (value === value) {}", args: [2, "always"] },
+        { code: "if (4 != value) {}", args: [2, "always"] },
+        { code: "if (foo & 4) {}", args: [2, "always"] },
+
+        // Range exception
+        {
+            code: "if (0 < x && x <= 1) {}",
+            args: [2, "never", { exceptRange: true }]
+        }, {
+            code: "if (x < 0 || 1 <= x) {}",
+            args: [2, "never", { exceptRange: true }]
+        }, {
+            code: "if (0 <= x && x < 1) {}",
+            args: [2, "always", { exceptRange: true }]
+        }, {
+            code: "if (x <= 'bar' || 'foo' < x) {}",
+            args: [2, "always", { exceptRange: true }]
+        }, {
+            code: "if ('blue' < x.y && x.y < 'green') {}",
+            args: [2, "never", { exceptRange: true }]
+        }, {
+            code: "if (0 <= x['y'] && x['y'] <= 100) {}",
+            args: [2, "never", { exceptRange: true }]
+        }, {
+            code: "if (a < 0 && (0 < b && b < 1)) {}",
+            args: [2, "never", { exceptRange: true }]
+        }, {
+            code: "if ((0 < a && a < 1) && b < 0) {}",
+            args: [2, "never", { exceptRange: true }]
+        }, {
+            code: "if (a < 4 || (b[c[0]].d['e'] < 0 || 1 <= b[c[0]].d['e'])) {}",
+            args: [2, "never", { exceptRange: true }]
+        }
     ],
     invalid: [
 
         {
             code: "if (\"red\" == value) {}",
-            args: ["2", "never"],
+            args: [2, "never"],
             errors: [
                 {
                     message: "Expected literal to be on the right side of ==.",
@@ -39,7 +72,7 @@ eslintTester.addRuleTest("lib/rules/yoda", {
         },
         {
             code: "if (true === value) {}",
-            args: ["2", "never"],
+            args: [2, "never"],
             errors: [
                 {
                     message: "Expected literal to be on the right side of ===.",
@@ -49,7 +82,7 @@ eslintTester.addRuleTest("lib/rules/yoda", {
         },
         {
             code: "if (5 != value) {}",
-            args: ["2", "never"],
+            args: [2, "never"],
             errors: [
                 {
                     message: "Expected literal to be on the right side of !=.",
@@ -59,7 +92,7 @@ eslintTester.addRuleTest("lib/rules/yoda", {
         },
         {
             code: "if (null !== value) {}",
-            args: ["2", "never"],
+            args: [2, "never"],
             errors: [
                 {
                     message: "Expected literal to be on the right side of !==.",
@@ -69,7 +102,7 @@ eslintTester.addRuleTest("lib/rules/yoda", {
         },
         {
             code: "if (\"red\" <= value) {}",
-            args: ["2", "never"],
+            args: [2, "never"],
             errors: [
                 {
                     message: "Expected literal to be on the right side of <=.",
@@ -79,7 +112,7 @@ eslintTester.addRuleTest("lib/rules/yoda", {
         },
         {
             code: "if (true >= value) {}",
-            args: ["2", "never"],
+            args: [2, "never"],
             errors: [
                 {
                     message: "Expected literal to be on the right side of >=.",
@@ -89,7 +122,7 @@ eslintTester.addRuleTest("lib/rules/yoda", {
         },
         {
             code: "var foo = (5 < value) ? true : false",
-            args: ["2", "never"],
+            args: [2, "never"],
             errors: [
                 {
                     message: "Expected literal to be on the right side of <.",
@@ -99,7 +132,7 @@ eslintTester.addRuleTest("lib/rules/yoda", {
         },
         {
             code: "function foo() { return (null > value); }",
-            args: ["2", "never"],
+            args: [2, "never"],
             errors: [
                 {
                     message: "Expected literal to be on the right side of >.",
@@ -109,7 +142,7 @@ eslintTester.addRuleTest("lib/rules/yoda", {
         },
         {
             code: "if (value == \"red\") {}",
-            args: ["2", "always"],
+            args: [2, "always"],
             errors: [
                 {
                     message: "Expected literal to be on the left side of ==.",
@@ -119,10 +152,79 @@ eslintTester.addRuleTest("lib/rules/yoda", {
         },
         {
             code: "if (value === true) {}",
-            args: ["2", "always"],
+            args: [2, "always"],
             errors: [
                 {
                     message: "Expected literal to be on the left side of ===.",
+                    type: "BinaryExpression"
+                }
+            ]
+        },
+        {
+            code: "if (a < 0 && 0 <= b && b < 1) {}",
+            args: [2, "never", { exceptRange: true }],
+            errors: [
+                {
+                    message: "Expected literal to be on the right side of <=.",
+                    type: "BinaryExpression"
+                }
+            ]
+        },
+        {
+            code: "if (0 <= a && a < 1 && b < 1) {}",
+            args: [2, "never", { exceptRange: true }],
+            errors: [
+                {
+                    message: "Expected literal to be on the right side of <=.",
+                    type: "BinaryExpression"
+                }
+            ]
+        },
+        {
+            code: "if (1 < a && a < 0) {}",
+            args: [2, "never", { exceptRange: true }],
+            errors: [
+                {
+                    message: "Expected literal to be on the right side of <.",
+                    type: "BinaryExpression"
+                }
+            ]
+        },
+        {
+            code: "0 < a && a < 1",
+            args: [2, "never", { exceptRange: true }],
+            errors: [
+                {
+                    message: "Expected literal to be on the right side of <.",
+                    type: "BinaryExpression"
+                }
+            ]
+        },
+        {
+            code: "var a = b < 0 || 1 <= b;",
+            args: [2, "never", { exceptRange: true }],
+            errors: [
+                {
+                    message: "Expected literal to be on the right side of <=.",
+                    type: "BinaryExpression"
+                }
+            ]
+        },
+        {
+            code: "var a = (b < 0 && 0 <= b);",
+            args: [2, "always", { exceptRange: true }],
+            errors: [
+                {
+                    message: "Expected literal to be on the left side of <.",
+                    type: "BinaryExpression"
+                }
+            ]
+        },
+        {
+            code: "if (0 <= x && x < 1) {}",
+            errors: [
+                {
+                    message: "Expected literal to be on the right side of <=.",
                     type: "BinaryExpression"
                 }
             ]
