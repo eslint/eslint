53acbe8859a089d4da0011b5d4af17569308276c
Elan Shanker <elan.shanker@gmail.com>
2014-12-16 15:52:06 -0500

    Update: Set environments w/ globals (fixes #1577)

---
+++ conf/environments.js
@@ -0,0 +1,37 @@
+/**
+ * @fileoverview Defines environment settings and globals.
+ * @author Elan Shanker
+ * @copyright 2014 Elan Shanker. All rights reserved.
+ */
+"use strict";
+
+var globals = require("globals");
+
+module.exports = {
+    builtin: globals.builtin,
+    browser: {
+        globals: globals.browser
+    },
+    node: {
+        globals: globals.node,
+        rules: {
+            "no-catch-shadow": 0,
+            "no-console": 0,
+            "no-mixed-requires": 2,
+            "no-new-require": 2,
+            "no-path-concat": 2,
+            "no-process-exit": 2,
+            "global-strict": [0, "always"],
+            "handle-callback-err": [2, "err"]
+        }
+    },
+    amd: {
+        globals: globals.amd
+    },
+    mocha: {
+        globals: globals.mocha
+    },
+    jasmine: {
+        globals: globals.jasmine
+    }
+};
