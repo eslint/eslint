405fada86aaa6d53541e2f58ab2d4109459c25e5
Nicholas C. Zakas <nicholas@nczconsulting.com>
2015-01-11 08:43:47 -0800

    Upgrade: several dependencies (fixes #1377)

--- Makefile.js
+++ Makefile.js
@@ -235,7 +235,7 @@ target.test = function() {
         lastReturn;
 
     // exec(ISTANBUL + " cover " + MOCHA + "-- -c " + TEST_FILES);
-    lastReturn = nodeCLI.exec("istanbul", "cover", MOCHA, "-- -c", TEST_FILES);
+    lastReturn = nodeCLI.exec("istanbul", "cover", MOCHA, "-- -R dot -c", TEST_FILES);
     if (lastReturn.code !== 0) {
         errors++;
     }
