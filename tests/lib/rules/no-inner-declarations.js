3517eb32709ded758a0957584ec8301aab8e4be9
Roberto Vidal <vidal.roberto.j@gmail.com>
2015-02-27 19:33:27 -0500

    Fix: allow block bindings in no-inner-declarations (fixes #1893)

--- tests/lib/rules/no-inner-declarations.js
+++ tests/lib/rules/no-inner-declarations.js
@@ -28,15 +28,25 @@ eslintTester.addRuleTest("lib/rules/no-inner-declarations", {
         "if (test) { var fn = function expr() { }; }",
         "function decl() { var fn = function expr() { }; }",
         "function decl(arg) { var fn; if (arg) { fn = function() { }; } }",
+        {code: "var x = {doSomething() {function doSomethingElse() {}}}", ecmaFeatures: {objectLiteralShorthandMethods: true}},
         { code: "function decl(arg) { var fn; if (arg) { fn = function expr() { }; } }", ecmaFeatures: { arrowFunctions: true } },
         "function decl(arg) { var fn; if (arg) { fn = function expr() { }; } }",
         "if (test) { var foo; }",
+        { code: "if (test) { let x = 1; }", ecmaFeatures: { blockBindings: true }, args: [2, "both"] },
+        { code: "if (test) { const x = 1; }", ecmaFeatures: { blockBindings: true }, args: [2, "both"] },
         "function doSomething() { while (test) { var foo; } }",
         { code: "var foo;", args: [2, "both"] },
         { code: "var foo = 42;", args: [2, "both"] },
         { code: "function doSomething() { var foo; }", args: [2, "both"] },
         { code: "(function() { var foo; }());", args: [2, "both"] },
-        { code: "foo(() => { function bar() { } });", ecmaFeatures: { arrowFunctions: true } }
+        { code: "foo(() => { function bar() { } });", ecmaFeatures: { arrowFunctions: true } },
+        { code: "var fn = () => {var foo;}", ecmaFeatures: { arrowFunctions: true }, args: [2, "both"] },
+        {
+            code: "var x = {doSomething() {var foo;}}",
+            ecmaFeatures: {objectLiteralShorthandMethods: true},
+            args: [2, "both"]
+        }
+
     ],
 
     // Examples of code that should trigger the rule
