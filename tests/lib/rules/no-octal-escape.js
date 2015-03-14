a2e4567684a4218e47a179e88fa16a9e7df2e777
Nicholas C. Zakas <nicholas@nczconsulting.com>
2015-03-14 08:31:42 -0700

    Fix: Exempt \0 from no-octal-escape (fixes #1923)

--- tests/lib/rules/no-octal-escape.js
+++ tests/lib/rules/no-octal-escape.js
@@ -22,9 +22,11 @@ eslintTester.addRuleTest("lib/rules/no-octal-escape", {
     valid: [
         "var foo = \"\\851\";",
         "var foo = \"foo \\\\251 bar\";",
-        "var foo = /([abc]) \\1/g;"
+        "var foo = /([abc]) \\1/g;",
+        "var foo = '\\0';"
     ],
     invalid: [
+        { code: "var foo = \"foo \\01 bar\";", errors: [{ message: "Don't use octal: '\\0'. Use '\\u....' instead.", type: "Literal"}] },
         { code: "var foo = \"foo \\251 bar\";", errors: [{ message: "Don't use octal: '\\2'. Use '\\u....' instead.", type: "Literal"}] },
         { code: "var foo = \"\\751\";", errors: [{ message: "Don't use octal: '\\7'. Use '\\u....' instead.", type: "Literal"}] },
         { code: "var foo = \"\\3s51\";", errors: [{ message: "Don't use octal: '\\3'. Use '\\u....' instead.", type: "Literal"}] },
