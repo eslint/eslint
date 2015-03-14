26ad3171d0c602ff77b5ed3846f26e9bfb4268af
Michael Ficarra <git@michael.ficarra.me>
2015-03-14 16:24:38 -0700

    Fix: Exempt \0 from no-octal-escape (fixes #1923)

--- tests/lib/rules/no-octal-escape.js
+++ tests/lib/rules/no-octal-escape.js
@@ -20,14 +20,16 @@ var eslint = require("../../../lib/eslint"),
 var eslintTester = new ESLintTester(eslint);
 eslintTester.addRuleTest("lib/rules/no-octal-escape", {
     valid: [
-        "var foo = \"\\851\";",
+        "var foo = \"\\x51\";",
         "var foo = \"foo \\\\251 bar\";",
-        "var foo = /([abc]) \\1/g;"
+        "var foo = /([abc]) \\1/g;",
+        "var foo = '\\0';"
     ],
     invalid: [
-        { code: "var foo = \"foo \\251 bar\";", errors: [{ message: "Don't use octal: '\\2'. Use '\\u....' instead.", type: "Literal"}] },
-        { code: "var foo = \"\\751\";", errors: [{ message: "Don't use octal: '\\7'. Use '\\u....' instead.", type: "Literal"}] },
+        { code: "var foo = \"foo \\01 bar\";", errors: [{ message: "Don't use octal: '\\01'. Use '\\u....' instead.", type: "Literal"}] },
+        { code: "var foo = \"foo \\251 bar\";", errors: [{ message: "Don't use octal: '\\251'. Use '\\u....' instead.", type: "Literal"}] },
+        { code: "var foo = \"\\751\";", errors: [{ message: "Don't use octal: '\\75'. Use '\\u....' instead.", type: "Literal"}] },
         { code: "var foo = \"\\3s51\";", errors: [{ message: "Don't use octal: '\\3'. Use '\\u....' instead.", type: "Literal"}] },
-        { code: "var foo = \"\\\\\\751\";", errors: [{ message: "Don't use octal: '\\7'. Use '\\u....' instead.", type: "Literal"}] }
+        { code: "var foo = \"\\\\\\751\";", errors: [{ message: "Don't use octal: '\\75'. Use '\\u....' instead.", type: "Literal"}] }
     ]
 });
