22b3c6239f71bdec92b0f7848f5aef33e7726621
Danny Fritz <dannyfritz@gmail.com>
2015-03-18 22:16:36 -0500

    New: Add "always" and "never" options to "one-var" rule. (fixes #1619)

--- tests/lib/rules/one-var.js
+++ tests/lib/rules/one-var.js
@@ -19,7 +19,26 @@ eslintTester.addRuleTest("lib/rules/one-var", {
     valid: [
         "function foo() { var bar = true; }",
         "function foo() { var bar = true, baz = 1; if (qux) { bar = false; } }",
-        "var foo = function () { var bar = true; baz(); }"
+        "var foo = function () { var bar = true; baz(); }",
+        {
+            code: "function foo() { var bar = true, baz = false; }",
+            args: [2, "always"]
+        },
+        {
+            code: "function foo() { var bar = true; var baz = false; }",
+            args: [2, "never"]
+        },
+        {
+            code: "function foo() { var bar = true; var baz = false; }",
+            args: [2, "never"]
+        },
+        {
+            code: "function foo() { var a = [1, 2, 3]; var [b, c, d] = a; }",
+            ecmaFeatures: {
+                destructuring: true
+            },
+            args: [2, "never"]
+        }
     ],
     invalid: [
         {
@@ -80,6 +99,33 @@ eslintTester.addRuleTest("lib/rules/one-var", {
                     type: "VariableDeclaration"
                 }
             ]
+        },
+        {
+            code: "function foo() { var bar = true, baz = false; }",
+            args: [2, "never"],
+            errors: [{
+                message: "Split 'var' declaration into multiple statements.",
+                type: "VariableDeclaration"
+            }]
+        },
+        {
+            code: "function foo() { var bar = true; var baz = false; }",
+            args: [2, "always"],
+            errors: [{
+                message: "Combine this with the previous 'var' statement.",
+                type: "VariableDeclaration"
+            }]
+        },
+        {
+            code: "function foo() { var a = [1, 2, 3]; var [b, c, d] = a; }",
+            ecmaFeatures: {
+                destructuring: true
+            },
+            args: [2, "always"],
+            errors: [{
+                message: "Combine this with the previous 'var' statement.",
+                type: "VariableDeclaration"
+            }]
         }
     ]
 });
