efdbf76d6409f9c4686d639949c4730d69e1cf5c
Nicholas C. Zakas <nicholas@nczconsulting.com>
2015-03-27 16:05:39 -0700

    Fix: no-dupe-args to work with destructuring (fixes #2148)

--- tests/lib/rules/no-dupe-args.js
+++ tests/lib/rules/no-dupe-args.js
@@ -21,10 +21,14 @@ var eslintTester = new ESLintTester(eslint);
 eslintTester.addRuleTest("lib/rules/no-dupe-args", {
     valid: [
         "function a(a, b, c){}",
-        "var a = function(a, b, c){}"
+        "var a = function(a, b, c){}",
+        { code: "function a({a, b}, {c, d}){}", ecmaFeatures: { destructuring: true } }
     ],
     invalid: [
         { code: "function a(a, b, b) {}", errors: [{ message: "Duplicate param 'b'." }] },
+        { code: "function a({a, b}, b) {}", ecmaFeatures: { destructuring: true }, errors: [{ message: "Duplicate param 'b'." }] },
+        { code: "function a([a, b], b) {}", ecmaFeatures: { destructuring: true }, errors: [{ message: "Duplicate param 'b'." }] },
+        { code: "function a([a, b], {b}) {}", ecmaFeatures: { destructuring: true }, errors: [{ message: "Duplicate param 'b'." }] },
         { code: "function a(a, a, a) {}", errors: [{ message: "Duplicate param 'a'." }] },
         { code: "function a(a, b, a) {}", errors: [{ message: "Duplicate param 'a'." }]},
         { code: "function a(a, b, a, b) {}", errors: [{ message: "Duplicate param 'a'." }, { message: "Duplicate param 'b'." }]},
