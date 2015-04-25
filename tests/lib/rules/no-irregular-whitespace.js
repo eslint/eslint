46f23aa9617138e765c9b272d2362241687463ce
Mathias Schreck <schreck.mathias@googlemail.com>
2015-04-25 23:29:00 +0200

    Fix: no-irregular-whitespace should work with irregular line breaks (fixes #2316)

--- tests/lib/rules/no-irregular-whitespace.js
+++ tests/lib/rules/no-irregular-whitespace.js
@@ -18,54 +18,186 @@ var eslint = require("../../../lib/eslint"),
 //------------------------------------------------------------------------------
 
 var eslintTester = new ESLintTester(eslint);
-/* Ignored chars as cause parse errors: \u0085 \u200B \u2028 \u2029*/
-var invalidChars = "\u000B\u000C\u00A0\u180E\ufeff\u2000\u2001\u2002\u2003\u2004\u2005\u2006\u2007\u2008\u2009\u200A\u202F\u205f\u3000";
-var invalid = [];
-var valid = [
-    "var thing1 = ' 	';",
-    "var thing2 = '\\u000B';",
-    "var thing3 = '\\u000C';",
-    "var thing4 = '\\u0085';",
-    "var thing5 = '\\u00A0'",
-    "var thing6 = '\\u180E';",
-    "var thing7 = '\\ufeff';",
-    "var thing8 = '\\u2000';",
-    "var thing9 = '\\u2001';",
-    "var thing10 = '\\u2002';",
-    "var thing11 = '\\u2003';",
-    "var thing12 = '\\u2004';",
-    "var thing13 = '\\u2005';",
-    "var thing14 = '\\u2006';",
-    "var thing15 = '\\u2007';",
-    "var thing16 = '\\u2008';",
-    "var thing17 = '\\u2009';",
-    "var thing18 = '\\u200A';",
-    "var thing19 = '\\u200B';",
-    "var thing20 = '\\u2028';",
-    "var thing21 = '\\u2029';",
-    "var thing22 = '\\u202F';",
-    "var thing23 = '\\u205f';",
-    "var thing24 = '\\u3000';",
-    "var thing25 = 'test';"
-];
-var errors = [{
+
+var expectedErrors = [{
     message: "Irregular whitespace not allowed",
     type: "Program"
 }];
-var charArray = invalidChars.split("");
-charArray.forEach(function (char, i) {
-    var invalidCode = "var thing" + i + " = 'test'; " + char + " var thingElse = 'test';";
-    invalid.push({
-      code: invalidCode,
-      errors: errors
-    });
-
-    valid.push("var test" + i + " = '" + char + "';");
-});
-
-valid.push("var test = 'Is this ok?';"); // Test regular string
 
 eslintTester.addRuleTest("lib/rules/no-irregular-whitespace", {
-    valid: valid,
-    invalid: invalid
+    valid: [
+        "'\\u000B';",
+        "'\\u000C';",
+        "'\\u0085';",
+        "'\\u00A0'",
+        "'\\u180E';",
+        "'\\ufeff';",
+        "'\\u2000';",
+        "'\\u2001';",
+        "'\\u2002';",
+        "'\\u2003';",
+        "'\\u2004';",
+        "'\\u2005';",
+        "'\\u2006';",
+        "'\\u2007';",
+        "'\\u2008';",
+        "'\\u2009';",
+        "'\\u200A';",
+        "'\\u200B';",
+        "'\\u2028';",
+        "'\\u2029';",
+        "'\\u202F';",
+        "'\\u205f';",
+        "'\\u3000';",
+        "'\u000B';",
+        "'\u000C';",
+        "'\u0085';",
+        "'\u00A0'",
+        "'\u180E';",
+        "'\ufeff';",
+        "'\u2000';",
+        "'\u2001';",
+        "'\u2002';",
+        "'\u2003';",
+        "'\u2004';",
+        "'\u2005';",
+        "'\u2006';",
+        "'\u2007';",
+        "'\u2008';",
+        "'\u2009';",
+        "'\u200A';",
+        "'\u200B';",
+        "'\\\u2028';", // multiline string
+        "'\\\u2029';", // multiline string
+        "'\u202F';",
+        "'\u205f';",
+        "'\u3000';"
+    ],
+
+    invalid: [
+        {
+            code: "var any \u000B = 'thing';",
+            errors: expectedErrors
+        },
+        {
+            code: "var any \u000C = 'thing';",
+            errors: expectedErrors
+        },
+        {
+            code: "var any \u00A0 = 'thing';",
+            errors: expectedErrors
+        },
+        {
+            code: "var any \u180E = 'thing';",
+            errors: expectedErrors
+        },
+        {
+            code: "var any \ufeff = 'thing';",
+            errors: expectedErrors
+        },
+        {
+            code: "var any \u2000 = 'thing';",
+            errors: expectedErrors
+        },
+        {
+            code: "var any \u2001 = 'thing';",
+            errors: expectedErrors
+        },
+        {
+            code: "var any \u2002 = 'thing';",
+            errors: expectedErrors
+        },
+        {
+            code: "var any \u2003 = 'thing';",
+            errors: expectedErrors
+        },
+        {
+            code: "var any \u2004 = 'thing';",
+            errors: expectedErrors
+        },
+        {
+            code: "var any \u2005 = 'thing';",
+            errors: expectedErrors
+        },
+        {
+            code: "var any \u2006 = 'thing';",
+            errors: expectedErrors
+        },
+        {
+            code: "var any \u2007 = 'thing';",
+            errors: expectedErrors
+        },
+        {
+            code: "var any \u2008 = 'thing';",
+            errors: expectedErrors
+        },
+        {
+            code: "var any \u2009 = 'thing';",
+            errors: expectedErrors
+        },
+        {
+            code: "var any \u200A = 'thing';",
+            errors: expectedErrors
+        },
+        {
+            code: "var any \u2028 = 'thing';",
+            errors: expectedErrors
+        },
+        {
+            code: "var any \u2029 = 'thing';",
+            errors: expectedErrors
+        },
+        {
+            code: "var any \u202F = 'thing';",
+            errors: expectedErrors
+        },
+        {
+            code: "var any \u205f = 'thing';",
+            errors: expectedErrors
+        },
+        {
+            code: "var any \u3000 = 'thing';",
+            errors: expectedErrors
+        },
+        {
+            code: "var a = 'b',\u2028c = 'd',\ne = 'f'\u2028",
+            errors: [
+                {
+                    message: "Irregular whitespace not allowed",
+                    type: "Program",
+                    line: 1,
+                    column: 12
+                },
+                {
+                    message: "Irregular whitespace not allowed",
+                    type: "Program",
+                    line: 3,
+                    column: 7
+                }
+            ]
+        },
+        {
+            code: "var any \u3000 = 'thing', other \u3000 = 'thing';\nvar third \u3000 = 'thing';",
+            errors: [
+                {
+                    message: "Irregular whitespace not allowed",
+                    type: "Program",
+                    line: 1,
+                    column: 8
+                },
+                {
+                    message: "Irregular whitespace not allowed",
+                    type: "Program",
+                    line: 1,
+                    column: 27
+                },
+                {
+                    message: "Irregular whitespace not allowed",
+                    type: "Program",
+                    line: 2,
+                    column: 10
+                }
+            ]
+        }
+    ]
 });
