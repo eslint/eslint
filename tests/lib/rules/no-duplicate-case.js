639e89031ab5ea3d8c220853c7a5cdd2ec6f7cf3
Matthias Osswald <mat.osswald@sap.com>
2015-05-11 16:46:08 +0200

    Fix: rule no-duplicate-case problem with CallExpressions. (fixes #2499)

--- tests/lib/rules/no-duplicate-case.js
+++ tests/lib/rules/no-duplicate-case.js
@@ -39,6 +39,22 @@ eslintTester.addRuleTest("lib/rules/no-duplicate-case", {
         {
             code: "var a = 1, p = {p: {p1: 1, p2: 1}}; switch (a) {case p.p.p1: break; case p.p.p2: break; default: break;}",
             args: 2
+        },
+        {
+            code: "var a = 1, f = function(b) { return b ? { p1: 1 } : { p1: 2 }; }; switch (a) {case f(true).p1: break; case f(true, false).p1: break; default: break;}",
+            args: 2
+        },
+        {
+            code: "var a = 1, f = function(s) { return { p1: s } }; switch (a) {case f(a + 1).p1: break; case f(a + 2).p1: break; default: break;}",
+            args: 2
+        },
+        {
+            code: "var a = 1, f = function(s) { return { p1: s } }; switch (a) {case f(a == 1 ? 2 : 3).p1: break; case f(a === 1 ? 2 : 3).p1: break; default: break;}",
+            args: 2
+        },
+        {
+            code: "var a = 1, f1 = function() { return { p1: 1 } }, f2 = function() { return { p1: 2 } }; switch (a) {case f1().p1: break; case f2().p1: break; default: break;}",
+            args: 2
         }
     ],
     invalid: [
@@ -68,6 +84,38 @@ eslintTester.addRuleTest("lib/rules/no-duplicate-case", {
         },
         {
             code: "var a = 1, p = {p: {p1: 1, p2: 1}}; switch (a) {case p.p.p1: break; case p.p.p1: break; default: break;}",
+            args: 2,
+            errors: [{
+                message: "Duplicate case label.",
+                type: "SwitchCase"
+            }]
+        },
+        {
+            code: "var a = 1, f = function(b) { return b ? { p1: 1 } : { p1: 2 }; }; switch (a) {case f(true).p1: break; case f(true).p1: break; default: break;}",
+            args: 2,
+            errors: [{
+                message: "Duplicate case label.",
+                type: "SwitchCase"
+            }]
+        },
+        {
+            code: "var a = 1, f = function(s) { return { p1: s } }; switch (a) {case f(a + 1).p1: break; case f(a + 1).p1: break; default: break;}",
+            args: 2,
+            errors: [{
+                message: "Duplicate case label.",
+                type: "SwitchCase"
+            }]
+        },
+        {
+            code: "var a = 1, f = function(s) { return { p1: s } }; switch (a) {case f(a === 1 ? 2 : 3).p1: break; case f(a === 1 ? 2 : 3).p1: break; default: break;}",
+            args: 2,
+            errors: [{
+                message: "Duplicate case label.",
+                type: "SwitchCase"
+            }]
+        },
+        {
+            code: "var a = 1, f1 = function() { return { p1: 1 } }; switch (a) {case f1().p1: break; case f1().p1: break; default: break;}",
             args: 2,
             errors: [{
                 message: "Duplicate case label.",
