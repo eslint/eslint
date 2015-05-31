52cd819f5b0e03034b222838bf0c4ac75e85af78
Jose Roberto Vidal <vidal.roberto.j@gmail.com>
2015-05-31 10:58:19 -0500

    Update: no-lone-blocks does not report block-level scopes (fixes  #2119)

--- tests/lib/rules/no-lone-blocks.js
+++ tests/lib/rules/no-lone-blocks.js
@@ -21,14 +21,52 @@ eslintTester.addRuleTest("lib/rules/no-lone-blocks", {
     valid: [
         "if (foo) { if (bar) { baz(); } }",
         "do { bar(); } while (foo)",
-        "function foo() { while (bar) { baz() } }"
+        "function foo() { while (bar) { baz() } }",
+
+        // Block-level bindings
+        {code: "{ let x = 1; }", ecmaFeatures: {blockBindings: true}},
+        {code: "{ const x = 1; }", ecmaFeatures: {blockBindings: true}},
+        {code: "'use strict'; { function bar() {} }", ecmaFeatures: {blockBindings: true}},
+        {code: "{ class Bar {} }", ecmaFeatures: {classes: true}},
+
+        {code: "{ {let y = 1;} let x = 1; }", ecmaFeatures: {blockBindings: true}}
     ],
     invalid: [
-        { code: "{}", errors: [{ message: "Block is nested inside another block.", type: "BlockStatement"}] },
-        { code: "foo(); {} bar();", errors: [{ message: "Block is nested inside another block.", type: "BlockStatement"}] },
-        { code: "if (foo) { bar(); {} baz(); }", errors: [{ message: "Block is nested inside another block.", type: "BlockStatement"}] },
-        { code: "{ { } }", errors: [{ message: "Block is nested inside another block.", type: "BlockStatement"}, { message: "Block is nested inside another block.", type: "BlockStatement"}] },
-        { code: "function foo() { bar(); {} baz(); }", errors: [{ message: "Block is nested inside another block.", type: "BlockStatement"}] },
-        { code: "while (foo) { {} }", errors: [{ message: "Block is nested inside another block.", type: "BlockStatement"}] }
+        { code: "{}", errors: [{ message: "Block is redundant.", type: "BlockStatement"}] },
+        { code: "{var x = 1;}", errors: [{ message: "Block is redundant.", type: "BlockStatement"}] },
+        { code: "foo(); {} bar();", errors: [{ message: "Block is redundant.", type: "BlockStatement"}] },
+        { code: "if (foo) { bar(); {} baz(); }", errors: [{ message: "Nested block is redundant.", type: "BlockStatement"}] },
+        { code: "{ \n{ } }", errors: [
+            { message: "Block is redundant.", type: "BlockStatement", line: 1},
+            { message: "Nested block is redundant.", type: "BlockStatement", line: 2}]
+        },
+        { code: "function foo() { bar(); {} baz(); }", errors: [{ message: "Nested block is redundant.", type: "BlockStatement"}] },
+        { code: "while (foo) { {} }", errors: [{ message: "Nested block is redundant.", type: "BlockStatement"}] },
+
+        // Non-block-level bindings, even in ES6
+        { code: "{ function bar() {} }", errors: [{ message: "Block is redundant.", type: "BlockStatement"}], ecmaFeatures: {blockBindings: true}},
+        { code: "{var x = 1;}", errors: [{ message: "Block is redundant.", type: "BlockStatement"}], ecmaFeatures: {blockBindings: true} },
+        { code: "{ function bar() {} }", errors: [{ message: "Block is redundant.", type: "BlockStatement"}], ecmaFeatures: {classes: true}},
+        { code: "{var x = 1;}", errors: [{ message: "Block is redundant.", type: "BlockStatement"}], ecmaFeatures: {classes: true} },
+
+        {
+            code: "{ \n{var x = 1;}\n let y = 2; } {let z = 1;}",
+            ecmaFeatures: {blockBindings: true},
+            errors: [{message: "Nested block is redundant.", type: "BlockStatement", line: 2}]
+        },
+        {
+            code: "{ \n{let x = 1;}\n var y = 2; } {let z = 1;}",
+            ecmaFeatures: {blockBindings: true},
+            errors: [{message: "Block is redundant.", type: "BlockStatement", line: 1}]
+        },
+        {
+            code: "{ \n{var x = 1;}\n var y = 2; }\n {var z = 1;}",
+            ecmaFeatures: {blockBindings: true},
+            errors: [
+                {message: "Block is redundant.", type: "BlockStatement", line: 1},
+                {message: "Nested block is redundant.", type: "BlockStatement", line: 2},
+                {message: "Block is redundant.", type: "BlockStatement", line: 4}
+            ]
+        }
     ]
 });
