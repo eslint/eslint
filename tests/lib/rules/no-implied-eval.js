600fbf9312857b9c3e76fbacfeb6b3026dac0713
Benjamin Woodruff <bgw@fb.com>
2015-07-01 13:57:28 -0700

    Fix: Make no-implied-eval match more types of strings (fixes #2898)
    - TemplateLiterals are strings
    - Concatinating with a literal string implies a string. Eg.
      `setInterval("helper(" + foo + ", " + bar + ")", 50);`

--- tests/lib/rules/no-implied-eval.js
+++ tests/lib/rules/no-implied-eval.js
@@ -22,13 +22,29 @@ var eslintTester = new ESLintTester(eslint),
 
 eslintTester.addRuleTest("lib/rules/no-implied-eval", {
     valid: [
+        // normal usage
         "setInterval(function() { x = 1; }, 100);",
+        // only checks on top-level statements or window.*
         "foo.setTimeout('hi')",
+        // identifiers are fine
         "setTimeout(foo, 10)",
+        // as are function expressions
         "setTimeout(function() {}, 10)",
+        // setInterval
         "foo.setInterval('hi')",
         "setInterval(foo, 10)",
-        "setInterval(function() {}, 10)"
+        "setInterval(function() {}, 10)",
+        // execScript
+        "foo.execScript('hi')",
+        "execScript(foo)",
+        "execScript(function() {})",
+        // a binary plus on non-strings doesn't guarantee a string
+        "setTimeout(foo + bar, 10)",
+        // doesn't check anything but the first argument
+        "setTimeout(foobar, 'buzz')",
+        "setTimeout(foobar, foo + 'bar')",
+        // only checks immediate subtrees of the argument
+        "setTimeout(function() { return 'foobar'; }, 10)"
     ],
 
     invalid: [
@@ -36,9 +52,39 @@ eslintTester.addRuleTest("lib/rules/no-implied-eval", {
         { code: "setTimeout(\"x = 1;\", 100);", errors: [expectedError] },
         { code: "setInterval(\"x = 1;\");", errors: [expectedError] },
         { code: "execScript(\"x = 1;\");", errors: [expectedError] },
+        // member expressions
         { code: "window.setTimeout('foo')", errors: [expectedError] },
         { code: "window.setInterval('foo')", errors: [expectedError] },
         { code: "window['setTimeout']('foo')", errors: [expectedError] },
-        { code: "window['setInterval']('foo')", errors: [expectedError] }
+        { code: "window['setInterval']('foo')", errors: [expectedError] },
+        // template literals
+        { code: "setTimeout(`foo${bar}`)", errors: [expectedError], ecmaFeatures: {templateStrings: true} },
+        // string concatination
+        { code: "setTimeout('foo' + bar)", errors: [expectedError] },
+        { code: "setTimeout(foo + 'bar')", errors: [expectedError] },
+        { code: "setTimeout(`foo` + bar)", errors: [expectedError], ecmaFeatures: {templateStrings: true} },
+        { code: "setTimeout(1 + ';' + 1)", errors: [expectedError] },
+        // gives the correct node when dealing with nesting
+        {
+            code:
+                "setTimeout('foo' + (function() {\n" +
+                "   setTimeout(helper);\n" +
+                "   execScript('str');\n" +
+                "   return 'bar';\n" +
+                "})())",
+            errors: [
+                {
+                    message: expectedErrorMessage,
+                    type: "CallExpression",
+                    line: "1"
+                },
+                // no error on line 2
+                {
+                    message: expectedErrorMessage,
+                    type: "CallExpression",
+                    line: "3"
+                }
+            ]
+        }
     ]
 });
