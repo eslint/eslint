04198408b24c5914af0f7b530534d08829b69739
Aparajita Fishman <aparajita@aparajita.com>
2015-07-19 07:12:41 +0600

    Fix false warnings when using break/continue
    Currently, the following valid code generates false `"<name>" used outside of binding context` warnings for break and continue statements:
    
    foo:
    for (var i = 0; i < 10; ++i)
    {
        bar:
        for (var j = 0; j < 13; ++j)
        {
            if (j === 7)
                continue foo;
    
            if (j === 2)
                break bar;
        }
    }
    
    This commit fixes the rule to allow identifiers in break and continue statements.

--- tests/lib/rules/block-scoped-var.js
+++ tests/lib/rules/block-scoped-var.js
@@ -59,6 +59,7 @@ eslintTester.addRuleTest("lib/rules/block-scoped-var", {
         "function f(){ for(var a=0, b=1; a; b) a, b; }",
         "function f(){ for(var a in {}) a; }",
         "function f(){ switch(2) { case 1: var b = 2; b; break; default: b; break;} b; }",
+        "foo: while (true) {bar: for (var i = 0; i < 13; ++i) {if (i === 7) continue foo; if (i === 2) break bar; } }",
         "a:;",
         { code: "const React = require(\"react/addons\");const cx = React.addons.classSet;", globals: { require: false }, ecmaFeatures: { globalReturn: true, modules: true, blockBindings: true }},
         { code: "var v = 1;  function x() { return v; };", ecmaFeatures: { globalReturn: true }},
