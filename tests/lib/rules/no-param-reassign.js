56c1e438564b9111e14e8e26826ceefdc7b1bdfa
Toru Nagashima <star.ctor@gmail.com>
2015-07-20 01:58:28 +0900

    Update: `props` option of `no-param-reassign` (fixes #1600)

--- tests/lib/rules/no-param-reassign.js
+++ tests/lib/rules/no-param-reassign.js
@@ -23,7 +23,14 @@ eslintTester.addRuleTest("lib/rules/no-param-reassign", {
         "function foo(a) { var b = a; }",
         "function foo(a) { a.prop = 'value'; }",
         "function foo(a) { (function() { var a = 12; a++; })(); }",
-        { code: "function foo() { global = 13; }", globals: ["global"] }
+        { code: "function foo() { global = 13; }", globals: ["global"] },
+        "function foo(a) { a.b = 0; }",
+        "function foo(a) { delete a.b; }",
+        "function foo(a) { ++a.b; }",
+        { code: "function foo(a) { [a.b] = []; }", ecmaFeatures: {destructuring: true} },
+        { code: "function foo(a) { bar(a.b).c = 0; }", options: [{props: true}] },
+        { code: "function foo(a) { data[a.b] = 0; }", options: [{props: true}] },
+        { code: "function foo(a) { +a.b; }", options: [{props: true}] }
     ],
 
     invalid: [
@@ -37,6 +44,33 @@ eslintTester.addRuleTest("lib/rules/no-param-reassign", {
         { code: "function foo({bar}) { bar = 13; }", ecmaFeatures: {destructuring: true}, errors: [{ message: "Assignment to function parameter 'bar'." }] },
         { code: "function foo([, {bar}]) { bar = 13; }", ecmaFeatures: {destructuring: true}, errors: [{ message: "Assignment to function parameter 'bar'." }] },
         { code: "function foo(bar) { ({bar}) = {}; }", ecmaFeatures: {destructuring: true}, errors: [{ message: "Assignment to function parameter 'bar'." }] },
-        { code: "function foo(bar) { ({x: [, bar = 0]}) = {}; }", ecmaFeatures: {destructuring: true}, errors: [{ message: "Assignment to function parameter 'bar'." }] }
+        { code: "function foo(bar) { ({x: [, bar = 0]}) = {}; }", ecmaFeatures: {destructuring: true}, errors: [{ message: "Assignment to function parameter 'bar'." }] },
+
+        {
+            code: "function foo(bar) { bar.a = 0; }",
+            options: [{props: true}],
+            errors: [{ message: "Assignment to function parameter 'bar'." }]
+        },
+        {
+            code: "function foo(bar) { bar.get(0).a = 0; }",
+            options: [{props: true}],
+            errors: [{ message: "Assignment to function parameter 'bar'." }]
+        },
+        {
+            code: "function foo(bar) { delete bar.a; }",
+            options: [{props: true}],
+            errors: [{ message: "Assignment to function parameter 'bar'." }]
+        },
+        {
+            code: "function foo(bar) { ++bar.a; }",
+            options: [{props: true}],
+            errors: [{ message: "Assignment to function parameter 'bar'." }]
+        },
+        {
+            code: "function foo(bar) { [bar.a] = []; }",
+            ecmaFeatures: {destructuring: true},
+            options: [{props: true}],
+            errors: [{ message: "Assignment to function parameter 'bar'." }]
+        }
     ]
 });
