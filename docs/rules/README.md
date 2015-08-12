fca96e5e84c8a6cf7268cf62a5655c7689dfe8e7
Toru Nagashima <star.ctor@gmail.com>
2015-08-12 22:41:15 +0900

    New: `prefer-arrow-callback` rule (fixes #3140)

--- docs/rules/README.md
+++ docs/rules/README.md
@@ -199,6 +199,7 @@ These rules are only relevant to ES6 environments.
 * [no-this-before-super](no-this-before-super.md) - disallow use of `this`/`super` before calling `super()` in constructors.
 * [no-var](no-var.md) - require `let` or `const` instead of `var`
 * [object-shorthand](object-shorthand.md) - require method and property shorthand syntax for object literals
+* [prefer-arrow-callback](prefer-arrow-callback.md) - suggest using arrow functions as callbacks
 * [prefer-const](prefer-const.md) - suggest using `const` declaration for variables that are never modified after declared
 * [prefer-spread](prefer-spread.md) - suggest using the spread operator instead of `.apply()`.
 * [prefer-reflect](prefer-reflect.md) - suggest using Reflect methods where applicable
---
+++ tests/lib/rules/prefer-arrow-callback.js
@@ -0,0 +1,46 @@
+/**
+ * @fileoverview Tests for prefer-arrow-callback rule.
+ * @author Toru Nagashima
+ * @copyright 2015 Toru Nagashima. All rights reserved.
+ */
+
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/prefer-arrow-callback");
+var RuleTester = require("../../../lib/testers/rule-tester");
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var errors = [{
+    message: "Unexpected function expression.",
+    type: "FunctionExpression"
+}];
+
+var ruleTester = new RuleTester();
+ruleTester.run("prefer-arrow-callback", rule, {
+    valid: [
+        {code: "foo(a => a);", ecmaFeatures: {arrowFunctions: true}},
+        {code: "foo(function*() {});", ecmaFeatures: {generators: true}},
+        {code: "foo(function() { this; });"},
+        {code: "foo(function() { (() => this); });", ecmaFeatures: {arrowFunctions: true}},
+        {code: "foo(function() { this; }.bind(obj));"},
+        {code: "foo(function() { this; }.call(this));"},
+        {code: "foo(a => { (function() {}); });", ecmaFeatures: {arrowFunctions: true}},
+        {code: "var foo = function foo() {};"},
+        {code: "(function foo() {})();"}
+    ],
+    invalid: [
+        {code: "foo(function() {});", errors: errors},
+        {code: "foo(nativeCb || function() {});", errors: errors},
+        {code: "foo(bar ? function() {} : function() {});", errors: [errors[0], errors[0]]},
+        {code: "foo(function() { (function() { this; }); });", errors: errors},
+        {code: "foo(function() { this; }.bind(this));", errors: errors},
+        {code: "foo(function() { (() => this); }.bind(this));", ecmaFeatures: {arrowFunctions: true}, errors: errors}
+    ]
+});
