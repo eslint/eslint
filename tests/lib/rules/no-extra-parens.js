8a83f862d237f0d19582e603ad0858b7ab63c839
Toru Nagashima <star.ctor@gmail.com>
2015-09-05 22:01:46 +0900

    Fix: `no-extra-parens` false positive at IIFE with member accessing (fixes #3653)

--- tests/lib/rules/no-extra-parens.js
+++ tests/lib/rules/no-extra-parens.js
@@ -2,6 +2,7 @@
  * @fileoverview Disallow parenthesesisng higher precedence subexpressions.
  * @author Michael Ficarra
  * @copyright 2014 Michael Ficarra. All rights reserved.
+ * See LICENSE file in root directory for full license.
  */
 
 "use strict";
@@ -122,6 +123,7 @@ ruleTester.run("no-extra-parens", rule, {
         "do; while(a);",
         "for(;;);",
         "for(a in b);",
+        { code: "for(a of b);", ecmaFeatures: { forOf: true } },
         "var a = (b, c);",
         "[]",
         "[a, b]",
@@ -152,7 +154,6 @@ ruleTester.run("no-extra-parens", rule, {
         "var o = { foo: (function() { return bar(); })() };",
         "o.foo = (function(){ return bar(); })();",
         "(function(){ return bar(); })(), (function(){ return bar(); })()",
-        "((function(){ return bar(); })())",
 
         // parens are required around yield
         { code: "var foo = (function*() { if ((yield foo()) + 1) { return; } }())", ecmaFeatures: { generators: true } },
@@ -175,7 +176,26 @@ ruleTester.run("no-extra-parens", rule, {
         {code: "a, (b = c)", options: ["functions"]},
         {code: "for(a in (0));", options: ["functions"]},
         {code: "var a = (b = c)", options: ["functions"]},
-        {code: "_ => (a = 0)", options: ["functions"], ecmaFeatures: {arrowFunctions: true}}
+        {code: "_ => (a = 0)", options: ["functions"], ecmaFeatures: {arrowFunctions: true}},
+
+        // https://github.com/eslint/eslint/issues/3653
+        "(function(){}).foo(), 1, 2;",
+        "(function(){}).foo++;",
+        "(function(){}).foo() || bar;",
+        "(function(){}).foo() + 1;",
+        "(function(){}).foo() ? bar : baz;",
+        "(function(){}).foo.bar();",
+        "(function(){}.foo());",
+        "(function(){}.foo.bar);",
+
+        {code: "(class{}).foo(), 1, 2;", ecmaFeatures: {classes: true}},
+        {code: "(class{}).foo++;", ecmaFeatures: {classes: true}},
+        {code: "(class{}).foo() || bar;", ecmaFeatures: {classes: true}},
+        {code: "(class{}).foo() + 1;", ecmaFeatures: {classes: true}},
+        {code: "(class{}).foo() ? bar : baz;", ecmaFeatures: {classes: true}},
+        {code: "(class{}).foo.bar();", ecmaFeatures: {classes: true}},
+        {code: "(class{}.foo());", ecmaFeatures: {classes: true}},
+        {code: "(class{}.foo.bar);", ecmaFeatures: {classes: true}}
     ],
     invalid: [
         invalid("(0)", "Literal"),
@@ -226,6 +246,7 @@ ruleTester.run("no-extra-parens", rule, {
         invalid("new (function(){})", "FunctionExpression"),
         invalid("new (\nfunction(){}\n)", "FunctionExpression", 1),
         invalid("((function foo() {return 1;}))()", "FunctionExpression"),
+        invalid("((function(){ return bar(); })())", "CallExpression"),
 
         invalid("0, (_ => 0)", "ArrowFunctionExpression", 1, {ecmaFeatures: { arrowFunctions: true }}),
         invalid("(_ => 0), 0", "ArrowFunctionExpression", 1, {ecmaFeatures: { arrowFunctions: true }}),
@@ -239,6 +260,37 @@ ruleTester.run("no-extra-parens", rule, {
         invalid("a[(function() {})]", "FunctionExpression", null, {options: ["functions"]}),
         invalid("0, (_ => 0)", "ArrowFunctionExpression", 1, {options: ["functions"], ecmaFeatures: { arrowFunctions: true }}),
         invalid("(_ => 0), 0", "ArrowFunctionExpression", 1, {options: ["functions"], ecmaFeatures: { arrowFunctions: true }}),
-        invalid("a = (_ => 0)", "ArrowFunctionExpression", 1, {options: ["functions"], ecmaFeatures: { arrowFunctions: true }})
+        invalid("a = (_ => 0)", "ArrowFunctionExpression", 1, {options: ["functions"], ecmaFeatures: { arrowFunctions: true }}),
+
+        // https://github.com/eslint/eslint/issues/3653
+        invalid("((function(){})).foo();", "FunctionExpression"),
+        invalid("((function(){}).foo());", "CallExpression"),
+        invalid("((function(){}).foo);", "MemberExpression"),
+        invalid("0, (function(){}).foo();", "FunctionExpression"),
+        invalid("void (function(){}).foo();", "FunctionExpression"),
+        invalid("++(function(){}).foo;", "FunctionExpression"),
+        invalid("bar || (function(){}).foo();", "FunctionExpression"),
+        invalid("1 + (function(){}).foo();", "FunctionExpression"),
+        invalid("bar ? (function(){}).foo() : baz;", "FunctionExpression"),
+        invalid("bar ? baz : (function(){}).foo();", "FunctionExpression"),
+        invalid("bar((function(){}).foo(), 0);", "FunctionExpression"),
+        invalid("bar[(function(){}).foo()];", "FunctionExpression"),
+        invalid("var bar = (function(){}).foo();", "FunctionExpression"),
+        invalid("((function(){}).foo.bar)();", "FunctionExpression", null, {options: ["functions"]}),
+        invalid("((function(){}).foo)();", "FunctionExpression", null, {options: ["functions"]}),
+
+        invalid("((class{})).foo();", "ClassExpression", null, {ecmaFeatures: {classes: true}}),
+        invalid("((class{}).foo());", "CallExpression", null, {ecmaFeatures: {classes: true}}),
+        invalid("((class{}).foo);", "MemberExpression", null, {ecmaFeatures: {classes: true}}),
+        invalid("0, (class{}).foo();", "ClassExpression", null, {ecmaFeatures: {classes: true}}),
+        invalid("void (class{}).foo();", "ClassExpression", null, {ecmaFeatures: {classes: true}}),
+        invalid("++(class{}).foo;", "ClassExpression", null, {ecmaFeatures: {classes: true}}),
+        invalid("bar || (class{}).foo();", "ClassExpression", null, {ecmaFeatures: {classes: true}}),
+        invalid("1 + (class{}).foo();", "ClassExpression", null, {ecmaFeatures: {classes: true}}),
+        invalid("bar ? (class{}).foo() : baz;", "ClassExpression", null, {ecmaFeatures: {classes: true}}),
+        invalid("bar ? baz : (class{}).foo();", "ClassExpression", null, {ecmaFeatures: {classes: true}}),
+        invalid("bar((class{}).foo(), 0);", "ClassExpression", null, {ecmaFeatures: {classes: true}}),
+        invalid("bar[(class{}).foo()];", "ClassExpression", null, {ecmaFeatures: {classes: true}}),
+        invalid("var bar = (class{}).foo();", "ClassExpression", null, {ecmaFeatures: {classes: true}})
     ]
 });
