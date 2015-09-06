f2473b3ff7fcf46a769a9f9af24928e215cc09ba
Mathias Schreck <schreck.mathias@googlemail.com>
2015-09-06 23:33:51 +0200

    New: `jsx-quotes` rule (fixes #2011)

---
+++ tests/lib/rules/jsx-quotes.js
@@ -0,0 +1,72 @@
+/**
+ * @fileoverview Tests for jsx-quotes rule.
+ * @author Mathias Schreck <https://github.com/lo1tuma>
+ * @copyright 2015 Mathias Schreck
+ */
+
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/jsx-quotes"),
+    RuleTester = require("../../../lib/testers/rule-tester");
+
+var ruleTester = new RuleTester();
+ruleTester.run("jsx-quotes", rule, {
+    valid: [
+        {
+            code: "<foo bar=\"baz\" />",
+            ecmaFeatures: { jsx: true }
+        },
+        {
+            code: "<foo bar='\"' />",
+            ecmaFeatures: { jsx: true }
+        },
+        {
+            code: "<foo bar='baz' />",
+            options: [ "prefer-single" ],
+            ecmaFeatures: { jsx: true }
+        },
+        {
+            code: "<foo bar=\"baz\">\"</foo>",
+            ecmaFeatures: { jsx: true }
+        },
+        {
+            code: "<foo bar='baz'>'</foo>",
+            options: [ "prefer-single" ],
+            ecmaFeatures: { jsx: true }
+        },
+        {
+            code: "<foo bar={'baz'} />",
+            ecmaFeatures: { jsx: true }
+        },
+        {
+            code: "<foo bar={\"baz\"} />",
+            options: [ "prefer-single" ],
+            ecmaFeatures: { jsx: true }
+        },
+        {
+            code: "<foo bar={baz} />",
+            ecmaFeatures: { jsx: true }
+        }
+    ],
+    invalid: [
+        {
+            code: "<foo bar=\'baz\' />",
+            ecmaFeatures: { jsx: true },
+            errors: [
+                { message: "Unexpected usage of singlequote.", line: 1, column: 10, type: "Literal" }
+            ]
+        },
+        {
+            code: "<foo bar=\"baz\" />",
+            options: [ "prefer-single" ],
+            ecmaFeatures: { jsx: true },
+            errors: [
+                { message: "Unexpected usage of doublequote.", line: 1, column: 10, type: "Literal" }
+            ]
+        }
+    ]
+});
