7c4e4a995bc1e9aa41fab05931ec02e2ce61fbcc
Brody McKee <mrmckeb@hotmail.com>
2015-10-17 23:54:31 +1100

    Update: Option to exclude afterthoughts from no-plusplus (fixes #4093)

--- tests/lib/rules/no-plusplus.js
+++ tests/lib/rules/no-plusplus.js
@@ -19,10 +19,17 @@ var rule = require("../../../lib/rules/no-plusplus"),
 var ruleTester = new RuleTester();
 ruleTester.run("no-plusplus", rule, {
     valid: [
-        "var foo = 0; foo=+1;"
+        "var foo = 0; foo=+1;",
+        // With "allowForLoopAfterthoughts" allowed
+        { code: "var foo = 0; foo=+1;", options: [{allowForLoopAfterthoughts: true}] },
+        { code: "for (i = 0; i < l; i++) { console.log(i); }", options: [{allowForLoopAfterthoughts: true}] }
     ],
     invalid: [
         { code: "var foo = 0; foo++;", errors: [{ message: "Unary operator '++' used.", type: "UpdateExpression"}] },
-        { code: "var foo = 0; foo--;", errors: [{ message: "Unary operator '--' used.", type: "UpdateExpression"}] }
+        { code: "var foo = 0; foo--;", errors: [{ message: "Unary operator '--' used.", type: "UpdateExpression"}] },
+        { code: "for (i = 0; i < l; i++) { console.log(i); }", errors: [{ message: "Unary operator '++' used.", type: "UpdateExpression"}] },
+        // With "allowForLoopAfterthoughts" allowed
+        { code: "var foo = 0; foo++;", options: [{allowForLoopAfterthoughts: true}], errors: [{ message: "Unary operator '++' used.", type: "UpdateExpression"}] },
+        { code: "for (i = 0; i < l; i++) { v++; }", options: [{allowForLoopAfterthoughts: true}], errors: [{ message: "Unary operator '++' used.", type: "UpdateExpression"}] }
     ]
 });
