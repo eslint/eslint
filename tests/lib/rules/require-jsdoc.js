5dc77d0e8d64d0f6e20901fd4a917435782467ba
Gyandeep Singh <gyandeeps@gmail.com>
2015-11-03 13:01:24 -0600

    Update: Add class support to `require-jsdoc` rule (fixes #4268)

--- tests/lib/rules/require-jsdoc.js
+++ tests/lib/rules/require-jsdoc.js
@@ -42,7 +42,65 @@ ruleTester.run("require-jsdoc", rule, {
         "var array = [1,2,3];\narray.forEach(function() {});",
         "var array = [1,2,3];\narray.filter(function() {});",
         "Object.keys(this.options.rules || {}).forEach(function(name) {}.bind(this));",
-        "var object = { name: 'key'};\nObject.keys(object).forEach(function() {})"
+        "var object = { name: 'key'};\nObject.keys(object).forEach(function() {})",
+        {
+            code: "function myFunction() {}",
+            options: [{
+                "require": {
+                    "FunctionDeclaration": false,
+                    "MethodDefinition": true,
+                    "ClassDeclaration": true
+                }
+            }]
+        },
+        {
+            code:
+                "/**\n" +
+                " * Description for A.\n" +
+                " */\n" +
+                "class A {\n" +
+                "    /**\n" +
+                "     * Description for constructor.\n" +
+                "     * @param {object[]} xs - xs\n" +
+                "     */\n" +
+                "    constructor(xs) {\n" +
+                "        this.a = xs;" +
+                "    }\n" +
+                "}",
+            ecmaFeatures: {
+                classes: true
+            },
+            options: [{
+                "require": {
+                    "MethodDefinition": true,
+                    "ClassDeclaration": true
+                }
+            }]
+        },
+        {
+            code:
+            "/**\n" +
+            " * Description for A.\n" +
+            " */\n" +
+            "class App extends Component {\n" +
+            "    /**\n" +
+            "     * Description for constructor.\n" +
+            "     * @param {object[]} xs - xs\n" +
+            "     */\n" +
+            "    constructor(xs) {\n" +
+            "        this.a = xs;" +
+            "    }\n" +
+            "}",
+            ecmaFeatures: {
+                classes: true
+            },
+            options: [{
+                "require": {
+                    "MethodDefinition": true,
+                    "ClassDeclaration": true
+                }
+            }]
+        }
     ],
 
     invalid: [
@@ -51,6 +109,106 @@ ruleTester.run("require-jsdoc", rule, {
             errors: [{
                 message: "Missing JSDoc comment.",
                 type: "FunctionDeclaration"
+            }]
+        },
+        {
+            code:
+                "/**\n" +
+                " * Description for A.\n" +
+                " */\n" +
+                "class A {\n" +
+                "    constructor(xs) {\n" +
+                "        this.a = xs;" +
+                "    }\n" +
+                "}",
+            ecmaFeatures: {
+                classes: true
+            },
+            options: [{
+                "require": {
+                    "MethodDefinition": true,
+                    "ClassDeclaration": true
+                }
+            }],
+            errors: [{
+                message: "Missing JSDoc comment.",
+                type: "FunctionExpression"
+            }]
+        },
+        {
+            code:
+                "class A {\n" +
+                "    /**\n" +
+                "     * Description for constructor.\n" +
+                "     * @param {object[]} xs - xs\n" +
+                "     */\n" +
+                "    constructor(xs) {\n" +
+                "        this.a = xs;" +
+                "    }\n" +
+                "}",
+            ecmaFeatures: {
+                classes: true
+            },
+            options: [{
+                "require": {
+                    "MethodDefinition": true,
+                    "ClassDeclaration": true
+                }
+            }],
+            errors: [{
+                message: "Missing JSDoc comment.",
+                type: "ClassDeclaration"
+            }]
+        },
+        {
+            code:
+            "class A extends B {\n" +
+            "    /**\n" +
+            "     * Description for constructor.\n" +
+            "     * @param {object[]} xs - xs\n" +
+            "     */\n" +
+            "    constructor(xs) {\n" +
+            "        this.a = xs;" +
+            "    }\n" +
+            "}",
+            ecmaFeatures: {
+                classes: true
+            },
+            options: [{
+                "require": {
+                    "MethodDefinition": true,
+                    "ClassDeclaration": true
+                }
+            }],
+            errors: [{
+                message: "Missing JSDoc comment.",
+                type: "ClassDeclaration"
+            }]
+        },
+        {
+            code:
+            "export class A extends B {\n" +
+            "    /**\n" +
+            "     * Description for constructor.\n" +
+            "     * @param {object[]} xs - xs\n" +
+            "     */\n" +
+            "    constructor(xs) {\n" +
+            "        this.a = xs;" +
+            "    }\n" +
+            "}",
+            ecmaFeatures: {
+                classes: true,
+                modules: true
+            },
+            options: [{
+                "require": {
+                    "MethodDefinition": true,
+                    "ClassDeclaration": true
+                }
+            }],
+            errors: [{
+                message: "Missing JSDoc comment.",
+                type: "ClassDeclaration"
             }]
         }
     ]
