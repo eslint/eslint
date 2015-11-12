a4b3114804c730d6176fe2d80cc94a890741be11
alberto <alberto.email@gmail.com>
2015-11-12 21:50:20 +0100

    Fix: Handle comments in comma-spacing (fixes #4389)

--- tests/lib/rules/comma-spacing.js
+++ tests/lib/rules/comma-spacing.js
@@ -21,7 +21,12 @@ var ruleTester = new RuleTester();
 ruleTester.run("comma-spacing", rule, {
     valid: [
         "myfunc(404, true/* bla bla bla */, 'hello');",
+        "myfunc(404, true /* bla bla bla */, 'hello');",
         "myfunc(404, true/* bla bla bla *//* hi */, 'hello');",
+        "myfunc(404, true/* bla bla bla */ /* hi */, 'hello');",
+        "myfunc(404, true, /* bla bla bla */ 'hello');",
+        "myfunc(404, // comment\n true, /* bla bla bla */ 'hello');",
+        {code: "myfunc(404, // comment\n true,/* bla bla bla */ 'hello');", options: [{before: false, after: false}]},
         "var a = 1, b = 2;",
         "var arr = [, ];",
         "var arr = [1, ];",
@@ -456,11 +461,21 @@ ruleTester.run("comma-spacing", rule, {
             ]
         },
         {
-            code: "myfunc(404, true/* bla bla bla */ /* hi */, 'hello');",
-            output: "myfunc(404, true/* bla bla bla *//* hi */, 'hello');",
+            code: "myfunc(404, true,/* bla bla bla */ 'hello');",
+            output: "myfunc(404, true, /* bla bla bla */ 'hello');",
             errors: [
                 {
-                    message: "There should be no space before ','.",
+                    message: "A space is required after ','.",
+                    type: "Punctuator"
+                }
+            ]
+        },
+        {
+            code: "myfunc(404,// comment\n true, 'hello');",
+            output: "myfunc(404, // comment\n true, 'hello');",
+            errors: [
+                {
+                    message: "A space is required after ','.",
                     type: "Punctuator"
                 }
             ]
