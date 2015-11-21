13c8b06f183d0e80945ac0ae323c787a0fb73864
Toru Nagashima <star.ctor@gmail.com>
2015-11-21 18:21:15 +0900

    Fix: `no-eval` come to catch indirect eval (fixes #4399, fixes #4441)
    - Functions to detect `this` binding is default were extracted to AST
    Utils from `no-invalid-this`. And `no-eval` is using this utility.
    - `allowIndirect` option was added to `no-eval` in order to keep old
    behavior.

--- docs/rules/no-eval.md
+++ docs/rules/no-eval.md
@@ -20,6 +20,28 @@ The following patterns are considered problems:
 var obj = { x: "foo" },
     key = "x",
     value = eval("obj." + key); /*error eval can be harmful.*/
+
+(0, eval)("var a = 0");         /*error eval can be harmful.*/
+
+var foo = eval;                 /*error eval can be harmful.*/
+foo("var a = 0");
+
+// This `this` is the global object.
+this.eval("var a = 0");         /*error eval can be harmful.*/
+```
+
+```js
+/*eslint no-eval: 2*/
+/*eslint-env browser*/
+
+window.eval("var a = 0"); /*error eval can be harmful.*/
+```
+
+```js
+/*eslint no-eval: 2*/
+/*eslint-env node*/
+
+global.eval("var a = 0"); /*error eval can be harmful.*/
 ```
 
 The following patterns are not considered problems:
@@ -30,7 +52,85 @@ The following patterns are not considered problems:
 var obj = { x: "foo" },
     key = "x",
     value = obj[key];
+
+class A {
+    foo() {
+        // This is a user-defined method.
+        this.eval("var a = 0");
+    }
+
+    eval() {
+    }
+}
 ```
+
+### Options
+
+This rule has an option to allow indirect calls to `eval`.
+Indirect calls to `eval` are less dangerous than direct calls to `eval` because they cannot dynamically change the scope. Because of this, they also will not negatively impact performance to the degree of direct `eval`.
+
+```js
+{
+    "no-eval": [2, {"allowIndirect": true}] // default is false
+}
+```
+
+With this option the following patterns are considered problems:
+
+```js
+/*eslint no-eval: 2*/
+
+var obj = { x: "foo" },
+    key = "x",
+    value = eval("obj." + key); /*error eval can be harmful.*/
+```
+
+With this option the following patterns are not considered problems:
+
+```js
+/*eslint no-eval: 2*/
+
+(0, eval)("var a = 0");
+
+var foo = eval;
+foo("var a = 0");
+
+this.eval("var a = 0");
+```
+
+```js
+/*eslint no-eval: 2*/
+/*eslint-env browser*/
+
+window.eval("var a = 0");
+```
+
+```js
+/*eslint no-eval: 2*/
+/*eslint-env node*/
+
+global.eval("var a = 0");
+```
+
+### Known Limitations
+
+* This rule is warning every `eval()` even if the `eval` is not global's.
+  This behavior is in order to detect calls of direct `eval`. Such as:
+
+  ```js
+  module.exports = function(eval) {
+      // If the value of this `eval` is built-in `eval` function, this is a
+      // call of direct `eval`.
+      eval("var a = 0");
+  };
+  ```
+
+* This rule cannot catch renaming the global object. Such as:
+
+  ```js
+  var foo = window;
+  foo.eval("var a = 0");
+  ```
 
 ## Further Reading
 
--- tests/lib/rules/no-eval.js
+++ tests/lib/rules/no-eval.js
@@ -1,6 +1,9 @@
 /**
  * @fileoverview Tests for no-eval rule.
  * @author Nicholas C. Zakas
+ * @copyright 2015 Toru Nagashima. All rights reserved.
+ * @copyright 2013 Nicholas C. Zakas. All rights reserved.
+ * See LICENSE file in root directory for full license.
  */
 
 "use strict";
@@ -23,11 +26,62 @@ ruleTester.run("no-eval", rule, {
         "setTimeout('foo')",
         "setInterval('foo')",
         "window.setTimeout('foo')",
-        "window.setInterval('foo')"
+        "window.setInterval('foo')",
+
+        // User-defined eval methods.
+        "window.eval('foo')",
+        { code: "window.eval('foo')", env: {"node": true} },
+        { code: "window.noeval('foo')", env: {"browser": true} },
+        { code: "function foo() { var eval = 'foo'; window[eval]('foo') }", env: {"browser": true} },
+        "global.eval('foo')",
+        { code: "global.eval('foo')", env: {"browser": true} },
+        { code: "global.noeval('foo')", env: {"node": true} },
+        { code: "function foo() { var eval = 'foo'; global[eval]('foo') }", env: {"node": true} },
+        "this.noeval('foo');",
+        "function foo() { 'use strict'; this.eval('foo'); }",
+        "var obj = {foo: function() { this.eval('foo'); }}",
+        "var obj = {}; obj.foo = function() { this.eval('foo'); }",
+        { code: "class A { foo() { this.eval(); } }", ecmaFeatures: {"classes": true} },
+
+        // Allows indirect eval
+        { code: "(0, eval)('foo')", options: [{"allowIndirect": true}] },
+        { code: "(0, window.eval)('foo')", env: {"browser": true}, options: [{"allowIndirect": true}] },
+        { code: "(0, window['eval'])('foo')", env: {"browser": true}, options: [{"allowIndirect": true}] },
+        { code: "var EVAL = eval; EVAL('foo')", options: [{"allowIndirect": true}] },
+        { code: "var EVAL = this.eval; EVAL('foo')", options: [{"allowIndirect": true}] },
+        { code: "(function(exe){ exe('foo') })(eval);", options: [{"allowIndirect": true}] },
+        { code: "window.eval('foo')", env: {"browser": true}, options: [{"allowIndirect": true}] },
+        { code: "window.window.eval('foo')", env: {"browser": true}, options: [{"allowIndirect": true}] },
+        { code: "window.window['eval']('foo')", env: {"browser": true}, options: [{"allowIndirect": true}] },
+        { code: "global.eval('foo')", env: {"node": true}, options: [{"allowIndirect": true}] },
+        { code: "global.global.eval('foo')", env: {"node": true}, options: [{"allowIndirect": true}] },
+        { code: "this.eval('foo')", options: [{"allowIndirect": true}] },
+        { code: "function foo() { this.eval('foo') }", options: [{"allowIndirect": true}] }
     ],
 
     invalid: [
+        // Direct eval
         { code: "eval(foo)", errors: [{ message: "eval can be harmful.", type: "CallExpression"}] },
-        { code: "eval('foo')", errors: [{ message: "eval can be harmful.", type: "CallExpression"}] }
+        { code: "eval('foo')", errors: [{ message: "eval can be harmful.", type: "CallExpression"}] },
+        { code: "function foo(eval) { eval('foo') }", errors: [{ message: "eval can be harmful.", type: "CallExpression"}] },
+        { code: "eval(foo)", options: [{"allowIndirect": true}], errors: [{ message: "eval can be harmful.", type: "CallExpression"}] },
+        { code: "eval('foo')", options: [{"allowIndirect": true}], errors: [{ message: "eval can be harmful.", type: "CallExpression"}] },
+        { code: "function foo(eval) { eval('foo') }", options: [{"allowIndirect": true}], errors: [{ message: "eval can be harmful.", type: "CallExpression"}] },
+
+        // Indirect eval
+        { code: "(0, eval)('foo')", errors: [{ message: "eval can be harmful.", type: "Identifier"}] },
+        { code: "(0, window.eval)('foo')", env: {"browser": true}, errors: [{ message: "eval can be harmful.", type: "MemberExpression"}] },
+        { code: "(0, window['eval'])('foo')", env: {"browser": true}, errors: [{ message: "eval can be harmful.", type: "MemberExpression"}] },
+        { code: "var EVAL = eval; EVAL('foo')", errors: [{ message: "eval can be harmful.", type: "Identifier"}] },
+        { code: "var EVAL = this.eval; EVAL('foo')", errors: [{ message: "eval can be harmful.", type: "MemberExpression"}] },
+        { code: "(function(exe){ exe('foo') })(eval);", errors: [{ message: "eval can be harmful.", type: "Identifier"}] },
+        { code: "window.eval('foo')", env: {"browser": true}, errors: [{ message: "eval can be harmful.", type: "CallExpression"}] },
+        { code: "window.window.eval('foo')", env: {"browser": true}, errors: [{ message: "eval can be harmful.", type: "CallExpression"}] },
+        { code: "window.window['eval']('foo')", env: {"browser": true}, errors: [{ message: "eval can be harmful.", type: "CallExpression"}] },
+        { code: "global.eval('foo')", env: {"node": true}, errors: [{ message: "eval can be harmful.", type: "CallExpression"}] },
+        { code: "global.global.eval('foo')", env: {"node": true}, errors: [{ message: "eval can be harmful.", type: "CallExpression"}] },
+        { code: "global.global[`eval`]('foo')", env: {"node": true}, ecmaFeatures: {templateStrings: true}, errors: [{ message: "eval can be harmful.", type: "CallExpression"}] },
+        { code: "this.eval('foo')", errors: [{ message: "eval can be harmful.", type: "CallExpression"}] },
+        { code: "function foo() { this.eval('foo') }", errors: [{ message: "eval can be harmful.", type: "CallExpression"}] }
     ]
 });
