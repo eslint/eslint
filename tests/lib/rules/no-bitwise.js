c10327282fb02623b35d120481147b002ccf6cd6
Maga D. Zandaqo <denelxan@gmail.com>
2016-01-26 15:28:12 +0300

    Update: add int32Hint option to `no-bitwise` rule (fixes #4873)

--- tests/lib/rules/no-bitwise.js
+++ tests/lib/rules/no-bitwise.js
@@ -24,7 +24,9 @@ ruleTester.run("no-bitwise", rule, {
         "a += b",
         { code: "~[1, 2, 3].indexOf(1)", options: [{ allow: ["~"] }]},
         { code: "~1<<2 === -8", options: [{ allow: ["~", "<<"] }]},
-        { code: "~1<<2 === -8", options: [{ allow: ["~", "<<"] }]}
+        { code: "~1<<2 === -8", options: [{ allow: ["~", "<<"] }]},
+        { code: "a|0", options: [{ int32Hint: true }]},
+        { code: "a|0", options: [{ allow: ["|"], int32Hint: false }]}
     ],
     invalid: [
         { code: "a ^ b", errors: [{ message: "Unexpected use of '^'.", type: "BinaryExpression"}] },
