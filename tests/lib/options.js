413247fef501b34dd6c14241b3ff42c2079edbfe
Christopher Crouzet <christopher.crouzet@gmail.com>
2016-02-04 08:59:36 +0700

    New: Add a --print-config flag (fixes #5099)

--- tests/lib/options.js
+++ tests/lib/options.js
@@ -318,4 +318,11 @@ describe("options", function() {
             assert.equal(currentOptions.parser, "espree");
         });
     });
+
+    describe("--print-config", function() {
+        it("should return true when passed --print-config", function() {
+            var currentOptions = options.parse("--print-config");
+            assert.isTrue(currentOptions.printConfig);
+        });
+    });
 });
