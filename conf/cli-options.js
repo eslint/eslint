8806a18525e005333a37c0608ebd39205a6e3182
alberto <alberto.email@gmail.com>
2016-05-15 11:18:59 +0200

    Fix: Remove default parser from CLIEngine options (fixes #6182)

--- conf/cli-options.js
+++ conf/cli-options.js
@@ -5,8 +5,6 @@
 
 "use strict";
 
-var DEFAULT_PARSER = require("../conf/eslint.json").parser;
-
 module.exports = {
     configFile: null,
     baseConfig: false,
@@ -18,8 +16,8 @@ module.exports = {
     extensions: [".js"],
     ignore: true,
     ignorePath: null,
-    parser: DEFAULT_PARSER,
     cache: false,
+
     // in order to honor the cacheFile option if specified
     // this option should not have a default value otherwise
     // it will always be used
