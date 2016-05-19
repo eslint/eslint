9fb65434dcbfc8719a83be0457905e6d9f0794fb
Andrew Johnston <ehjay.github@gmail.com>
2016-05-19 17:13:36 -0400

    New: `unicode-bom` rule to allow or disallow BOM (fixes #5502)

---
+++ tests/lib/rules/unicode-bom.js
@@ -0,0 +1,56 @@
+/**
+ * @fileoverview Check that the Unicode BOM can be required and disallowed
+ * @author Andrew Johnston <https://github.com/ehjay>
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+var rule = require("../../../lib/rules/unicode-bom"),
+    RuleTester = require("../../../lib/testers/rule-tester");
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+var ruleTester = new RuleTester();
+
+ruleTester.run("unicode-bom", rule, {
+
+    valid: [
+        {
+            code: "\uFEFF var a = 123;",
+            options: ["always"]
+        },
+        {
+            code: "var a = 123;",
+            options: ["never"]
+        },
+        {
+            code: "var a = 123; \uFEFF",
+            options: ["never"]
+        }
+    ],
+
+    invalid: [
+        {
+            code: "var a = 123;",
+            errors: [{ message: "Expected Unicode BOM (Byte Order Mark).", type: "Program" }],
+            options: ["always"],
+            output: "\uFEFFvar a = 123;"
+        },
+        {
+            code: "\uFEFF var a = 123;",
+            errors: [{ message: "Unexpected Unicode BOM (Byte Order Mark).", type: "Program" }],
+            output: " var a = 123;"
+        },
+        {
+            code: "\uFEFF var a = 123;",
+            errors: [{ message: "Unexpected Unicode BOM (Byte Order Mark).", type: "Program" }],
+            options: ["never"],
+            output: " var a = 123;"
+        }
+    ]
+});
