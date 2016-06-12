ff4569a278992b41c1b9d64cbc2cb008cd9c0604
alberto <alberto.email@gmail.com>
2016-06-12 12:28:20 +0200

    Update: Add `never` option for `func-names` (fixes #6059)

--- docs/rules/func-names.md
+++ docs/rules/func-names.md
@@ -1,4 +1,4 @@
-# Require Function Expressions to have a Name (func-names)
+# Require or disallow named `function` expressions (func-names)
 
 A pattern that's becoming more common is to give function expressions names to aid in debugging. For example:
 
@@ -10,7 +10,17 @@ Adding the second `bar` in the above example is optional.  If you leave off the 
 
 ## Rule Details
 
-Examples of **incorrect** code for this rule:
+This rule can enforce or disallow the use of named function expressions.
+
+## Options
+
+This rule has a string option:
+
+* `"always"` (default) requires function expressions to have a name
+* `"never"` disallows named function expressions
+
+
+Examples of **incorrect** code for this rule with the default `"always"` option:
 
 ```js
 /*eslint func-names: "error"*/
@@ -22,7 +32,7 @@ Foo.prototype.bar = function() {};
 }())
 ```
 
-Examples of **correct** code for this rule:
+Examples of **correct** code for this rule with the default `"always"` option:
 
 ```js
 /*eslint func-names: "error"*/
@@ -34,6 +44,35 @@ Foo.prototype.bar = function bar() {};
 }())
 ```
 
+Examples of **incorrect** code for this rule with the `"never"` option:
+
+```js
+/*eslint func-names: "error"*/
+
+Foo.prototype.bar = function bar() {};
+
+(function bar() {
+    // ...
+}())
+```
+
+Examples of **correct** code for this rule with the `"never"` option:
+
+```js
+/*eslint func-names: "error"*/
+
+Foo.prototype.bar = function() {};
+
+(function() {
+    // ...
+}())
+```
+
 ## Further Reading
 
 * [Functions Explained](http://markdaggett.com/blog/2013/02/15/functions-explained/)
+
+## Compatibility
+
+* **JSCS**: [requireAnonymousFunctions](http://jscs.info/rule/requireAnonymousFunctions)
+* **JSCS**: [disallowAnonymousFunctions](http://jscs.info/rule/disallowAnonymousFunctions)
--- tests/lib/rules/func-names.js
+++ tests/lib/rules/func-names.js
@@ -33,6 +33,44 @@ ruleTester.run("func-names", rule, {
         {
             code: "class A { constructor(){} foo(){} get bar(){} set baz(value){} static qux(){}}",
             parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "function foo() {}",
+            options: ["always"]
+        },
+        {
+            code: "var a = function foo() {};",
+            options: ["always"]
+        },
+        {
+            code: "function foo() {}",
+            options: ["never"]
+        },
+        {
+            code: "var a = function() {};",
+            options: ["never"]
+        },
+        {
+            code: "var foo = {bar: function() {}};",
+            options: ["never"]
+        },
+        {
+            code: "$('#foo').click(function() {});",
+            options: ["never"]
+        },
+        {
+            code: "Foo.prototype.bar = function() {};",
+            options: ["never"]
+        },
+        {
+            code: "class A { constructor(){} foo(){} get bar(){} set baz(value){} static qux(){}}",
+            options: ["never"],
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "({ foo() {} });",
+            options: ["never"],
+            parserOptions: { ecmaVersion: 6 }
         }
     ],
     invalid: [
@@ -41,6 +79,21 @@ ruleTester.run("func-names", rule, {
         { code: "f(function(){})", errors: [{ message: "Missing function expression name.", type: "FunctionExpression"}] },
         { code: "var a = new Date(function() {});", errors: [{ message: "Missing function expression name.", type: "FunctionExpression"}] },
         { code: "var test = function(d, e, f) {};", errors: [{ message: "Missing function expression name.", type: "FunctionExpression"}] },
-        { code: "new function() {}", errors: [{ message: "Missing function expression name.", type: "FunctionExpression"}] }
+        { code: "new function() {}", errors: [{ message: "Missing function expression name.", type: "FunctionExpression"}] },
+        {
+            code: "var x = function named() {};",
+            options: ["never"],
+            errors: [{ message: "Unexpected function expression name.", type: "FunctionExpression"}]
+        },
+        {
+            code: "Foo.prototype.bar = function named() {};",
+            options: ["never"],
+            errors: [{ message: "Unexpected function expression name.", type: "FunctionExpression"}]
+        },
+        {
+            code: "({foo: function named() {}})",
+            options: ["never"],
+            errors: [{ message: "Unexpected function expression name.", type: "FunctionExpression"}]
+        }
     ]
 });
