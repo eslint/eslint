5122f738dc6bdc85c602758b2fa3560c1ac5dceb
Ruurd Moelker <RRMoelker@users.noreply.github.com>
2016-06-13 21:08:18 +0200

    Update: no-multiple-empty-lines fixer (fixes #6225) (#6226)

--- tests/lib/rules/no-multiple-empty-lines.js
+++ tests/lib/rules/no-multiple-empty-lines.js
@@ -116,6 +116,10 @@ ruleTester.run("no-multiple-empty-lines", rule, {
             code: "\n// valid 11\nvar a = 1;\n",
             options: [{ max: 2, maxBOF: 1 }]
         },
+        {
+            code: "// valid 12\r\n// windows line endings\r\nvar a = 5;\r\nvar b = 3;\r\n\r\n",
+            options: [ { max: 1 } ]
+        },
 
         // template strings
         {
@@ -151,99 +155,130 @@ ruleTester.run("no-multiple-empty-lines", rule, {
     invalid: [
         {
             code: "// invalid 1\nvar a = 5;\n\n\nvar b = 3;",
+            output: "// invalid 1\nvar a = 5;\n\nvar b = 3;",
             errors: [ getExpectedError(1) ],
             options: [ { max: 1 } ]
         },
         {
             code: "// invalid 2\n\n\n\n\nvar a = 5;",
+            output: "// invalid 2\n\n\nvar a = 5;",
             errors: [ getExpectedError(2) ],
             options: [ { max: 2 } ]
         },
         {
             code: "// invalid 3\nvar a = 5;\n\n\n\n",
+            output: "// invalid 3\nvar a = 5;\n\n\n",
             errors: [ getExpectedErrorEOF(2) ],
             options: [ { max: 2 } ]
         },
         {
             code: "// invalid 4\nvar a = 5;\n \n \n \n",
+            output: "// invalid 4\nvar a = 5;\n \n \n",
             errors: [ getExpectedErrorEOF(2) ],
             options: [ { max: 2 } ]
         },
         {
             code: "// invalid 5\nvar a=5;\n\n\n\nvar b = 3;",
+            output: "// invalid 5\nvar a=5;\n\n\nvar b = 3;",
             errors: [ getExpectedError(2) ],
             options: [ { max: 2 } ]
         },
         {
             code: "// invalid 6\nvar a=5;\n\n\n\nvar b = 3;\n",
+            output: "// invalid 6\nvar a=5;\n\n\nvar b = 3;\n",
             errors: [ getExpectedError(2) ],
             options: [ { max: 2 } ]
         },
         {
             code: "// invalid 7\nvar a = 5;\n\n\n\nb = 3;\nvar c = 5;\n\n\n\nvar d = 3;",
+            output: "// invalid 7\nvar a = 5;\n\n\nb = 3;\nvar c = 5;\n\n\nvar d = 3;",
             errors: 2,
             options: [ { max: 2 } ]
         },
         {
             code: "// invalid 8\nvar a = 5;\n\n\n\n\n\n\n\n\n\n\n\n\n\nb = 3;",
+            output: "// invalid 8\nvar a = 5;\n\n\nb = 3;",
             errors: [ getExpectedError(2) ],
             options: [ { max: 2 } ]
         },
         {
             code: "// invalid 9\nvar a=5;\n\n\n\n\n",
+            output: "// invalid 9\nvar a=5;\n\n\n",
             errors: [ getExpectedErrorEOF(2) ],
             options: [ { max: 2 } ]
         },
         {
             code: "// invalid 10\nvar a = 5;\n\nvar b = 3;",
+            output: "// invalid 10\nvar a = 5;\nvar b = 3;",
             errors: [ getExpectedError(0) ],
             options: [ { max: 0 } ]
         },
         {
             code: "// invalid 11\nvar a = 5;\n\n\n",
+            output: "// invalid 11\nvar a = 5;\n\n",
             errors: [ getExpectedErrorEOF(1) ],
             options: [ { max: 5, maxEOF: 1 } ]
         },
         {
             code: "// invalid 12\nvar a = 5;\n\n\n\n\n\n",
+            output: "// invalid 12\nvar a = 5;\n\n\n\n\n",
             errors: [ getExpectedErrorEOF(4) ],
             options: [ { max: 0, maxEOF: 4 } ]
         },
         {
             code: "// invalid 13\n\n\n\n\n\n\n\n\nvar a = 5;\n\n\n",
+            output: "// invalid 13\n\n\n\n\n\n\n\n\nvar a = 5;\n\n",
             errors: [ getExpectedErrorEOF(1) ],
             options: [ { max: 10, maxEOF: 1 } ]
         },
         {
             code: "// invalid 14\nvar a = 5;\n\n",
+            output: "// invalid 14\nvar a = 5;\n",
             errors: [ getExpectedErrorEOF(0) ],
             options: [ { max: 2, maxEOF: 0 } ]
         },
         {
             code: "\n\n// invalid 15\nvar a = 5;\n",
+            output: "\n// invalid 15\nvar a = 5;\n",
             errors: [ getExpectedErrorBOF(1) ],
             options: [ { max: 5, maxBOF: 1 } ]
         },
         {
             code: "\n\n\n\n\n// invalid 16\nvar a = 5;\n",
+            output: "\n\n\n\n// invalid 16\nvar a = 5;\n",
             errors: [ getExpectedErrorBOF(4)],
             options: [ { max: 0, maxBOF: 4 } ]
         },
         {
             code: "\n\n// invalid 17\n\n\n\n\n\n\n\n\nvar a = 5;\n",
+            output: "\n// invalid 17\n\n\n\n\n\n\n\n\nvar a = 5;\n",
             errors: [ getExpectedErrorBOF(1) ],
             options: [ { max: 10, maxBOF: 1 } ]
         },
         {
             code: "\n// invalid 18\nvar a = 5;\n",
+            output: "// invalid 18\nvar a = 5;\n",
             errors: [ getExpectedErrorBOF(0) ],
             options: [ { max: 2, maxBOF: 0 } ]
         },
         {
             code: "\n\n\n// invalid 19\nvar a = 5;\n\n",
+            output: "// invalid 19\nvar a = 5;\n",
             errors: [ getExpectedErrorBOF(0),
                       getExpectedErrorEOF(0) ],
             options: [ { max: 2, maxBOF: 0, maxEOF: 0 } ]
+        },
+        {
+            code: "// invalid 20\r\n// windows line endings\r\nvar a = 5;\r\nvar b = 3;\r\n\r\n\r\n",
+            output: "// invalid 20\r\n// windows line endings\r\nvar a = 5;\r\nvar b = 3;\r\n\r\n",
+            errors: [ getExpectedErrorEOF(1) ],
+            options: [ { max: 1 } ]
+        },
+        {
+            code: "// invalid 21\n// unix line endings\nvar a = 5;\nvar b = 3;\n\n\n",
+            output: "// invalid 21\n// unix line endings\nvar a = 5;\nvar b = 3;\n\n",
+            errors: [ getExpectedErrorEOF(1) ],
+            options: [ { max: 1 } ]
         }
     ]
 });
