d857d653d0ef2a223b944a1ddf7186a67187f4ce
Toru Nagashima <star.ctor@gmail.com>
2016-07-16 00:08:00 +0900

    Update: make `no-var` fixable (fixes #6639) (#6644)
    * Update: make `no-var` fixable (fixes #6639)
    
    * Fix: modify `no-var` not fixing `var` on a case chunk.
    
    * Chore: add the notes of not-fixable cases.

--- tests/lib/rules/no-var.js
+++ tests/lib/rules/no-var.js
@@ -33,6 +33,7 @@ ruleTester.run("no-var", rule, {
     invalid: [
         {
             code: "var foo = bar;",
+            output: "let foo = bar;",
             parserOptions: { ecmaVersion: 6 },
             errors: [
                 {
@@ -43,6 +44,7 @@ ruleTester.run("no-var", rule, {
         },
         {
             code: "var foo = bar, toast = most;",
+            output: "let foo = bar, toast = most;",
             parserOptions: { ecmaVersion: 6 },
             errors: [
                 {
@@ -53,6 +55,7 @@ ruleTester.run("no-var", rule, {
         },
         {
             code: "var foo = bar; let toast = most;",
+            output: "let foo = bar; let toast = most;",
             parserOptions: { ecmaVersion: 6 },
             errors: [
                 {
@@ -60,6 +63,60 @@ ruleTester.run("no-var", rule, {
                     type: "VariableDeclaration"
                 }
             ]
-        }
+        },
+
+        // Not fix if it's redeclared or it's used from outside of the scope or it's declared on a case chunk.
+        {
+            code: "var a, b, c; var a;",
+            output: "var a, b, c; var a;",
+            errors: [
+                "Unexpected var, use let or const instead.",
+                "Unexpected var, use let or const instead."
+            ]
+        },
+        {
+            code: "var a; if (b) { var a; }",
+            output: "var a; if (b) { var a; }",
+            errors: [
+                "Unexpected var, use let or const instead.",
+                "Unexpected var, use let or const instead."
+            ]
+        },
+        {
+            code: "if (foo) { var a, b, c; } a;",
+            output: "if (foo) { var a, b, c; } a;",
+            errors: [
+                "Unexpected var, use let or const instead."
+            ]
+        },
+        {
+            code: "for (var i = 0; i < 10; ++i) {} i;",
+            output: "for (var i = 0; i < 10; ++i) {} i;",
+            errors: [
+                "Unexpected var, use let or const instead."
+            ]
+        },
+        {
+            code: "for (var a in obj) {} a;",
+            output: "for (var a in obj) {} a;",
+            errors: [
+                "Unexpected var, use let or const instead."
+            ]
+        },
+        {
+            code: "for (var a of list) {} a;",
+            output: "for (var a of list) {} a;",
+            parserOptions: {ecmaVersion: 6},
+            errors: [
+                "Unexpected var, use let or const instead."
+            ]
+        },
+        {
+            code: "switch (a) { case 0: var b = 1 }",
+            output: "switch (a) { case 0: var b = 1 }",
+            errors: [
+                "Unexpected var, use let or const instead."
+            ]
+        },
     ]
 });
