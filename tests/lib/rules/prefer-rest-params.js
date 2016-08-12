d46d00f83398a1858171caecb0533899fe338f87
Toru Nagashima <star.ctor@gmail.com>
2016-08-13 02:29:54 +0900

    Update: `prefer-rest-params` relax for member accesses (fixes #5990) (#6871)

--- tests/lib/rules/prefer-rest-params.js
+++ tests/lib/rules/prefer-rest-params.js
@@ -20,9 +20,14 @@ ruleTester.run("prefer-rest-params", rule, {
         "function foo(arguments) { arguments; }",
         "function foo() { var arguments; arguments; }",
         {code: "var foo = () => arguments;", parserOptions: { ecmaVersion: 6 }}, // Arrows don't have "arguments".,
-        {code: "function foo(...args) { args; }", parserOptions: { ecmaVersion: 6 }}
+        {code: "function foo(...args) { args; }", parserOptions: { ecmaVersion: 6 }},
+        "function foo() { arguments.length; }",
+        "function foo() { arguments.callee; }",
     ],
     invalid: [
-        {code: "function foo() { arguments; }", errors: [{type: "Identifier", message: "Use the rest parameters instead of 'arguments'."}]}
+        {code: "function foo() { arguments; }", errors: [{type: "Identifier", message: "Use the rest parameters instead of 'arguments'."}]},
+        {code: "function foo() { arguments[0]; }", errors: [{type: "Identifier", message: "Use the rest parameters instead of 'arguments'."}]},
+        {code: "function foo() { arguments[1]; }", errors: [{type: "Identifier", message: "Use the rest parameters instead of 'arguments'."}]},
+        {code: "function foo() { arguments[Symbol.iterator]; }", errors: [{type: "Identifier", message: "Use the rest parameters instead of 'arguments'."}]},
     ]
 });
