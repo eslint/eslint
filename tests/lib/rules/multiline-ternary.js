8ffda6d9774dc52f22dcad076e874d0071488dc4
Kai Cataldo <kaicataldo@users.noreply.github.com>
2016-08-23 13:18:03 -0400

    Update: Add never option to multiline-ternary (fixes #6751) (#6905)

--- tests/lib/rules/multiline-ternary.js
+++ tests/lib/rules/multiline-ternary.js
@@ -17,26 +17,44 @@ const RuleTester = require("../../../lib/testers/rule-tester");
 //------------------------------------------------------------------------------
 
 const ruleTester = new RuleTester();
-const testConsMsg = "Expected newline between test and consequent of ternary expression.";
-const consAltMsg = "Expected newline between consequent and alternate of ternary expression.";
+const expectedTestConsMsg = "Expected newline between test and consequent of ternary expression.";
+const expectedConsAltMsg = "Expected newline between consequent and alternate of ternary expression.";
+const unexpectedTestConsMsg = "Unexpected newline between test and consequent of ternary expression.";
+const unexpectedConsAltMsg = "Unexpected newline between consequent and alternate of ternary expression.";
 
 ruleTester.run("multiline-ternary", rule, {
     valid: [
+
+        // default "always"
         "a\n? b\n: c",
         "a ?\nb :\nc",
         "a\n? b\n? c\n: d\n: e",
+        "a\n? (b\n? c\n: d)\n: e",
+
+        // "always"
+        { code: "a\n? b\n: c", options: ["always"] },
+        { code: "a ?\nb :\nc", options: ["always"] },
+        { code: "a\n? b\n? c\n: d\n: e", options: ["always"] },
+        { code: "a\n? (b\n? c\n: d)\n: e", options: ["always"] },
+
+        // "never"
+        { code: "a ? b : c", options: ["never"] },
+        { code: "a ? b ? c : d : e", options: ["never"] },
+        { code: "a ? (b ? c : d) : e", options: ["never"] }
     ],
 
     invalid: [
+
+        // default "always"
         {
             code: "a ? b : c",
             errors: [{
-                message: testConsMsg,
+                message: expectedTestConsMsg,
                 line: 1,
                 column: 1
             },
             {
-                message: consAltMsg,
+                message: expectedConsAltMsg,
                 line: 1,
                 column: 5
             }]
@@ -44,7 +62,7 @@ ruleTester.run("multiline-ternary", rule, {
         {
             code: "a\n? b : c",
             errors: [{
-                message: consAltMsg,
+                message: expectedConsAltMsg,
                 line: 2,
                 column: 3
             }]
@@ -52,7 +70,7 @@ ruleTester.run("multiline-ternary", rule, {
         {
             code: "a ? b\n: c",
             errors: [{
-                message: testConsMsg,
+                message: expectedTestConsMsg,
                 line: 1,
                 column: 1
             }]
@@ -60,22 +78,22 @@ ruleTester.run("multiline-ternary", rule, {
         {
             code: "a ? (b ? c : d) : e",
             errors: [{
-                message: testConsMsg,
+                message: expectedTestConsMsg,
                 line: 1,
                 column: 1
             },
             {
-                message: consAltMsg,
+                message: expectedConsAltMsg,
                 line: 1,
                 column: 6
             },
             {
-                message: testConsMsg,
+                message: expectedTestConsMsg,
                 line: 1,
                 column: 6
             },
             {
-                message: consAltMsg,
+                message: expectedConsAltMsg,
                 line: 1,
                 column: 10
             }]
@@ -83,12 +101,12 @@ ruleTester.run("multiline-ternary", rule, {
         {
             code: "a ?\n(b ? c : d) :\ne",
             errors: [{
-                message: testConsMsg,
+                message: expectedTestConsMsg,
                 line: 2,
                 column: 2
             },
             {
-                message: consAltMsg,
+                message: expectedConsAltMsg,
                 line: 2,
                 column: 6
             }]
@@ -96,12 +114,12 @@ ruleTester.run("multiline-ternary", rule, {
         {
             code: "a ? (b\n? c\n: d) : e",
             errors: [{
-                message: testConsMsg,
+                message: expectedTestConsMsg,
                 line: 1,
                 column: 1
             },
             {
-                message: consAltMsg,
+                message: expectedConsAltMsg,
                 line: 1,
                 column: 6
             }]
@@ -109,12 +127,12 @@ ruleTester.run("multiline-ternary", rule, {
         {
             code: "a ?\n(b? c\n: d) : e",
             errors: [{
-                message: consAltMsg,
+                message: expectedConsAltMsg,
                 line: 2,
                 column: 2
             },
             {
-                message: testConsMsg,
+                message: expectedTestConsMsg,
                 line: 2,
                 column: 2
             }]
@@ -122,12 +140,12 @@ ruleTester.run("multiline-ternary", rule, {
         {
             code: "a ?\n(b\n? c : d) : e",
             errors: [{
-                message: consAltMsg,
+                message: expectedConsAltMsg,
                 line: 2,
                 column: 2
             },
             {
-                message: consAltMsg,
+                message: expectedConsAltMsg,
                 line: 3,
                 column: 3
             }]
@@ -135,9 +153,270 @@ ruleTester.run("multiline-ternary", rule, {
         {
             code: "a ?\n(b\n? c\n : d) : e",
             errors: [{
-                message: consAltMsg,
+                message: expectedConsAltMsg,
                 line: 2,
                 column: 2
+            }]
+        },
+
+        // "always"
+        {
+            code: "a ? b : c",
+            options: ["always"],
+            errors: [{
+                message: expectedTestConsMsg,
+                line: 1,
+                column: 1
+            },
+            {
+                message: expectedConsAltMsg,
+                line: 1,
+                column: 5
+            }]
+        },
+        {
+            code: "a\n? b : c",
+            options: ["always"],
+            errors: [{
+                message: expectedConsAltMsg,
+                line: 2,
+                column: 3
+            }]
+        },
+        {
+            code: "a ? b\n: c",
+            options: ["always"],
+            errors: [{
+                message: expectedTestConsMsg,
+                line: 1,
+                column: 1
+            }]
+        },
+        {
+            code: "a ? (b ? c : d) : e",
+            options: ["always"],
+            errors: [{
+                message: expectedTestConsMsg,
+                line: 1,
+                column: 1
+            },
+            {
+                message: expectedConsAltMsg,
+                line: 1,
+                column: 6
+            },
+            {
+                message: expectedTestConsMsg,
+                line: 1,
+                column: 6
+            },
+            {
+                message: expectedConsAltMsg,
+                line: 1,
+                column: 10
+            }]
+        },
+        {
+            code: "a ?\n(b ? c : d) :\ne",
+            options: ["always"],
+            errors: [{
+                message: expectedTestConsMsg,
+                line: 2,
+                column: 2
+            },
+            {
+                message: expectedConsAltMsg,
+                line: 2,
+                column: 6
+            }]
+        },
+        {
+            code: "a ? (b\n? c\n: d) : e",
+            options: ["always"],
+            errors: [{
+                message: expectedTestConsMsg,
+                line: 1,
+                column: 1
+            },
+            {
+                message: expectedConsAltMsg,
+                line: 1,
+                column: 6
+            }]
+        },
+        {
+            code: "a ?\n(b? c\n: d) : e",
+            options: ["always"],
+            errors: [{
+                message: expectedConsAltMsg,
+                line: 2,
+                column: 2
+            },
+            {
+                message: expectedTestConsMsg,
+                line: 2,
+                column: 2
+            }]
+        },
+        {
+            code: "a ?\n(b\n? c : d) : e",
+            options: ["always"],
+            errors: [{
+                message: expectedConsAltMsg,
+                line: 2,
+                column: 2
+            },
+            {
+                message: expectedConsAltMsg,
+                line: 3,
+                column: 3
+            }]
+        },
+        {
+            code: "a ?\n(b\n? c\n : d) : e",
+            options: ["always"],
+            errors: [{
+                message: expectedConsAltMsg,
+                line: 2,
+                column: 2
+            }]
+        },
+
+        // "never"
+        {
+            code: "a\n? b : c",
+            options: ["never"],
+            errors: [{
+                message: unexpectedTestConsMsg,
+                line: 1,
+                column: 1
+            }]
+        },
+        {
+            code: "a ? b\n: c",
+            options: ["never"],
+            errors: [{
+                message: unexpectedConsAltMsg,
+                line: 1,
+                column: 5
+            }]
+        },
+        {
+            code: "a ?\n(b ? c : d) :\ne",
+            options: ["never"],
+            errors: [{
+                message: unexpectedTestConsMsg,
+                line: 1,
+                column: 1
+            },
+            {
+                message: unexpectedConsAltMsg,
+                line: 2,
+                column: 2
+            }]
+        },
+        {
+            code: "a ? (b\n? c\n: d) : e",
+            options: ["never"],
+            errors: [{
+                message: unexpectedTestConsMsg,
+                line: 1,
+                column: 6
+            },
+            {
+                message: unexpectedConsAltMsg,
+                line: 2,
+                column: 3
+            }]
+        },
+        {
+            code: "a ?\n(b? c\n: d) : e",
+            options: ["never"],
+            errors: [{
+                message: unexpectedTestConsMsg,
+                line: 1,
+                column: 1
+            },
+            {
+                message: unexpectedConsAltMsg,
+                line: 2,
+                column: 5
+            }]
+        },
+        {
+            code: "a ?\n(b\n? c : d) : e",
+            options: ["never"],
+            errors: [{
+                message: unexpectedTestConsMsg,
+                line: 1,
+                column: 1
+            },
+            {
+                message: unexpectedTestConsMsg,
+                line: 2,
+                column: 2
+            }]
+        },
+        {
+            code: "a ?\n(b\n? c\n : d) : e",
+            options: ["never"],
+            errors: [{
+                message: unexpectedTestConsMsg,
+                line: 1,
+                column: 1
+            },
+            {
+                message: unexpectedTestConsMsg,
+                line: 2,
+                column: 2
+            },
+            {
+                message: unexpectedConsAltMsg,
+                line: 3,
+                column: 3
+            }]
+        },
+        {
+            code: "a ? (b\n? c\n: d)\n: e",
+            options: ["never"],
+            errors: [{
+                message: unexpectedConsAltMsg,
+                line: 1,
+                column: 6
+            },
+            {
+                message: unexpectedTestConsMsg,
+                line: 1,
+                column: 6
+            },
+            {
+                message: unexpectedConsAltMsg,
+                line: 2,
+                column: 3
+            }]
+        },
+        {
+            code: "a\n?\n(b\n?\nc\n:\nd)\n:\ne",
+            options: ["never"],
+            errors: [{
+                message: unexpectedTestConsMsg,
+                line: 1,
+                column: 1
+            },
+            {
+                message: unexpectedConsAltMsg,
+                line: 3,
+                column: 2
+            },
+            {
+                message: unexpectedTestConsMsg,
+                line: 3,
+                column: 2
+            },
+            {
+                message: unexpectedConsAltMsg,
+                line: 5,
+                column: 1
             }]
         }
     ]
