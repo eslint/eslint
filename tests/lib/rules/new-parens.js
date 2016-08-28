0c1f31dc45379c5e942ed741e0e148eca60fdbb0
Toru Nagashima <star.ctor@gmail.com>
2016-08-28 15:05:09 +0900

    Update: `new-parens` false negative (fixes #6997)

--- tests/lib/rules/new-parens.js
+++ tests/lib/rules/new-parens.js
@@ -21,9 +21,19 @@ const ruleTester = new RuleTester();
 ruleTester.run("new-parens", rule, {
     valid: [
         "var a = new Date();",
-        "var a = new Date(function() {});"
+        "var a = new Date(function() {});",
+        "var a = new (Date)();",
+        "var a = new ((Date))();",
+        "var a = (new Date());",
     ],
     invalid: [
-        { code: "var a = new Date;", errors: [{ message: "Missing '()' invoking a constructor.", type: "NewExpression"}] }
+        { code: "var a = new Date;", errors: [{ message: "Missing '()' invoking a constructor.", type: "NewExpression"}] },
+        { code: "var a = new Date", errors: [{ message: "Missing '()' invoking a constructor.", type: "NewExpression"}] },
+        { code: "var a = new (Date);", errors: [{ message: "Missing '()' invoking a constructor.", type: "NewExpression"}] },
+        { code: "var a = new (Date)", errors: [{ message: "Missing '()' invoking a constructor.", type: "NewExpression"}] },
+        { code: "var a = (new Date)", errors: [{ message: "Missing '()' invoking a constructor.", type: "NewExpression"}] },
+
+        // This `()` is `CallExpression`'s. This is a call of the result of `new Date`.
+        { code: "var a = (new Date)()", errors: [{ message: "Missing '()' invoking a constructor.", type: "NewExpression"}] },
     ]
 });
