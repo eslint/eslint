de7e7e00e8972c4a971cd1bdddfd6ca0861cd392
Jordan Harband <ljharb@gmail.com>
2016-09-13 08:28:09 -0700

    Update: `class-methods-use-this`: `exceptions` option (fixes #7085) (#7120)

--- tests/lib/rules/class-methods-use-this.js
+++ tests/lib/rules/class-methods-use-this.js
@@ -29,7 +29,8 @@ ruleTester.run("class-methods-use-this", rule, {
         {code: "class A { static foo() {} }", parserOptions: { ecmaVersion: 6 }},
         {code: "({ a(){} });", parserOptions: { ecmaVersion: 6 }},
         {code: "class A { foo() { () => this; } }", parserOptions: { ecmaVersion: 6 }},
-        {code: "({ a: function () {} });", parserOptions: { ecmaVersion: 6 }}
+        {code: "({ a: function () {} });", parserOptions: { ecmaVersion: 6 }},
+        {code: "class A { foo() {this} bar() {} }", parserOptions: { ecmaVersion: 6 }, options: [{ exceptMethods: ["bar"] }]}
     ],
     invalid: [
         {
@@ -79,6 +80,22 @@ ruleTester.run("class-methods-use-this", rule, {
             parserOptions: { ecmaVersion: 6 },
             errors: [
                 {type: "FunctionExpression", line: 1, column: 14, message: "Expected 'this' to be used by class method 'foo'."}
+            ]
+        },
+        {
+            code: "class A { foo() {} bar() {} }",
+            parserOptions: { ecmaVersion: 6 },
+            options: [{ exceptMethods: ["bar"] }],
+            errors: [
+                {type: "FunctionExpression", line: 1, column: 14, message: "Expected 'this' to be used by class method 'foo'."},
+            ]
+        },
+        {
+            code: "class A { foo() {} hasOwnProperty() {} }",
+            parserOptions: { ecmaVersion: 6 },
+            options: [{ exceptMethods: ["foo"] }],
+            errors: [
+                {type: "FunctionExpression", line: 1, column: 34, message: "Expected 'this' to be used by class method 'hasOwnProperty'."}
             ]
         }
     ]
