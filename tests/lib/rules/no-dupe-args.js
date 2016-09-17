37de86d9907c1ebf1359a23e2afb9a3655243dce
Teddy Katz <not-an-aardvark@users.noreply.github.com>
2016-09-16 20:21:49 -0400

    Upgrade: espree to 3.2.0, remove tests with SyntaxErrors (fixes #7169) (#7170)

--- tests/lib/rules/no-dupe-args.js
+++ tests/lib/rules/no-dupe-args.js
@@ -28,10 +28,6 @@ ruleTester.run("no-dupe-args", rule, {
     ],
     invalid: [
         { code: "function a(a, b, b) {}", errors: [{ message: "Duplicate param 'b'." }] },
-        { code: "function a({a, b}, b) {}", parserOptions: { ecmaVersion: 6 }, errors: [{ message: "Duplicate param 'b'." }] },
-        { code: "function a([a, b], b) {}", parserOptions: { ecmaVersion: 6 }, errors: [{ message: "Duplicate param 'b'." }] },
-        { code: "function a([ , a], [b, , a]) {}", parserOptions: { ecmaVersion: 6 }, errors: [{ message: "Duplicate param 'a'." }] },
-        { code: "function a([a, b], {b}) {}", parserOptions: { ecmaVersion: 6 }, errors: [{ message: "Duplicate param 'b'." }] },
         { code: "function a(a, a, a) {}", errors: [{ message: "Duplicate param 'a'." }] },
         { code: "function a(a, b, a) {}", errors: [{ message: "Duplicate param 'a'." }]},
         { code: "function a(a, b, a, b) {}", errors: [{ message: "Duplicate param 'a'." }, { message: "Duplicate param 'b'." }]},
