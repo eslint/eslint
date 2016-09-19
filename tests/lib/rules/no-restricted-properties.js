95c777ad86096f0423d8acbf242f1cf352d1ac22
Teddy Katz <not-an-aardvark@users.noreply.github.com>
2016-09-19 06:05:19 -0400

    Update: Make `no-restricted-properties` more flexible (fixes #7137) (#7139)

--- tests/lib/rules/no-restricted-properties.js
+++ tests/lib/rules/no-restricted-properties.js
@@ -75,6 +75,26 @@ ruleTester.run("no-restricted-properties", rule, {
                 object: "obj",
                 property: "foo"
             }]
+        }, {
+            code: "foo.bar",
+            options: [{
+                property: "baz"
+            }]
+        }, {
+            code: "foo.bar",
+            options: [{
+                object: "baz"
+            }]
+        }, {
+            code: "foo()",
+            options: [{
+                object: "foo"
+            }]
+        }, {
+            code: "foo;",
+            options: [{
+                object: "foo"
+            }]
         }
     ],
 
@@ -114,6 +134,69 @@ ruleTester.run("no-restricted-properties", rule, {
                 type: "MemberExpression"
             }, {
                 message: "'anotherObject.anotherDisallowedProperty' is restricted from being used.",
+                type: "MemberExpression"
+            }]
+        }, {
+            code: "foo.__proto__",
+            options: [{
+                property: "__proto__",
+                message: "Please use Object.getPrototypeOf instead."
+            }],
+            errors: [{
+                message: "'__proto__' is restricted from being used. Please use Object.getPrototypeOf instead.",
+                type: "MemberExpression"
+            }]
+        }, {
+            code: "foo['__proto__']",
+            options: [{
+                property: "__proto__",
+                message: "Please use Object.getPrototypeOf instead."
+            }],
+            errors: [{
+                message: "'__proto__' is restricted from being used. Please use Object.getPrototypeOf instead.",
+                type: "MemberExpression"
+            }]
+        }, {
+            code: "foo.bar.baz;",
+            options: [{ object: "foo" }],
+            errors: [{ message: "'foo.bar' is restricted from being used.", type: "MemberExpression" }]
+        }, {
+            code: "foo.bar();",
+            options: [{ object: "foo" }],
+            errors: [{ message: "'foo.bar' is restricted from being used.", type: "MemberExpression" }]
+        }, {
+            code: "foo.bar.baz();",
+            options: [{ object: "foo" }],
+            errors: [{ message: "'foo.bar' is restricted from being used.", type: "MemberExpression" }]
+        }, {
+            code: "foo.bar.baz;",
+            options: [{ property: "bar" }],
+            errors: [{ message: "'bar' is restricted from being used.", type: "MemberExpression" }]
+        }, {
+            code: "foo.bar();",
+            options: [{ property: "bar" }],
+            errors: [{ message: "'bar' is restricted from being used.", type: "MemberExpression" }]
+        }, {
+            code: "foo.bar.baz();",
+            options: [{ property: "bar" }],
+            errors: [{ message: "'bar' is restricted from being used.", type: "MemberExpression" }]
+        }, {
+            code: "require.call({}, 'foo')",
+            options: [{
+                object: "require",
+                message: "Please call require() directly."
+            }],
+            errors: [{
+                message: "'require.call' is restricted from being used. Please call require() directly.",
+                type: "MemberExpression"
+            }]
+        }, {
+            code: "require['resolve']",
+            options: [{
+                object: "require"
+            }],
+            errors: [{
+                message: "'require.resolve' is restricted from being used.",
                 type: "MemberExpression"
             }]
         }
