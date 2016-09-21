68a23167b1beaa3edc199963d22e60593252964c
Teddy Katz <teddy.katz@gmail.com>
2016-09-20 23:17:38 -0400

    Update: add fixer for `no-lonely-if`

--- tests/lib/rules/no-lonely-if.js
+++ tests/lib/rules/no-lonely-if.js
@@ -16,6 +16,7 @@ const rule = require("../../../lib/rules/no-lonely-if"),
 //------------------------------------------------------------------------------
 
 const ruleTester = new RuleTester();
+const errors = [{ message: "Unexpected if as the only statement in an else block.", type: "IfStatement" }];
 
 ruleTester.run("no-lonely-if", rule, {
 
@@ -26,11 +27,105 @@ ruleTester.run("no-lonely-if", rule, {
     ],
 
     // Examples of code that should trigger the rule
-    invalid: [{
-        code: "if (a) {;} else { if (b) {;} }",
-        errors: [{
-            message: "Unexpected if as the only statement in an else block.",
-            type: "IfStatement"
-        }]
-    }]
+    invalid: [
+        {
+            code: "if (a) {;} else { if (b) {;} }",
+            output: "if (a) {;} else if (b) {;}",
+            errors
+        },
+        {
+            code:
+            "if (a) {\n" +
+            "  foo();\n" +
+            "} else {\n" +
+            "  if (b) {\n" +
+            "    bar();\n" +
+            "  }\n" +
+            "}",
+            output:
+            "if (a) {\n" +
+            "  foo();\n" +
+            "} else if (b) {\n" +
+            "    bar();\n" +
+            "  }",
+            errors
+        },
+        {
+            code:
+            "if (a) {\n" +
+            "  foo();\n" +
+            "} else /* comment */ {\n" +
+            "  if (b) {\n" +
+            "    bar();\n" +
+            "  }\n" +
+            "}",
+            output:
+            "if (a) {\n" +
+            "  foo();\n" +
+            "} else if (b) /* comment */ {\n" +
+            "    bar();\n" +
+            "  }",
+            errors
+        },
+        {
+            code:
+            "if (a) {\n" +
+            "  foo();\n" +
+            "} else {\n" +
+            "  /* otherwise, do the other thing */ if (b) {\n" +
+            "    bar();\n" +
+            "  }\n" +
+            "}",
+            output: // Not fixed, comment interferes
+            "if (a) {\n" +
+            "  foo();\n" +
+            "} else {\n" +
+            "  /* otherwise, do the other thing */ if (b) {\n" +
+            "    bar();\n" +
+            "  }\n" +
+            "}",
+            errors
+        },
+        {
+            code:
+            "if (a) {\n" +
+            "  foo();\n" +
+            "} else {\n" +
+            "  if /* this comment is ok */ (b) {\n" +
+            "    bar();\n" +
+            "  }\n" +
+            "}",
+            output:
+            "if (a) {\n" +
+            "  foo();\n" +
+            "} else if /* this comment is ok */ (b) {\n" +
+            "    bar();\n" +
+            "  }",
+            errors
+        },
+        {
+            code:
+            "if (a) {\n" +
+            "  foo();\n" +
+            "} else {\n" +
+            "  if (b) {\n" +
+            "    bar();\n" +
+            "  } /* this comment will prevent this test case from being autofixed. */\n" +
+            "}",
+            output:
+            "if (a) {\n" +
+            "  foo();\n" +
+            "} else {\n" +
+            "  if (b) {\n" +
+            "    bar();\n" +
+            "  } /* this comment will prevent this test case from being autofixed. */\n" +
+            "}",
+            errors
+        },
+        {
+            code: "if (foo) {} else { if (bar) baz(); }",
+            output: "if (foo) {} else if (bar) {baz();}",
+            errors
+        }
+    ]
 });
