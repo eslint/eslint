17fd71d9327f41a84dc06a620c4d142ea7517819
Teddy Katz <not-an-aardvark@users.noreply.github.com>
2016-09-30 00:04:22 -0400

    Update: add fixer for `wrap-iife` (#7196)

--- tests/lib/rules/wrap-iife.js
+++ tests/lib/rules/wrap-iife.js
@@ -49,29 +49,49 @@ ruleTester.run("wrap-iife", rule, {
     invalid: [
         {
             code: "0, function(){ }();",
+            output: "0, (function(){ }());",
             errors: [{ message: "Wrap an immediate function invocation in parentheses.", type: "CallExpression"}]
         },
         {
             code: "[function(){ }()];",
+            output: "[(function(){ }())];",
             errors: [{ message: "Wrap an immediate function invocation in parentheses.", type: "CallExpression"}]
         },
         {
             code: "var a = function(){ }();",
+            output: "var a = (function(){ }());",
             errors: [{ message: "Wrap an immediate function invocation in parentheses.", type: "CallExpression"}]
         },
         {
             code: "(function(){ }(), 0);",
+            output: "((function(){ }()), 0);",
             errors: [{ message: "Wrap an immediate function invocation in parentheses.", type: "CallExpression"}]
         },
         {
             code: "(function a(){ })();",
+            output: "(function a(){ }());",
             options: ["outside"],
             errors: [{ message: "Move the invocation into the parens that contain the function.", type: "CallExpression" }]
         },
         {
             code: "(function a(){ }());",
+            output: "(function a(){ })();",
             options: ["inside"],
             errors: [{ message: "Wrap only the function expression in parens.", type: "CallExpression" }]
+        },
+        {
+
+            // Ensure all comments get preserved when autofixing.
+            code: "( /* a */ function /* b */ foo /* c */ ( /* d */ bar /* e */ ) /* f */ { /* g */ return; /* h */ } /* i */ ( /* j */ baz /* k */) /* l */ ) /* m */ ;",
+            output: "( /* a */ function /* b */ foo /* c */ ( /* d */ bar /* e */ ) /* f */ { /* g */ return; /* h */ }) /* i */ ( /* j */ baz /* k */) /* l */  /* m */ ;",
+            options: ["inside"],
+            errors: [{ message: "Wrap only the function expression in parens.", type: "CallExpression" }]
+        },
+        {
+            code: "( /* a */ function /* b */ foo /* c */ ( /* d */ bar /* e */ ) /* f */ { /* g */ return; /* h */ } /* i */ ) /* j */ ( /* k */ baz /* l */) /* m */ ;",
+            output: "( /* a */ function /* b */ foo /* c */ ( /* d */ bar /* e */ ) /* f */ { /* g */ return; /* h */ } /* i */  /* j */ ( /* k */ baz /* l */)) /* m */ ;",
+            options: ["outside"],
+            errors: [{ message: "Move the invocation into the parens that contain the function.", type: "CallExpression" }]
         }
     ]
 });
