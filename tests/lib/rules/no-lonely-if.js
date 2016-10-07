a12d1a97e668255e5653d9eefd156164ce2e3340
Teddy Katz <not-an-aardvark@users.noreply.github.com>
2016-10-07 12:56:26 -0400

    Update: add fixer for `no-lonely-if` (#7202)
    * Update: add fixer for `no-lonely-if`
    
    * (squash) Don't do a fix if it would change semantics due to ASI

--- tests/lib/rules/no-lonely-if.js
+++ tests/lib/rules/no-lonely-if.js
@@ -16,6 +16,7 @@ const rule = require("../../../lib/rules/no-lonely-if"),
 //------------------------------------------------------------------------------
 
 const ruleTester = new RuleTester();
+const errors = [{ message: "Unexpected if as the only statement in an else block.", type: "IfStatement" }];
 
 ruleTester.run("no-lonely-if", rule, {
 
@@ -26,11 +27,213 @@ ruleTester.run("no-lonely-if", rule, {
     ],
 
     // Examples of code that should trigger the rule
-    invalid: [{
-        code: "if (a) {;} else { if (b) {;} }",
-        errors: [{
-            message: "Unexpected if as the only statement in an else block.",
-            type: "IfStatement"
-        }]
-    }]
+    invalid: [
+        {
+            code: "if (a) {;} else { if (b) {;} }",
+            output: "if (a) {;} else if (b) {;}",
+            errors
+        },
+        {
+            code:
+            "if (a) {\n" +
+            "  foo();\n" +
+            "} else {\n" +
+            "  if (b) {\n" +
+            "    bar();\n" +
+            "  }\n" +
+            "}",
+            output:
+            "if (a) {\n" +
+            "  foo();\n" +
+            "} else if (b) {\n" +
+            "    bar();\n" +
+            "  }",
+            errors
+        },
+        {
+            code:
+            "if (a) {\n" +
+            "  foo();\n" +
+            "} else /* comment */ {\n" +
+            "  if (b) {\n" +
+            "    bar();\n" +
+            "  }\n" +
+            "}",
+            output:
+            "if (a) {\n" +
+            "  foo();\n" +
+            "} else /* comment */ if (b) {\n" +
+            "    bar();\n" +
+            "  }",
+            errors
+        },
+        {
+            code:
+            "if (a) {\n" +
+            "  foo();\n" +
+            "} else {\n" +
+            "  /* otherwise, do the other thing */ if (b) {\n" +
+            "    bar();\n" +
+            "  }\n" +
+            "}",
+            output: // Not fixed, comment interferes
+            "if (a) {\n" +
+            "  foo();\n" +
+            "} else {\n" +
+            "  /* otherwise, do the other thing */ if (b) {\n" +
+            "    bar();\n" +
+            "  }\n" +
+            "}",
+            errors
+        },
+        {
+            code:
+            "if (a) {\n" +
+            "  foo();\n" +
+            "} else {\n" +
+            "  if /* this comment is ok */ (b) {\n" +
+            "    bar();\n" +
+            "  }\n" +
+            "}",
+            output:
+            "if (a) {\n" +
+            "  foo();\n" +
+            "} else if /* this comment is ok */ (b) {\n" +
+            "    bar();\n" +
+            "  }",
+            errors
+        },
+        {
+            code:
+            "if (a) {\n" +
+            "  foo();\n" +
+            "} else {\n" +
+            "  if (b) {\n" +
+            "    bar();\n" +
+            "  } /* this comment will prevent this test case from being autofixed. */\n" +
+            "}",
+            output:
+            "if (a) {\n" +
+            "  foo();\n" +
+            "} else {\n" +
+            "  if (b) {\n" +
+            "    bar();\n" +
+            "  } /* this comment will prevent this test case from being autofixed. */\n" +
+            "}",
+            errors
+        },
+        {
+            code: "if (foo) {} else { if (bar) baz(); }",
+            output: "if (foo) {} else if (bar) baz();",
+            errors
+        },
+        {
+
+            // Not fixed; removing the braces would cause a SyntaxError.
+            code: "if (foo) {} else { if (bar) baz() } qux();",
+            output: "if (foo) {} else { if (bar) baz() } qux();",
+            errors
+        },
+        {
+
+            // This is fixed because there is a semicolon after baz().
+            code: "if (foo) {} else { if (bar) baz(); } qux();",
+            output: "if (foo) {} else if (bar) baz(); qux();",
+            errors
+        },
+        {
+
+            // Not fixed; removing the braces would change the semantics due to ASI.
+            code:
+            "if (foo) {\n" +
+            "} else {\n" +
+            "  if (bar) baz()\n" +
+            "}\n" +
+            "[1, 2, 3].forEach(foo);",
+            output:
+            "if (foo) {\n" +
+            "} else {\n" +
+            "  if (bar) baz()\n" +
+            "}\n" +
+            "[1, 2, 3].forEach(foo);",
+            errors
+        },
+        {
+
+            // Not fixed; removing the braces would change the semantics due to ASI.
+            code:
+            "if (foo) {\n" +
+            "} else {\n" +
+            "  if (bar) baz++\n" +
+            "}\n" +
+            "foo;",
+            output:
+            "if (foo) {\n" +
+            "} else {\n" +
+            "  if (bar) baz++\n" +
+            "}\n" +
+            "foo;",
+            errors
+        },
+        {
+
+            // This is fixed because there is a semicolon after baz++
+            code:
+            "if (foo) {\n" +
+            "} else {\n" +
+            "  if (bar) baz++;\n" +
+            "}\n" +
+            "foo;",
+            output:
+            "if (foo) {\n" +
+            "} else if (bar) baz++;\n" +
+            "foo;",
+            errors
+        },
+        {
+
+            // Not fixed; bar() would be interpreted as a template literal tag
+            code:
+            "if (a) {\n" +
+            "  foo();\n" +
+            "} else {\n" +
+            "  if (b) bar()\n" +
+            "}\n" +
+            "`template literal`;",
+            output:
+            "if (a) {\n" +
+            "  foo();\n" +
+            "} else {\n" +
+            "  if (b) bar()\n" +
+            "}\n" +
+            "`template literal`;",
+            parserOptions: { ecmaVersion: 6 },
+            errors
+        },
+        {
+            code:
+            "if (a) {\n" +
+            "  foo();\n" +
+            "} else {\n" +
+            "  if (b) {\n" +
+            "    bar();\n" +
+            "  } else if (c) {\n" +
+            "    baz();\n" +
+            "  } else {\n" +
+            "    qux();\n" +
+            "  }\n" +
+            "}",
+            output:
+            "if (a) {\n" +
+            "  foo();\n" +
+            "} else if (b) {\n" +
+            "    bar();\n" +
+            "  } else if (c) {\n" +
+            "    baz();\n" +
+            "  } else {\n" +
+            "    qux();\n" +
+            "  }",
+            errors
+        }
+    ]
 });
