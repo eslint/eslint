c2a7698501d9ddbc2575854f47555f1bc300b403
Teddy Katz <not-an-aardvark@users.noreply.github.com>
2016-10-10 17:57:02 -0400

    Update: add fixer for `prefer-spread` (#7283)

--- tests/lib/rules/prefer-spread.js
+++ tests/lib/rules/prefer-spread.js
@@ -16,7 +16,7 @@ const RuleTester = require("../../../lib/testers/rule-tester");
 // Tests
 //------------------------------------------------------------------------------
 
-const errors = [{message: "use the spread operator instead of the '.apply()'.", type: "CallExpression"}];
+const errors = [{message: "Use the spread operator instead of '.apply()'.", type: "CallExpression"}];
 
 const ruleTester = new RuleTester();
 
@@ -38,16 +38,60 @@ ruleTester.run("prefer-spread", rule, {
 
         // ignores incomplete things.
         {code: "foo.apply();"},
-        {code: "obj.foo.apply();"}
+        {code: "obj.foo.apply();"},
+        {
+            code: "obj.foo.apply(obj, ...args)",
+            parserOptions: {ecmaVersion: 6}
+        }
     ],
     invalid: [
-        {code: "foo.apply(undefined, args);", errors},
-        {code: "foo.apply(void 0, args);", errors},
-        {code: "foo.apply(null, args);", errors},
-        {code: "obj.foo.apply(obj, args);", errors},
-        {code: "a.b.c.foo.apply(a.b.c, args);", errors},
-        {code: "a.b(x, y).c.foo.apply(a.b(x, y).c, args);", errors},
-        {code: "[].concat.apply([ ], args);", errors},
-        {code: "[].concat.apply([\n/*empty*/\n], args);", errors}
+        {
+            code: "foo.apply(undefined, args);",
+            output: "foo(...args);",
+            errors
+        },
+        {
+            code: "foo.apply(void 0, args);",
+            output: "foo(...args);",
+            errors
+        },
+        {
+            code: "foo.apply(null, args);",
+            output: "foo(...args);",
+            errors
+        },
+        {
+            code: "obj.foo.apply(obj, args);",
+            output: "obj.foo(...args);",
+            errors
+        },
+        {
+
+            // Not fixed: a.b.c might activate getters
+            code: "a.b.c.foo.apply(a.b.c, args);",
+            output: "a.b.c.foo.apply(a.b.c, args);",
+            errors
+        },
+        {
+
+            // Not fixed: a.b(x, y).c might activate getters
+            code: "a.b(x, y).c.foo.apply(a.b(x, y).c, args);",
+            output: "a.b(x, y).c.foo.apply(a.b(x, y).c, args);",
+            errors
+        },
+        {
+
+            // Not fixed (not an identifier)
+            code: "[].concat.apply([ ], args);",
+            output: "[].concat.apply([ ], args);",
+            errors
+        },
+        {
+
+            // Not fixed (not an identifier)
+            code: "[].concat.apply([\n/*empty*/\n], args);",
+            output: "[].concat.apply([\n/*empty*/\n], args);",
+            errors
+        }
     ]
 });
