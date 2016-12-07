eebf40a67a445374b10cfa772a8e8f68de091478
Teddy Katz <teddy.katz@gmail.com>
2016-12-07 15:10:52 -0500

    Fix: Allow duplicated let declarations in `prefer-const` (fixes #7712)
    Due to https://github.com/ternjs/acorn/issues/487, espree does not throw a syntax error for duplicate `let` or `const` declarations. Previously, the `prefer-const` rule assumed that a `let` variable would always have exactly one declaration. This adds a sanity check to avoid reporting (and possibly creating incorrect autofixes) a `let` variable if it has more than one declaration.

--- tests/lib/rules/prefer-const.js
+++ tests/lib/rules/prefer-const.js
@@ -94,6 +94,14 @@ ruleTester.run("prefer-const", rule, {
             options: [{ignoreReadBeforeAssign: true}]
         },
 
+        // https://github.com/eslint/eslint/issues/7712
+        // https://github.com/ternjs/acorn/issues/487
+        // This should be a SyntaxError, but espree parses it correctly. Don't throw an error if the variable has multiple declarations.
+        {
+            code: "let foo; const foo = 1;",
+            parserOptions: {ecmaVersion: 6},
+            errors: [{ message: "'foo' is never reassigned. Use 'const' instead.", type: "Identifier" }]
+        }
 
     ],
     invalid: [
