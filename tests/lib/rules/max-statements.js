f398d185885ae17b920a0a375ab91cbc896b0566
Dieter Luypaert <dieterluypaert@gmail.com>
2017-02-23 19:39:20 +0100

    Update: include function name in report message if possible (fixes #7260) (#8058)

--- tests/lib/rules/max-statements.js
+++ tests/lib/rules/max-statements.js
@@ -36,24 +36,101 @@ ruleTester.run("max-statements", rule, {
         { code: "var foo = { thing: function() { var bar = 1; var baz = 2; } }", options: [{ max: 2 }] }
     ],
     invalid: [
-        { code: "function foo() { var bar = 1; var baz = 2; var qux = 3; }", options: [2], errors: [{ message: "Function 'foo' has too many statements (3). Maximum allowed is 2." }] },
-        { code: "var foo = () => { var bar = 1; var baz = 2; var qux = 3; };", options: [2], parserOptions: { ecmaVersion: 6 }, errors: [{ message: "This function has too many statements (3). Maximum allowed is 2." }] },
-        { code: "var foo = function() { var bar = 1; var baz = 2; var qux = 3; };", options: [2], errors: [{ message: "This function has too many statements (3). Maximum allowed is 2." }] },
-        { code: "function foo() { var bar = 1; if (true) { while (false) { var qux = null; } } return 3; }", options: [4], errors: [{ message: "Function 'foo' has too many statements (5). Maximum allowed is 4." }] },
-        { code: "function foo() { var bar = 1; if (true) { for (;;) { var qux = null; } } return 3; }", options: [4], errors: [{ message: "Function 'foo' has too many statements (5). Maximum allowed is 4." }] },
-        { code: "function foo() { var bar = 1; if (true) { for (;;) { var qux = null; } } else { quxx(); } return 3; }", options: [5], errors: [{ message: "Function 'foo' has too many statements (6). Maximum allowed is 5." }] },
-        { code: "function foo() { var x = 5; function bar() { var y = 6; } bar(); z = 10; baz(); }", options: [3], errors: [{ message: "Function 'foo' has too many statements (5). Maximum allowed is 3." }] },
-        { code: "function foo() { var x = 5; function bar() { var y = 6; } bar(); z = 10; baz(); }", options: [4], errors: [{ message: "Function 'foo' has too many statements (5). Maximum allowed is 4." }] },
-        { code: ";(function() { var bar = 1; return function () { var z; return 42; }; })()", options: [1, { ignoreTopLevelFunctions: true }], errors: [{ message: "This function has too many statements (2). Maximum allowed is 1." }] },
-        { code: ";(function() { var bar = 1; var baz = 2; })(); (function() { var bar = 1; var baz = 2; })()", options: [1, { ignoreTopLevelFunctions: true }], errors: [{ message: "This function has too many statements (2). Maximum allowed is 1." }, { message: "This function has too many statements (2). Maximum allowed is 1." }] },
-        { code: "define(['foo', 'qux'], function(foo, qux) { var bar = 1; var baz = 2; return function () { var z; return 42; }; })", options: [1, { ignoreTopLevelFunctions: true }], errors: [{ message: "This function has too many statements (2). Maximum allowed is 1." }] },
-        { code: "function foo() { var a; var b; var c; var x; var y; var z; bar(); baz(); qux(); quxx(); foo(); }", errors: [{ message: "Function 'foo' has too many statements (11). Maximum allowed is 10." }] },
+        {
+            code: "function foo() { var bar = 1; var baz = 2; var qux = 3; }",
+            options: [2],
+            errors: [{ message: "Function 'foo' has too many statements (3). Maximum allowed is 2." }]
+        },
+        {
+            code: "var foo = () => { var bar = 1; var baz = 2; var qux = 3; };",
+            options: [2],
+            parserOptions: { ecmaVersion: 6 },
+            errors: [{ message: "Arrow function has too many statements (3). Maximum allowed is 2." }]
+        },
+        {
+            code: "var foo = function() { var bar = 1; var baz = 2; var qux = 3; };",
+            options: [2],
+            errors: [{ message: "Function has too many statements (3). Maximum allowed is 2." }]
+        },
+        {
+            code: "function foo() { var bar = 1; if (true) { while (false) { var qux = null; } } return 3; }",
+            options: [4],
+            errors: [{ message: "Function 'foo' has too many statements (5). Maximum allowed is 4." }]
+        },
+        {
+            code: "function foo() { var bar = 1; if (true) { for (;;) { var qux = null; } } return 3; }",
+            options: [4],
+            errors: [{ message: "Function 'foo' has too many statements (5). Maximum allowed is 4." }]
+        },
+        {
+            code: "function foo() { var bar = 1; if (true) { for (;;) { var qux = null; } } else { quxx(); } return 3; }",
+            options: [5],
+            errors: [{ message: "Function 'foo' has too many statements (6). Maximum allowed is 5." }]
+        },
+        {
+            code: "function foo() { var x = 5; function bar() { var y = 6; } bar(); z = 10; baz(); }",
+            options: [3],
+            errors: [{ message: "Function 'foo' has too many statements (5). Maximum allowed is 3." }]
+        },
+        {
+            code: "function foo() { var x = 5; function bar() { var y = 6; } bar(); z = 10; baz(); }",
+            options: [4],
+            errors: [{ message: "Function 'foo' has too many statements (5). Maximum allowed is 4." }]
+        },
+        {
+            code: ";(function() { var bar = 1; return function () { var z; return 42; }; })()",
+            options: [1, { ignoreTopLevelFunctions: true }],
+            errors: [{ message: "Function has too many statements (2). Maximum allowed is 1." }]
+        },
+        {
+            code: ";(function() { var bar = 1; var baz = 2; })(); (function() { var bar = 1; var baz = 2; })()",
+            options: [1, { ignoreTopLevelFunctions: true }],
+            errors: [
+                { message: "Function has too many statements (2). Maximum allowed is 1." },
+                { message: "Function has too many statements (2). Maximum allowed is 1." }
+            ]
+        },
+        {
+            code: "define(['foo', 'qux'], function(foo, qux) { var bar = 1; var baz = 2; return function () { var z; return 42; }; })",
+            options: [1, { ignoreTopLevelFunctions: true }],
+            errors: [{ message: "Function has too many statements (2). Maximum allowed is 1." }]
+        },
+        {
+            code: "function foo() { var a; var b; var c; var x; var y; var z; bar(); baz(); qux(); quxx(); foo(); }",
+            errors: [{ message: "Function 'foo' has too many statements (11). Maximum allowed is 10." }]
+        },
 
         // object property options
-        { code: "var foo = { thing: function() { var bar = 1; var baz = 2; var baz2; } }", options: [2], errors: [{ message: "Function 'thing' has too many statements (3). Maximum allowed is 2." }] },
-        { code: "var foo = { thing() { var bar = 1; var baz = 2; var baz2; } }", options: [2], parserOptions: { ecmaVersion: 6 }, errors: [{ message: "Function 'thing' has too many statements (3). Maximum allowed is 2." }] },
-        { code: "var foo = { ['thing']() { var bar = 1; var baz = 2; var baz2; } }", options: [2], parserOptions: { ecmaVersion: 6 }, errors: [{ message: "Function ''thing'' has too many statements (3). Maximum allowed is 2." }] },
-        { code: "var foo = { thing: () => { var bar = 1; var baz = 2; var baz2; } }", options: [2], parserOptions: { ecmaVersion: 6 }, errors: [{ message: "Function 'thing' has too many statements (3). Maximum allowed is 2." }] },
-        { code: "var foo = { thing: function() { var bar = 1; var baz = 2; var baz2; } }", options: [{ max: 2 }], errors: [{ message: "Function 'thing' has too many statements (3). Maximum allowed is 2." }] }
+        {
+            code: "var foo = { thing: function() { var bar = 1; var baz = 2; var baz2; } }",
+            options: [2],
+            errors: [{ message: "Method 'thing' has too many statements (3). Maximum allowed is 2." }]
+        },
+        {
+            code: "var foo = { thing() { var bar = 1; var baz = 2; var baz2; } }",
+            options: [2],
+            parserOptions: { ecmaVersion: 6 },
+            errors: [{ message: "Method 'thing' has too many statements (3). Maximum allowed is 2." }]
+        },
+
+        // TODO decide if we want this or not
+        // {
+        //     code: "var foo = { ['thing']() { var bar = 1; var baz = 2; var baz2; } }",
+        //     options: [2],
+        //     parserOptions: { ecmaVersion: 6 },
+        //     errors: [{ message: "Method ''thing'' has too many statements (3). Maximum allowed is 2." }]
+        // },
+
+        {
+            code: "var foo = { thing: () => { var bar = 1; var baz = 2; var baz2; } }",
+            options: [2],
+            parserOptions: { ecmaVersion: 6 },
+            errors: [{ message: "Arrow function 'thing' has too many statements (3). Maximum allowed is 2." }]
+        },
+        {
+            code: "var foo = { thing: function() { var bar = 1; var baz = 2; var baz2; } }",
+            options: [{ max: 2 }],
+            errors: [{ message: "Method 'thing' has too many statements (3). Maximum allowed is 2." }]
+        }
     ]
 });
