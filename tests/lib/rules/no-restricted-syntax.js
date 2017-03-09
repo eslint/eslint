e7a52b1a490fd3bfdfca00cb6df0b71fa7bfde0d
Teddy Katz <teddy.katz@gmail.com>
2017-03-08 20:03:39 -0500

    Allow no-restricted-syntax to handle query selectors as options

--- tests/lib/rules/no-restricted-syntax.js
+++ tests/lib/rules/no-restricted-syntax.js
@@ -21,7 +21,12 @@ ruleTester.run("no-restricted-syntax", rule, {
     valid: [
         { code: "doSomething();" },
         { code: "var foo = 42;", options: ["ConditionalExpression"] },
-        { code: "foo += 42;", options: ["VariableDeclaration", "FunctionExpression"] }
+        { code: "foo += 42;", options: ["VariableDeclaration", "FunctionExpression"] },
+        { code: "foo;", options: ["Identifier[name=\"bar\"]"] },
+        { code: "() => 5", options: ["ArrowFunctionExpression > BlockStatement"], parserOptions: { ecmaVersion: 6 } },
+        { code: "({ foo: 1, bar: 2 })", options: ["Property > Literal.key"] },
+        { code: "A: for (;;) break;", options: ["BreakStatement[label]"] },
+        { code: "function foo(bar, baz) {}", options: ["FunctionDeclaration[params.length>2]"] }
     ],
     invalid: [
         {
@@ -43,6 +48,40 @@ ruleTester.run("no-restricted-syntax", rule, {
                 { message: "Using 'CatchClause' is not allowed.", type: "CatchClause" },
                 { message: "Using 'CallExpression' is not allowed.", type: "CallExpression" }
             ]
-        }
+        },
+        {
+            code: "bar;",
+            options: ["Identifier[name=\"bar\"]"],
+            errors: [{ message: "Using 'Identifier[name=\"bar\"]' is not allowed.", type: "Identifier" }]
+        },
+        {
+            code: "bar;",
+            options: ["Identifier", "Identifier[name=\"bar\"]"],
+            errors: [
+                { message: "Using 'Identifier' is not allowed.", type: "Identifier" },
+                { message: "Using 'Identifier[name=\"bar\"]' is not allowed.", type: "Identifier" }
+            ]
+        },
+        {
+            code: "() => {}",
+            parserOptions: { ecmaVersion: 6 },
+            options: ["ArrowFunctionExpression > BlockStatement"],
+            errors: [{ message: "Using 'ArrowFunctionExpression > BlockStatement' is not allowed.", type: "BlockStatement" }]
+        },
+        {
+            code: "({ foo: 1, 'bar': 2 })",
+            options: ["Property > Literal.key"],
+            errors: [{ message: "Using 'Property > Literal.key' is not allowed.", type: "Literal" }]
+        },
+        {
+            code: "A: for (;;) break A;",
+            options: ["BreakStatement[label]"],
+            errors: [{ message: "Using 'BreakStatement[label]' is not allowed.", type: "BreakStatement" }]
+        },
+        {
+            code: "function foo(bar, baz, qux) {}",
+            options: ["FunctionDeclaration[params.length>2]"],
+            errors: [{ message: "Using 'FunctionDeclaration[params.length>2]' is not allowed.", type: "FunctionDeclaration" }]
+        },
     ]
 });
