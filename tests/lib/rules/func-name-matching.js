6d12fc903ee93b138f6af1c27a0594f5d0ce9df9
Teddy Katz <teddy.katz@gmail.com>
2017-03-15 22:50:34 -0400

    Fix: func-name-matching crash on destructuring assignment to functions (#8247)
    The func-name-matching rule checks for assignments to function expressions so that it can determine whether names match. However, due to a missing type-check, the rule would crash when run on a destructuring assignment to a function expression. This commit updates the rule to ignore destructuring assignments to function expressions.

--- tests/lib/rules/func-name-matching.js
+++ tests/lib/rules/func-name-matching.js
@@ -145,6 +145,38 @@ ruleTester.run("func-name-matching", rule, {
         {
             code: "({[{x: 1}]: function foo() {}})",
             parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "[] = function foo() {}",
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "({} = function foo() {})",
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "[a] = function foo() {}",
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "({a} = function foo() {})",
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "var [] = function foo() {}",
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "var {} = function foo() {}",
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "var [a] = function foo() {}",
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "var {a} = function foo() {}",
+            parserOptions: { ecmaVersion: 6 }
         }
     ],
     invalid: [
