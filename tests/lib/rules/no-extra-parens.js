eef2770dbeb4b30b9b42a428d183fe77c382b8d7
Toru Nagashima <star.ctor@gmail.com>
2017-03-18 01:58:22 +0900

    Fix: fix for double parens

--- tests/lib/rules/no-extra-parens.js
+++ tests/lib/rules/no-extra-parens.js
@@ -392,6 +392,10 @@ ruleTester.run("no-extra-parens", rule, {
             }
         },
         {
+            code: "var [x = (1, foo)] = bar",
+            parserOptions: { ecmaVersion: 2015 }
+        },
+        {
             code: "class A extends B {}",
             parserOptions: { ecmaVersion: 2015 }
         },
@@ -894,6 +898,25 @@ ruleTester.run("no-extra-parens", rule, {
             "let a = {...(b)}",
             "let a = {...b}",
             "Identifier",
+            1,
+            {
+                parserOptions: {
+                    ecmaVersion: 2015,
+                    ecmaFeatures: { experimentalObjectRestSpread: true }
+                }
+            }
+        ),
+        invalid(
+            "let a = [...((b, c))]",
+            "let a = [...(b, c)]",
+            "SequenceExpression",
+            1,
+            { parserOptions: { ecmaVersion: 2015 } }
+        ),
+        invalid(
+            "let a = {...((b, c))}",
+            "let a = {...(b, c)}",
+            "SequenceExpression",
             1,
             {
                 parserOptions: {
