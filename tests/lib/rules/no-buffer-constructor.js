a08d83d742590a85e7ecb912a688d51edb1c63cc
Teddy Katz <teddy.katz@gmail.com>
2017-04-05 00:11:00 -0400

    New: no-buffer-constructor rule (fixes #5614)

---
+++ tests/lib/rules/no-buffer-constructor.js
@@ -0,0 +1,58 @@
+/**
+ * @fileoverview disallow use of the Buffer() constructor
+ * @author Teddy Katz
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/no-buffer-constructor");
+const RuleTester = require("../../../lib/testers/rule-tester");
+
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const CALL_ERROR = {
+    message: "Buffer() is deprecated. Use Buffer.from(), Buffer.alloc(), or Buffer.allocUnsafe() instead.",
+    type: "CallExpression"
+};
+const CONSTRUCT_ERROR = {
+    message: "new Buffer() is deprecated. Use Buffer.from(), Buffer.alloc(), or Buffer.allocUnsafe() instead.",
+    type: "NewExpression"
+};
+
+const ruleTester = new RuleTester();
+
+ruleTester.run("no-buffer-constructor", rule, {
+
+    valid: [
+        "Buffer.alloc(5)",
+        "Buffer.allocUnsafe(5)",
+        "new Buffer.Foo()",
+        "Buffer.from([1, 2, 3])",
+        "foo(Buffer)"
+    ],
+
+    invalid: [
+        {
+            code: "Buffer(5)",
+            errors: [CALL_ERROR]
+        },
+        {
+            code: "new Buffer(5)",
+            errors: [CONSTRUCT_ERROR]
+        },
+        {
+            code: "Buffer([1, 2, 3])",
+            errors: [CALL_ERROR]
+        },
+        {
+            code: "new Buffer([1, 2, 3])",
+            errors: [CONSTRUCT_ERROR]
+        }
+    ]
+});
