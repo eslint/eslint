9723ee00c581de30c9dfbeea5de27cfd5855196e
薛定谔的猫 <hh_2013@foxmail.com>
2017-05-02 06:10:31 +0800

    add more test

--- tests/lib/rules/getter-return.js
+++ tests/lib/rules/getter-return.js
@@ -19,30 +19,56 @@ const RuleTester = require("../../../lib/testers/rule-tester");
 const ruleTester = new RuleTester();
 
 // TODO: data is not working, so specify a name: "getter 'bar'"
-const noReturnMessage = "Expected to return a value in getter 'bar'.";
-const noLastReturnMessage = "Expected to return a value at the end of getter 'bar'.";
+const name = "getter 'bar'";
+const noReturnMessage = `Expected to return a value in ${name}.`;
+const noLastReturnMessage = `Expected to return a value at the end of ${name}.`;
+const parserOptions = { ecmaVersion: 6 };
 
 ruleTester.run("enforce-return-in-getter", rule, {
 
     valid: [
-        { code: "var foo = { get: function(){} };" },
+
+        // test obj: get
         { code: "var foo = { get bar(){return true;} };" },
-        { code: "var foo = { bar(){return true;} };", env: { es6: true } },
-        { code: "var foo = { bar(){} };", env: { es6: true } },
+        { code: "var foo = { bar: function(){return true;} };" },
+        { code: "var foo = { bar(){return true;} };", parserOptions },
+        { code: "var foo = { bar: function(){} };" },
+        { code: "var foo = { bar(){} };", parserOptions },
 
-        { code: "class foo { get bar(){return true;} }", env: { es6: true } },
-        { code: "class foo { get(){} }", env: { es6: true } },
-        { code: "class foo { get(){return true;} }", env: { es6: true } },
+        // test class: get
+        { code: "class foo { get bar(){return true;} }", parserOptions },
+        { code: "class foo { get bar(){if(baz){return true;} else {return false;} } }", parserOptions },
+        { code: "class foo { get(){return true;} }", parserOptions },
 
+        // add object.defineProperty
+        { code: "Object.defineProperty(foo, \"bar\", { get: function () {return true;}});" },
+
+        // not getter.
         { code: "var get = function(){};" }
     ],
 
     invalid: [
 
         // TODO: why data: { name: "getter 'bar'"} is not working?
-        { code: "var foo = { get bar() {return;} }", env: { es6: true }, errors: [{ message: noReturnMessage, data: { name: "getter 'bar'" } }] },
-        { code: "var foo = { get bar(){if(bar) {return true;}} }", env: { es6: true }, errors: [{ message: noLastReturnMessage, data: { name: "getter 'bar'" } }] },
-        { code: "var foo = { get bar(){if(bar) {return;} return true;} }", env: { es6: true }, errors: [{ message: noReturnMessage, data: { name: "getter 'bar'" } }] }
+        // test obj: get
+        { code: "var foo = { get bar() {return;} };", parserOptions, errors: [{ message: noReturnMessage, data: { name: "getter 'bar'" } }] },
+        { code: "var foo = { get bar() {return; ;} };", parserOptions, errors: [{ message: noReturnMessage, data: { name: "getter 'bar'" } }] },
+        { code: "var foo = { get bar() {return 1; return;} };", parserOptions, errors: [{ message: noReturnMessage, data: { name: "getter 'bar'" } }] },
+        { code: "var foo = { get bar() {return; return 1;} };", parserOptions, errors: [{ message: noReturnMessage, data: { name: "getter 'bar'" } }] },
+        { code: "var foo = { get bar(){if(bar) {return true;}} };", parserOptions, errors: [{ message: noLastReturnMessage, data: { name: "getter 'bar'" } }] },
+        { code: "var foo = { get bar(){if(bar) {return true;} return;} };", parserOptions, errors: [{ message: noReturnMessage, data: { name: "getter 'bar'" } }] },
+        { code: "var foo = { get bar(){if(bar) {return true;} ;} };", parserOptions, errors: [{ message: noLastReturnMessage, data: { name: "getter 'bar'" } }] },
+        { code: "var foo = { get bar(){if(bar) {return;} return true;} };", parserOptions, errors: [{ message: noReturnMessage, data: { name: "getter 'bar'" } }] },
+
+        // test class: get
+        { code: "class foo { get bar(){return;} }", parserOptions, errors: [{ message: noReturnMessage, data: { name: "getter 'bar'" } }] },
+        { code: "class foo { get bar(){if(bar) {return true;}} }", parserOptions, errors: [{ message: noLastReturnMessage, data: { name: "getter 'bar'" } }] },
+        { code: "class foo { get bar(){if(bar) {return;} return true;} }", parserOptions, errors: [{ message: noReturnMessage, data: { name: "getter 'bar'" } }] },
+
+        // add object.defineProperty
+        { code: "Object.defineProperty(foo, \"bar\", { get: function (){return;}});", errors: [{ message: "Expected to return a value in method 'get'.", data: { name: "getter 'bar'" } }] },
+        { code: "Object.defineProperty(foo, \"bar\", { get: function (){if(bar) {return true;}}});", errors: [{ message: "Expected to return a value at the end of method 'get'.", data: { name: "getter 'bar'" } }] },
+        { code: "Object.defineProperty(foo, \"bar\", { get: function (){if(bar) {return;} return true;}});", errors: [{ message: "Expected to return a value in method 'get'.", data: { name: "getter 'bar'" } }] }
 
     ]
 });
