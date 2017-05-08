6bc00fdcea587024aac4b001a6f5b7d28105aa06
薛定谔的猫 <hh_2013@foxmail.com>
2017-05-09 05:24:09 +0800

    add more tests: IIFE...

--- tests/lib/rules/getter-return.js
+++ tests/lib/rules/getter-return.js
@@ -36,6 +36,8 @@ ruleTester.run("getter-return", rule, {
         { code: "var foo = { bar: function(){return;} };" },
         { code: "var foo = { bar(){return true;} };", parserOptions },
         { code: "var foo = { bar(){return;} };", parserOptions },
+        { code: "var foo = { bar(){~function (){}();return;} };", parserOptions },
+        { code: "var foo = { bar(){~function (){return true;}();return;} };", parserOptions },
 
         // test class: get, option: {noImplicit: false}
         { code: "class foo { get bar(){return true;} }", parserOptions },
@@ -43,12 +45,18 @@ ruleTester.run("getter-return", rule, {
         { code: "class foo { get bar(){if(baz){return;} else {return false;} } }", parserOptions },
         { code: "class foo { get(){return true;} }", parserOptions },
         { code: "var foo = { get bar(){if(bar) {return;} return true;} };", parserOptions, errors: [] },
+        { code: "var foo = { get bar(){ ~function (){ return true; }(); return; } };", parserOptions, errors: [] },
+        { code: "var foo = { get bar(){ ~function (){}(); return; } };", parserOptions, errors: [] },
 
         // test object.defineProperty(s), option: {noImplicit: false}
         { code: "Object.defineProperty(foo, \"bar\", { get: function () {return true;}});" },
         { code: "Object.defineProperty(foo, \"bar\", { get: function () {return;}});" },
         { code: "Object.defineProperies(foo, { bar: { get: function () {return true;}} });" },
         { code: "Object.defineProperies(foo, { bar: { get: function () {return;}} });" },
+        { code: "Object.defineProperty(foo, \"bar\", { get: function () { ~function (){ return true; }();return true;}});" },
+        { code: "Object.defineProperty(foo, \"bar\", { get: function () { ~function (){}();return;}});" },
+        { code: "Object.defineProperies(foo, { bar: { get: function () { ~function (){ return true; }(); return true;}} });" },
+        { code: "Object.defineProperies(foo, { bar: { get: function () { ~function (){}(); return;}} });" },
 
         // test option: {noImplicit: true}
         { code: "var foo = { get bar(){return true;} };", options },
@@ -67,30 +75,31 @@ ruleTester.run("getter-return", rule, {
 
     invalid: [
 
-        // test obj: get
+        // test obj: get, option: {noImplicit: false}
         { code: "var foo = { get bar() {} };", parserOptions, errors: [{ message: noReturnMessage }] },
+        { code: "var foo = { get bar(){if(bar) {return true;}} };", parserOptions, errors: [{ message: noLastReturnMessage }] },
+        { code: "var foo = { get bar(){if(bar) {return true;} ;} };", parserOptions, errors: [{ message: noLastReturnMessage }] },
+
+        // test class: get, option: {noImplicit: false}
+        { code: "class foo { get bar(){} }", parserOptions, errors: [{ message: noReturnMessage }] },
+
+        // test object.defineProperty, option: {noImplicit: false}
+        { code: "Object.defineProperty(foo, \"bar\", { get: function (){if(bar) {return true;}}});", errors: [{ message: "Expected to return a value at the end of method 'get'." }] },
+
+        // test option: {noImplicit: true}
         { code: "var foo = { get bar() {return;} };", options, parserOptions, errors: [{ message: noReturnMessage }] },
         { code: "var foo = { get bar() {return; ;} };", options, parserOptions, errors: [{ message: noReturnMessage }] },
         { code: "var foo = { get bar() {return; return 1;} };", options, parserOptions, errors: [{ message: noReturnMessage }] },
-        { code: "var foo = { get bar(){if(bar) {return true;}} };", parserOptions, errors: [{ message: noLastReturnMessage }] },
         { code: "var foo = { get bar(){if(bar) {return true;} return;} };", options, parserOptions, errors: [{ message: noReturnMessage }] },
-        { code: "var foo = { get bar(){if(bar) {return true;} ;} };", parserOptions, errors: [{ message: noLastReturnMessage }] },
         { code: "var foo = { get bar(){if(bar) {return;} return true;} };", options, parserOptions, errors: [{ message: noReturnMessage }] },
-
-        // test class: get
-        { code: "class foo { get bar(){} }", parserOptions, errors: [{ message: noReturnMessage }] },
-        { code: "class foo { get bar(){} }", options, parserOptions, errors: [{ message: noReturnMessage }] },
         { code: "class foo { get bar(){return;} }", options, parserOptions, errors: [{ message: noReturnMessage }] },
         { code: "class foo { get bar(){if(bar) {return true;}} }", options, parserOptions, errors: [{ message: noLastReturnMessage }] },
         { code: "class foo { get bar(){if(bar) {return;} return true;} }", options, parserOptions, errors: [{ message: noReturnMessage }] },
-
-        // test object.defineProperty
         { code: "Object.defineProperty(foo, \"bar\", { get: function (){return;}});", options, errors: [{ message: "Expected to return a value in method 'get'." }] },
-        { code: "Object.defineProperty(foo, \"bar\", { get: function (){if(bar) {return true;}}});", errors: [{ message: "Expected to return a value at the end of method 'get'." }] },
-        { code: "Object.defineProperty(foo, \"bar\", { get: function (){if(bar) {return;} return true;}});", options: [{ noImplicit: true }], errors: [{ message: "Expected to return a value in method 'get'." }] },
+        { code: "Object.defineProperty(foo, \"bar\", { get: function (){if(bar) {return;} return true;}});", options, errors: [{ message: "Expected to return a value in method 'get'." }] },
+        { code: "Object.defineProperies(foo, { bar: { get: function () {return;}} });", options, errors: [{ message: "Expected to return a value in method 'get'." }] },
 
-        // test option: {noImplicit: true}
-        { code: "Object.defineProperty(foo, \"bar\", { get: function (){return;}});", options, errors: [{ message: "Expected to return a value in method 'get'." }] }
+        { code: "class foo { get bar(){} }", options, parserOptions, errors: [{ message: noReturnMessage }] }
 
     ]
 });
