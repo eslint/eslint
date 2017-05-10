00f1c814d43545c6d1ce04997667af93dee8dc58
Reyad Attiyat <reyad.attiyat@gmail.com>
2017-05-09 23:59:04 -0500

    Fix: Indent Ignore Variable Declaration init operator (fixes #8546) (#8563)
    * Fix: Indent Ignore Variable Declaration init operator (fixes #8546)
    
    * Refactor
     - Ensure token before init is not parenthese
     - Remove check for equal operator
     - Remove check for token on first line
     - Add test for init with parenthese
    
    * Remove if statement for null token check

--- tests/lib/rules/indent.js
+++ tests/lib/rules/indent.js
@@ -532,6 +532,43 @@ ruleTester.run("indent", rule, {
         {
             code: unIndent`
                 var foo = 1,
+                  bar
+                    = 2
+            `,
+            options: [2, { VariableDeclarator: 1 }]
+        },
+        {
+            code: unIndent`
+                var foo
+                  = 1,
+                  bar
+                    = 2
+            `,
+            options: [2, { VariableDeclarator: 1 }]
+        },
+        {
+            code: unIndent`
+                var foo
+                  =
+                  1,
+                  bar
+                    =
+                    2
+            `,
+            options: [2, { VariableDeclarator: 1 }]
+        },
+        {
+            code: unIndent`
+                var foo
+                  = (1),
+                  bar
+                    = (2)
+            `,
+            options: [2, { VariableDeclarator: 1 }]
+        },
+        {
+            code: unIndent`
+                var foo = 1,
                     bar = 2,
                     baz = 3
                 ;
