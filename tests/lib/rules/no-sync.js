fb1b641540ac9eb547519556013238db2375c873
Victor Hom <vh62@cornell.edu>
2017-07-01 20:31:12 -0400

    add tests

--- tests/lib/rules/no-sync.js
+++ tests/lib/rules/no-sync.js
@@ -21,11 +21,13 @@ const ruleTester = new RuleTester();
 ruleTester.run("no-sync", rule, {
     valid: [
         "var foo = fs.foo.foo();",
-        { code: "var foo = fs.fooSync;", options: [{ allowAtRootLevel: true }] }
+        { code: "var foo = fs.fooSync;", options: [{ allowAtRootLevel: true }] },
+        { code: "if (true) {fs.fooSync();}", options: [{ allowAtRootLevel: true }] },
     ],
     invalid: [
         { code: "var foo = fs.fooSync();", errors: [{ message: "Unexpected sync method: 'fooSync'.", type: "MemberExpression" }] },
         { code: "var foo = fs.fooSync();", options: [{ allowAtRootLevel: false }], errors: [{ message: "Unexpected sync method: 'fooSync'.", type: "MemberExpression" }] },
+        { code: "if (true) {fs.fooSync();}", errors: [{ message: "Unexpected sync method: 'fooSync'.", type: "MemberExpression" }] },
         { code: "var foo = fs.fooSync;", errors: [{ message: "Unexpected sync method: 'fooSync'.", type: "MemberExpression" }] },
         { code: "function someFunction() {fs.fooSync();}", errors: [{ message: "Unexpected sync method: 'fooSync'.", type: "MemberExpression" }] },
         { code: "function someFunction() {fs.fooSync();}", options: [{ allowAtRootLevel: true }], errors: [{ message: "Unexpected sync method: 'fooSync'.", type: "FunctionDeclaration" }] },
