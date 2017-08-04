038f382294636b23ecf47d904eecf12c66c467a4
薛定谔的猫 <hh_2013@foxmail.com>
2017-08-05 00:15:04 +0800

    use  pkg.version.

--- bin/eslint.js
+++ bin/eslint.js
@@ -43,9 +43,10 @@ process.once("uncaughtException", err => {
 
     if (typeof err.messageTemplate === "string" && err.messageTemplate.length > 0) {
         const template = lodash.template(fs.readFileSync(path.resolve(__dirname, `../messages/${err.messageTemplate}.txt`), "utf-8"));
+        const pkg = require("../package.json");
 
         console.error("\nOops! Something went wrong! :(");
-        console.error(`\nESLint: ${cli.execute("-v")}.${template(err.messageData || {})}`);
+        console.error(`\nESLint: ${pkg.version}.${template(err.messageData || {})}`);
     } else {
 
         console.error(err.message);
