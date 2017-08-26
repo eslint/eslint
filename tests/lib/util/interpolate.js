10e1e18434867c4c18a3d343bfdd76083bdc4ee5
Jed Fox <git@twopointzero.us>
2017-08-26 07:47:44 -0400

    Chore: Test interpolate()

---
+++ tests/lib/util/interpolate.js
@@ -0,0 +1,22 @@
+"use strict";
+
+const assert = require("chai").assert;
+const interpolate = require("../../../lib/util/interpolate");
+
+describe("interpolate()", () => {
+    it("passes through text without {{ }}", () => {
+        const message = "This is a very important message!";
+
+        assert.equal(interpolate(message, {}), message);
+    });
+    it("passes through text with {{ }} that don’t match a key", () => {
+        const message = "This is a very important {{ message }}!";
+
+        assert.equal(interpolate(message, {}), message);
+    });
+    it("Properly interpolates keys in {{ }}", () => {
+        assert.equal(interpolate("This is a very important {{ message }}!", {
+            message: "test"
+        }), "This is a very important test!");
+    });
+});
