0d86820f211fd1cb8cad125a2a7337adf9daf23a
薛定谔的猫 <hh_2013@foxmail.com>
2017-09-06 02:19:59 +0800

    Fix: should consider comments before methods.

--- tests/lib/rules/lines-between-class-members.js
+++ tests/lib/rules/lines-between-class-members.js
@@ -31,13 +31,34 @@ ruleTester.run("lines-between-class-methods", rule, {
         "class foo{\n\n}",
         "class foo{constructor(){}\n}",
         "class foo{\nconstructor(){}}",
+
         "class foo{ bar(){}\n\nbaz(){}}",
+        "class foo{ bar(){}\n\n/*comments*/baz(){}}",
+        "class foo{ bar(){}\n\n//comments\nbaz(){}}",
+
         { code: "class foo{ bar(){}\nbaz(){}}", options: ["never"] },
+        { code: "class foo{ bar(){}\n/*comments*/baz(){}}", options: ["never"] },
+        { code: "class foo{ bar(){}\n//comments\nbaz(){}}", options: ["never"] },
+
         { code: "class foo{ bar(){}\n\nbaz(){}}", options: ["always"] },
+        { code: "class foo{ bar(){}\n\n/*comments*/baz(){}}", options: ["always"] },
+        { code: "class foo{ bar(){}\n\n//comments\nbaz(){}}", options: ["always"] },
+
         { code: "class foo{ bar(){\n}\n\nbaz(){}}", options: [{ multiline: "always" }] },
+        { code: "class foo{ bar(){\n}\n\n/*comments*/baz(){}}", options: [{ multiline: "always" }] },
+        { code: "class foo{ bar(){\n}\n\n//comments\nbaz(){}}", options: [{ multiline: "always" }] },
+
         { code: "class foo{ bar(){\n}\nbaz(){}}", options: [{ multiline: "never" }] },
+        { code: "class foo{ bar(){\n}\n/*comments*/baz(){}}", options: [{ multiline: "never" }] },
+        { code: "class foo{ bar(){\n}\n//comments\nbaz(){}}", options: [{ multiline: "never" }] },
+
         { code: "class foo{ bar(){}\n\nbaz(){}}", options: [{ singleline: "always" }] },
-        { code: "class foo{ bar(){}\nbaz(){}}", options: [{ singleline: "never" }] }
+        { code: "class foo{ bar(){}\n\n/*comments*/baz(){}}", options: [{ singleline: "always" }] },
+        { code: "class foo{ bar(){}\n\n//comments\nbaz(){}}", options: [{ singleline: "always" }] },
+
+        { code: "class foo{ bar(){}\nbaz(){}}", options: [{ singleline: "never" }] },
+        { code: "class foo{ bar(){}\n/*comments*/baz(){}}", options: [{ singleline: "never" }] },
+        { code: "class foo{ bar(){}\n//comments\nbaz(){}}", options: [{ singleline: "never" }] }
     ],
     invalid: [
         {
@@ -45,8 +66,26 @@ ruleTester.run("lines-between-class-methods", rule, {
             output: "class foo{ bar(){}\n\nbaz(){}}",
             errors: [{ message: ALWAYS_MESSAGE }]
         }, {
+            code: "class foo{ bar(){}\n/*comments*/baz(){}}",
+            output: "class foo{ bar(){}\n\n/*comments*/baz(){}}",
+            errors: [{ message: ALWAYS_MESSAGE }]
+        }, {
+            code: "class foo{ bar(){}\n//comments\nbaz(){}}",
+            output: "class foo{ bar(){}\n\n//comments\nbaz(){}}",
+            errors: [{ message: ALWAYS_MESSAGE }]
+        }, {
             code: "class foo{ bar(){}\n\nbaz(){}}",
             output: "class foo{ bar(){}\nbaz(){}}",
+            options: ["never"],
+            errors: [{ message: NEVER_MESSAGE }]
+        }, {
+            code: "class foo{ bar(){}\n\n/*comments*/baz(){}}",
+            output: "class foo{ bar(){}\n/*comments*/baz(){}}",
+            options: ["never"],
+            errors: [{ message: NEVER_MESSAGE }]
+        }, {
+            code: "class foo{ bar(){}\n\n//comments\nbaz(){}}",
+            output: "class foo{ bar(){}\n//comments\nbaz(){}}",
             options: ["never"],
             errors: [{ message: NEVER_MESSAGE }]
         }, {
@@ -55,8 +94,28 @@ ruleTester.run("lines-between-class-methods", rule, {
             options: [{ multiline: "always" }],
             errors: [{ message: ALWAYS_MESSAGE }]
         }, {
+            code: "class foo{ bar(){\n}\n/*comments*/baz(){}}",
+            output: "class foo{ bar(){\n}\n\n/*comments*/baz(){}}",
+            options: [{ multiline: "always" }],
+            errors: [{ message: ALWAYS_MESSAGE }]
+        }, {
+            code: "class foo{ bar(){\n}\n//comments\nbaz(){}}",
+            output: "class foo{ bar(){\n}\n\n//comments\nbaz(){}}",
+            options: [{ multiline: "always" }],
+            errors: [{ message: ALWAYS_MESSAGE }]
+        }, {
             code: "class foo{ bar(){\n}\n\nbaz(){}}",
             output: "class foo{ bar(){\n}\nbaz(){}}",
+            options: [{ multiline: "never" }],
+            errors: [{ message: NEVER_MESSAGE }]
+        }, {
+            code: "class foo{ bar(){\n}\n\n/*comments*/baz(){}}",
+            output: "class foo{ bar(){\n}\n/*comments*/baz(){}}",
+            options: [{ multiline: "never" }],
+            errors: [{ message: NEVER_MESSAGE }]
+        }, {
+            code: "class foo{ bar(){\n}\n\n//comments\nbaz(){}}",
+            output: "class foo{ bar(){\n}\n//comments\nbaz(){}}",
             options: [{ multiline: "never" }],
             errors: [{ message: NEVER_MESSAGE }]
         }, {
@@ -65,8 +124,28 @@ ruleTester.run("lines-between-class-methods", rule, {
             options: [{ singleline: "always" }],
             errors: [{ message: ALWAYS_MESSAGE }]
         }, {
+            code: "class foo{ bar(){}\n/*comments*/baz(){}}",
+            output: "class foo{ bar(){}\n\n/*comments*/baz(){}}",
+            options: [{ singleline: "always" }],
+            errors: [{ message: ALWAYS_MESSAGE }]
+        }, {
+            code: "class foo{ bar(){}\n//comments\nbaz(){}}",
+            output: "class foo{ bar(){}\n\n//comments\nbaz(){}}",
+            options: [{ singleline: "always" }],
+            errors: [{ message: ALWAYS_MESSAGE }]
+        }, {
             code: "class foo{ bar(){}\n\nbaz(){}}",
             output: "class foo{ bar(){}\nbaz(){}}",
+            options: [{ singleline: "never" }],
+            errors: [{ message: NEVER_MESSAGE }]
+        }, {
+            code: "class foo{ bar(){}\n\n/*comments*/baz(){}}",
+            output: "class foo{ bar(){}\n/*comments*/baz(){}}",
+            options: [{ singleline: "never" }],
+            errors: [{ message: NEVER_MESSAGE }]
+        }, {
+            code: "class foo{ bar(){}\n\n//comments\nbaz(){}}",
+            output: "class foo{ bar(){}\n//comments\nbaz(){}}",
             options: [{ singleline: "never" }],
             errors: [{ message: NEVER_MESSAGE }]
         }
