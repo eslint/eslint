0cf081ef7249c8e3b9e2cba4710f6205ef996b02
Benjamin R Gibson <brgibson@indiana.edu>
2017-11-14 13:06:53 -0600

    Update: add importNames option to no-restricted-imports (#9506)

--- tests/lib/rules/no-restricted-imports.js
+++ tests/lib/rules/no-restricted-imports.js
@@ -35,6 +35,134 @@ ruleTester.run("no-restricted-imports", rule, {
         {
             code: "import withGitignores from \"foo/bar\";",
             options: [{ patterns: ["foo/*", "!foo/bar"] }]
+        },
+        {
+            code: "import AllowedObject from \"foo\";",
+            options: [{
+                paths: [{
+                    name: "foo",
+                    importNames: ["DisallowedObject"]
+                }]
+            }]
+        },
+        {
+            code: "import DisallowedObject from \"foo\";",
+            options: [{
+                paths: [{
+                    name: "foo",
+                    importNames: ["DisallowedObject"]
+                }]
+            }]
+        },
+        {
+            code: "import * as DisallowedObject from \"foo\";",
+            options: [{
+                paths: [{
+                    name: "bar",
+                    importNames: ["DisallowedObject"],
+                    message: "Please import 'DisallowedObject' from /bar/ instead."
+                }]
+            }]
+        },
+        {
+            code: "import { AllowedObject } from \"foo\";",
+            options: [{
+                paths: [{
+                    name: "foo",
+                    importNames: ["DisallowedObject"],
+                    message: "Please import 'DisallowedObject' from /bar/ instead."
+                }]
+            }]
+        },
+        {
+            code: "import { DisallowedObject } from \"foo\";",
+            options: [{
+                paths: [{
+                    name: "bar",
+                    importNames: ["DisallowedObject"],
+                    message: "Please import 'DisallowedObject' from /bar/ instead."
+                }]
+            }]
+        },
+        {
+            code: "import { AllowedObject as DisallowedObject } from \"foo\";",
+            options: [{
+                paths: [{
+                    name: "foo",
+                    importNames: ["DisallowedObject"],
+                    message: "Please import 'DisallowedObject' from /bar/ instead."
+                }]
+            }]
+        },
+        {
+            code: "import { AllowedObject, AllowedObjectTwo } from \"foo\";",
+            options: [{
+                paths: [{
+                    name: "foo",
+                    importNames: ["DisallowedObject"],
+                    message: "Please import 'DisallowedObject' from /bar/ instead."
+                }]
+            }]
+        },
+        {
+            code: "import { AllowedObject, AllowedObjectTwo  as DisallowedObject } from \"foo\";",
+            options: [{
+                paths: [{
+                    name: "foo",
+                    importNames: ["DisallowedObject"],
+                    message: "Please import 'DisallowedObject' from /bar/ instead."
+                }]
+            }]
+        },
+        {
+            code: "import AllowedObjectThree, { AllowedObject as AllowedObjectTwo } from \"foo\";",
+            options: [{
+                paths: [{
+                    name: "foo",
+                    importNames: ["DisallowedObject"],
+                    message: "Please import 'DisallowedObject' from /bar/ instead."
+                }]
+            }]
+        },
+        {
+            code: "import AllowedObject, { AllowedObjectTwo as DisallowedObject } from \"foo\";",
+            options: [{
+                paths: [{
+                    name: "foo",
+                    importNames: ["DisallowedObject"],
+                    message: "Please import 'DisallowedObject' from /bar/ instead."
+                }]
+            }]
+        },
+        {
+            code: "import AllowedObject, { AllowedObjectTwo as DisallowedObject } from \"foo\";",
+            options: [{
+                paths: [{
+                    name: "foo",
+                    importNames: ["DisallowedObject", "DisallowedObjectTwo"],
+                    message: "Please import 'DisallowedObject' and 'DisallowedObjectTwo' from /bar/ instead."
+                }]
+            }]
+        },
+        {
+            code: "import AllowedObject, * as DisallowedObject from \"foo\";",
+            options: [{
+                paths: [{
+                    name: "bar",
+                    importNames: ["DisallowedObject"],
+                    message: "Please import 'DisallowedObject' from /bar/ instead."
+                }]
+            }]
+        },
+        {
+            code: "import \"foo\";",
+            options: [{
+                paths: [{
+                    name: "foo",
+                    importNames: ["DisallowedObject", "DisallowedObjectTwo"],
+                    message: "Please import 'DisallowedObject' and 'DisallowedObjectTwo' from /bar/ instead."
+                }]
+            }]
         }
     ],
     invalid: [{
@@ -55,7 +183,11 @@ ruleTester.run("no-restricted-imports", rule, {
         errors: [{ message: "'foo/bar' import is restricted from being used.", type: "ImportDeclaration" }]
     }, {
         code: "import withPatterns from \"foo/bar\";",
-        options: [{ patterns: ["foo/*"] }],
+        options: [{ patterns: ["foo"] }],
+        errors: [{ message: "'foo/bar' import is restricted from being used by a pattern.", type: "ImportDeclaration" }]
+    }, {
+        code: "import withPatterns from \"foo/bar\";",
+        options: [{ patterns: ["bar"] }],
         errors: [{ message: "'foo/bar' import is restricted from being used by a pattern.", type: "ImportDeclaration" }]
     }, {
         code: "import withGitignores from \"foo/bar\";",
@@ -97,5 +229,174 @@ ruleTester.run("no-restricted-imports", rule, {
             message: "'foo' import is restricted from being used. Please import from 'bar' instead.",
             type: "ImportDeclaration"
         }]
-    }]
+    },
+    {
+        code: "import DisallowedObject from \"foo\";",
+        options: [{
+            paths: [{
+                name: "foo",
+                importNames: ["default"],
+                message: "Please import the default import of 'foo' from /bar/ instead."
+            }]
+        }],
+        errors: [{
+            message: "'foo' import is restricted from being used. Please import the default import of 'foo' from /bar/ instead.",
+            type: "ImportDeclaration"
+        }]
+    },
+    {
+        code: "import * as All from \"foo\";",
+        options: [{
+            paths: [{
+                name: "foo",
+                importNames: ["DisallowedObject"],
+                message: "Please import 'DisallowedObject' from /bar/ instead."
+            }]
+        }],
+        errors: [{
+            message: "* import is invalid because 'DisallowedObject' from 'foo' is restricted.",
+            type: "ImportDeclaration"
+        }]
+    },
+    {
+        code: "import { DisallowedObject } from \"foo\";",
+        options: [{
+            paths: [{
+                name: "foo",
+                importNames: ["DisallowedObject"],
+                message: "Please import 'DisallowedObject' from /bar/ instead."
+            }]
+        }],
+        errors: [{
+            message: "'foo' import is restricted from being used. Please import 'DisallowedObject' from /bar/ instead.",
+            type: "ImportDeclaration"
+        }]
+    },
+    {
+        code: "import { DisallowedObject as AllowedObject } from \"foo\";",
+        options: [{
+            paths: [{
+                name: "foo",
+                importNames: ["DisallowedObject"],
+                message: "Please import 'DisallowedObject' from /bar/ instead."
+            }]
+        }],
+        errors: [{
+            message: "'foo' import is restricted from being used. Please import 'DisallowedObject' from /bar/ instead.",
+            type: "ImportDeclaration"
+        }]
+    },
+    {
+        code: "import { AllowedObject, DisallowedObject } from \"foo\";",
+        options: [{
+            paths: [{
+                name: "foo",
+                importNames: ["DisallowedObject"],
+                message: "Please import 'DisallowedObject' from /bar/ instead."
+            }]
+        }],
+        errors: [{
+            message: "'foo' import is restricted from being used. Please import 'DisallowedObject' from /bar/ instead.",
+            type: "ImportDeclaration"
+        }]
+    },
+    {
+        code: "import { AllowedObject, DisallowedObject as AllowedObjectTwo } from \"foo\";",
+        options: [{
+            paths: [{
+                name: "foo",
+                importNames: ["DisallowedObject"],
+                message: "Please import 'DisallowedObject' from /bar/ instead."
+            }]
+        }],
+        errors: [{
+            message: "'foo' import is restricted from being used. Please import 'DisallowedObject' from /bar/ instead.",
+            type: "ImportDeclaration"
+        }]
+    },
+    {
+        code: "import { AllowedObject, DisallowedObject as AllowedObjectTwo } from \"foo\";",
+        options: [{
+            paths: [{
+                name: "foo",
+                importNames: ["DisallowedObjectTwo", "DisallowedObject"],
+                message: "Please import 'DisallowedObject' and 'DisallowedObjectTwo' from /bar/ instead."
+            }]
+        }],
+        errors: [{
+            message: "'foo' import is restricted from being used. Please import 'DisallowedObject' and 'DisallowedObjectTwo' from /bar/ instead.",
+            type: "ImportDeclaration"
+        }]
+    },
+    {
+        code: "import { AllowedObject, DisallowedObject as AllowedObjectTwo } from \"foo\";",
+        options: [{
+            paths: [{
+                name: "foo",
+                importNames: ["DisallowedObject", "DisallowedObjectTwo"],
+                message: "Please import 'DisallowedObject' and 'DisallowedObjectTwo' from /bar/ instead."
+            }]
+        }],
+        errors: [{
+            message: "'foo' import is restricted from being used. Please import 'DisallowedObject' and 'DisallowedObjectTwo' from /bar/ instead.",
+            type: "ImportDeclaration"
+        }]
+    },
+    {
+        code: "import DisallowedObject, { AllowedObject as AllowedObjectTwo } from \"foo\";",
+        options: [{
+            paths: [{
+                name: "foo",
+                importNames: ["default"],
+                message: "Please import the default import of 'foo' from /bar/ instead."
+            }]
+        }],
+        errors: [{
+            message: "'foo' import is restricted from being used. Please import the default import of 'foo' from /bar/ instead.",
+            type: "ImportDeclaration"
+        }]
+    },
+    {
+        code: "import AllowedObject, { DisallowedObject as AllowedObjectTwo } from \"foo\";",
+        options: [{
+            paths: [{
+                name: "foo",
+                importNames: ["DisallowedObject"],
+                message: "Please import 'DisallowedObject' from /bar/ instead."
+            }]
+        }],
+        errors: [{
+            message: "'foo' import is restricted from being used. Please import 'DisallowedObject' from /bar/ instead.",
+            type: "ImportDeclaration"
+        }]
+    },
+    {
+        code: "import AllowedObject, * as AllowedObjectTwo from \"foo\";",
+        options: [{
+            paths: [{
+                name: "foo",
+                importNames: ["DisallowedObject"],
+                message: "Please import 'DisallowedObject' from /bar/ instead."
+            }]
+        }],
+        errors: [{
+            message: "* import is invalid because 'DisallowedObject' from 'foo' is restricted.",
+            type: "ImportDeclaration"
+        }]
+    },
+    {
+        code: "import AllowedObject, * as AllowedObjectTwo from \"foo\";",
+        options: [{
+            paths: [{
+                name: "foo",
+                importNames: ["DisallowedObject", "DisallowedObjectTwo"],
+                message: "Please import 'DisallowedObject' and 'DisallowedObjectTwo' from /bar/ instead."
+            }]
+        }],
+        errors: [{
+            message: "* import is invalid because 'DisallowedObject,DisallowedObjectTwo' from 'foo' is restricted.",
+            type: "ImportDeclaration"
+        }]
+    }
+    ]
 });
