c64195fb53570829b8a916503c56d7681687af97
Weijia Wang <wjwang13@fudan.edu.cn>
2018-01-05 02:59:14 +0800

    Update: More detailed assert message for rule-tester (#9769)
    * Update: More detailed assert message for rule-tester
    
    * [squash]add try/finally block
    
    * [squash]append message instead of overwriting it
    
    * [squash]add parentheses surrounding error message

---
+++ tests/lib/testers/no-test-runners.js
@@ -0,0 +1,35 @@
+/**
+ * @fileoverview Tests for RuleTester without any test runner
+ * @author Weijia Wang <starkwang@126.com>
+ */
+"use strict";
+
+/* global describe, it */
+/* eslint-disable no-global-assign*/
+const assert = require("assert"),
+    RuleTester = require("../../../lib/testers/rule-tester");
+const tmpIt = it;
+const tmpDescribe = describe;
+
+it = null;
+describe = null;
+
+try {
+    const ruleTester = new RuleTester();
+
+    assert.throws(() => {
+        ruleTester.run("no-var", require("../../fixtures/testers/rule-tester/no-var"), {
+            valid: [
+                "bar = baz;"
+            ],
+            invalid: [
+                { code: "var foo = bar;", output: "invalid output", errors: 1 }
+            ]
+        });
+    }, /Output is incorrect\. \(' foo = bar;' == 'invalid output'\)$/);
+} catch (e) {
+    throw e;
+} finally {
+    it = tmpIt;
+    describe = tmpDescribe;
+}
