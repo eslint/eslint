a1a9571c0a6392b8f07e52ef75f43eb4eea034e3
Kevin Partington <platinum.azure@kernelpanicstudios.com>
2018-04-03 09:24:59 -0500

    Upgrade: esquery@^1.0.1 (fixes #8733) (#10170)
    * Upgrade: esquery@^1.0.1 (fixes #8733)
    
    * Chore: Add extra tests
    
    * Chore: Fix lint errors
    
    * Chore: Removing accidental copy/paste test

--- tests/lib/rules/no-restricted-syntax.js
+++ tests/lib/rules/no-restricted-syntax.js
@@ -36,7 +36,10 @@ ruleTester.run("no-restricted-syntax", rule, {
         {
             code: "({ foo: 1, bar: 2 })",
             options: [{ selector: "FunctionDeclaration[params.length>2]", message: "custom error message." }]
-        }
+        },
+
+        // https://github.com/eslint/eslint/issues/8733
+        { code: "console.log(/a/);", options: ["Literal[regex.flags=/./]"] }
     ],
     invalid: [
 
@@ -118,6 +121,13 @@ ruleTester.run("no-restricted-syntax", rule, {
             code: "function foo(bar, baz, qux) {}",
             options: [{ selector: "FunctionDeclaration[params.length>2]", message: "custom message with {{selector}}" }],
             errors: [{ message: "custom message with {{selector}}", type: "FunctionDeclaration" }]
+        },
+
+        // https://github.com/eslint/eslint/issues/8733
+        {
+            code: "console.log(/a/i);",
+            options: ["Literal[regex.flags=/./]"],
+            errors: [{ message: "Using 'Literal[regex.flags=/./]' is not allowed.", type: "Literal" }]
         }
     ]
 });
