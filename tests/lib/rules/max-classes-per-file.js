0059bfb83c657214469352751e4fe88b039361b6
James Garbutt <43081j@users.noreply.github.com>
2018-04-19 13:15:03 +0100

    chore: pass parser options to rule tester directly

--- tests/lib/rules/max-classes-per-file.js
+++ tests/lib/rules/max-classes-per-file.js
@@ -15,60 +15,44 @@ const rule = require("../../../lib/rules/max-classes-per-file"),
 // Tests
 //------------------------------------------------------------------------------
 
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 6 } });
 
 ruleTester.run("max-classes-per-file", rule, {
     valid: [
+        "class Foo {}",
+        "var x = class {};",
+        "var x = 5;",
         {
             code: "class Foo {}",
-            parserOptions: { ecmaVersion: 6 }
-        },
-        {
-            code: "var x = class {};",
-            parserOptions: { ecmaVersion: 6 }
-        },
-        {
-            code: "var x = 5;",
-            parserOptions: { ecmaVersion: 6 }
-        },
-        {
-            code: "class Foo {}",
-            options: [1],
-            parserOptions: { ecmaVersion: 6 }
+            options: [1]
         },
         {
             code: "class Foo {}\nclass Bar {}",
-            options: [2],
-            parserOptions: { ecmaVersion: 6 }
+            options: [2]
         }
     ],
 
     invalid: [
         {
             code: "class Foo {}\nclass Bar {}",
-            parserOptions: { ecmaVersion: 6 },
             errors: [{ messageId: "maximumExceeded", type: "Program" }]
         },
         {
             code: "var x = class {};\nvar y = class {};",
-            parserOptions: { ecmaVersion: 6 },
             errors: [{ messageId: "maximumExceeded", type: "Program" }]
         },
         {
             code: "class Foo {}\nvar x = class {};",
-            parserOptions: { ecmaVersion: 6 },
             errors: [{ messageId: "maximumExceeded", type: "Program" }]
         },
         {
             code: "class Foo {} class Bar {}",
             options: [1],
-            parserOptions: { ecmaVersion: 6 },
             errors: [{ messageId: "maximumExceeded", type: "Program" }]
         },
         {
             code: "class Foo {} class Bar {} class Baz {}",
             options: [2],
-            parserOptions: { ecmaVersion: 6 },
             errors: [{ messageId: "maximumExceeded", type: "Program" }]
         }
     ]
