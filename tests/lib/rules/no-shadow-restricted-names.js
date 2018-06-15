d477c5e684b52b307e56ef92714e66b32bc0db5a
Pig Fang <g-plane@hotmail.com>
2018-06-16 03:46:35 +0800

    Fix: check destructuring for "no-shadow-restricted-names" (fixes #10467) (#10470)
    * Fix: check destructing for "no-shadow-restricted-names" (fixes #10467)
    
    * Fix: object destructing for "no-shadow-restricted-names"
    
    * Chore: simplify rule "no-shadow-restricted-names"

--- tests/lib/rules/no-shadow-restricted-names.js
+++ tests/lib/rules/no-shadow-restricted-names.js
@@ -93,6 +93,23 @@ ruleTester.run("no-shadow-restricted-names", rule, {
                 { message: "Shadowing of global property 'eval'.", type: "Identifier" },
                 { message: "Shadowing of global property 'eval'.", type: "Identifier" }
             ]
+        },
+        {
+            code: "var [undefined] = [1]",
+            parserOptions: { ecmaVersion: 6 },
+            errors: [
+                { message: "Shadowing of global property 'undefined'.", type: "Identifier" }
+            ]
+        },
+        {
+            code: "var {undefined} = obj; var {a: undefined} = obj; var {a: {b: {undefined}}} = obj; var {a, ...undefined} = obj;",
+            parserOptions: { ecmaVersion: 9 },
+            errors: [
+                { message: "Shadowing of global property 'undefined'.", type: "Identifier" },
+                { message: "Shadowing of global property 'undefined'.", type: "Identifier" },
+                { message: "Shadowing of global property 'undefined'.", type: "Identifier" },
+                { message: "Shadowing of global property 'undefined'.", type: "Identifier" }
+            ]
         }
     ]
 });
