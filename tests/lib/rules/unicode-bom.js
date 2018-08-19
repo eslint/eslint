8ae2834a8794863ff10f1fb58ecda3295708642e
weiran.zsd <weiran.zsd@outlook.com>
2018-08-20 01:51:15 +0800

    Chore: use messageId in rule unicode-bom

--- tests/lib/rules/unicode-bom.js
+++ tests/lib/rules/unicode-bom.js
@@ -16,6 +16,8 @@ const rule = require("../../../lib/rules/unicode-bom"),
 //------------------------------------------------------------------------------
 
 const ruleTester = new RuleTester();
+const expectedError = { messageId: "expected", type: "Program" };
+const unexpectedError = { messageId: "unexpected", type: "Program" };
 
 ruleTester.run("unicode-bom", rule, {
 
@@ -39,24 +41,24 @@ ruleTester.run("unicode-bom", rule, {
             code: "var a = 123;",
             output: "\uFEFFvar a = 123;",
             options: ["always"],
-            errors: [{ message: "Expected Unicode BOM (Byte Order Mark).", type: "Program" }]
+            errors: [expectedError]
         },
         {
             code: " // here's a comment \nvar a = 123;",
             output: "\uFEFF // here's a comment \nvar a = 123;",
             options: ["always"],
-            errors: [{ message: "Expected Unicode BOM (Byte Order Mark).", type: "Program" }]
+            errors: [expectedError]
         },
         {
             code: "\uFEFF var a = 123;",
             output: " var a = 123;",
-            errors: [{ message: "Unexpected Unicode BOM (Byte Order Mark).", type: "Program" }]
+            errors: [unexpectedError]
         },
         {
             code: "\uFEFF var a = 123;",
             output: " var a = 123;",
             options: ["never"],
-            errors: [{ message: "Unexpected Unicode BOM (Byte Order Mark).", type: "Program" }]
+            errors: [unexpectedError]
         }
     ]
 });
