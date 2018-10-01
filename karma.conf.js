9bc3f7c5a13b5d4ff8fa82ca3bce37129d1c7676
ossdev07 <39188636+ossdev07@users.noreply.github.com>
2018-10-01 22:19:01 +0530

    Build: Use karma-chrome-launcher to run tests (#10898)
    Removed phantomjs dependency from package.json and
    karma.conf.js.
    
    Signed-off-by: ossdev07 <ossdev@puresoftware.com>

--- karma.conf.js
+++ karma.conf.js
@@ -67,8 +67,13 @@ module.exports = function(config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: ["PhantomJS"],
-
+        browsers: ["HeadlessChrome"],
+        customLaunchers: {
+          HeadlessChrome: {
+            base: 'ChromeHeadless',
+            flags: [ '--no-sandbox', ],
+             },
+            },
 
         // Continuous Integration mode
         // if true, Karma captures browsers, runs the tests and exits
