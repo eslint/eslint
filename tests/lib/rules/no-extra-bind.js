94399729fddeab5c058332cf7abc758be83c3658
Kevin Partington <kevin.partington@kernelpanicstudios.com>
2018-10-04 19:03:36 -0500

    Fix: no-extra-bind No autofix if arg may have side effect (fixes #10846)

--- tests/lib/rules/no-extra-bind.js
+++ tests/lib/rules/no-extra-bind.js
@@ -74,6 +74,23 @@ ruleTester.run("no-extra-bind", rule, {
             code: "var a = function() { (function(){ (function(){ this.d }.bind(c)) }) }.bind(b)",
             output: "var a = function() { (function(){ (function(){ this.d }.bind(c)) }) }",
             errors: [{ messageId: "unexpected", type: "CallExpression", column: 71 }]
+        },
+
+        // Should not autofix if bind expression args have side effects
+        {
+            code: "var a = function() {}.bind(b++)",
+            output: null,
+            errors
+        },
+        {
+            code: "var a = function() {}.bind(b())",
+            output: null,
+            errors
+        },
+        {
+            code: "var a = function() {}.bind(b.c)",
+            output: null,
+            errors
         }
     ]
 });
