7433d644ef0552d0ccef81bba7fcb10b91ccf0ee
Alex Grasley <agrasley@marketo.com>
2018-12-16 12:27:45 -0800

    New: no-useless-catch rule (fixes #11174)
    Moves no-useless-catch from an internal rule to a core rule. Adds docs
    and test cases.

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,8 +23,7 @@ module.exports = {
         "eslint-plugin/report-message-format": ["error", "[^a-z].*\\.$"],
         "eslint-plugin/test-case-property-ordering": "error",
         "eslint-plugin/test-case-shorthand-strings": "error",
-        "rulesdir/multiline-comment-style": "error",
-        "rulesdir/no-useless-catch": "error"
+        "rulesdir/multiline-comment-style": "error"
     },
     overrides: [
         {
