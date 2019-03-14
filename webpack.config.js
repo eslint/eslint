6685a48226a12163d2ca98187ec51f49b7eebf4b
Teddy Katz <teddy.katz@gmail.com>
2019-03-13 20:30:47 -0400

    Don't compile lodash

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,8 @@ module.exports = {
                 loader: "babel-loader",
                 options: {
                     presets: ["@babel/preset-env"]
-                }
+                },
+                exclude: /node_modules\/lodash/u
             }
         ]
     },
