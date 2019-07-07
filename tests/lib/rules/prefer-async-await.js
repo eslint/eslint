3202cb789df3a96348504f5f2ac9ce2b0dca6c1e
Alexander Lichter <manniL@gmx.net>
2019-07-07 12:18:43 +0200

    Fix: lint and testing

--- tests/lib/rules/prefer-async-await.js
+++ tests/lib/rules/prefer-async-await.js
@@ -7,34 +7,34 @@
 "use strict";
 
 const rule = require("../../../lib/rules/prefer-async-await");
-const RuleTester = require("../../../lib/testers/rule-tester");
+const { RuleTester } = require("../../../lib/rule-tester");
 
 const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2017 } });
 
 ruleTester.run("prefer-async-await", rule, {
-  valid: [
-    "async function a() { await getData(); }",
-    "async function hi() { await thing() }",
-    "async function hi() { await thing().catch() }",
-    "a = async () => (await something())"
-  ],
-  invalid: [
-    { code: "getData().then(() => {});", errors: [{ messageId: "unexpected" }] },
-    {
-      code: "function foo() { hey.then(x => {}) }",
-      errors: [{ messageId: "unexpected" }]
-    },
-    {
-      code: "function foo() { hey.then(function() { }).then() }",
-      errors: [{ messageId: "unexpected" }, { messageId: "unexpected" }]
-    },
-    {
-      code: "function foo() { hey.then(function() { }).then(x).catch() }",
-      errors: [{ messageId: "unexpected" }, { messageId: "unexpected" }]
-    },
-    {
-      code: "async function a() { hey.then(function() { }).then(function() { }) }",
-      errors: [{ messageId: "unexpected" }, { messageId: "unexpected" }]
-    }
-  ]
+    valid: [
+        "async function a() { await getData(); }",
+        "async function hi() { await thing() }",
+        "async function hi() { await thing().catch() }",
+        "a = async () => (await something())"
+    ],
+    invalid: [
+        { code: "getData().then(() => {});", errors: [{ messageId: "unexpected" }] },
+        {
+            code: "function foo() { hey.then(x => {}) }",
+            errors: [{ messageId: "unexpected" }]
+        },
+        {
+            code: "function foo() { hey.then(function() { }).then() }",
+            errors: [{ messageId: "unexpected" }, { messageId: "unexpected" }]
+        },
+        {
+            code: "function foo() { hey.then(function() { }).then(x).catch() }",
+            errors: [{ messageId: "unexpected" }, { messageId: "unexpected" }]
+        },
+        {
+            code: "async function a() { hey.then(function() { }).then(function() { }) }",
+            errors: [{ messageId: "unexpected" }, { messageId: "unexpected" }]
+        }
+    ]
 });
