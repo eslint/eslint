1a36d5226562ae4c20a25f7d79664b2404e5dbed
Milos Djermanovic <milos.djermanovic@gmail.com>
2019-07-08 03:40:14 +0200

    Add await and yield options, clarify docs

--- docs/rules/prefer-async-await.md
+++ docs/rules/prefer-async-await.md
@@ -1,10 +1,15 @@
-# Require using the `async`/`await` syntax instead of the Promise methods (prefer-async-await)
+# Require using the `async`/`await` syntax instead of the Promise prototype methods (prefer-async-await)
 
 With the addition of the `async`/`await` syntax in ECMAScript 2017, one can write asynchronous code
 in a synchronous-like manner.
 
-This rule restricts the use of the Promise methods in favor of the `async`/`await` syntax,
+This rule restricts the use of the Promise prototype methods in favor of the `async`/`await` syntax,
 which is easier to write and to read afterwards.
+
+Namely, restricted methods are: `.then()`, `.catch()` and `.finally()`.
+
+Static methods, such as `Promise.all()`, are **not** restricted.
+You should still use `Promise.all()` for parallel execution.
 
 ## Rule Details
 
@@ -16,25 +21,25 @@ Examples of **incorrect** code for this rule:
 /*eslint prefer-async-await: "error"*/
 
 function show(id) {
-  getBook()
-      .then(book => {
-          getAuthor(book.authorId)
-              .then((author) => {
-                  display(book, author)
-              })
-      })
+    return getBook()
+        .then(book => {
+            return getAuthor(book.authorId)
+                .then((author) => {
+                    display(book, author);
+                });
+        });
 }
 
 function show(id) {
-  let book;
-  getBook()
-      .then(b => {
-          book = b;
-          return getAuthor(book.authorId);
-      })
-      .then((author) => {
-          display(book, author);
-      })
+    let book;
+    return getBook()
+        .then(b => {
+            book = b;
+            return getAuthor(book.authorId);
+        })
+        .then((author) => {
+            display(book, author);
+        });
 }
 ```
 
@@ -44,25 +49,71 @@ Examples of **correct** code for this rule:
 /*eslint prefer-async-await: "error"*/
 
 async function show(id) {
-  const book = await getBook();
-  const author = await getAuthor(book.authorId);
-  display(book, author);
+    const book = await getBook();
+    const author = await getAuthor(book.authorId);
+    display(book, author);
+}
+```
+
+### Promise.all()
+
+`Promise.all()` can be used along with the `async`/`await` syntax.
+
+Example of **incorrect** code for this rule:
+
+```js
+/*eslint prefer-async-await: "error"*/
+
+function foo() {
+    return Promise.all([
+        asyncFunc1(),
+        asyncFunc2(),
+    ]).then(([result1, result2]) => {
+        doSomething(result1, result2);
+    });
+}
+```
+
+Example of **correct** code for this rule:
+
+```js
+/*eslint prefer-async-await: "error"*/
+
+async function foo() {
+    const [result1, result2] = await Promise.all([
+        asyncFunc1(),
+        asyncFunc2(),
+    ]);
+    doSomething(result1, result2);
+}
+```
+
+Note that the following code is **not equivalent** to the previous two examples.
+In this example, the function calls are executed **sequentially**:
+
+```js
+async function foo() {
+    const result1 = await asyncFunc1();
+    const result2 = await asyncFunc2();
+    doSomething(result1, result2);
 }
 ```
 
 ## Options
 
-This rule has an object option with two options:
+This rule has an object option with four options:
 
-* `"atTopLevel": false` allows promise method calls at the top level
-* `"inGetOrSet": false` allows promise method calls in getters and setters
+* `"atTopLevel": false` allows Promise prototype method calls at the top level
+* `"inGetOrSet": false` allows Promise prototype method calls in getters and setters
+* `"inAwaitExpressions": false` allows Promise prototype method calls in await expressions
+* `"inYieldExpressions": false` allows Promise prototype method calls in yield expressions
 
 ### atTopLevel
 
 The `await` expression cannot be used at the top level, yet. An alternative is to
 wrap the expression in an async IIFE.
 
-If you want to allow Promise method calls at the top level, set this option to `false`.
+If you want to allow Promise prototype method calls at the top level, set this option to `false`.
 
 Examples of **incorrect** code for this rule with `"atTopLevel"` option set to `true` (default):
 
@@ -96,7 +147,7 @@ getSomething().then(x => doSomething(x));
 If you consume promises in getters or setters, switching to the `async`/`await` syntax would require
 certain refactoring, since the getters and setters cannot be async.
 
-If you want to allow Promise method calls in getters and setters, set this option to `false`.
+If you want to allow Promise prototype method calls in getters and setters, set this option to `false`.
 
 Examples of **incorrect** code for this rule with `"inGetOrSet"` option set to `true` (default):
 
@@ -119,6 +170,60 @@ class foo {
     set bar(x) {
         doSomething(x).then(y => this.y = y);
     }
+}
+```
+
+### inAwaitExpressions
+
+If you want to allow Promise prototype method calls in `await` expressions, set this option to `false`.
+
+Examples of **incorrect** code for this rule with `"inAwaitExpressions"` option set to `true` (default):
+
+```js
+/*eslint prefer-async-await: ["error", {"inAwaitExpressions": true}]*/
+
+async function foo() {
+    const x = await getSomething().catch(err => {});
+    if (x) {
+        doSomething();
+    }
+}
+```
+
+Examples of **correct** code for this rule with `"inAwaitExpressions"` option set to `false`:
+
+```js
+/*eslint prefer-async-await: ["error", {"inAwaitExpressions": false}]*/
+
+async function foo() {
+    const x = await getSomething().catch(err => {});
+    if (x) {
+        doSomething();
+    }
+}
+```
+
+### inYieldExpressions
+
+If you want to allow Promise prototype method calls in `yield` expressions, set this option to `false`.
+
+Examples of **incorrect** code for this rule with `"inYieldExpressions"` option set to `true` (default):
+
+```js
+/*eslint prefer-async-await: ["error", {"inYieldExpressions": true}]*/
+
+function *foo() {
+    yield getSomething().catch(err => {});
+}
+```
+
+Examples of **correct** code for this rule with `"inYieldExpressions"` option set to `false`:
+
+```js
+/*eslint prefer-async-await: ["error", {"inYieldExpressions": false}]*/
+
+function *foo() {
+    yield getSomething().catch(err => {});
 }
 ```
 
--- tests/lib/rules/prefer-async-await.js
+++ tests/lib/rules/prefer-async-await.js
@@ -54,12 +54,57 @@ ruleTester.run("prefer-async-await", rule, {
         {
             code: "class foo { get a() { bar.then(); } set a(b) { baz.then(); } }",
             options: [{ inGetOrSet: false }],
-            parserOptions: { ecmaVersion: 6 }
+            parserOptions: { ecmaVersion: 2015 }
         },
         {
             code: "class foo { get a() { bar.then(); } set b(c) { baz.then(); } }",
             options: [{ inGetOrSet: false }],
-            parserOptions: { ecmaVersion: 6 }
+            parserOptions: { ecmaVersion: 2015 }
+        },
+        {
+            code: "async function foo() { await bar.then(); }",
+            options: [{ inAwaitExpressions: false }],
+            parserOptions: { ecmaVersion: 2017 }
+        },
+        {
+            code: "async function foo() { await bar.catch(); }",
+            options: [{ inAwaitExpressions: false }],
+            parserOptions: { ecmaVersion: 2017 }
+        },
+        {
+            code: "async function foo() { await bar.baz.then(); }",
+            options: [{ inAwaitExpressions: false }],
+            parserOptions: { ecmaVersion: 2017 }
+        },
+        {
+            code: "async function foo() { await bar().then().baz().catch().finally(); }",
+            options: [{ inAwaitExpressions: false }],
+            parserOptions: { ecmaVersion: 2017 }
+        },
+        {
+            code: "function *foo() { yield bar.then(); }",
+            options: [{ inYieldExpressions: false }],
+            parserOptions: { ecmaVersion: 2015 }
+        },
+        {
+            code: "function *foo() { yield bar.catch(); }",
+            options: [{ inYieldExpressions: false }],
+            parserOptions: { ecmaVersion: 2015 }
+        },
+        {
+            code: "function *foo() { yield bar.baz.then(); }",
+            options: [{ inYieldExpressions: false }],
+            parserOptions: { ecmaVersion: 2015 }
+        },
+        {
+            code: "function *foo() { yield bar().then().baz().catch().finally(); }",
+            options: [{ inYieldExpressions: false }],
+            parserOptions: { ecmaVersion: 2015 }
+        },
+        {
+            code: "function *foo() { yield 1 + bar.then() + 2; }",
+            options: [{ inYieldExpressions: false }],
+            parserOptions: { ecmaVersion: 2015 }
         }
     ],
     invalid: [
@@ -154,7 +199,7 @@ ruleTester.run("prefer-async-await", rule, {
         {
             code: "function foo() { bar.then(); }",
             options: [{ atTopLevel: false, inGetOrSet: false }],
-            parserOptions: { ecmaVersion: 6 },
+            parserOptions: { ecmaVersion: 2015 },
             errors: [{
                 messageId: "promiseMethodCall",
                 type: "CallExpression"
@@ -163,7 +208,7 @@ ruleTester.run("prefer-async-await", rule, {
         {
             code: "const foo = function() { bar.then(); }",
             options: [{ atTopLevel: false, inGetOrSet: false }],
-            parserOptions: { ecmaVersion: 6 },
+            parserOptions: { ecmaVersion: 2015 },
             errors: [{
                 messageId: "promiseMethodCall",
                 type: "CallExpression"
@@ -172,7 +217,7 @@ ruleTester.run("prefer-async-await", rule, {
         {
             code: "const foo = () => { bar.then(); }",
             options: [{ atTopLevel: false, inGetOrSet: false }],
-            parserOptions: { ecmaVersion: 6 },
+            parserOptions: { ecmaVersion: 2015 },
             errors: [{
                 messageId: "promiseMethodCall",
                 type: "CallExpression"
@@ -181,7 +226,7 @@ ruleTester.run("prefer-async-await", rule, {
         {
             code: "const foo = { a() { bar.then(); } }",
             options: [{ atTopLevel: false, inGetOrSet: false }],
-            parserOptions: { ecmaVersion: 6 },
+            parserOptions: { ecmaVersion: 2015 },
             errors: [{
                 messageId: "promiseMethodCall",
                 type: "CallExpression"
@@ -190,7 +235,7 @@ ruleTester.run("prefer-async-await", rule, {
         {
             code: "class foo { a() { bar.then(); } }",
             options: [{ atTopLevel: false, inGetOrSet: false }],
-            parserOptions: { ecmaVersion: 6 },
+            parserOptions: { ecmaVersion: 2015 },
             errors: [{
                 messageId: "promiseMethodCall",
                 type: "CallExpression"
@@ -242,7 +287,7 @@ ruleTester.run("prefer-async-await", rule, {
         },
         {
             code: "class foo { get a() { bar.then(); } set a(b) { baz.then(); } }",
-            parserOptions: { ecmaVersion: 6 },
+            parserOptions: { ecmaVersion: 2015 },
             errors: [
                 {
                     messageId: "promiseMethodCall",
@@ -285,12 +330,98 @@ ruleTester.run("prefer-async-await", rule, {
         {
             code: "class foo { get a() { bar.then(); } set a(b) { baz.then(); } }",
             options: [{ inGetOrSet: true }],
-            parserOptions: { ecmaVersion: 6 },
+            parserOptions: { ecmaVersion: 2015 },
             errors: [
                 {
                     messageId: "promiseMethodCall",
                     type: "CallExpression"
                 },
+                {
+                    messageId: "promiseMethodCall",
+                    type: "CallExpression"
+                }
+            ]
+        },
+        {
+            code: "async function foo() { await bar.then(); }",
+            parserOptions: { ecmaVersion: 2017 },
+            errors: [
+                {
+                    messageId: "promiseMethodCall",
+                    type: "CallExpression"
+                }
+            ]
+        },
+        {
+            code: "async function foo() { await bar.then(); }",
+            options: [{ inAwaitExpressions: true }],
+            parserOptions: { ecmaVersion: 2017 },
+            errors: [
+                {
+                    messageId: "promiseMethodCall",
+                    type: "CallExpression"
+                }
+            ]
+        },
+        {
+            code: "async function foo() { await (() => bar.then()) }",
+            options: [{ inAwaitExpressions: false }],
+            parserOptions: { ecmaVersion: 2017 },
+            errors: [
+                {
+                    messageId: "promiseMethodCall",
+                    type: "CallExpression"
+                }
+            ]
+        },
+        {
+            code: "async function foo() { await bar.then(); }",
+            options: [{ inAwaitExpressions: true, inYieldExpressions: false }],
+            parserOptions: { ecmaVersion: 2017 },
+            errors: [
+                {
+                    messageId: "promiseMethodCall",
+                    type: "CallExpression"
+                }
+            ]
+        },
+        {
+            code: "function *foo() { yield bar.then(); }",
+            parserOptions: { ecmaVersion: 2015 },
+            errors: [
+                {
+                    messageId: "promiseMethodCall",
+                    type: "CallExpression"
+                }
+            ]
+        },
+        {
+            code: "function *foo() { yield bar.then(); }",
+            options: [{ inYieldExpressions: true }],
+            parserOptions: { ecmaVersion: 2015 },
+            errors: [
+                {
+                    messageId: "promiseMethodCall",
+                    type: "CallExpression"
+                }
+            ]
+        },
+        {
+            code: "function *foo() { yield (() => bar.then()) }",
+            options: [{ inYieldExpressions: false }],
+            parserOptions: { ecmaVersion: 2015 },
+            errors: [
+                {
+                    messageId: "promiseMethodCall",
+                    type: "CallExpression"
+                }
+            ]
+        },
+        {
+            code: "function *foo() { yield bar.then(); }",
+            options: [{ inAwaitExpressions: false, inYieldExpressions: true }],
+            parserOptions: { ecmaVersion: 2015 },
+            errors: [
                 {
                     messageId: "promiseMethodCall",
                     type: "CallExpression"
