4985bfd5f7a3f97fbcab5b31b8b2cad2d9d727fb
Toru Nagashima <public@mysticatea.dev>
2019-07-14 09:52:30 +0900

    Fix: no-new-wrappers reports only wrappers

--- tests/lib/rules/no-new-wrappers.js
+++ tests/lib/rules/no-new-wrappers.js
@@ -26,8 +26,6 @@ ruleTester.run("no-new-wrappers", rule, {
     invalid: [
         { code: "var a = new String('hello');", errors: [{ message: "Do not use String as a constructor.", type: "NewExpression" }] },
         { code: "var a = new Number(10);", errors: [{ message: "Do not use Number as a constructor.", type: "NewExpression" }] },
-        { code: "var a = new Boolean(false);", errors: [{ message: "Do not use Boolean as a constructor.", type: "NewExpression" }] },
-        { code: "var a = new Math();", errors: [{ message: "Do not use Math as a constructor.", type: "NewExpression" }] },
-        { code: "var a = new JSON({ myProp: 10 });", errors: [{ message: "Do not use JSON as a constructor.", type: "NewExpression" }] }
+        { code: "var a = new Boolean(false);", errors: [{ message: "Do not use Boolean as a constructor.", type: "NewExpression" }] }
     ]
 });
