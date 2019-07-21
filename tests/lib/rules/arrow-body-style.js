824d74ff30095c11459701620ce33e425c20f74e
Pig Fang <g-plane@hotmail.com>
2019-07-21 10:18:05 +0800

    Fix: add parens for sequence expr in arrow-body-style (fixes #11917) (#11918)

--- tests/lib/rules/arrow-body-style.js
+++ tests/lib/rules/arrow-body-style.js
@@ -163,6 +163,18 @@ ruleTester.run("arrow-body-style", rule, {
             ]
         },
         {
+            code: "var foo = () => { return a, b }",
+            output: "var foo = () => (a, b)",
+            errors: [
+                {
+                    line: 1,
+                    column: 17,
+                    type: "ArrowFunctionExpression",
+                    messageId: "unexpectedSingleBlock"
+                }
+            ]
+        },
+        {
             code: "var foo = () => { return };",
             output: null, // not fixed
             options: ["as-needed", { requireReturnForObjectLiteral: true }],
