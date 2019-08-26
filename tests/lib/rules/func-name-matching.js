197f4432fca70a574028e5568c48afad12213224
Milos Djermanovic <milos.djermanovic@gmail.com>
2019-08-26 19:27:28 +0200

    Fix: func-name-matching crash on descriptor-like arguments (#12100)

--- tests/lib/rules/func-name-matching.js
+++ tests/lib/rules/func-name-matching.js
@@ -255,6 +255,10 @@ ruleTester.run("func-name-matching", rule, {
             code: "Reflect.defineProperty(foo, 'bar', { value() {} })",
             options: ["never", { considerPropertyDescriptor: true }],
             parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "foo({ value: function value() {} })",
+            options: ["always", { considerPropertyDescriptor: true }]
         }
     ],
     invalid: [
@@ -445,6 +449,13 @@ ruleTester.run("func-name-matching", rule, {
             options: ["never", { considerPropertyDescriptor: true }],
             errors: [
                 { messageId: "notMatchProperty", data: { funcName: "bar", name: "bar" } }
+            ]
+        },
+        {
+            code: "foo({ value: function bar() {} })",
+            options: ["always", { considerPropertyDescriptor: true }],
+            errors: [
+                { messageId: "matchProperty", data: { funcName: "bar", name: "value" } }
             ]
         }
     ]
