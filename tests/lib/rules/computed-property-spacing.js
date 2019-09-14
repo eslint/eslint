ab017b81daa3c9f19f4450a2d514692c6353d21c
Milos Djermanovic <milos.djermanovic@gmail.com>
2019-09-14 02:52:55 +0200

    Update: enforceForClassMembers computed-property-spacing (fixes #12049) (#12214)

--- tests/lib/rules/computed-property-spacing.js
+++ tests/lib/rules/computed-property-spacing.js
@@ -72,7 +72,118 @@ ruleTester.run("computed-property-spacing", rule, {
 
         // never - unrelated cases
         { code: "var foo = {};", options: ["never"] },
-        { code: "var foo = [];", options: ["never"] }
+        { code: "var foo = [];", options: ["never"] },
+
+        //------------------------------------------------------------------------------
+        // Classes
+        //------------------------------------------------------------------------------
+
+        // test default settings
+        {
+            code: "class A { [ a ](){} }",
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "class A { [ a ](){} get [ b ](){} set [ c ](foo){} static [ d ](){} static get [ e ](){} static set [ f ](bar){} }",
+            options: ["never"],
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "A = class { [ a ](){} get [ b ](){} set [ c ](foo){} static [ d ](){} static get [ e ](){} static set [ f ](bar){} }",
+            options: ["never", {}],
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "A = class { [a](){} }",
+            options: ["always"],
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "A = class { [a](){} get [b](){} set [c](foo){} static [d](){} static get [e](){} static set [f](bar){} }",
+            options: ["always"],
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "class A { [a](){} get [b](){} set [c](foo){} static [d](){} static get [e](){} static set [f](bar){} }",
+            options: ["always", {}],
+            parserOptions: { ecmaVersion: 6 }
+        },
+
+        // explicitly disabled option
+        {
+            code: "class A { [ a ](){} }",
+            options: ["never", { enforceForClassMembers: false }],
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "A = class { [ a ](){} get [ b ](){} set [ c ](foo){} static [ d ](){} static get [ e ](){} static set [ f ](bar){} }",
+            options: ["never", { enforceForClassMembers: false }],
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "A = class { [a](){} }",
+            options: ["always", { enforceForClassMembers: false }],
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "class A { [a](){} get [b](){} set [b](foo){} static [c](){} static get [d](){} static set [d](bar){} }",
+            options: ["always", { enforceForClassMembers: false }],
+            parserOptions: { ecmaVersion: 6 }
+        },
+
+        // valid spacing
+        {
+            code: "A = class { [a](){} }",
+            options: ["never", { enforceForClassMembers: true }],
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "class A { [a] ( ) { } }",
+            options: ["never", { enforceForClassMembers: true }],
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "A = class { [ \n a \n ](){} }",
+            options: ["never", { enforceForClassMembers: true }],
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "class A { [a](){} get [b](){} set [b](foo){} static [c](){} static get [d](){} static set [d](bar){} }",
+            options: ["never", { enforceForClassMembers: true }],
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "class A { [ a ](){} }",
+            options: ["always", { enforceForClassMembers: true }],
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "class A { [ a ](){}[ b ](){} }",
+            options: ["always", { enforceForClassMembers: true }],
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "A = class { [\na\n](){} }",
+            options: ["always", { enforceForClassMembers: true }],
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "A = class { [ a ](){} get [ b ](){} set [ c ](foo){} static [ d ](){} static get [ e ](){} static set [ f ](bar){} }",
+            options: ["always", { enforceForClassMembers: true }],
+            parserOptions: { ecmaVersion: 6 }
+        },
+
+        // non-computed
+        {
+            code: "class A { a ( ) { } get b(){} set b ( foo ){} static c (){} static get d() {} static set d( bar ) {} }",
+            options: ["never", { enforceForClassMembers: true }],
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: "A = class {a(){}get b(){}set b(foo){}static c(){}static get d(){}static set d(bar){}}",
+            options: ["always", { enforceForClassMembers: true }],
+            parserOptions: { ecmaVersion: 6 }
+        }
     ],
 
     invalid: [
@@ -322,7 +433,330 @@ ruleTester.run("computed-property-spacing", rule, {
                     line: 1
                 }
             ]
-        }
+        },
 
+        //------------------------------------------------------------------------------
+        // Classes
+        //------------------------------------------------------------------------------
+
+        // never
+        {
+            code: "class A { [ a](){} }",
+            output: "class A { [a](){} }",
+            options: ["never", { enforceForClassMembers: true }],
+            parserOptions: { ecmaVersion: 6 },
+            errors: [
+                {
+                    messageId: "unexpectedSpaceAfter",
+                    data: { tokenValue: "[" },
+                    type: "MethodDefinition",
+                    column: 11,
+                    line: 1
+                }
+            ]
+        },
+        {
+            code: "A = class { [a](){} b(){} static [c ](){} static [d](){}}",
+            output: "A = class { [a](){} b(){} static [c](){} static [d](){}}",
+            options: ["never", { enforceForClassMembers: true }],
+            parserOptions: { ecmaVersion: 6 },
+            errors: [
+                {
+                    messageId: "unexpectedSpaceBefore",
+                    data: { tokenValue: "]" },
+                    type: "MethodDefinition",
+                    column: 37,
+                    line: 1
+                }
+            ]
+        },
+        {
+            code: "class A { get [a ](){} set [ a](foo){} get b(){} static set b(bar){} static get [ a](){} static set [a ](baz){} }",
+            output: "class A { get [a](){} set [a](foo){} get b(){} static set b(bar){} static get [a](){} static set [a](baz){} }",
+            options: ["never", { enforceForClassMembers: true }],
+            parserOptions: { ecmaVersion: 6 },
+            errors: [
+                {
+                    messageId: "unexpectedSpaceBefore",
+                    data: { tokenValue: "]" },
+                    type: "MethodDefinition",
+                    column: 18,
+                    line: 1
+                },
+                {
+                    messageId: "unexpectedSpaceAfter",
+                    data: { tokenValue: "[" },
+                    type: "MethodDefinition",
+                    column: 28,
+                    line: 1
+                },
+                {
+                    messageId: "unexpectedSpaceAfter",
+                    data: { tokenValue: "[" },
+                    type: "MethodDefinition",
+                    column: 81,
+                    line: 1
+                },
+                {
+                    messageId: "unexpectedSpaceBefore",
+                    data: { tokenValue: "]" },
+                    type: "MethodDefinition",
+                    column: 104,
+                    line: 1
+                }
+            ]
+        },
+        {
+            code: "A = class { [ a ](){} get [ b ](){} set [ c ](foo){} static [ d ](){} static get [ e ](){} static set [ f ](bar){} }",
+            output: "A = class { [a](){} get [b](){} set [c](foo){} static [d](){} static get [e](){} static set [f](bar){} }",
+            options: ["never", { enforceForClassMembers: true }],
+            parserOptions: { ecmaVersion: 6 },
+            errors: [
+                {
+                    messageId: "unexpectedSpaceAfter",
+                    data: { tokenValue: "[" },
+                    type: "MethodDefinition",
+                    column: 13,
+                    line: 1
+                },
+                {
+                    messageId: "unexpectedSpaceBefore",
+                    data: { tokenValue: "]" },
+                    type: "MethodDefinition",
+                    column: 17,
+                    line: 1
+                },
+                {
+                    messageId: "unexpectedSpaceAfter",
+                    data: { tokenValue: "[" },
+                    type: "MethodDefinition",
+                    column: 27,
+                    line: 1
+                },
+                {
+                    messageId: "unexpectedSpaceBefore",
+                    data: { tokenValue: "]" },
+                    type: "MethodDefinition",
+                    column: 31,
+                    line: 1
+                },
+                {
+                    messageId: "unexpectedSpaceAfter",
+                    data: { tokenValue: "[" },
+                    type: "MethodDefinition",
+                    column: 41,
+                    line: 1
+                },
+                {
+                    messageId: "unexpectedSpaceBefore",
+                    data: { tokenValue: "]" },
+                    type: "MethodDefinition",
+                    column: 45,
+                    line: 1
+                },
+                {
+                    messageId: "unexpectedSpaceAfter",
+                    data: { tokenValue: "[" },
+                    type: "MethodDefinition",
+                    column: 61,
+                    line: 1
+                },
+                {
+                    messageId: "unexpectedSpaceBefore",
+                    data: { tokenValue: "]" },
+                    type: "MethodDefinition",
+                    column: 65,
+                    line: 1
+                },
+                {
+                    messageId: "unexpectedSpaceAfter",
+                    data: { tokenValue: "[" },
+                    type: "MethodDefinition",
+                    column: 82,
+                    line: 1
+                },
+                {
+                    messageId: "unexpectedSpaceBefore",
+                    data: { tokenValue: "]" },
+                    type: "MethodDefinition",
+                    column: 86,
+                    line: 1
+                },
+                {
+                    messageId: "unexpectedSpaceAfter",
+                    data: { tokenValue: "[" },
+                    type: "MethodDefinition",
+                    column: 103,
+                    line: 1
+                },
+                {
+                    messageId: "unexpectedSpaceBefore",
+                    data: { tokenValue: "]" },
+                    type: "MethodDefinition",
+                    column: 107,
+                    line: 1
+                }
+            ]
+        },
+
+        // always
+        {
+            code: "class A { [ a](){} }",
+            output: "class A { [ a ](){} }",
+            options: ["always", { enforceForClassMembers: true }],
+            parserOptions: { ecmaVersion: 6 },
+            errors: [
+                {
+                    messageId: "missingSpaceBefore",
+                    data: { tokenValue: "]" },
+                    type: "MethodDefinition",
+                    column: 14,
+                    line: 1
+                }
+            ]
+        },
+        {
+            code: "A = class { [ a ](){} b(){} static [c ](){} static [ d ](){}}",
+            output: "A = class { [ a ](){} b(){} static [ c ](){} static [ d ](){}}",
+            options: ["always", { enforceForClassMembers: true }],
+            parserOptions: { ecmaVersion: 6 },
+            errors: [
+                {
+                    messageId: "missingSpaceAfter",
+                    data: { tokenValue: "[" },
+                    type: "MethodDefinition",
+                    column: 36,
+                    line: 1
+                }
+            ]
+        },
+        {
+            code: "class A { get [a ](){} set [ a](foo){} get b(){} static set b(bar){} static get [ a](){} static set [a ](baz){} }",
+            output: "class A { get [ a ](){} set [ a ](foo){} get b(){} static set b(bar){} static get [ a ](){} static set [ a ](baz){} }",
+            options: ["always", { enforceForClassMembers: true }],
+            parserOptions: { ecmaVersion: 6 },
+            errors: [
+                {
+                    messageId: "missingSpaceAfter",
+                    data: { tokenValue: "[" },
+                    type: "MethodDefinition",
+                    column: 15,
+                    line: 1
+                },
+                {
+                    messageId: "missingSpaceBefore",
+                    data: { tokenValue: "]" },
+                    type: "MethodDefinition",
+                    column: 31,
+                    line: 1
+                },
+                {
+                    messageId: "missingSpaceBefore",
+                    data: { tokenValue: "]" },
+                    type: "MethodDefinition",
+                    column: 84,
+                    line: 1
+                },
+                {
+                    messageId: "missingSpaceAfter",
+                    data: { tokenValue: "[" },
+                    type: "MethodDefinition",
+                    column: 101,
+                    line: 1
+                }
+            ]
+        },
+        {
+            code: "A = class { [a](){} get [b](){} set [c](foo){} static [d](){} static get [e](){} static set [f](bar){} }",
+            output: "A = class { [ a ](){} get [ b ](){} set [ c ](foo){} static [ d ](){} static get [ e ](){} static set [ f ](bar){} }",
+            options: ["always", { enforceForClassMembers: true }],
+            parserOptions: { ecmaVersion: 6 },
+            errors: [
+                {
+                    messageId: "missingSpaceAfter",
+                    data: { tokenValue: "[" },
+                    type: "MethodDefinition",
+                    column: 13,
+                    line: 1
+                },
+                {
+                    messageId: "missingSpaceBefore",
+                    data: { tokenValue: "]" },
+                    type: "MethodDefinition",
+                    column: 15,
+                    line: 1
+                },
+                {
+                    messageId: "missingSpaceAfter",
+                    data: { tokenValue: "[" },
+                    type: "MethodDefinition",
+                    column: 25,
+                    line: 1
+                },
+                {
+                    messageId: "missingSpaceBefore",
+                    data: { tokenValue: "]" },
+                    type: "MethodDefinition",
+                    column: 27,
+                    line: 1
+                },
+                {
+                    messageId: "missingSpaceAfter",
+                    data: { tokenValue: "[" },
+                    type: "MethodDefinition",
+                    column: 37,
+                    line: 1
+                },
+                {
+                    messageId: "missingSpaceBefore",
+                    data: { tokenValue: "]" },
+                    type: "MethodDefinition",
+                    column: 39,
+                    line: 1
+                },
+                {
+                    messageId: "missingSpaceAfter",
+                    data: { tokenValue: "[" },
+                    type: "MethodDefinition",
+                    column: 55,
+                    line: 1
+                },
+                {
+                    messageId: "missingSpaceBefore",
+                    data: { tokenValue: "]" },
+                    type: "MethodDefinition",
+                    column: 57,
+                    line: 1
+                },
+                {
+                    messageId: "missingSpaceAfter",
+                    data: { tokenValue: "[" },
+                    type: "MethodDefinition",
+                    column: 74,
+                    line: 1
+                },
+                {
+                    messageId: "missingSpaceBefore",
+                    data: { tokenValue: "]" },
+                    type: "MethodDefinition",
+                    column: 76,
+                    line: 1
+                },
+                {
+                    messageId: "missingSpaceAfter",
+                    data: { tokenValue: "[" },
+                    type: "MethodDefinition",
+                    column: 93,
+                    line: 1
+                },
+                {
+                    messageId: "missingSpaceBefore",
+                    data: { tokenValue: "]" },
+                    type: "MethodDefinition",
+                    column: 95,
+                    line: 1
+                }
+            ]
+        }
     ]
 });
