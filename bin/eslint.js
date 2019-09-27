7d704f4f55ea7be6aa104f9d278383b5a265c6a6
Barrie Treloar <baerrach@gmail.com>
2019-09-27 11:28:11 +0930

    Fix: set encoding to utf for stdin, use string concat for chunking

--- bin/eslint.js
+++ bin/eslint.js
@@ -79,20 +79,19 @@ if (useStdIn) {
      *
      * Using the nodejs code example for reading from stdin.
      */
-    const input = [];
+    let contents = "",
+        chunk = "";
 
+    process.stdin.setEncoding("utf8");
     process.stdin.on("readable", () => {
-        let chunk;
 
         // Use a loop to make sure we read all available data.
         while ((chunk = process.stdin.read()) !== null) {
-            input.push(chunk);
+            contents += chunk;
         }
     });
 
     process.stdin.on("end", () => {
-        const contents = input.join("");
-
         process.exitCode = cli.execute(process.argv, contents, "utf8");
     });
 } else if (init) {
