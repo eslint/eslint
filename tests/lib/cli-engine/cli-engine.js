8e67006731d28fd82922e67f576acbbbd715d208
Kai Cataldo <kaicataldo@gmail.com>
2019-09-28 01:24:55 -0400

    Chore: fix test

--- tests/lib/cli-engine/cli-engine.js
+++ tests/lib/cli-engine/cli-engine.js
@@ -1544,7 +1544,11 @@ describe("CLIEngine", () => {
             engine = new CLIEngine({
                 cwd: originalDir,
                 configFile: ".eslintrc.js",
-                rules: { "indent-legacy": 1 }
+                rules: {
+                    "indent-legacy": 1,
+                    "require-jsdoc": 1,
+                    "valid-jsdoc": 1
+                }
             });
 
             const report = engine.executeOnFiles(["lib/cli*.js"]);
