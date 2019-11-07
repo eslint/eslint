d679dd89cfc672252eee3396c506dd45ed28f1d5
yeonjuan <yeonjuan93@naver.com>
2019-11-07 21:15:02 +0900

    Fix: invalid autofix in function-call-argument-newline (fixes #12454)

--- tests/lib/rules/function-call-argument-newline.js
+++ tests/lib/rules/function-call-argument-newline.js
@@ -505,6 +505,50 @@ ruleTester.run("function-call-argument-newline", rule, {
                     endColumn: 1
                 }
             ]
+        },
+        {
+            code: "fn(a,// comment\n{b, c})",
+            output: null,
+            options: ["never"],
+            parserOptions: { ecmaVersion: 6 },
+            errors: [
+                {
+                    messageId: "unexpectedLineBreak",
+                    line: 1,
+                    column: 16,
+                    endLine: 2,
+                    endColumn: 1
+                }
+            ]
+        },
+        {
+            code: "fn(a, // comment\nb)",
+            output: null,
+            options: ["never"],
+            errors: [
+                {
+                    messageId: "unexpectedLineBreak",
+                    line: 1,
+                    column: 17,
+                    endLine: 2,
+                    endColumn: 1
+                }
+            ]
+        },
+        {
+            code: "fn(`\n`, b, // comment\nc)",
+            output: null,
+            options: ["consistent"],
+            parserOptions: { ecmaVersion: 6 },
+            errors: [
+                {
+                    messageId: "unexpectedLineBreak",
+                    line: 2,
+                    column: 17,
+                    endLine: 3,
+                    endColumn: 1
+                }
+            ]
         }
     ]
 });
