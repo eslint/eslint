39f5a453579b2ad732212edeb71f84ecb0991f97
YeonJuan <yeonjuan93@naver.com>
2019-12-23 09:18:07 +0900

    Chore: add test cases for for-direction (#12698)

--- tests/lib/rules/for-direction.js
+++ tests/lib/rules/for-direction.js
@@ -53,7 +53,13 @@ ruleTester.run("for-direction", rule, {
         "for(var i = 10; i >= 0; i %= 2){}",
         "for(var i = 0; i < MAX; i += STEP_SIZE);",
         "for(var i = 0; i < MAX; i -= STEP_SIZE);",
-        "for(var i = 10; i > 0; i += STEP_SIZE);"
+        "for(var i = 10; i > 0; i += STEP_SIZE);",
+
+        // other cond-expressions.
+        "for(var i = 0; i !== 10; i+=1){}",
+        "for(var i = 0; i === 10; i+=1){}",
+        "for(var i = 0; i == 10; i+=1){}",
+        "for(var i = 0; i != 10; i+=1){}"
     ],
     invalid: [
 
