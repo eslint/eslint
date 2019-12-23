8f1020ff711b0c57d590bf666e2841f64186d083
Brad Zacher <brad.zacher@gmail.com>
2019-12-24 07:21:53 +1030

    Update: no-void add an option to allow void as a statement (#12613)
    * Update: no-void add an option to allow void as a statement
    
    * Test: add extra options test

--- tests/lib/rules/no-void.js
+++ tests/lib/rules/no-void.js
@@ -8,8 +8,8 @@
 // Requirements
 //------------------------------------------------------------------------------
 
-const rule = require("../../../lib/rules/no-void"),
-    { RuleTester } = require("../../../lib/rule-tester");
+const rule = require("../../../lib/rules/no-void");
+const { RuleTester } = require("../../../lib/rule-tester");
 
 //------------------------------------------------------------------------------
 // Tests
@@ -18,32 +18,48 @@ const rule = require("../../../lib/rules/no-void"),
 const ruleTester = new RuleTester();
 
 ruleTester.run("no-void", rule, {
-
     valid: [
         "var foo = bar()",
         "foo.void()",
         "foo.void = bar",
-        "delete foo;"
+        "delete foo;",
+        {
+            code: "void 0",
+            options: [{ allowAsStatement: true }]
+        },
+        {
+            code: "void(0)",
+            options: [{ allowAsStatement: true }]
+        }
     ],
 
     invalid: [
         {
             code: "void 0",
-            errors: [{
-                message: "Expected 'undefined' and instead saw 'void'."
-            }]
+            errors: [{ messageId: "noVoid" }]
+        },
+        {
+            code: "void 0",
+            options: [{}],
+            errors: [{ messageId: "noVoid" }]
+        },
+        {
+            code: "void 0",
+            options: [{ allowAsStatement: false }],
+            errors: [{ messageId: "noVoid" }]
         },
         {
             code: "void(0)",
-            errors: [{
-                message: "Expected 'undefined' and instead saw 'void'."
-            }]
+            errors: [{ messageId: "noVoid" }]
         },
         {
             code: "var foo = void 0",
-            errors: [{
-                message: "Expected 'undefined' and instead saw 'void'."
-            }]
+            errors: [{ messageId: "noVoid" }]
+        },
+        {
+            code: "var foo = void 0",
+            options: [{ allowAsStatement: true }],
+            errors: [{ messageId: "noVoid" }]
         }
     ]
 });
