787b8dba04e856e47d9f490ebeae0df518679b05
YeonJuan <yeonjuan93@naver.com>
2020-01-29 05:54:17 +0900

    Update: check template literal in no-proto, no-iterator (fixes #12801) (#12806)
    * Fix: check template literal prop in no-proto (#12801)
    
    * Fix: check template literal prop in no-iterator (#12801)
    
    * add test cases

--- tests/lib/rules/no-iterator.js
+++ tests/lib/rules/no-iterator.js
@@ -21,7 +21,9 @@ const ruleTester = new RuleTester();
 ruleTester.run("no-iterator", rule, {
     valid: [
         "var a = test[__iterator__];",
-        "var __iterator__ = null;"
+        "var __iterator__ = null;",
+        { code: "foo[`__iterator`] = null;", parserOptions: { ecmaVersion: 6 } },
+        { code: "foo[`__iterator__\n`] = null;", parserOptions: { ecmaVersion: 6 } }
     ],
     invalid: [
         {
@@ -40,6 +42,22 @@ ruleTester.run("no-iterator", rule, {
         },
         {
             code: "var a = test['__iterator__'];",
+            errors: [{
+                messageId: "noIterator",
+                type: "MemberExpression"
+            }]
+        },
+        {
+            code: "var a = test[`__iterator__`];",
+            parserOptions: { ecmaVersion: 6 },
+            errors: [{
+                messageId: "noIterator",
+                type: "MemberExpression"
+            }]
+        },
+        {
+            code: "test[`__iterator__`] = function () {};",
+            parserOptions: { ecmaVersion: 6 },
             errors: [{
                 messageId: "noIterator",
                 type: "MemberExpression"
--- tests/lib/rules/no-proto.js
+++ tests/lib/rules/no-proto.js
@@ -21,10 +21,14 @@ const ruleTester = new RuleTester();
 ruleTester.run("no-proto", rule, {
     valid: [
         "var a = test[__proto__];",
-        "var __proto__ = null;"
+        "var __proto__ = null;",
+        { code: "foo[`__proto`] = null;", parserOptions: { ecmaVersion: 6 } },
+        { code: "foo[`__proto__\n`] = null;", parserOptions: { ecmaVersion: 6 } }
     ],
     invalid: [
         { code: "var a = test.__proto__;", errors: [{ message: "The '__proto__' property is deprecated.", type: "MemberExpression" }] },
-        { code: "var a = test['__proto__'];", errors: [{ message: "The '__proto__' property is deprecated.", type: "MemberExpression" }] }
+        { code: "var a = test['__proto__'];", errors: [{ message: "The '__proto__' property is deprecated.", type: "MemberExpression" }] },
+        { code: "var a = test[`__proto__`];", parserOptions: { ecmaVersion: 6 }, errors: [{ message: "The '__proto__' property is deprecated.", type: "MemberExpression" }] },
+        { code: "test[`__proto__`] = function () {};", parserOptions: { ecmaVersion: 6 }, errors: [{ message: "The '__proto__' property is deprecated.", type: "MemberExpression" }] }
     ]
 });
