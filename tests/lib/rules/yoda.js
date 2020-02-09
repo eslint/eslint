77b66432d51d37c2eeaa63609416e1c9d5543895
YeonJuan <yeonjuan93@naver.com>
2020-02-10 05:23:16 +0900

    Update: check template literal in yoda (fixes #12863) (#12876)
    * Fix: check template literal in yoda (fixes #12863)
    
    * fix JSDOC, typo, add test cases
    
    * add test cases
    
    * update the documentation
    
    * Edit jsdoc
    
    * Add test case

--- tests/lib/rules/yoda.js
+++ tests/lib/rules/yoda.js
@@ -26,6 +26,13 @@ ruleTester.run("yoda", rule, {
         { code: "if (value != 5) {}", options: ["never"] },
         { code: "if (5 & foo) {}", options: ["never"] },
         { code: "if (5 === 4) {}", options: ["never"] },
+        { code: "if (value === `red`) {}", options: ["never"], parserOptions: { ecmaVersion: 2015 } },
+        { code: "if (`red` === `red`) {}", options: ["never"], parserOptions: { ecmaVersion: 2015 } },
+        { code: "if (`${foo}` === `red`) {}", options: ["never"], parserOptions: { ecmaVersion: 2015 } },
+        { code: "if (`${\"\"}` === `red`) {}", options: ["never"], parserOptions: { ecmaVersion: 2015 } },
+        { code: "if (`${\"red\"}` === foo) {}", options: ["never"], parserOptions: { ecmaVersion: 2015 } },
+        { code: "if (b > `a` && b > `a`) {}", options: ["never"], parserOptions: { ecmaVersion: 2015 } },
+        { code: "if (`b` > `a` && \"b\" > \"a\") {}", options: ["never"], parserOptions: { ecmaVersion: 2015 } },
 
         // "always" mode
         { code: "if (\"blue\" === value) {}", options: ["always"] },
@@ -33,6 +40,13 @@ ruleTester.run("yoda", rule, {
         { code: "if (4 != value) {}", options: ["always"] },
         { code: "if (foo & 4) {}", options: ["always"] },
         { code: "if (5 === 4) {}", options: ["always"] },
+        { code: "if (`red` === value) {}", options: ["always"], parserOptions: { ecmaVersion: 2015 } },
+        { code: "if (`red` === `red`) {}", options: ["always"], parserOptions: { ecmaVersion: 2015 } },
+        { code: "if (`red` === `${foo}`) {}", options: ["always"], parserOptions: { ecmaVersion: 2015 } },
+        { code: "if (`red` === `${\"\"}`) {}", options: ["always"], parserOptions: { ecmaVersion: 2015 } },
+        { code: "if (foo === `${\"red\"}`) {}", options: ["always"], parserOptions: { ecmaVersion: 2015 } },
+        { code: "if (`a` > b && `a` > b) {}", options: ["always"], parserOptions: { ecmaVersion: 2015 } },
+        { code: "if (`b` > `a` && \"b\" > \"a\") {}", options: ["always"], parserOptions: { ecmaVersion: 2015 } },
 
         // Range exception
         {
@@ -92,6 +106,10 @@ ruleTester.run("yoda", rule, {
             code: "if (0 <= a.b && a[\"b\"] <= 100) {}",
             options: ["never", { exceptRange: true }]
         }, {
+            code: "if (0 <= a.b && a[`b`] <= 100) {}",
+            options: ["never", { exceptRange: true }],
+            parserOptions: { ecmaVersion: 2015 }
+        }, {
             code: "if (-1n < x && x <= 1n) {}",
             options: ["never", { exceptRange: true }],
             parserOptions: { ecmaVersion: 2020 }
@@ -108,15 +126,37 @@ ruleTester.run("yoda", rule, {
             options: ["always", { exceptRange: true }],
             parserOptions: { ecmaVersion: 2020 }
         }, {
+            code: "if (x < `1` || `1` < x) {}",
+            options: ["always", { exceptRange: true }],
+            parserOptions: { ecmaVersion: 2020 }
+        }, {
             code: "if (1 <= a['/(?<zero>0)/'] && a[/(?<zero>0)/] <= 100) {}",
             options: ["never", { exceptRange: true }],
             parserOptions: { ecmaVersion: 2018 }
+        }, {
+            code: "if (x <= `bar` || `foo` < x) {}",
+            options: ["always", { exceptRange: true }],
+            parserOptions: { ecmaVersion: 2015 }
+        }, {
+            code: "if (`blue` < x.y && x.y < `green`) {}",
+            options: ["never", { exceptRange: true }],
+            parserOptions: { ecmaVersion: 2015 }
+        }, {
+            code: "if (0 <= x[`y`] && x[`y`] <= 100) {}",
+            options: ["never", { exceptRange: true }],
+            parserOptions: { ecmaVersion: 2015 }
+        }, {
+            code: "if (0 <= x[`y`] && x[\"y\"] <= 100) {}",
+            options: ["never", { exceptRange: true }],
+            parserOptions: { ecmaVersion: 2015 }
         },
 
         // onlyEquality
         { code: "if (0 < x && x <= 1) {}", options: ["never", { onlyEquality: true }] },
         { code: "if (x !== 'foo' && 'foo' !== x) {}", options: ["never", { onlyEquality: true }] },
-        { code: "if (x < 2 && x !== -3) {}", options: ["always", { onlyEquality: true }] }
+        { code: "if (x < 2 && x !== -3) {}", options: ["always", { onlyEquality: true }] },
+        { code: "if (x !== `foo` && `foo` !== x) {}", options: ["never", { onlyEquality: true }], parserOptions: { ecmaVersion: 2015 } },
+        { code: "if (x < `2` && x !== `-3`) {}", options: ["always", { onlyEquality: true }], parserOptions: { ecmaVersion: 2015 } }
     ],
     invalid: [
 
@@ -194,6 +234,45 @@ ruleTester.run("yoda", rule, {
             ]
         },
         {
+            code: "if (`red` <= value) {}",
+            output: "if (value >= `red`) {}",
+            options: ["never"],
+            parserOptions: { ecmaVersion: 2015 },
+            errors: [
+                {
+                    messageId: "expected",
+                    data: { expectedSide: "right", operator: "<=" },
+                    type: "BinaryExpression"
+                }
+            ]
+        },
+        {
+            code: "if (`red` <= `${foo}`) {}",
+            output: "if (`${foo}` >= `red`) {}",
+            options: ["never"],
+            parserOptions: { ecmaVersion: 2015 },
+            errors: [
+                {
+                    messageId: "expected",
+                    data: { expectedSide: "right", operator: "<=" },
+                    type: "BinaryExpression"
+                }
+            ]
+        },
+        {
+            code: "if (`red` <= `${\"red\"}`) {}",
+            output: "if (`${\"red\"}` >= `red`) {}",
+            options: ["never"],
+            parserOptions: { ecmaVersion: 2015 },
+            errors: [
+                {
+                    messageId: "expected",
+                    data: { expectedSide: "right", operator: "<=" },
+                    type: "BinaryExpression"
+                }
+            ]
+        },
+        {
             code: "if (true >= value) {}",
             output: "if (value <= true) {}",
             options: ["never"],
@@ -254,6 +333,19 @@ ruleTester.run("yoda", rule, {
             ]
         },
         {
+            code: "if (value == `red`) {}",
+            output: "if (`red` == value) {}",
+            options: ["always"],
+            parserOptions: { ecmaVersion: 2015 },
+            errors: [
+                {
+                    messageId: "expected",
+                    data: { expectedSide: "left", operator: "==" },
+                    type: "BinaryExpression"
+                }
+            ]
+        },
+        {
             code: "if (value === true) {}",
             output: "if (true === value) {}",
             options: ["always"],
@@ -274,6 +366,19 @@ ruleTester.run("yoda", rule, {
                 {
                     messageId: "expected",
                     data: { expectedSide: "left", operator: "===" },
+                    type: "BinaryExpression"
+                }
+            ]
+        },
+        {
+            code: "if (`${\"red\"}` <= `red`) {}",
+            output: "if (`red` >= `${\"red\"}`) {}",
+            options: ["always"],
+            parserOptions: { ecmaVersion: 2015 },
+            errors: [
+                {
+                    messageId: "expected",
+                    data: { expectedSide: "left", operator: "<=" },
                     type: "BinaryExpression"
                 }
             ]
@@ -363,9 +468,48 @@ ruleTester.run("yoda", rule, {
             ]
         },
         {
+            code: "var a = (b < `0` && `0` <= b);",
+            output: "var a = (`0` > b && `0` <= b);",
+            options: ["always", { exceptRange: true }],
+            parserOptions: { ecmaVersion: 2015 },
+            errors: [
+                {
+                    messageId: "expected",
+                    data: { expectedSide: "left", operator: "<" },
+                    type: "BinaryExpression"
+                }
+            ]
+        },
+        {
             code: "if (0 <= a[b] && a['b'] < 1) {}",
             output: "if (a[b] >= 0 && a['b'] < 1) {}",
             options: ["never", { exceptRange: true }],
+            errors: [
+                {
+                    messageId: "expected",
+                    data: { expectedSide: "right", operator: "<=" },
+                    type: "BinaryExpression"
+                }
+            ]
+        },
+        {
+            code: "if (0 <= a[b] && a[`b`] < 1) {}",
+            output: "if (a[b] >= 0 && a[`b`] < 1) {}",
+            options: ["never", { exceptRange: true }],
+            parserOptions: { ecmaVersion: 2015 },
+            errors: [
+                {
+                    messageId: "expected",
+                    data: { expectedSide: "right", operator: "<=" },
+                    type: "BinaryExpression"
+                }
+            ]
+        },
+        {
+            code: "if (`0` <= a[b] && a[`b`] < `1`) {}",
+            output: "if (a[b] >= `0` && a[`b`] < `1`) {}",
+            options: ["never", { exceptRange: true }],
+            parserOptions: { ecmaVersion: 2015 },
             errors: [
                 {
                     messageId: "expected",
@@ -423,6 +567,19 @@ ruleTester.run("yoda", rule, {
             ]
         },
         {
+            code: "if (0 <= a[``] && a[null] < 1) {}",
+            output: "if (a[``] >= 0 && a[null] < 1) {}",
+            options: ["never", { exceptRange: true }],
+            parserOptions: { ecmaVersion: 2015 },
+            errors: [
+                {
+                    messageId: "expected",
+                    data: { expectedSide: "right", operator: "<=" },
+                    type: "BinaryExpression"
+                }
+            ]
+        },
+        {
             code: "if (0 <= a[''] && a[b] < 1) {}",
             output: "if (a[''] >= 0 && a[b] < 1) {}",
             options: ["never", { exceptRange: true }],
@@ -438,6 +595,19 @@ ruleTester.run("yoda", rule, {
             code: "if (0 <= a[''] && a[b()] < 1) {}",
             output: "if (a[''] >= 0 && a[b()] < 1) {}",
             options: ["never", { exceptRange: true }],
+            errors: [
+                {
+                    messageId: "expected",
+                    data: { expectedSide: "right", operator: "<=" },
+                    type: "BinaryExpression"
+                }
+            ]
+        },
+        {
+            code: "if (0 <= a[``] && a[b()] < 1) {}",
+            output: "if (a[``] >= 0 && a[b()] < 1) {}",
+            options: ["never", { exceptRange: true }],
+            parserOptions: { ecmaVersion: 2015 },
             errors: [
                 {
                     messageId: "expected",
@@ -472,6 +642,19 @@ ruleTester.run("yoda", rule, {
             ]
         },
         {
+            code: "if (`green` < x.y && x.y < `blue`) {}",
+            output: "if (x.y > `green` && x.y < `blue`) {}",
+            options: ["never", { exceptRange: true }],
+            parserOptions: { ecmaVersion: 2015 },
+            errors: [
+                {
+                    messageId: "expected",
+                    data: { expectedSide: "right", operator: "<" },
+                    type: "BinaryExpression"
+                }
+            ]
+        },
+        {
             code: "if (3 == a) {}",
             output: "if (a == 3) {}",
             options: ["never", { onlyEquality: true }],
@@ -499,6 +682,19 @@ ruleTester.run("yoda", rule, {
             code: "foo(a === 3);",
             output: "foo(3 === a);",
             options: ["always", { onlyEquality: true }],
+            errors: [
+                {
+                    messageId: "expected",
+                    data: { expectedSide: "left", operator: "===" },
+                    type: "BinaryExpression"
+                }
+            ]
+        },
+        {
+            code: "foo(a === `3`);",
+            output: "foo(`3` === a);",
+            options: ["always", { onlyEquality: true }],
+            parserOptions: { ecmaVersion: 2015 },
             errors: [
                 {
                     messageId: "expected",
