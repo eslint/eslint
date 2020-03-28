c567445d659d7f8df2119920928c3c35bdaa9251
Anix <anik220798@gmail.com>
2020-03-28 15:15:47 +0000

    Fix: no check for shadowed Object (fixes #12809)

--- tests/lib/rules/no-new-object.js
+++ tests/lib/rules/no-new-object.js
@@ -20,15 +20,50 @@ const ruleTester = new RuleTester();
 
 ruleTester.run("no-new-object", rule, {
     valid: [
-        "var foo = new foo.Object()"
+        "var myObject = {};",
+        "var myObject = new CustomObject();",
+        "var foo = new foo.Object()",
+        `var Object = function Object() {};
+            new Object();`,
+        `var x = something ? MyClass : Object;
+        var y = new x();`,
+        {
+            code: `
+        class Object {
+            constructor(){
+
+            }
+        }
+        new Object();
+        `,
+            parserOptions: { ecmaVersion: 6 }
+        },
+        {
+            code: `
+        import { Object } from './'
+        new Object();
+        `,
+            parserOptions: { ecmaVersion: 6, sourceType: "module" }
+        }
     ],
     invalid: [
         {
             code: "var foo = new Object()",
-            errors: [{
-                messageId: "preferLiteral",
-                type: "NewExpression"
-            }]
+            errors: [
+                {
+                    messageId: "preferLiteral",
+                    type: "NewExpression"
+                }
+            ]
+        },
+        {
+            code: "new Object();",
+            errors: [{ messageId: "preferLiteral", type: "NewExpression" }]
+        },
+        {
+            code: "const a = new Object()",
+            parserOptions: { ecmaVersion: 6 },
+            errors: [{ messageId: "preferLiteral", type: "NewExpression" }]
         }
     ]
 });
