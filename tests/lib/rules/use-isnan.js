df673322060b5b6e169ad89eed9aea5a0fd24192
Anix <anik220798@gmail.com>
2020-04-29 08:46:15 +0000

    Chore: rules tests fixes

--- tests/lib/rules/use-isnan.js
+++ tests/lib/rules/use-isnan.js
@@ -281,16 +281,6 @@ ruleTester.run("use-isnan", rule, {
             errors: [{ messageId: "caseNaN", type: "SwitchCase", column: 15 }]
         },
         {
-            code: "switch(NaN) { case foo: break; }",
-            options: [{}],
-            errors: [{ messageId: "switchNaN", type: "SwitchStatement", column: 1 }]
-        },
-        {
-            code: "switch(foo) { case NaN: break; }",
-            options: [{}],
-            errors: [{ messageId: "caseNaN", type: "SwitchCase", column: 15 }]
-        },
-        {
             code: "switch(NaN) {}",
             options: [{ enforceForSwitchCase: true }],
             errors: [{ messageId: "switchNaN", type: "SwitchStatement", column: 1 }]
