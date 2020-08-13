1a2cf7e7cd5325c6e1bdded4bf2bad3dd937cbbc
Anix <anik220798@gmail.com>
2020-08-13 07:35:42 +0000

    Chore: refactor small

--- tests/lib/rules/no-warning-comments.js
+++ tests/lib/rules/no-warning-comments.js
@@ -43,179 +43,179 @@ ruleTester.run("no-warning-comments", rule, {
         {
             code: "// fixme",
             errors: [
-                { messageId: "unexpectedComment", data: { type: "fixme", comment: "fixme" } }
+                { messageId: "unexpectedComment", data: { matchedTerm: "fixme", comment: "fixme" } }
             ]
         },
         {
             code: "// any fixme",
             options: [{ location: "anywhere" }],
             errors: [
-                { messageId: "unexpectedComment", data: { type: "fixme", comment: "any fixme" } }
+                { messageId: "unexpectedComment", data: { matchedTerm: "fixme", comment: "any fixme" } }
             ]
         },
         {
             code: "// any fixme",
             options: [{ terms: ["fixme"], location: "anywhere" }],
             errors: [
-                { messageId: "unexpectedComment", data: { type: "fixme", comment: "any fixme" } }
+                { messageId: "unexpectedComment", data: { matchedTerm: "fixme", comment: "any fixme" } }
             ]
         },
         {
             code: "// any FIXME",
             options: [{ terms: ["fixme"], location: "anywhere" }],
             errors: [
-                { messageId: "unexpectedComment", data: { type: "fixme", comment: "any FIXME" } }
+                { messageId: "unexpectedComment", data: { matchedTerm: "fixme", comment: "any FIXME" } }
             ]
         },
         {
             code: "// any fIxMe",
             options: [{ terms: ["fixme"], location: "anywhere" }],
             errors: [
-                { messageId: "unexpectedComment", data: { type: "fixme", comment: "any fIxMe" } }
+                { messageId: "unexpectedComment", data: { matchedTerm: "fixme", comment: "any fIxMe" } }
             ]
         },
         {
             code: "/* any fixme */",
             options: [{ terms: ["FIXME"], location: "anywhere" }],
             errors: [
-                { messageId: "unexpectedComment", data: { type: "FIXME", comment: "any fixme" } }
+                { messageId: "unexpectedComment", data: { matchedTerm: "FIXME", comment: "any fixme" } }
             ]
         },
         {
             code: "/* any FIXME */",
             options: [{ terms: ["FIXME"], location: "anywhere" }],
             errors: [
-                { messageId: "unexpectedComment", data: { type: "FIXME", comment: "any FIXME" } }
+                { messageId: "unexpectedComment", data: { matchedTerm: "FIXME", comment: "any FIXME" } }
             ]
         },
         {
             code: "/* any fIxMe */",
             options: [{ terms: ["FIXME"], location: "anywhere" }],
             errors: [
-                { messageId: "unexpectedComment", data: { type: "FIXME", comment: "any fIxMe" } }
+                { messageId: "unexpectedComment", data: { matchedTerm: "FIXME", comment: "any fIxMe" } }
             ]
         },
         {
             code: "// any fixme or todo",
             options: [{ terms: ["fixme", "todo"], location: "anywhere" }],
             errors: [
-                { messageId: "unexpectedComment", data: { type: "fixme", comment: "any fixme or todo" } },
-                { messageId: "unexpectedComment", data: { type: "todo", comment: "any fixme or todo" } }
+                { messageId: "unexpectedComment", data: { matchedTerm: "fixme", comment: "any fixme or todo" } },
+                { messageId: "unexpectedComment", data: { matchedTerm: "todo", comment: "any fixme or todo" } }
             ]
         },
         {
             code: "/* any fixme or todo */",
             options: [{ terms: ["fixme", "todo"], location: "anywhere" }],
             errors: [
-                { messageId: "unexpectedComment", data: { type: "fixme", comment: "any fixme or todo" } },
-                { messageId: "unexpectedComment", data: { type: "todo", comment: "any fixme or todo" } }
+                { messageId: "unexpectedComment", data: { matchedTerm: "fixme", comment: "any fixme or todo" } },
+                { messageId: "unexpectedComment", data: { matchedTerm: "todo", comment: "any fixme or todo" } }
             ]
         },
         {
             code: "/* any fixme or todo */",
             options: [{ location: "anywhere" }],
             errors: [
-                { messageId: "unexpectedComment", data: { type: "todo", comment: "any fixme or todo" } },
-                { messageId: "unexpectedComment", data: { type: "fixme", comment: "any fixme or todo" } }
+                { messageId: "unexpectedComment", data: { matchedTerm: "todo", comment: "any fixme or todo" } },
+                { messageId: "unexpectedComment", data: { matchedTerm: "fixme", comment: "any fixme or todo" } }
             ]
         },
         {
             code: "/* fixme and todo */",
             errors: [
-                { messageId: "unexpectedComment", data: { type: "fixme", comment: "fixme and todo" } }
+                { messageId: "unexpectedComment", data: { matchedTerm: "fixme", comment: "fixme and todo" } }
             ]
         },
         {
             code: "/* fixme and todo */",
             options: [{ location: "anywhere" }],
             errors: [
-                { messageId: "unexpectedComment", data: { type: "todo", comment: "fixme and todo" } },
-                { messageId: "unexpectedComment", data: { type: "fixme", comment: "fixme and todo" } }
+                { messageId: "unexpectedComment", data: { matchedTerm: "todo", comment: "fixme and todo" } },
+                { messageId: "unexpectedComment", data: { matchedTerm: "fixme", comment: "fixme and todo" } }
             ]
         },
         {
             code: "/* any fixme */",
             options: [{ location: "anywhere" }],
             errors: [
-                { messageId: "unexpectedComment", data: { type: "fixme", comment: "any fixme" } }
+                { messageId: "unexpectedComment", data: { matchedTerm: "fixme", comment: "any fixme" } }
             ]
         },
         {
             code: "/* fixme! */",
             options: [{ terms: ["fixme"] }],
             errors: [
-                { messageId: "unexpectedComment", data: { type: "fixme", comment: "fixme!" } }
+                { messageId: "unexpectedComment", data: { matchedTerm: "fixme", comment: "fixme!" } }
             ]
         },
         {
             code: "// regex [litera|$]",
             options: [{ terms: ["[litera|$]"], location: "anywhere" }],
             errors: [
-                { messageId: "unexpectedComment", data: { type: "[litera|$]", comment: "regex [litera|$]" } }
+                { messageId: "unexpectedComment", data: { matchedTerm: "[litera|$]", comment: "regex [litera|$]" } }
             ]
         },
         {
             code: "/* eslint one-var: 2 */",
             options: [{ terms: ["eslint"] }],
             errors: [
-                { messageId: "unexpectedComment", data: { type: "eslint", comment: "eslint one-var: 2" } }
+                { messageId: "unexpectedComment", data: { matchedTerm: "eslint", comment: "eslint one-var: 2" } }
             ]
         },
         {
             code: "/* eslint one-var: 2 */",
             options: [{ terms: ["one"], location: "anywhere" }],
             errors: [
-                { messageId: "unexpectedComment", data: { type: "one", comment: "eslint one-var: 2" } }
+                { messageId: "unexpectedComment", data: { matchedTerm: "one", comment: "eslint one-var: 2" } }
             ]
         },
         {
             code: "/* any block comment with TODO, FIXME or XXX */",
             options: [{ location: "anywhere" }],
             errors: [
-                { messageId: "unexpectedComment", data: { type: "todo", comment: "any block comment with TODO, FIXME or..." } },
-                { messageId: "unexpectedComment", data: { type: "fixme", comment: "any block comment with TODO, FIXME or..." } },
-                { messageId: "unexpectedComment", data: { type: "xxx", comment: "any block comment with TODO, FIXME or..." } }
+                { messageId: "unexpectedComment", data: { matchedTerm: "todo", comment: "any block comment with TODO, FIXME or..." } },
+                { messageId: "unexpectedComment", data: { matchedTerm: "fixme", comment: "any block comment with TODO, FIXME or..." } },
+                { messageId: "unexpectedComment", data: { matchedTerm: "xxx", comment: "any block comment with TODO, FIXME or..." } }
             ]
         },
         {
             code: "/* any block comment with (TODO, FIXME's or XXX!) */",
             options: [{ location: "anywhere" }],
             errors: [
-                { messageId: "unexpectedComment", data: { type: "todo", comment: "any block comment with (TODO, FIXME's or..." } },
-                { messageId: "unexpectedComment", data: { type: "fixme", comment: "any block comment with (TODO, FIXME's or..." } },
-                { messageId: "unexpectedComment", data: { type: "xxx", comment: "any block comment with (TODO, FIXME's or..." } }
+                { messageId: "unexpectedComment", data: { matchedTerm: "todo", comment: "any block comment with (TODO, FIXME's or..." } },
+                { messageId: "unexpectedComment", data: { matchedTerm: "fixme", comment: "any block comment with (TODO, FIXME's or..." } },
+                { messageId: "unexpectedComment", data: { matchedTerm: "xxx", comment: "any block comment with (TODO, FIXME's or..." } }
             ]
         },
         {
             code: "/** \n *any block comment \n*with (TODO, FIXME's or XXX!) **/",
             options: [{ location: "anywhere" }],
             errors: [
-                { messageId: "unexpectedComment", data: { type: "todo", comment: "* *any block comment *with (TODO,..." } },
-                { messageId: "unexpectedComment", data: { type: "fixme", comment: "* *any block comment *with (TODO,..." } },
-                { messageId: "unexpectedComment", data: { type: "xxx", comment: "* *any block comment *with (TODO,..." } }
+                { messageId: "unexpectedComment", data: { matchedTerm: "todo", comment: "* *any block comment *with (TODO,..." } },
+                { messageId: "unexpectedComment", data: { matchedTerm: "fixme", comment: "* *any block comment *with (TODO,..." } },
+                { messageId: "unexpectedComment", data: { matchedTerm: "xxx", comment: "* *any block comment *with (TODO,..." } }
             ]
         },
         {
             code: "// any comment with TODO, FIXME or XXX",
             options: [{ location: "anywhere" }],
             errors: [
-                { messageId: "unexpectedComment", data: { type: "todo", comment: "any comment with TODO, FIXME or XXX" } },
-                { messageId: "unexpectedComment", data: { type: "fixme", comment: "any comment with TODO, FIXME or XXX" } },
-                { messageId: "unexpectedComment", data: { type: "xxx", comment: "any comment with TODO, FIXME or XXX" } }
+                { messageId: "unexpectedComment", data: { matchedTerm: "todo", comment: "any comment with TODO, FIXME or XXX" } },
+                { messageId: "unexpectedComment", data: { matchedTerm: "fixme", comment: "any comment with TODO, FIXME or XXX" } },
+                { messageId: "unexpectedComment", data: { matchedTerm: "xxx", comment: "any comment with TODO, FIXME or XXX" } }
             ]
         },
         {
             code: "// TODO: something small",
             options: [{ location: "anywhere" }],
             errors: [
-                { messageId: "unexpectedComment", data: { type: "todo", comment: "TODO: something small" } }
+                { messageId: "unexpectedComment", data: { matchedTerm: "todo", comment: "TODO: something small" } }
             ]
         },
         {
             code: "// TODO: something really longer than 40 characters",
             options: [{ location: "anywhere" }],
             errors: [
-                { messageId: "unexpectedComment", data: { type: "todo", comment: "TODO: something really longer than 40..." } }
+                { messageId: "unexpectedComment", data: { matchedTerm: "todo", comment: "TODO: something really longer than 40..." } }
             ]
         },
         {
@@ -226,7 +226,7 @@ ruleTester.run("no-warning-comments", rule, {
                 {
                     messageId: "unexpectedComment",
                     data: {
-                        type: "todo",
+                        matchedTerm: "todo",
                         comment: "TODO: something really longer than 40..."
                     }
                 }
@@ -239,7 +239,7 @@ ruleTester.run("no-warning-comments", rule, {
                 {
                     messageId: "unexpectedComment",
                     data: {
-                        type: "todo",
+                        matchedTerm: "todo",
                         comment: "TODO: small"
                     }
                 }
