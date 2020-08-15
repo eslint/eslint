370374ee4b9ebbe3f56710f0bfa8c75efc9f82cf
sodam2 <sodamlee315@gmail.com>
2020-08-16 01:20:46 +0900

    Add valid test with multiple exception patterns

--- tests/lib/rules/id-length.js
+++ tests/lib/rules/id-length.js
@@ -78,7 +78,9 @@ ruleTester.run("id-length", rule, {
         { code: "var {x, y: {z}} = foo;", options: [{ properties: "never" }], parserOptions: { ecmaVersion: 6 } },
         { code: "let foo = { [a]: 1 };", options: [{ properties: "always" }], parserOptions: { ecmaVersion: 6 } },
         { code: "let foo = { [a + b]: 1 };", options: [{ properties: "always" }], parserOptions: { ecmaVersion: 6 } },
-        { code: "function BEFORE_send() {};", options: [{ min: 3, max: 5, exceptionPatterns: ["^BEFORE_"] }] }
+        { code: "function BEFORE_send() {};", options: [{ min: 3, max: 5, exceptionPatterns: ["^BEFORE_"] }] },
+        { code: "function BEFORE_send() {};", options: [{ min: 3, max: 5, exceptionPatterns: ["^BEFORE_", "send$"] }] },
+        { code: "function BEFORE_send() {};", options: [{ min: 3, max: 5, exceptionPatterns: ["send$", "^A", "Z*"] }] }
     ],
     invalid: [
         { code: "var x = 1;", errors: [tooShortError] },
