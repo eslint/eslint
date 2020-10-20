84fd591c234accc41bb5af555f178825012fd35d
Brandon Mills <btmills@users.noreply.github.com>
2020-10-20 09:04:17 -0400

    Chore: Increase Mocha timeout for copying fixtures (#13768)
    For the last several weeks, our CI jobs have been flaky due to test
    hook timeouts on the Windows and macOS runners. I traced most of the
    failures to four `before` hooks that copy the test fixtures to a
    temporary directory. They usually take ~2.5s, but that occasionally
    spikes to tens of seconds: in the two times I was able to repro the
    timeouts, the slowest copy jobs were 25 and 32 seconds. Rather than
    increasing the global test timeout, I instead bumped it just for these
    four hooks that were causing problems.

--- tests/lib/cli.js
+++ tests/lib/cli.js
@@ -82,7 +82,15 @@ describe("cli", () => {
     }
 
     // copy into clean area so as not to get "infected" by this project's .eslintrc files
-    before(() => {
+    before(function() {
+
+        /*
+         * GitHub Actions Windows and macOS runners occasionally exhibit
+         * extremely slow filesystem operations, during which copying fixtures
+         * exceeds the default test timeout, so raise it just for this hook.
+         * Mocha uses `this` to set timeouts on an individual hook level.
+         */
+        this.timeout(60 * 1000); // eslint-disable-line no-invalid-this
         fixtureDir = `${os.tmpdir()}/eslint/fixtures`;
         sh.mkdir("-p", fixtureDir);
         sh.cp("-r", "./tests/fixtures/.", fixtureDir);
