723958e34b9b5e4baab8f5d8e8e3f2a8cf51d3c8
Zen <843968788@qq.com>
2020-10-23 23:03:12 +0800

    Update: treat all literals like boolean literal in no-constant-condition (#13245)
    * Fix: treet all literals like boolean literal in no-constant-condition
    
    * fix unexpected short circuit
    
    * handle bigin/regex on unsupported runtime
    
    * fix bigint tests
    
    * simplify literal truthiness
    
    * handle regex literal in unsupported environments
    
    * Update lib/rules/no-constant-condition.js
    
    Co-authored-by: Milos Djermanovic <milos.djermanovic@gmail.com>
    
    Co-authored-by: Milos Djermanovic <milos.djermanovic@gmail.com>

--- tests/lib/rules/no-constant-condition.js
+++ tests/lib/rules/no-constant-condition.js
@@ -74,12 +74,11 @@ ruleTester.run("no-constant-condition", rule, {
         "if(true && typeof abc==='string'){}",
 
         // #11181, string literals
-        "if('str' || a){}",
         "if('str1' && a){}",
         "if(a && 'str'){}",
-        "if('str' || abc==='str'){}",
 
         // #11306
+        "if ((foo || true) === 'baz') {}",
         "if ((foo || 'bar') === 'baz') {}",
         "if ((foo || 'bar') !== 'baz') {}",
         "if ((foo || 'bar') == 'baz') {}",
@@ -90,6 +89,19 @@ ruleTester.run("no-constant-condition", rule, {
         "if ((foo || 233) <= 666) {}",
         "if ((key || 'k') in obj) {}",
         "if ((foo || {}) instanceof obj) {}",
+        "if ((foo || 'bar' || 'bar') === 'bar');",
+        {
+            code: "if ((foo || 1n) === 'baz') {}",
+            parserOptions: { ecmaVersion: 11 }
+        },
+        {
+            code: "if (a && 0n || b);",
+            parserOptions: { ecmaVersion: 11 }
+        },
+        {
+            code: "if(1n && a){};",
+            parserOptions: { ecmaVersion: 11 }
+        },
 
         // #12225
         "if ('' + [y] === '' + [ty]) {}",
@@ -121,11 +133,14 @@ ruleTester.run("no-constant-condition", rule, {
         { code: "for(;`foo`;);", errors: [{ messageId: "unexpected", type: "TemplateLiteral" }] },
         { code: "for(;`foo${bar}`;);", errors: [{ messageId: "unexpected", type: "TemplateLiteral" }] },
         { code: "do{}while(true)", errors: [{ messageId: "unexpected", type: "Literal" }] },
+        { code: "do{}while('1')", errors: [{ messageId: "unexpected", type: "Literal" }] },
+        { code: "do{}while(0)", errors: [{ messageId: "unexpected", type: "Literal" }] },
         { code: "do{}while(t = -2)", errors: [{ messageId: "unexpected", type: "AssignmentExpression" }] },
         { code: "do{}while(``)", errors: [{ messageId: "unexpected", type: "TemplateLiteral" }] },
         { code: "do{}while(`foo`)", errors: [{ messageId: "unexpected", type: "TemplateLiteral" }] },
         { code: "do{}while(`foo${bar}`)", errors: [{ messageId: "unexpected", type: "TemplateLiteral" }] },
         { code: "true ? 1 : 2;", errors: [{ messageId: "unexpected", type: "Literal" }] },
+        { code: "1 ? 1 : 2;", errors: [{ messageId: "unexpected", type: "Literal" }] },
         { code: "q = 0 ? 1 : 2;", errors: [{ messageId: "unexpected", type: "Literal" }] },
         { code: "(q = 0) ? 1 : 2;", errors: [{ messageId: "unexpected", type: "AssignmentExpression" }] },
         { code: "`` ? 1 : 2;", errors: [{ messageId: "unexpected", type: "TemplateLiteral" }] },
@@ -133,6 +148,7 @@ ruleTester.run("no-constant-condition", rule, {
         { code: "`foo${bar}` ? 1 : 2;", errors: [{ messageId: "unexpected", type: "TemplateLiteral" }] },
         { code: "if(-2);", errors: [{ messageId: "unexpected", type: "UnaryExpression" }] },
         { code: "if(true);", errors: [{ messageId: "unexpected", type: "Literal" }] },
+        { code: "if(1);", errors: [{ messageId: "unexpected", type: "Literal" }] },
         { code: "if({});", errors: [{ messageId: "unexpected", type: "ObjectExpression" }] },
         { code: "if(0 < 1);", errors: [{ messageId: "unexpected", type: "BinaryExpression" }] },
         { code: "if(0 || 1);", errors: [{ messageId: "unexpected", type: "LogicalExpression" }] },
@@ -143,6 +159,7 @@ ruleTester.run("no-constant-condition", rule, {
         { code: "if(`${'bar'}`);", errors: [{ messageId: "unexpected", type: "TemplateLiteral" }] },
         { code: "if(`${'bar' + `foo`}`);", errors: [{ messageId: "unexpected", type: "TemplateLiteral" }] },
         { code: "if(`foo${false || true}`);", errors: [{ messageId: "unexpected", type: "TemplateLiteral" }] },
+        { code: "if(`foo${0 || 1}`);", errors: [{ messageId: "unexpected", type: "TemplateLiteral" }] },
         { code: "if(`foo${bar}`);", errors: [{ messageId: "unexpected", type: "TemplateLiteral" }] },
         { code: "if(`${bar}foo`);", errors: [{ messageId: "unexpected", type: "TemplateLiteral" }] },
 
@@ -152,6 +169,7 @@ ruleTester.run("no-constant-condition", rule, {
         { code: "while(x = 1);", errors: [{ messageId: "unexpected", type: "AssignmentExpression" }] },
         { code: "while(function(){});", errors: [{ messageId: "unexpected", type: "FunctionExpression" }] },
         { code: "while(true);", errors: [{ messageId: "unexpected", type: "Literal" }] },
+        { code: "while(1);", errors: [{ messageId: "unexpected", type: "Literal" }] },
         { code: "while(() => {});", errors: [{ messageId: "unexpected", type: "ArrowFunctionExpression" }] },
         { code: "while(`foo`);", errors: [{ messageId: "unexpected", type: "TemplateLiteral" }] },
         { code: "while(``);", errors: [{ messageId: "unexpected", type: "TemplateLiteral" }] },
@@ -180,12 +198,15 @@ ruleTester.run("no-constant-condition", rule, {
         // #5693
         { code: "if(false && abc==='str'){}", errors: [{ messageId: "unexpected", type: "LogicalExpression" }] },
         { code: "if(true || abc==='str'){}", errors: [{ messageId: "unexpected", type: "LogicalExpression" }] },
+        { code: "if(1 || abc==='str'){}", errors: [{ messageId: "unexpected", type: "LogicalExpression" }] },
         { code: "if(abc==='str' || true){}", errors: [{ messageId: "unexpected", type: "LogicalExpression" }] },
         { code: "if(abc==='str' || true || def ==='str'){}", errors: [{ messageId: "unexpected", type: "LogicalExpression" }] },
         { code: "if(false || true){}", errors: [{ messageId: "unexpected", type: "LogicalExpression" }] },
         { code: "if(typeof abc==='str' || true){}", errors: [{ messageId: "unexpected", type: "LogicalExpression" }] },
 
         // #11181, string literals
+        { code: "if('str' || a){}", errors: [{ messageId: "unexpected", type: "LogicalExpression" }] },
+        { code: "if('str' || abc==='str'){}", errors: [{ messageId: "unexpected", type: "LogicalExpression" }] },
         { code: "if('str1' || 'str2'){}", errors: [{ messageId: "unexpected", type: "LogicalExpression" }] },
         { code: "if('str1' && 'str2'){}", errors: [{ messageId: "unexpected", type: "LogicalExpression" }] },
         { code: "if(abc==='str' || 'str'){}", errors: [{ messageId: "unexpected", type: "LogicalExpression" }] },
@@ -272,6 +293,17 @@ ruleTester.run("no-constant-condition", rule, {
         {
             code: "if ([,] + ''){}",
             errors: [{ messageId: "unexpected", type: "BinaryExpression" }]
-        }
+        },
+
+        // #13238
+        { code: "if(/foo/ui);", parserOptions: { ecmaVersion: 11 }, errors: [{ messageId: "unexpected", type: "Literal" }] },
+        { code: "if(0n);", parserOptions: { ecmaVersion: 11 }, errors: [{ messageId: "unexpected", type: "Literal" }] },
+        { code: "if(0b0n);", parserOptions: { ecmaVersion: 11 }, errors: [{ messageId: "unexpected", type: "Literal" }] },
+        { code: "if(0o0n);", parserOptions: { ecmaVersion: 11 }, errors: [{ messageId: "unexpected", type: "Literal" }] },
+        { code: "if(0x0n);", parserOptions: { ecmaVersion: 11 }, errors: [{ messageId: "unexpected", type: "Literal" }] },
+        { code: "if(0b1n);", parserOptions: { ecmaVersion: 11 }, errors: [{ messageId: "unexpected", type: "Literal" }] },
+        { code: "if(0o1n);", parserOptions: { ecmaVersion: 11 }, errors: [{ messageId: "unexpected", type: "Literal" }] },
+        { code: "if(0x1n);", parserOptions: { ecmaVersion: 11 }, errors: [{ messageId: "unexpected", type: "Literal" }] },
+        { code: "if(0x1n || foo);", parserOptions: { ecmaVersion: 11 }, errors: [{ messageId: "unexpected", type: "LogicalExpression" }] }
     ]
 });
