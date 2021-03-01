3b6d404c564d8e262fe18c0764873d397625b79a
Jacob Bandes-Storch <jacob@bandes-stor.ch>
2021-02-28 18:33:29 -0600

    Move validation to rule-fixer and add error logging

--- tests/lib/linter/rule-fixer.js
+++ tests/lib/linter/rule-fixer.js
@@ -9,13 +9,27 @@
 //------------------------------------------------------------------------------
 
 const assert = require("chai").assert,
-    ruleFixer = require("../../../lib/linter/rule-fixer");
+    sinon = require("sinon");
+
+const proxyquire = require("proxyquire").noCallThru().noPreserveCache();
 
 //------------------------------------------------------------------------------
 // Tests
 //------------------------------------------------------------------------------
 
 describe("RuleFixer", () => {
+    const log = {
+        info: sinon.spy(),
+        error: sinon.spy()
+    };
+    const ruleFixer = proxyquire("../../../lib/linter/rule-fixer", {
+        "../shared/logging": log
+    });
+
+    afterEach(() => {
+        log.info.resetHistory();
+        log.error.resetHistory();
+    });
 
     describe("insertTextBefore", () => {
 
@@ -136,6 +150,35 @@ describe("RuleFixer", () => {
 
         });
 
+    });
+
+    describe("validation", () => {
+        it("should return null given non-numeric indices", () => {
+            const invalidRanges = [
+                [0, null],
+                ["1", 2],
+                [void 0, void 0],
+                [1]
+            ];
+            const functions = [
+                (range, text) => ruleFixer.insertTextBefore({ range }, text),
+                (range, text) => ruleFixer.insertTextBeforeRange(range, text),
+                (range, text) => ruleFixer.insertTextAfter({ range }, text),
+                (range, text) => ruleFixer.insertTextAfterRange(range, text),
+                range => ruleFixer.remove({ range }),
+                range => ruleFixer.removeRange(range),
+                range => ruleFixer.replaceText({ range }),
+                range => ruleFixer.replaceTextRange(range)
+            ];
+
+            for (const range of invalidRanges) {
+                for (const fn of functions) {
+                    assert.isNull(fn(range, "x"));
+                    assert.isTrue(log.error.called);
+                    log.error.resetHistory();
+                }
+            }
+        });
     });
 
 });
