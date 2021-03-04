b0e3f18277167b3ff49e0292c9cd6531f3a687f1
Pig Fang <g-plane@hotmail.com>
2021-03-04 22:19:34 +0800

    Chore: clean up entries in Karma config

--- karma.conf.js
+++ karma.conf.js
@@ -26,7 +26,6 @@ module.exports = function(config) {
 
         // list of files / patterns to load in the browser
         files: [
-            "build/eslint.js",
             "tests/lib/linter/linter.js"
         ],
 
