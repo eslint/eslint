f596b2b54930e00980bf9bca92fdf2f12ccd7e7d
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2021-03-22 11:54:21 -0700

    Add schemas for linterOptions, processor, plugins

--- tests/lib/config/flat-config-array.js
+++ tests/lib/config/flat-config-array.js
@@ -30,6 +30,15 @@ async function assertMergedResult(values, result) {
     assert.deepStrictEqual(config, result);
 }
 
+async function assertInvalidConfig(values, message) {
+    const configs = createFlatConfigArray(values);
+    await configs.normalize();
+
+    assert.throws(() => {
+        configs.getConfig("foo.js");
+    }, message);
+}
+
 //-----------------------------------------------------------------------------
 // Tests
 //-----------------------------------------------------------------------------
@@ -106,6 +115,275 @@ describe("FlatConfigArray", () => {
                         b: false
                     }
                 });
+
+            });
+
+        });
+
+        describe("plugins", () => {
+
+            const pluginA = {};
+            const pluginB = {};
+            const pluginC = {};
+
+            it("should merge two objects", () => {
+
+                return assertMergedResult([
+                    {
+                        plugins: {
+                            a: pluginA,
+                            b: pluginB
+                        }
+                    },
+                    {
+                        plugins: {
+                            c: pluginC
+                        }
+                    }
+                ], {
+                    plugins: {
+                        a: pluginA,
+                        b: pluginB,
+                        c: pluginC
+                    }
+                });
+            });
+
+            it("should merge an object and undefined into one object", () => {
+
+                return assertMergedResult([
+                    {
+                        plugins: {
+                            a: pluginA,
+                            b: pluginB
+                        }
+                    },
+                    {
+                    }
+                ], {
+                    plugins: {
+                        a: pluginA,
+                        b: pluginB
+                    }
+                });
+
+            });
+
+            it("should error when attempting to redefine a plugin", async () => {
+
+                await assertInvalidConfig([
+                    {
+                        plugins: {
+                            a: pluginA,
+                            b: pluginB
+                        }
+                    },
+                    {
+                        plugins: {
+                            a: pluginC
+                        }
+                    }
+                ], "redefine plugin");
+            });
+
+            it("should error when plugin is not an object", async () => {
+
+                await assertInvalidConfig([
+                    {
+                        plugins: {
+                            a: true,
+                        }
+                    }
+                ], "Key \"a\": Expected an object.");
+            });
+
+
+        });
+
+        describe("processor", () => {
+
+            it("should merge two values when second is a string", () => {
+
+                return assertMergedResult([
+                    {
+                        processor: {
+                            preprocess() {},
+                            postprocess() {}
+                        }
+                    },
+                    {
+                        processor: "markdown/markdown"
+                    }
+                ], {
+                    processor: "markdown/markdown"
+                });
+            });
+
+            it("should merge two values when second is an object", () => {
+
+                const processor = {
+                    preprocess() { },
+                    postprocess() { }
+                };
+
+                return assertMergedResult([
+                    {
+                        processor: "markdown/markdown"
+                    },
+                    {
+                        processor
+                    }
+                ], {
+                    processor
+                });
+            });
+
+            it("should error when an invalid string is used", async () => {
+
+                await assertInvalidConfig([
+                    {
+                        processor: "foo"
+                    }
+                ], "plugin-name/object-name");
+            });
+
+            it("should error when an empty string is used", async () => {
+
+                await assertInvalidConfig([
+                    {
+                        processor: ""
+                    }
+                ], "plugin-name/object-name");
+            });
+
+            it("should error when an invalid processor is used", async () => {
+                await assertInvalidConfig([
+                    {
+                        processor: {}
+                    }
+                ], "preprocess() and a postprocess()");
+
+            });
+
+        });
+
+        describe("linterOptions", () => {
+
+            it("should error when an unexpected key is found", async () => {
+
+                await assertInvalidConfig([
+                    {
+                        linterOptions: {
+                            foo: true
+                        }
+                    }
+                ], "Unexpected key \"foo\" found.");
+
+            });
+
+            describe("noInlineConfig", () => {
+
+                it("should error when an unexpected value is found", async () => {
+
+                    await assertInvalidConfig([
+                        {
+                            linterOptions: {
+                                noInlineConfig: "true"
+                            }
+                        }
+                    ], "Expected a Boolean.")
+                });
+
+                it("should merge two objects when second object has overrides", () => {
+
+                    return assertMergedResult([
+                        {
+                            linterOptions: {
+                                noInlineConfig: true
+                            }
+                        },
+                        {
+                            linterOptions: {
+                                noInlineConfig: false
+                            }
+                        }
+                    ], {
+                        linterOptions: {
+                            noInlineConfig: false
+                        }
+                    });
+                });
+
+                it("should merge an object and undefined into one object", () => {
+
+                    return assertMergedResult([
+                        {
+                            linterOptions: {
+                                noInlineConfig: false
+                            }
+                        },
+                        {
+                        }
+                    ], {
+                        linterOptions: {
+                            noInlineConfig: false
+                        }
+                    });
+
+                });
+
+
+            });
+            describe("reportUnusedDisableDirectives", () => {
+
+                it("should error when an unexpected value is found", async () => {
+
+                    await assertInvalidConfig([
+                        {
+                            linterOptions: {
+                                reportUnusedDisableDirectives: "true"
+                            }
+                        }
+                    ], "Value must be \"off\", \"warn\", or \"error\".");
+                });
+
+                it("should merge two objects when second object has overrides", () => {
+
+                    return assertMergedResult([
+                        {
+                            linterOptions: {
+                                reportUnusedDisableDirectives: "off"
+                            }
+                        },
+                        {
+                            linterOptions: {
+                                reportUnusedDisableDirectives: "error"
+                            }
+                        }
+                    ], {
+                        linterOptions: {
+                            reportUnusedDisableDirectives: "error"
+                        }
+                    });
+                });
+
+                it("should merge an object and undefined into one object", () => {
+
+                    return assertMergedResult([
+                        {},
+                        {
+                            linterOptions: {
+                                reportUnusedDisableDirectives: "warn"
+                            }
+                        }
+                    ], {
+                        linterOptions: {
+                            reportUnusedDisableDirectives: "warn"
+                        }
+                    });
+
+                });
+
 
             });
 
