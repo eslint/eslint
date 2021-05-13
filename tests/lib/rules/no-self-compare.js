832e19c16c129029a18d33d4e8f1a1bfeed20de9
Toru Nagashima <public@mysticatea.dev>
2021-05-13 19:02:38 +0900

    update no-self-compare (test-only)

--- tests/lib/rules/no-self-compare.js
+++ tests/lib/rules/no-self-compare.js
@@ -23,7 +23,15 @@ ruleTester.run("no-self-compare", rule, {
         "if (x === y) { }",
         "if (1 === 2) { }",
         "y=x*x",
-        "foo.bar.baz === foo.bar.qux"
+        "foo.bar.baz === foo.bar.qux",
+        {
+            code: "class C { #field; foo() { this.#field === this['#field']; } }",
+            parserOptions: { ecmaVersion: 2022 }
+        },
+        {
+            code: "class C { #field; foo() { this['#field'] === this.#field; } }",
+            parserOptions: { ecmaVersion: 2022 }
+        }
     ],
     invalid: [
         { code: "if (x === x) { }", errors: [{ messageId: "comparingToSelf", type: "BinaryExpression" }] },
@@ -39,6 +47,11 @@ ruleTester.run("no-self-compare", rule, {
         { code: "x < x", errors: [{ messageId: "comparingToSelf", type: "BinaryExpression" }] },
         { code: "x >= x", errors: [{ messageId: "comparingToSelf", type: "BinaryExpression" }] },
         { code: "x <= x", errors: [{ messageId: "comparingToSelf", type: "BinaryExpression" }] },
-        { code: "foo.bar().baz.qux >= foo.bar ().baz .qux", errors: [{ messageId: "comparingToSelf", type: "BinaryExpression" }] }
+        { code: "foo.bar().baz.qux >= foo.bar ().baz .qux", errors: [{ messageId: "comparingToSelf", type: "BinaryExpression" }] },
+        {
+            code: "class C { #field; foo() { this.#field === this.#field; } }",
+            parserOptions: { ecmaVersion: 2022 },
+            errors: [{ messageId: "comparingToSelf", type: "BinaryExpression" }]
+        }
     ]
 });
