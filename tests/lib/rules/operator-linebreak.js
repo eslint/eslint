b320bbd88f6da038272f3659fb0b1cc3562565f7
Toru Nagashima <public@mysticatea.dev>
2021-05-14 17:54:24 +0900

    update operator-linebreak

--- tests/lib/rules/operator-linebreak.js
+++ tests/lib/rules/operator-linebreak.js
@@ -98,6 +98,16 @@ ruleTester.run("operator-linebreak", rule, {
             code: "a ??= \n b",
             options: ["after", { overrides: { "??": "before" } }],
             parserOptions: { ecmaVersion: 2021 }
+        },
+
+        {
+            code: "class C { foo =\n0 }",
+            parserOptions: { ecmaVersion: 2022 }
+        },
+        {
+            code: "class C { foo\n= 0 }",
+            options: ["before"],
+            parserOptions: { ecmaVersion: 2022 }
         }
     ],
 
@@ -769,6 +779,52 @@ ruleTester.run("operator-linebreak", rule, {
                 column: 1,
                 endLine: 2,
                 endColumn: 4
+            }]
+        },
+
+        {
+            code: "class C { a\n= 0; }",
+            output: "class C { a =\n0; }",
+            options: ["after"],
+            parserOptions: { ecmaVersion: 2022 },
+            errors: [{
+                messageId: "operatorAtEnd",
+                data: { operator: "=" },
+                type: "PropertyDefinition",
+                line: 2,
+                column: 1,
+                endLine: 2,
+                endColumn: 2
+            }]
+        },
+        {
+            code: "class C { a =\n0; }",
+            output: "class C { a\n= 0; }",
+            options: ["before"],
+            parserOptions: { ecmaVersion: 2022 },
+            errors: [{
+                messageId: "operatorAtBeginning",
+                data: { operator: "=" },
+                type: "PropertyDefinition",
+                line: 1,
+                column: 13,
+                endLine: 1,
+                endColumn: 14
+            }]
+        },
+        {
+            code: "class C { a =\n0; }",
+            output: "class C { a =0; }",
+            options: ["none"],
+            parserOptions: { ecmaVersion: 2022 },
+            errors: [{
+                messageId: "noLinebreak",
+                data: { operator: "=" },
+                type: "PropertyDefinition",
+                line: 1,
+                column: 13,
+                endLine: 1,
+                endColumn: 14
             }]
         }
     ]
