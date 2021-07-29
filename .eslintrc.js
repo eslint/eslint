ed007c82ee9d2170c87500d98303554b5f90b915
Bryan Mishkin <698306+bmish@users.noreply.github.com>
2021-07-29 11:11:19 -0700

    Chore: Simplify internal `no-invalid-meta` rule (#14842)

--- .eslintrc.js
+++ .eslintrc.js
@@ -66,10 +66,13 @@ module.exports = {
     rules: {
         "eslint-plugin/consistent-output": "error",
         "eslint-plugin/no-deprecated-context-methods": "error",
+        "eslint-plugin/no-only-tests": "error",
         "eslint-plugin/prefer-output-null": "error",
         "eslint-plugin/prefer-placeholders": "error",
+        "eslint-plugin/prefer-replace-text": "error",
         "eslint-plugin/report-message-format": ["error", "[^a-z].*\\.$"],
         "eslint-plugin/require-meta-docs-description": "error",
+        "eslint-plugin/require-meta-schema": "error",
         "eslint-plugin/require-meta-type": "error",
         "eslint-plugin/test-case-property-ordering": "error",
         "eslint-plugin/test-case-shorthand-strings": "error",
@@ -80,6 +83,7 @@ module.exports = {
             files: ["lib/rules/*", "tools/internal-rules/*"],
             excludedFiles: ["index.js"],
             rules: {
+                "eslint-plugin/prefer-object-rule": "error",
                 "internal-rules/no-invalid-meta": "error",
                 "internal-rules/consistent-meta-messages": "error"
             }
