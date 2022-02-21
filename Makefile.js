c08888a4492d3c6aecbebea2cafbc55916c9e81a
Piggy NL <piggynl@outlook.com>
2022-02-21 18:35:23 +0800

    feat: read `ESLINT_MOCHA_TIMEOUT` env var in Makefile.js
    Closes #15582

--- Makefile.js
+++ Makefile.js
@@ -79,7 +79,7 @@ const NODE = "node ", // intentional extra space
     PERF_MULTIFILES_TARGETS = `"${PERF_MULTIFILES_TARGET_DIR + path.sep}{lib,tests${path.sep}lib}${path.sep}**${path.sep}*.js"`,
 
     // Settings
-    MOCHA_TIMEOUT = 10000;
+    MOCHA_TIMEOUT = parseInt(process.env.ESLINT_MOCHA_TIMEOUT, 10) || 10000;
 
 //------------------------------------------------------------------------------
 // Helpers
