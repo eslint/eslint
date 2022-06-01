ae3004a9e0ad74344fec3d6cad1668e403efc043
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2022-05-31 17:27:09 -0700

    Update docs/src/assets/js/main.js
    Co-authored-by: Milos Djermanovic <milos.djermanovic@gmail.com>

--- docs/src/assets/js/main.js
+++ docs/src/assets/js/main.js
@@ -93,7 +93,7 @@
 
     if (matchMedia) {
         const mq = window.matchMedia("(max-width: 1023px)");
-        mq.addListener(WidthChange);
+        mq.addEventListener('change', WidthChange);
         WidthChange(mq);
     }
 
