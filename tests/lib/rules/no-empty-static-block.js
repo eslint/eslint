978799bd5c76fecf4ce8f17d89ad6c9f436c3228
Sosuke Suzuki <sosuke.suzuki@dr-ubie.com>
2022-11-06 09:55:12 +0900

    feat: add new rule `no-empty-static-block` (#16325)
    * feat: add `no-empty-static-block`
    
    * docs: add docs for `no-empty-static-block`
    
    * chore: add `no-empty-static-block`
    
    * test: add tests for multiple static blocks
    
    * Add `further_reading`
    
    * test: specify `ecmaVersion` at `RuleTester`
    
    * docs: remove options section
    
    * fix: `disallows` -> `disallow`
    
    * fix: remove `loc` parameters
    
    * feat: ignore comment that isn't inside block
    
    * fix: address review

---
+++ tests/lib/rules/no-empty-static-block.js
@@ -0,0 +1,51 @@
+/**
+ * @fileoverview Tests for no-empty-static-block rule.
+ * @author Sosuke Suzuki
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/no-empty-static-block"),
+    { RuleTester } = require("../../../lib/rule-tester");
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester({
+    parserOptions: { ecmaVersion: 2022 }
+});
+
+ruleTester.run("no-empty-static-block", rule, {
+    valid: [
+        "class Foo { static { bar(); } }",
+        "class Foo { static { /* comments */ } }",
+        "class Foo { static {\n// comment\n} }",
+        "class Foo { static { bar(); } static { bar(); } }"
+    ],
+    invalid: [
+        {
+            code: "class Foo { static {} }",
+            errors: [{ messageId: "unexpected" }]
+        },
+        {
+            code: "class Foo { static { } }",
+            errors: [{ messageId: "unexpected" }]
+        },
+        {
+            code: "class Foo { static { \n\n } }",
+            errors: [{ messageId: "unexpected" }]
+        },
+        {
+            code: "class Foo { static { bar(); } static {} }",
+            errors: [{ messageId: "unexpected" }]
+        },
+        {
+            code: "class Foo { static // comment\n {} }",
+            errors: [{ messageId: "unexpected" }]
+        }
+    ]
+});
