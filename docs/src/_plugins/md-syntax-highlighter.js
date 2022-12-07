90c94157977a54e19c10e5b5236b0eaf77d4a684
Sam Chen <chenxsan@gmail.com>
2022-12-07 12:03:32 +0800

    add license to file header

--- docs/src/_plugins/md-syntax-highlighter.js
+++ docs/src/_plugins/md-syntax-highlighter.js
@@ -1,3 +1,27 @@
+/**
+ * MIT License
+
+Copyright (c) 2019-present, Yuxi (Evan) You
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+
 /** @typedef {import("markdown-it")} MarkdownIt */
 
 const Prism = require("prismjs");
@@ -30,6 +54,7 @@ const highlighter = function (md, str, lang) {
  *
  * modified from https://github.com/vuejs/vitepress/blob/main/src/node/markdown/plugins/preWrapper.ts
  * @param {MarkdownIt} md
+ * @license MIT License. See file header.
  */
 const preWrapperPlugin = (md) => {
     const fence = md.renderer.rules.fence;
@@ -45,6 +70,7 @@ const preWrapperPlugin = (md) => {
  *
  * modified from https://github.com/vuejs/vitepress/blob/main/src/node/markdown/plugins/lineNumbers.ts
  * @param {MarkdownIt} md
+ * @license MIT License. See file header.
  */
 const lineNumberPlugin = (md) => {
     const fence = md.renderer.rules.fence;
