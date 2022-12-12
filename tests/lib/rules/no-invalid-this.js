2adea183a2e3b91ec3c5cf368313671223137570
neostfox <neo.st.fox@outlook.com>
2022-12-12 16:28:04 +0800

    fix: Update lodash to lodash.merge

--- tests/lib/rules/no-invalid-this.js
+++ tests/lib/rules/no-invalid-this.js
@@ -9,7 +9,7 @@
 // Requirements
 //------------------------------------------------------------------------------
 
-const { merge } = require("lodash");
+const merge = require("lodash.merge");
 
 const rule = require("../../../lib/rules/no-invalid-this");
 const { RuleTester } = require("../../../lib/rule-tester");
