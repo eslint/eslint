cd9f9aa462244be64ba86cc76d597b153dd60bfd
tanujkanti4441 <tanujkanti4441@gmail.com>
2023-03-18 15:45:56 +0530

    docs: set the scroll btn to work without js

--- docs/src/assets/js/scroll-up-btn.js
+++ docs/src/assets/js/scroll-up-btn.js
@@ -4,15 +4,10 @@
     if(window.innerWidth < 1400) {
         window.addEventListener("scroll", function () {
             if(this.document.body.scrollTop > 500 || this.document.documentElement.scrollTop > 500) {
-                scrollUpBtn.style.display = "block";
+                scrollUpBtn.style.display = "flex";
             } else {
                 scrollUpBtn.style.display = "none";
             }
         });
     }
-
-    scrollUpBtn.addEventListener("click", function () {
-        document.body.scrollTop = 0;
-        document.documentElement.scrollTop = 0;
-    });
 })();
