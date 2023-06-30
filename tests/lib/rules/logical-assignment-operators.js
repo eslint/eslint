d360ce61d6f26439a362801a3f77a5184c68153b
Kyle Hensel <k.y.l.e@outlook.co.nz>
2023-06-30 18:45:35 +1200

    feat: [logical-assignment-operators] flag a few more situations

--- tests/lib/rules/logical-assignment-operators.js
+++ tests/lib/rules/logical-assignment-operators.js
@@ -121,6 +121,12 @@ ruleTester.run("logical-assignment-operators", rule, {
             code: "if (a) a ||= b",
             options: ["always", { enforceForIfStatements: true }]
         }, {
+            code: "if (a) a ??= b",
+            options: ["always", { enforceForIfStatements: true }]
+        }, {
+            code: "if (!a) a &&= b",
+            options: ["always", { enforceForIfStatements: true }]
+        }, {
             code: "if (a) b = a",
             options: ["always", { enforceForIfStatements: true }]
         }, {
@@ -908,6 +914,91 @@ ruleTester.run("logical-assignment-operators", rule, {
         }, {
             code: [
                 "if (a == undefined) a = b",
+                "{ const undefined = 0; }"
+            ].join("\n"),
+            output: [
+                "a ??= b",
+                "{ const undefined = 0; }"
+            ].join("\n"),
+            options: ["always", { enforceForIfStatements: true }],
+            errors: [{ messageId: "if", type: "IfStatement", data: { operator: "??=" } }]
+        },
+
+        // If with logical assignment operator
+        {
+            code: "if (a) a &&= b",
+            output: "a &&= b",
+            options: ["always", { enforceForIfStatements: true }],
+            errors: [{ messageId: "if", type: "IfStatement", data: { operator: "&&=" } }]
+        }, {
+            code: "if (Boolean(a)) a &&= b",
+            output: "a &&= b",
+            options: ["always", { enforceForIfStatements: true }],
+            errors: [{ messageId: "if", type: "IfStatement", data: { operator: "&&=" } }]
+        }, {
+            code: "if (!!a) a &&= b",
+            output: "a &&= b",
+            options: ["always", { enforceForIfStatements: true }],
+            errors: [{ messageId: "if", type: "IfStatement", data: { operator: "&&=" } }]
+        }, {
+            code: "if (!a) a ||= b",
+            output: "a ||= b",
+            options: ["always", { enforceForIfStatements: true }],
+            errors: [{ messageId: "if", type: "IfStatement", data: { operator: "||=" } }]
+        }, {
+            code: "if (!Boolean(a)) a ||= b",
+            output: "a ||= b",
+            options: ["always", { enforceForIfStatements: true }],
+            errors: [{ messageId: "if", type: "IfStatement", data: { operator: "||=" } }]
+        }, {
+            code: "if (a == undefined) a ??= b",
+            output: "a ??= b",
+            options: ["always", { enforceForIfStatements: true }],
+            errors: [{ messageId: "if", type: "IfStatement", data: { operator: "??=" } }]
+        }, {
+            code: "if (a == null) a ??= b",
+            output: "a ??= b",
+            options: ["always", { enforceForIfStatements: true }],
+            errors: [{ messageId: "if", type: "IfStatement", data: { operator: "??=" } }]
+        }, {
+            code: "if (a === null || a === undefined) a ??= b",
+            output: "a ??= b",
+            options: ["always", { enforceForIfStatements: true }],
+            errors: [{ messageId: "if", type: "IfStatement", data: { operator: "??=" } }]
+        }, {
+            code: "if (a === undefined || a === null) a ??= b",
+            output: "a ??= b",
+            options: ["always", { enforceForIfStatements: true }],
+            errors: [{ messageId: "if", type: "IfStatement", data: { operator: "??=" } }]
+        }, {
+            code: "if (a === null || a === void 0) a ??= b",
+            output: "a ??= b",
+            options: ["always", { enforceForIfStatements: true }],
+            errors: [{ messageId: "if", type: "IfStatement", data: { operator: "??=" } }]
+        }, {
+            code: "if (a === void 0 || a === null) a ??= b",
+            output: "a ??= b",
+            options: ["always", { enforceForIfStatements: true }],
+            errors: [{ messageId: "if", type: "IfStatement", data: { operator: "??=" } }]
+        }, {
+            code: "if (a) { a &&= b; }",
+            output: "a &&= b;",
+            options: ["always", { enforceForIfStatements: true }],
+            errors: [{ messageId: "if", type: "IfStatement", data: { operator: "&&=" } }]
+        }, {
+            code: [
+                "{ const undefined = 0; }",
+                "if (a == undefined) a ??= b"
+            ].join("\n"),
+            output: [
+                "{ const undefined = 0; }",
+                "a ??= b"
+            ].join("\n"),
+            options: ["always", { enforceForIfStatements: true }],
+            errors: [{ messageId: "if", type: "IfStatement", data: { operator: "??=" } }]
+        }, {
+            code: [
+                "if (a == undefined) a ??= b",
                 "{ const undefined = 0; }"
             ].join("\n"),
             output: [
