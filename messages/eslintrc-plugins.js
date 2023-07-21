0bca34bdf73891e0cf15e07ee5f7b55acd1531bb
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2023-07-21 13:58:45 -0400

    feat: Better error message for flat config plugins
    Refs #17370

---
+++ messages/eslintrc-plugins.js
@@ -0,0 +1,22 @@
+"use strict";
+
+module.exports = function({ plugins }) {
+
+    return `
+A config object has a "plugins" key defined as an array of strings.
+
+Flat config requires "plugins" to be an object in this form:
+
+    {
+        plugins: {
+            ${plugins[0] || "namespace"}: pluginObject
+        }
+    }
+
+Please see the following page for information on how to convert your config object into the correct format:
+https://eslint.org/docs/latest/use/configure/migration-guide#importing-plugins-and-custom-parsers
+
+If you're using a shareable config that you cannot rewrite in flat config format, then use the compatibility utility:
+https://eslint.org/docs/latest/use/configure/migration-guide#using-eslintrc-configs-in-flat-config
+`;
+};
