d5838104507a035710d0be878ea4bfdd3e50efa2
ota-meshi <otameshiyo23@gmail.com>
2023-07-25 09:52:08 +0900

    test: add testcases

--- tests/lib/rules/no-control-regex.js
+++ tests/lib/rules/no-control-regex.js
@@ -34,7 +34,9 @@ ruleTester.run("no-control-regex", rule, {
         String.raw`new RegExp("\\u{1F}")`,
         String.raw`new RegExp("\\u{1F}", "g")`,
         String.raw`new RegExp("\\u{1F}", flags)`, // when flags are unknown, this rule assumes there's no `u` flag
-        String.raw`new RegExp("[\\q{\\u{20}}]", "v")`
+        String.raw`new RegExp("[\\q{\\u{20}}]", "v")`,
+        { code: String.raw`/[\u{20}--B]/v`, parserOptions: { ecmaVersion: 2024 } }
+
     ],
     invalid: [
         { code: String.raw`var regex = /\x1f/`, errors: [{ messageId: "unexpected", data: { controlChars: "\\x1f" }, type: "Literal" }] },
@@ -89,6 +91,11 @@ ruleTester.run("no-control-regex", rule, {
         },
         {
             code: String.raw`new RegExp("[\\q{\\u{1F}}]", "v")`,
+            errors: [{ messageId: "unexpected", data: { controlChars: "\\x1f" }, type: "Literal" }]
+        },
+        {
+            code: String.raw`/[\u{1F}--B]/v`,
+            parserOptions: { ecmaVersion: 2024 },
             errors: [{ messageId: "unexpected", data: { controlChars: "\\x1f" }, type: "Literal" }]
         }
     ]
