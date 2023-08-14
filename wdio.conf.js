72e6a7f51ff378a213d9045f6bd2ef0703219cb8
Christian Bromann <git@bromann.dev>
2023-08-14 08:03:49 -0700

    remove Chromedriver deps

--- wdio.conf.js
+++ wdio.conf.js
@@ -163,15 +163,6 @@ exports.config = {
     connectionRetryCount: 3,
 
     /*
-     *
-     * Test runner services
-     * Services take over a specific job you don't want to take care of. They enhance
-     * your test setup with almost no effort. Unlike plugins, they don't add new
-     * commands. Instead, they hook themselves up into the test process.
-     */
-    services: ["chromedriver"],
-
-    /*
      * Framework you want to run your specs with.
      * The following are supported: Mocha, Jasmine, and Cucumber
      * see also: https://webdriver.io/docs/frameworks
