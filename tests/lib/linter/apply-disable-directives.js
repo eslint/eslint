c4b7dc90298057db17a9fd579f2a3575991ecefd
ota-meshi <otameshiyo23@gmail.com>
2023-09-28 16:39:15 +0900

    test: fix test case

--- tests/lib/linter/apply-disable-directives.js
+++ tests/lib/linter/apply-disable-directives.js
@@ -680,7 +680,7 @@ describe("apply-disable-directives", () => {
                 applyDisableDirectives({
                     directives: [
                         { parentComment: createParentComment([0, 31]), type: "disable-next-line", line: 1, column: 1, ruleId: null, justification: "j1" },
-                        { type: "enable", line: 1, column: 5, ruleId: null, justification: "j2" }
+                        { parentComment: createParentComment([31, 50]), type: "enable", line: 1, column: 31, ruleId: null, justification: "j2" }
                     ],
                     problems: [{ line: 2, column: 2, ruleId: "foo" }]
                 }),
