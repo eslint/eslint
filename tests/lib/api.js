53f0f47badffa1b04ec2836f2ae599f4fc464da2
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2024-02-09 12:10:36 -0700

    feat: Add loadESLint() API method for v9 (#18097)
    * feat: Add loadESLint() API method for v9
    
    refs #18075
    
    * Fix docs
    
    * Add more tests using environment variables

--- tests/lib/api.js
+++ tests/lib/api.js
@@ -10,7 +10,8 @@
 //-----------------------------------------------------------------------------
 
 const assert = require("chai").assert,
-    api = require("../../lib/api");
+    api = require("../../lib/api"),
+    { LegacyESLint } = require("../../lib/eslint/legacy-eslint");
 
 //-----------------------------------------------------------------------------
 // Tests
@@ -41,4 +42,42 @@ describe("api", () => {
     it("should have SourceCode exposed", () => {
         assert.isFunction(api.SourceCode);
     });
+
+    describe("loadESLint", () => {
+
+        afterEach(() => {
+            delete process.env.ESLINT_USE_FLAT_CONFIG;
+        });
+
+        it("should be a function", () => {
+            assert.isFunction(api.loadESLint);
+        });
+
+        it("should return a Promise", () => {
+            assert.instanceOf(api.loadESLint(), Promise);
+        });
+
+        it("should return ESLint when useFlatConfig is true", async () => {
+            assert.strictEqual(await api.loadESLint({ useFlatConfig: true }), api.ESLint);
+        });
+
+        it("should return LegacyESLint when useFlatConfig is false", async () => {
+            assert.strictEqual(await api.loadESLint({ useFlatConfig: false }), LegacyESLint);
+        });
+
+        it("should return ESLint when useFlatConfig is not provided", async () => {
+            assert.strictEqual(await api.loadESLint(), api.ESLint);
+        });
+
+        it("should return LegacyESLint when useFlatConfig is not provided and ESLINT_USE_FLAT_CONFIG is false", async () => {
+            process.env.ESLINT_USE_FLAT_CONFIG = "false";
+            assert.strictEqual(await api.loadESLint(), LegacyESLint);
+        });
+
+        it("should return ESLint when useFlatConfig is not provided and ESLINT_USE_FLAT_CONFIG is true", async () => {
+            process.env.ESLINT_USE_FLAT_CONFIG = "true";
+            assert.strictEqual(await api.loadESLint(), api.ESLint);
+        });
+    });
+
 });
