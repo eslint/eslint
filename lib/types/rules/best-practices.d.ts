fa11b2ede6e5dc1f55dfe4b9b65d9760828900e8
Kristóf Poduszló <14854048+kripod@users.noreply.github.com>
2024-09-16 09:03:34 +0200

    fix: add missing types for `no-empty-function` options (#18901)

--- lib/types/rules/best-practices.d.ts
+++ lib/types/rules/best-practices.d.ts
@@ -324,6 +324,8 @@ export interface BestPractices extends Linter.RulesRecord {
                     | "getters"
                     | "setters"
                     | "constructors"
+                    | "asyncFunctions"
+                    | "asyncMethods"
                 >;
             }>,
         ]
