3bc8d437845e2b31f32dbe2d3e68f5d18af7e7fe
Jay <jay.sanjay.karia@gmail.com>
2024-09-28 12:51:38 +0530

    chore: add command conversion json data

---
+++ docs/src/_data/conversions.js
@@ -0,0 +1,17 @@
+/**
+ * @fileoverview Helper for command conversions
+ * @author Jay-Karia
+ */
+
+"use strict";
+
+//-----------------------------------------------------------------------------
+// Exports
+//-----------------------------------------------------------------------------
+
+module.exports = async function() {
+
+    const conversions = require("../_data/conversion_map.json");
+
+    return conversions
+};
