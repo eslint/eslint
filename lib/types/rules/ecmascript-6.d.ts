d7347e9cdd17458a3760bd257d9867b9c8f4d94a
Nitin Kumar <snitin315@gmail.com>
2024-11-02 08:21:42 +0530

    chore: export all types

--- lib/types/rules/ecmascript-6.d.ts
+++ lib/types/rules/ecmascript-6.d.ts
@@ -27,36 +27,36 @@
 
 import { Linter } from "../index";
 
-interface NoRestrictedImportPathCommonOptions {
+export interface NoRestrictedImportPathCommonOptions {
     name: string;
     message?: string;
 }
 
-type EitherImportNamesOrAllowImportName =
+export type EitherImportNamesOrAllowImportName =
   | { importNames?: string[]; allowImportNames?: never }
   | { allowImportNames?: string[]; importNames?: never }
 
-type ValidNoRestrictedImportPathOptions = NoRestrictedImportPathCommonOptions & EitherImportNamesOrAllowImportName;
+export type ValidNoRestrictedImportPathOptions = NoRestrictedImportPathCommonOptions & EitherImportNamesOrAllowImportName;
 
-interface NoRestrictedImportPatternCommonOptions {
+export interface NoRestrictedImportPatternCommonOptions {
     message?: string;
     caseSensitive?: boolean;
 }
 
 // Base type for group or regex constraint, ensuring mutual exclusivity
-type EitherGroupOrRegEx =
+export type EitherGroupOrRegEx =
   | { group: string[]; regex?: never }
   | { regex: string; group?: never };
 
 // Base type for import name specifiers, ensuring mutual exclusivity
-type EitherNameSpecifiers = 
+export type EitherNameSpecifiers = 
     | { importNames: string[]; allowImportNames?: never; importNamePattern?: never; allowImportNamePattern?: never }
     | { importNamePattern: string; allowImportNames?: never; importNames?: never; allowImportNamePattern?: never }
     | { allowImportNames: string[]; importNames?: never; importNamePattern?: never; allowImportNamePattern?: never }
     | { allowImportNamePattern: string; importNames?: never; allowImportNames?: never; importNamePattern?: never }
 
 // Adds oneOf and not constraints, ensuring group or regex are present and mutually exclusive sets for importNames, allowImportNames, etc., as per the schema.
-type ValidNoRestrictedImportPatternOptions = NoRestrictedImportPatternCommonOptions & EitherGroupOrRegEx & EitherNameSpecifiers;
+export type ValidNoRestrictedImportPatternOptions = NoRestrictedImportPatternCommonOptions & EitherGroupOrRegEx & EitherNameSpecifiers;
 
 export interface ECMAScript6 extends Linter.RulesRecord {
     /**
