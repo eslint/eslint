46000ac3c1e4f89917b6c2d32bbe725d6ee19463
Francesco Trotta <github@fasttime.org>
2024-12-21 22:12:09 +0100

    feat: unflag TypeScript config files

--- tests/fixtures/ts-config-files/helper.ts
+++ tests/fixtures/ts-config-files/helper.ts
@@ -4,12 +4,7 @@
  * and namespaces from other TypeScript files.
  */
 
-export type RuleLevelAndOptions<Options extends any[] = any[]> = Prepend<
-    Partial<Options>,
-    RuleLevel
->;
-
-export type StringSeverity = "off" | "warn" | "error";
+import { Linter } from "eslint";
 
 export const enum Severity {
     "Off" = 0,
@@ -17,29 +12,7 @@ export const enum Severity {
     "Error" = 2,
 }
 
-export type RuleLevel = Severity | StringSeverity;
-
-export type RuleEntry<Options extends any[] = any[]> =
-    | RuleLevel
-    | RuleLevelAndOptions<Options>;
-
-export interface RulesRecord {
-    [rule: string]: RuleEntry;
-}
-
-export interface FlatConfig<Rules extends RulesRecord = RulesRecord> {
-    name?: string;
-    files?: Array<string | string[]>;
-    ignores?: string[];
-    linterOptions?: {
-        noInlineConfig?: boolean;
-        reportUnusedDisableDirectives?: Severity | StringSeverity | boolean;
-    };
-    processor?: string;
-    plugins?: Record<string, any>;
-    rules?: Partial<Rules>;
-    settings?: Record<string, unknown>;
-}
+export type FlatConfig = Linter.Config;
 
 export namespace ESLintNameSpace {
     export const enum StringSeverity {
