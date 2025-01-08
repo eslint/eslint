52c40cdb0967e85e8a0e6740c2a1eeab086ee05e
Tanuj Kanti <tanujkanti4441@gmail.com>
2025-01-08 12:26:12 +0530

    get prerelease info from eslintVersion,js

--- docs/.eleventy.js
+++ docs/.eleventy.js
@@ -56,22 +56,11 @@ module.exports = function(eleventyConfig) {
     // Load site-specific data
     const siteName = process.env.ESLINT_SITE_NAME || "en";
 
-    /**
-     * Determines whether we are in the prerelease phase.
-     * @returns {Promise<boolean>} `true` if there is a prerelease of the next major version, `false` otherwise.
-     */
-    async function isPrereleasePhase() {
-        const eslintVersions = await require("./src/_data/eslintVersions")();
-
-        return eslintVersions.items.some(item => item.branch === "next");
-    }
-
     eleventyConfig.addGlobalData("site_name", siteName);
     eleventyConfig.addGlobalData("GIT_BRANCH", process.env.BRANCH);
     eleventyConfig.addGlobalData("HEAD", process.env.BRANCH === "main");
     eleventyConfig.addGlobalData("NOINDEX", process.env.BRANCH !== "latest");
     eleventyConfig.addGlobalData("PATH_PREFIX", pathPrefix);
-    eleventyConfig.addGlobalData("is_prerelease_phase", isPrereleasePhase);
     eleventyConfig.addGlobalData("is_number_version", isNumberVersion);
     eleventyConfig.addDataExtension("yml", contents => yaml.load(contents));
 
