db5340d57bff6b6e3a148f0f2bb56c7da6614ec0
Milos Djermanovic <milos.djermanovic@gmail.com>
2025-02-20 18:51:00 +0100

    fix: update missing plugin message template (#19445)
    fix: update missing plugin message template

--- messages/config-plugin-missing.js
+++ messages/config-plugin-missing.js
@@ -4,6 +4,8 @@ module.exports = function(it) {
     const { pluginName, ruleId } = it;
 
     return `
+A configuration object specifies rule "${ruleId}", but could not find plugin "${pluginName}".
+
 Common causes of this problem include:
 
 1. The "${pluginName}" plugin is not defined in your configuration file.
