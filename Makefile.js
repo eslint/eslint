35c15bc7010a82cb9941d14211795d757ca7d3db
Pixel998 <mrhbs998@gmail.com>
2025-03-03 06:29:58 +0300

    switch to chrome and fix missing term var

--- Makefile.js
+++ Makefile.js
@@ -616,7 +616,7 @@ target.mocha = () => {
 target.cypress = () => {
     echo("Running unit tests on browsers");
     target.webpack("production");
-    const lastReturn = exec(`${getBinFile("cypress")} run --browser firefox --no-runner-ui`);
+    const lastReturn = exec(`${getBinFile("cypress")} run --browser chrome --no-runner-ui`);
 
     if (lastReturn.code !== 0) {
         exit(1);
