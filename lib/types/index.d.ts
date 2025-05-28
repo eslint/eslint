02f176bb443d0f4f8768cb80ae3cdb0a0eaa0e99
sethamus <sethamus@proton.me>
2025-05-28 04:09:58 +0300

    fix: add explicit undefined to Linter.Config optional properties

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -1763,64 +1763,64 @@ export namespace Linter {
 		 * An string to identify the configuration object. Used in error messages and
 		 * inspection tools.
 		 */
-		name?: string;
+		name?: string | undefined;
 
 		/**
 		 * An array of glob patterns indicating the files that the configuration
 		 * object should apply to. If not specified, the configuration object applies
 		 * to all files
 		 */
-		files?: Array<string | string[]>;
+		files?: Array<string | string[]> | undefined;
 
 		/**
 		 * An array of glob patterns indicating the files that the configuration
 		 * object should not apply to. If not specified, the configuration object
 		 * applies to all files matched by files
 		 */
-		ignores?: string[];
+		ignores?: string[] | undefined;
 
 		/**
 		 * The name of the language used for linting. This is used to determine the
 		 * parser and other language-specific settings.
 		 * @since 9.7.0
 		 */
-		language?: string;
+		language?: string | undefined;
 
 		/**
 		 * An object containing settings related to how JavaScript is configured for
 		 * linting.
 		 */
-		languageOptions?: LanguageOptions;
+		languageOptions?: LanguageOptions | undefined;
 
 		/**
 		 * An object containing settings related to the linting process
 		 */
-		linterOptions?: LinterOptions;
+		linterOptions?: LinterOptions | undefined;
 
 		/**
 		 * Either an object containing preprocess() and postprocess() methods or a
 		 * string indicating the name of a processor inside of a plugin
 		 * (i.e., "pluginName/processorName").
 		 */
-		processor?: string | Processor;
+		processor?: string | Processor | undefined;
 
 		/**
 		 * An object containing a name-value mapping of plugin names to plugin objects.
 		 * When files is specified, these plugins are only available to the matching files.
 		 */
-		plugins?: Record<string, ESLint.Plugin>;
+		plugins?: Record<string, ESLint.Plugin> | undefined;
 
 		/**
 		 * An object containing the configured rules. When files or ignores are specified,
 		 * these rule configurations are only available to the matching files.
 		 */
-		rules?: Partial<Rules>;
+		rules?: Partial<Rules> | undefined;
 
 		/**
 		 * An object containing name-value pairs of information that should be
 		 * available to all rules.
 		 */
-		settings?: Record<string, unknown>;
+		settings?: Record<string, unknown> | undefined;
 	}
 
 	/** @deprecated  Use `Config` instead of `FlatConfig` */
