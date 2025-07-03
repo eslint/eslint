0d87c7defbc7692d44286cb86298c8ff55bc0bcf
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2025-07-03 14:04:49 -0400

    fix: Remove incorrect RuleContext types
    fixes #19903

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -1186,25 +1186,7 @@ export namespace Rule {
 			RuleOptions: any[];
 			Node: JSSyntaxElement;
 			MessageIds: string;
-		}> {
-		/*
-		 * Need to extend the `RuleContext` interface to include the
-		 * deprecated methods that have not yet been removed.
-		 * TODO: Remove in v10.0.0.
-		 */
-
-		/** @deprecated Use `sourceCode.getAncestors()` instead */
-		getAncestors(): ESTree.Node[];
-
-		/** @deprecated Use `sourceCode.getDeclaredVariables()` instead */
-		getDeclaredVariables(node: ESTree.Node): Scope.Variable[];
-
-		/** @deprecated Use `sourceCode.getScope()` instead */
-		getScope(): Scope.Scope;
-
-		/** @deprecated Use `sourceCode.markVariableAsUsed()` instead */
-		markVariableAsUsed(name: string): boolean;
-	}
+		}> {}
 
 	type ReportFixer = (
 		fixer: RuleFixer,
