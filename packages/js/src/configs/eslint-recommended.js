190b528701502a7b77415dc9077a9a26897afd71
Amnish Singh Arora <amnishsingh04@gmail.com>
2025-07-06 18:11:05 -0400

    Add `preserve-caught-error` to recommended config

--- packages/js/src/configs/eslint-recommended.js
+++ packages/js/src/configs/eslint-recommended.js
@@ -76,6 +76,7 @@ module.exports = Object.freeze({
 		"no-useless-catch": "error",
 		"no-useless-escape": "error",
 		"no-with": "error",
+		"preserve-caught-error": "error",
 		"require-yield": "error",
 		"use-isnan": "error",
 		"valid-typeof": "error",
