375f2d08b660911e0a6fba42ace77dd85d3711e5
Nitin Kumar <snitin315@gmail.com>
2025-07-10 13:14:36 +0530

    chore: fix formatting

--- packages/js/tests/types/test.ts
+++
@@ -1,9 +0,0 @@
-/*eslint no-restricted-imports: ["error", {
-  "paths": [{
-    "name": "some-module",
-    "importNames": ["someModule"]
-  }]
-}]*/
-
-// This is allowed
-import someModule = require("some-module");
