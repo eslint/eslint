0a07dc914ee173969f4f5e5431af7c7d99a418a1
Nicholas C. Zakas <nicholas@humanwhocodes.com>
2025-09-23 15:44:37 -0400

    Fix failing test

--- tests/lib/eslint/eslint.js
+++ tests/lib/eslint/eslint.js
@@ -12699,7 +12699,7 @@ describe("ESLint", () => {
 				delete process.env.ESLINT_USE_FLAT_CONFIG;
 			});
 
-			testShouldUseFlatConfig(false, false);
+			testShouldUseFlatConfig(true, true);
 		});
 
 		describe("when the env variable `ESLINT_USE_FLAT_CONFIG` is unset", () => {
