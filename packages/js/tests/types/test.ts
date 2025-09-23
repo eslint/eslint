f851cf8e8f0a0a6b7f18935d8a9ce81ff7e0b165
Nitin Kumar <snitin315@gmail.com>
2025-09-23 23:36:40 +0530

    chore: remove unwanted changes

--- packages/js/tests/types/test.ts
+++
@@ -1,8 +0,0 @@
-/*eslint no-restricted-imports: ["error", { patterns: [{
-    group: ["import/private/*"],
-	allowImportNamePattern: ["Bar", "Foo"],
-	allowTypeImports: true,
-}]}]*/
-
-import type { Bar } from "import/private/bar";
-import { Fooz, type Foo  } from "import/private/bar";
