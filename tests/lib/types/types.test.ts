6fbfa27f21192bef72186456672940b10509f583
jaymarvelz <jaymarvelz@outlook.com>
2025-10-17 03:14:21 +0300

    test: add `DefinitionType` tests

--- tests/lib/types/types.test.ts
+++ tests/lib/types/types.test.ts
@@ -528,6 +528,37 @@ reference.isWrite();
 reference.isWriteOnly();
 reference.isReadWrite();
 
+let catchDef!: Extract<Scope.DefinitionType, { type: "CatchClause" }>;
+catchDef.node; // $ExpectType CatchClause
+catchDef.parent; // $ExpectType null
+
+let classNameDef!: Extract<Scope.DefinitionType, { type: "ClassName" }>;
+classNameDef.node; // $ExpectType ClassDeclaration | ClassExpression
+classNameDef.parent; // $ExpectType null
+
+let functionNameDef!: Extract<Scope.DefinitionType, { type: "FunctionName" }>;
+functionNameDef.node; // $ExpectType FunctionDeclaration | FunctionExpression
+functionNameDef.parent; // $ExpectType null
+
+let implicitGlobalVarDef!: Extract<
+	Scope.DefinitionType,
+	{ type: "ImplicitGlobalVariable" }
+>;
+implicitGlobalVarDef.node; // $ExpectType AssignmentExpression | ForInStatement | ForOfStatement
+implicitGlobalVarDef.parent; // $ExpectType null
+
+let importBindingDef!: Extract<Scope.DefinitionType, { type: "ImportBinding" }>;
+importBindingDef.node; // $ExpectType ImportSpecifier | ImportDefaultSpecifier | ImportNamespaceSpecifier
+importBindingDef.parent; // $ExpectType ImportDeclaration
+
+let parameterDef!: Extract<Scope.DefinitionType, { type: "Parameter" }>;
+parameterDef.node; // $ExpectType FunctionDeclaration | FunctionExpression | ArrowFunctionExpression
+parameterDef.parent; // $ExpectType null
+
+let variableDef!: Extract<Scope.DefinitionType, { type: "Variable" }>;
+variableDef.node; // $ExpectType VariableDeclarator
+variableDef.parent; // $ExpectType VariableDeclaration
+
 // #endregion
 
 // #region Rule
