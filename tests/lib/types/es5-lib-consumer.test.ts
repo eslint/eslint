1568c707d89f149b35f59f7839c78a10b78afe83
Raashish Aggarwal <94279692+raashish1601@users.noreply.github.com>
2026-03-28 15:33:43 +0530

    fix: add es2015 lib reference for package types

---
+++ tests/lib/types/es5-lib-consumer.test.ts
@@ -0,0 +1,8 @@
+import { ESLint } from "eslint";
+import { builtinRules, shouldUseFlatConfig } from "eslint/use-at-your-own-risk";
+
+const eslint = new ESLint();
+
+eslint;
+builtinRules;
+shouldUseFlatConfig();
